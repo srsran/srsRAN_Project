b082e4aefe499dcc611e9ff65e73fb5e9a3f882c
Francisco <francisco.paisana@softwareradiosystems.com>
2022-01-28 11:58:53 +0000

    add circular buffer, expected, move callback, pools to adt and support libraries

---
+++ include/srsgnb/support/task_worker.h
@@ -0,0 +1,74 @@
+
+#ifndef SRSRAN_THREAD_POOL_H
+#define SRSRAN_THREAD_POOL_H
+
+#include "srsgnb/adt/circular_buffer.h"
+#include "srsgnb/adt/move_callback.h"
+#include "srsgnb/srslog/srslog.h"
+#include "task_executor.h"
+#include "threads.h"
+#include <atomic>
+#include <condition_variable>
+#include <cstdint>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <stack>
+#include <string>
+#include <vector>
+
+namespace srsgnb {
+
+/// Class used to create a single worker with an input task queue with a single reader
+class task_worker : public thread
+{
+  using task_t = move_task_t;
+
+public:
+  task_worker(std::string thread_name_,
+              uint32_t    queue_size,
+              bool        start_deferred = false,
+              int32_t     prio_          = -1,
+              uint32_t    mask_          = 255);
+  task_worker(const task_worker&) = delete;
+  task_worker(task_worker&&)      = delete;
+  task_worker& operator=(const task_worker&) = delete;
+  task_worker& operator=(task_worker&&) = delete;
+  ~task_worker();
+
+  void stop();
+  void start(int32_t prio_ = -1, uint32_t mask_ = 255);
+
+  void     push_task(task_t&& task);
+  uint32_t nof_pending_tasks() const;
+
+private:
+  void run_thread() override;
+
+  // args
+  int32_t               prio = -1;
+  uint32_t              mask = 255;
+  srslog::basic_logger& logger;
+
+  srsgnb::dyn_blocking_queue<task_t> pending_tasks;
+};
+
+class task_worker_executor : public task_executor
+{
+public:
+  task_worker_executor() = default;
+  explicit task_worker_executor(task_worker& worker_) : worker(&worker_) {}
+  void execute(move_task_t task) override { worker->push_task(std::move(task)); }
+
+private:
+  task_worker* worker = nullptr;
+};
+
+inline std::unique_ptr<task_executor> make_task_executor(task_worker& w)
+{
+  return std::make_unique<task_worker_executor>(task_worker_executor{w});
+}
+
+} // namespace srsgnb
+
+#endif // SRSRAN_THREAD_POOL_H
---
+++ lib/support/task_worker.cc
@@ -0,0 +1,97 @@
+/**
+ *
+ * \section COPYRIGHT
+ *
+ * Copyright 2013-2021 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "srsgnb/support/task_worker.h"
+#include "srsgnb/srslog/srslog.h"
+#include <assert.h>
+#include <chrono>
+#include <stdio.h>
+
+#define DEBUG 0
+#define debug_thread(fmt, ...)                                                                                         \
+  do {                                                                                                                 \
+    if (DEBUG)                                                                                                         \
+      printf(fmt, __VA_ARGS__);                                                                                        \
+  } while (0)
+
+namespace srsgnb {
+
+task_worker::task_worker(std::string thread_name_,
+                         uint32_t    queue_size,
+                         bool        start_deferred,
+                         int32_t     prio_,
+                         uint32_t    mask_) :
+  thread(std::move(thread_name_)),
+  prio(prio_),
+  mask(mask_),
+  logger(srslog::fetch_basic_logger("POOL")),
+  pending_tasks(queue_size)
+{
+  if (not start_deferred) {
+    start(prio_, mask_);
+  }
+}
+
+task_worker::~task_worker()
+{
+  stop();
+}
+
+void task_worker::stop()
+{
+  if (not pending_tasks.is_stopped()) {
+    pending_tasks.stop();
+    wait_thread_finish();
+  }
+}
+
+void task_worker::start(int32_t prio_, uint32_t mask_)
+{
+  prio = prio_;
+  mask = mask_;
+
+  if (mask == 255) {
+    thread::start(prio);
+  } else {
+    thread::start_cpu_mask(prio, mask);
+  }
+}
+
+void task_worker::push_task(task_t&& task)
+{
+  auto ret = pending_tasks.try_push(std::move(task));
+  if (ret.is_error()) {
+    logger.error("Cannot push anymore tasks into the worker queue. maximum size is %u",
+                 uint32_t(pending_tasks.max_size()));
+    return;
+  }
+}
+
+uint32_t task_worker::nof_pending_tasks() const
+{
+  return pending_tasks.size();
+}
+
+void task_worker::run_thread()
+{
+  while (true) {
+    bool   success;
+    task_t task = pending_tasks.pop_blocking(&success);
+    if (not success) {
+      break;
+    }
+    task();
+  }
+  logger.info("Task worker %s finished.", thread::get_name().c_str());
+}
+
+} // namespace srsgnb
