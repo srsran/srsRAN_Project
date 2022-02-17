b7bfa6f7188061c4363ed592be4c72a609d39cbf
Xavier Arteaga <xavier.arteaga@softwareradiosystems.com>
2022-02-17 10:35:24 +0100

    PHY: ammended notification interfaces

--- include/srsgnb/phy/phy.h
+++ include/srsgnb/phy/phy.h
@@ -22,54 +22,63 @@ public:
   virtual void send(const resource_grid_context& context, const resource_grid_reader& grid) = 0;
 };
 
-class phy_notifier
+/// Describes context of the new received symbol
+struct phy_rx_symbol_context_t {
+  slot_context_t slot;
+  unsigned       sector;
+  unsigned       symbol;
+};
+
+/// Physical layer receive symbol notification interface
+class phy_rx_symbol_notifier
 {
 public:
-  /// Describes an RX packet notifier context
-  struct rx_context_t {
-    slot_context_t slot;
-    unsigned       sector;
-    unsigned       symbol;
-  };
-
-  /// Describes an event notifier context
-  struct event_context_t {
-    slot_context_t slot;
-  };
-
   /// Default destructor
-  virtual ~phy_notifier() = default;
+  virtual ~phy_rx_symbol_notifier() = default;
 
   /// Notifies the arrival of all packets for given CC
   ///
   /// \param [in] context Provides the notification context
   /// \param [in] grid Provides the resource grids for each port of the given sector
-  virtual void rx(const rx_context_t& context, const span<resource_grid_reader>& grid) = 0;
+  virtual void on_rx_symbol(const phy_rx_symbol_context_t& context, const span<resource_grid_reader>& grid) = 0;
 
   /// Notifies the arrival of PRACH packets for given CC
   ///
   /// \param [in] context Provides the notification context
-  virtual void rx_prach(const rx_context_t& context) = 0;
+  virtual void on_rx_prach_symbol(const phy_rx_symbol_context_t& context) = 0;
 
   /// Notifies the arrival of SRS packets for given CC
   ///
   /// \param [in] context Provides the notification context
-  virtual void rx_srs(const rx_context_t& context) = 0;
+  virtual void on_rx_srs_symbol(const phy_rx_symbol_context_t& context) = 0;
+};
+
+/// Describes the context of the current timing boundary
+struct phy_boundary_context_t {
+  slot_context_t slot;
+};
+
+/// Physical layer timing boundary notification interface
+class phy_timing_notifier
+{
+public:
+  /// Default destructor
+  virtual ~phy_timing_notifier() = default;
 
   /// Notifies a new TTI boundary event
   ///
   /// \param [in] context Provides the notification context
-  virtual void tti(const event_context_t& context) = 0;
+  virtual void on_tti_boundary(const phy_boundary_context_t& context) = 0;
 
   /// Notifies a half slot (sym 7) packet arrival
   ///
   /// \param [in] context Provides the notification context
-  virtual void ul_half_slot(const event_context_t& context) = 0;
+  virtual void on_ul_half_slot_boundary(const phy_boundary_context_t& context) = 0;
 
   /// Notifies a full slot (sym 14) packet arrival
   ///
   /// \param [in] context Provides the notification context
-  virtual void ul_full_slot(const event_context_t& context) = 0;
+  virtual void on_ul_full_slot_boundary(const phy_boundary_context_t& context) = 0;
 };
 
 } // namespace srsgnb
