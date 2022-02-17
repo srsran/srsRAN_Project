36623659965edb03f2d48931c8914dec7ed4db6d
Francisco <francisco.paisana@softwareradiosystems.com>
2022-02-17 11:17:32 +0000

    remove uneeded pool utils file

--- include/srsgnb/adt/byte_buffer.h
+++ include/srsgnb/adt/byte_buffer.h
@@ -2,11 +2,12 @@
 #define SRSGNB_ADT_BYTE_BUFFER_H
 
 #include <vector>
+#include <cstdint>
 
 namespace srsgnb {
 
 /// Type to store a dynamic amount of contiguous bytes.
-using byte_buffer = std::vector<unsigned char>;
+using byte_buffer = std::vector<uint8_t>;
 
 } // namespace srsgnb
 
