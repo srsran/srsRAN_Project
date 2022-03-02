67495bd7b299d533a63e43c8577a6925265dca81
Francisco <francisco.paisana@softwareradiosystems.com>
2022-03-02 11:52:56 +0000

    fix get() return of async_task

--- include/srsgnb/support/async/async_task.h
+++ include/srsgnb/support/async/async_task.h
@@ -154,15 +154,14 @@ public:
   bool ready() const { return empty() or handle.promise().ready(); }
 
   /// Called to get result of task once it is complete
-  template <bool enable = not std::is_same<result_type, void>::value>
-  // TODO: return const reference of the result
-  typename std::enable_if<enable, std::decay_t<result_type> >::type get() &
+  template <typename R = std::decay_t<result_type>, std::enable_if_t<not std::is_same<R, void>::value, bool> = true>
+  const R& get() &
   {
     srsran_assert(not empty() and ready(), "Called async_task::get() for task that is not ready");
     return handle.promise().get();
   }
-  template <bool enable = not std::is_same<result_type, void>::value>
-  typename std::enable_if<enable, std::decay_t<result_type> >::type get() &&
+  template <typename R = std::decay_t<result_type>, std::enable_if_t<not std::is_same<R, void>::value, bool> = true>
+  R get() &&
   {
     srsran_assert(not empty() and ready(), "Called async_task::get() for task that is not ready");
     return std::move(handle.promise()).get();
