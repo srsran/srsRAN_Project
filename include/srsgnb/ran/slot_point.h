3a9be3e324bbf43f56776c37eb508aef1fbb19af
Francisco <francisco.paisana@softwareradiosystems.com>
2022-03-08 20:02:00 +0000

    update slot point comments

--- include/srsgnb/ran/slot_point.h
+++ include/srsgnb/ran/slot_point.h
@@ -9,27 +9,27 @@ namespace srsgnb {
 
 class slot_point
 {
-  /// Tag of numerology used: {0: 15 kHz, 1: 30 kHz, 2: 60 kHz, 3: 120 kHz, 4: 240 kHz}
-  uint32_t numerology_ : 3;
-  /// Count value that encodes both SFN and slot index
-  uint32_t count_ : 29;
+  /// Tag of numerology used: {0: 15 kHz, 1: 30 kHz, 2: 60 kHz, 3: 120 kHz, 4: 240 kHz}.
+  uint32_t numerology_val : 3;
+  /// Count value that encodes both SFN and slot index.
+  uint32_t count_val : 29;
 
-  const static uint32_t NOF_NUMEROLOGIES        = 5;
-  const static uint16_t NOF_SFNS                = 1024;
-  const static uint8_t  NOF_SUBFRAMES_PER_FRAME = 10;
+  static constexpr uint32_t NOF_NUMEROLOGIES        = 5;
+  static constexpr uint16_t NOF_SFNS                = 1024;
+  static constexpr uint8_t  NOF_SUBFRAMES_PER_FRAME = 10;
 
-  /// Number of slots per hyperframe
+  /// Number of slots per hyperframe.
   uint32_t nof_slots_per_hf() const { return nof_slots_per_frame() * NOF_SFNS; }
 
 public:
-  slot_point() : numerology_(NOF_NUMEROLOGIES), count_(0) {}
-  slot_point(uint8_t numerology, uint32_t count) : numerology_(numerology), count_(count)
+  slot_point() : numerology_val(NOF_NUMEROLOGIES), count_val(0) {}
+  slot_point(uint8_t numerology, uint32_t count) : numerology_val(numerology), count_val(count)
   {
     srsran_assert(numerology < NOF_NUMEROLOGIES, "Invalid numerology idx={} passed", (int)numerology);
     srsran_assert(count < nof_slots_per_hf(), "Invalid slot count={} passed", (int)count);
   }
   slot_point(uint8_t numerology, uint16_t sfn_val, uint8_t slot) :
-    numerology_(numerology), count_(slot + sfn_val * nof_slots_per_frame())
+    numerology_val(numerology), count_val(slot + sfn_val * nof_slots_per_frame())
   {
     srsran_assert(numerology < NOF_NUMEROLOGIES, "Invalid numerology idx={} passed", (int)numerology);
     srsran_assert(sfn_val < NOF_SFNS, "Invalid SFN={} provided", (int)sfn_val);
@@ -40,39 +40,42 @@ public:
   }
 
   /// Determines whether slot_point was correctly initialized.
-  bool valid() const { return numerology_ < NOF_NUMEROLOGIES; }
+  bool valid() const { return numerology_val < NOF_NUMEROLOGIES; }
 
   /// Number of slots present in a subframe.
-  uint8_t nof_slots_per_subframe() const { return 1U << numerology_; }
+  uint8_t nof_slots_per_subframe() const { return 1U << numerology_val; }
 
   /// Number of slots present in a frame.
   uint8_t nof_slots_per_frame() const { return nof_slots_per_subframe() * NOF_SUBFRAMES_PER_FRAME; }
 
-  /// Frame number
-  uint16_t sfn() const { return count_ / nof_slots_per_frame(); }
+  /// Frame number.
+  uint16_t sfn() const { return count_val / nof_slots_per_frame(); }
 
-  /// Subframe index in a frame
+  /// Subframe index in a frame.
   uint16_t subframe_idx() const { return slot_idx() / nof_slots_per_subframe(); }
 
-  /// Slot index
-  uint8_t slot_idx() const { return count_ % nof_slots_per_frame(); }
+  /// Slot index.
+  uint8_t slot_idx() const { return count_val % nof_slots_per_frame(); }
 
-  /// Numberology index (0..4)
-  uint8_t numerology_idx() const { return numerology_; }
+  /// Numberology index (0..4).
+  uint8_t numerology_idx() const { return numerology_val; }
 
-  /// Conversion of slot_point to integer
-  uint32_t to_uint() const { return count_; }
-  explicit operator uint32_t() const { return count_; }
+  /// Conversion of slot_point to integer.
+  uint32_t to_uint() const { return count_val; }
+  explicit operator uint32_t() const { return count_val; }
 
-  void clear() { numerology_ = NOF_NUMEROLOGIES; }
+  void clear() { numerology_val = NOF_NUMEROLOGIES; }
 
-  /// Comparison operators
-  bool operator==(const slot_point& other) const { return other.count_ == count_ and other.numerology_ == numerology_; }
+  /// Comparison operators.
+  bool operator==(const slot_point& other) const
+  {
+    return other.count_val == count_val and other.numerology_val == numerology_val;
+  }
   bool operator!=(const slot_point& other) const { return not(*this == other); }
   bool operator<(const slot_point& other) const
   {
     srsran_assert(numerology_idx() == other.numerology_idx(), "Comparing slots of different numerologies");
-    int a = static_cast<int>(other.count_) - static_cast<int>(count_);
+    int a = static_cast<int>(other.count_val) - static_cast<int>(count_val);
     if (a > 0) {
       return (a < (int)nof_slots_per_hf() / 2);
     }
@@ -85,7 +88,7 @@ public:
   /// Subtraction of slot points.
   int32_t operator-(const slot_point& other) const
   {
-    int a = static_cast<int>(count_) - static_cast<int>(other.count_);
+    int a = static_cast<int>(count_val) - static_cast<int>(other.count_val);
     if (a >= (int)nof_slots_per_hf() / 2) {
       return a - nof_slots_per_hf();
     }
@@ -98,9 +101,9 @@ public:
   /// Sum of slot points by a jump value.
   slot_point& operator++()
   {
-    count_++;
-    if (count_ == nof_slots_per_hf()) {
-      count_ = 0;
+    count_val++;
+    if (count_val == nof_slots_per_hf()) {
+      count_val = 0;
     }
     return *this;
   }
@@ -112,26 +115,26 @@ public:
   }
   slot_point& operator+=(uint32_t jump)
   {
-    count_ = (count_ + jump) % nof_slots_per_hf();
+    count_val = (count_val + jump) % nof_slots_per_hf();
     return *this;
   }
 
-  /// Subtraction of slot point by a jump value
+  /// Subtraction of slot point by a jump value.
   slot_point& operator-=(uint32_t jump)
   {
-    int a = (static_cast<int>(count_) - static_cast<int>(jump)) % static_cast<int>(nof_slots_per_hf());
+    int a = (static_cast<int>(count_val) - static_cast<int>(jump)) % static_cast<int>(nof_slots_per_hf());
     if (a < 0) {
       a += nof_slots_per_hf();
     }
-    count_ = a;
+    count_val = a;
     return *this;
   }
 
-  /// Determines whether this fits within provided slot points
+  /// Determines whether this fits within provided slot points.
   bool is_in_interval(slot_point begin, slot_point end) const { return (*this >= begin and *this < end); }
 };
 
-/// Sum and subtraction operations over slot_point types
+/// Sum and subtraction operations over slot_point types.
 inline slot_point operator+(slot_point slot, uint32_t jump)
 {
   slot += jump;
@@ -148,7 +151,7 @@ inline slot_point operator-(slot_point slot, uint32_t jump)
   return slot;
 }
 
-/// Get maximum of two slots
+/// Get maximum of two slots.
 inline slot_point max(slot_point lhs, slot_point rhs)
 {
   return lhs > rhs ? lhs : rhs;
@@ -158,7 +161,7 @@ inline slot_point min(slot_point lhs, slot_point rhs)
   return lhs < rhs ? lhs : rhs;
 }
 
-/// Slot Interval [start, stop)
+/// Slot Interval [start, stop).
 using slot_interval = interval<slot_point>;
 
 } // namespace srsgnb
