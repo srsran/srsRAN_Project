8fd763295c0bb2eb43fc0ba9039655d2ae98eccf
Andre Puschmann <andre@softwareradiosystems.com>
2022-03-10 14:33:59 +0100

    f1ap: add basic (un)packing test and PCAP writer

---
+++ include/srsgnb/pcap/f1ap_pcap.h
@@ -0,0 +1,38 @@
+/**
+ *
+ * \section COPYRIGHT
+ *
+ * Copyright 2013-2021 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#ifndef SRSGNB_F1AP_PCAP_H
+#define SRSGNB_F1AP_PCAP_H
+
+#include "pcap.h"
+
+namespace srsgnb {
+
+class f1ap_pcap : pcap_file_base
+{
+public:
+  f1ap_pcap()= default;
+  ~f1ap_pcap();
+  f1ap_pcap(const f1ap_pcap& other) = delete;
+  f1ap_pcap& operator=(const f1ap_pcap& other) = delete;
+  f1ap_pcap(f1ap_pcap&& other)                 = delete;
+  f1ap_pcap& operator=(f1ap_pcap&& other) = delete;
+
+  void enable();
+  void open(const char* filename_);
+  void close();
+  void write_pdu(srsgnb::const_span<uint8_t> pdu);
+};
+
+} // namespace srsran
+
+#endif // SRSGNB_F1AP_PCAP_H
---
+++ lib/pcap/f1ap_pcap.cc
@@ -0,0 +1,49 @@
+/**
+ *
+ * \section COPYRIGHT
+ *
+ * Copyright 2013-2021 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "srsgnb/pcap/f1ap_pcap.h"
+#include "srsgnb/adt/byte_buffer.h"
+#include <stdint.h>
+
+namespace srsgnb {
+
+#define F1AP_DLT 154
+
+f1ap_pcap::~f1ap_pcap()
+{
+  close();
+}
+
+void f1ap_pcap::open(const char* filename_)
+{
+  dlt_pcap_open(F1AP_DLT, filename_);
+}
+void f1ap_pcap::close()
+{
+  dlt_pcap_close();
+}
+
+void f1ap_pcap::write_pdu(srsgnb::const_span<uint8_t> pdu)
+{
+  if (!is_write_enabled() || pdu.empty()) {
+    // skip
+    return;
+  }
+  
+  // write packet header
+  write_pcap_header(pdu.size_bytes());
+
+  // write PDU payload
+  write_pcap_pdu(pdu); 
+}
+
+} // namespace srsgnb
