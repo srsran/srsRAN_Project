49c695948c665e6e8a366a7722c728ad2c7aaeca
Francisco <francisco.paisana@softwareradiosystems.com>
2022-03-14 09:46:23 +0000

    add overload of to_array for lvalue arrays

--- include/srsgnb/adt/to_array.h
+++ include/srsgnb/adt/to_array.h
@@ -9,6 +9,13 @@ namespace srsgnb {
 namespace detail {
 
 template <class T, std::size_t N, std::size_t... I>
+constexpr std::array<std::remove_cv_t<T>, N>
+          to_array_impl(T (&a)[N], std::index_sequence<I...>)
+{
+  return { {a[I]...} };
+}
+
+template <class T, std::size_t N, std::size_t... I>
 constexpr std::array<std::remove_cv_t<T>, N> to_array_impl(T(&&a)[N], std::index_sequence<I...>)
 {
   return {{std::move(a[I])...}};
@@ -16,7 +23,12 @@ constexpr std::array<std::remove_cv_t<T>, N> to_array_impl(T(&&a)[N], std::index
 
 } // namespace detail
 
-/// Helper method to generate an std::array from a C array.
+/// Helper method to generate an std::array from a C array without needing to derive the size N.
+template <class T, std::size_t N>
+constexpr std::array<std::remove_cv_t<T>, N> to_array(T (&a)[N])
+{
+  return detail::to_array_impl(a, std::make_index_sequence<N>{});
+}
 template <class T, std::size_t N>
 constexpr std::array<std::remove_cv_t<T>, N> to_array(T(&&a)[N])
 {
