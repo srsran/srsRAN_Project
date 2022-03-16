e36c62b39b9894b7783a1f5f600bc6ae229781c8
Xavier Arteaga <xavier.arteaga@softwareradiosystems.com>
2022-03-16 15:42:02 +0100

    PHY: review resource grid spy

--- unittests/phy/resource_grid_test_doubles.h
+++ unittests/phy/resource_grid_test_doubles.h
@@ -4,51 +4,27 @@
 #include "srsgnb/phy/resource_grid.h"
 #include "srsgnb/support/srsran_assert.h"
 #include <map>
+#include <tuple>
 
 namespace srsgnb {
 
-/// Describes a resource grid writer spy.
+/// Describes a resource grid writer spy for testing classes that write in the resource grid.
 class resource_grid_writer_spy : public resource_grid_writer
 {
 public:
   /// Describes an expected resource grid writer spy entry.
   struct expected_entry_t {
     /// Indicates the port index.
-    unsigned port;
+    uint8_t port;
     /// Indicates the symbol index.
-    unsigned symbol;
+    uint8_t symbol;
     /// Indicates the subcarrier index.
-    unsigned subcarrier;
-    /// Provides the complex resource element value
+    uint16_t subcarrier;
+    /// Provides the complex resource element value.
     cf_t value;
   };
 
-  /// Describes the resource grid key to index values in the grid map.
-  struct entry_key_t {
-    /// Indicates the port index.
-    unsigned port;
-    /// Indicates the symbol index.
-    unsigned symbol;
-    /// Indicates the subcarrier index.
-    unsigned subcarrier;
-
-    /// Implement lower than comparison to enable \c std::map indexing. The mapping order is port, symbol and subcarrier
-    /// indexes.
-    bool operator<(const entry_key_t& other) const
-    {
-      if (port != other.port) {
-        return port < other.port;
-      }
-
-      if (symbol != other.symbol) {
-        return symbol < other.symbol;
-      }
-
-      return subcarrier < other.subcarrier;
-    }
-  };
-
-  void put(unsigned port, unsigned l, unsigned k, cf_t value) { entries.emplace(entry_key_t{port, l, k}, value); }
+  // See interface for documentation.
   void put(unsigned port, span<const resource_grid_coordinate> coordinates, span<const cf_t> symbols) override
   {
     const cf_t* symbol_ptr = symbols.begin();
@@ -56,6 +32,8 @@ public:
       put(port, coordinate.symbol, coordinate.subcarrier, *(symbol_ptr++));
     }
   }
+
+  // See interface for documentation.
   void put(unsigned port, unsigned l, unsigned k_init, span<const bool> mask, span<const cf_t>& symbol_buffer) override
   {
     unsigned count = 0;
@@ -69,6 +47,8 @@ public:
     // Consume buffer.
     symbol_buffer = symbol_buffer.last(symbol_buffer.size() - count);
   }
+
+  // See interface for documentation.
   void put(unsigned port, unsigned l, unsigned k_init, span<const cf_t> symbols) override
   {
     for (unsigned i = 0; i != symbols.size(); ++i) {
@@ -78,11 +58,11 @@ public:
 
   /// \brief Asserts that the mapped resource elements match with a list of expected entries.
   ///
-  /// This method asserts that mapped resource elements using the \c put methods match a list of expected entries
+  /// This method asserts that mapped resource elements using the put () methods match a list of expected entries
   /// without considering any writing order.
   ///
-  /// \param[in] golden_entries Provides a list of golden symbols to assert.
-  /// \note The test is terminated in case of miss-match.
+  /// \param[in] expected_entries Provides a list of golden symbols to assert.
+  /// \note The test is terminated in case of mismatch.
   void assert_entries(span<const expected_entry_t> expected_entries) const
   {
     // Make sure the number of elements match.
@@ -94,7 +74,11 @@ public:
     // Iterate each expected entry check that there is an entry and the expected value error is below a threshold.
     for (const auto& entry : expected_entries) {
       entry_key_t key = {entry.port, entry.symbol, entry.subcarrier};
-      srsran_assert(entries.count(key), "No resource element was written for port=%d, symbol=%d and subcarrier=%d.");
+      srsran_assert(entries.count(key),
+                    "No resource element was written for port=%d, symbol=%d and subcarrier=%d.",
+                    entry.port,
+                    entry.symbol,
+                    entry.subcarrier);
 
       cf_t  value = entries.at(key);
       float err   = std::abs(entry.value - value);
@@ -111,25 +95,73 @@ public:
   void reset() { entries.clear(); }
 
 private:
+  /// \brief Defines the resource grid indexing key as the tuple of the port, symbol and subcarrier indexes.
+  using entry_key_t = std::tuple<uint8_t, uint8_t, uint16_t>;
+
   /// Defines the maximum allowed error when asserting the resource grid.
   static constexpr float assert_max_error = 1e-6;
 
   /// Stores the resource grid written entries.
   std::map<entry_key_t, cf_t> entries;
+
+  /// \brief This method writes a resource element in the allocation given by the port, symbol and subcarrier indexes.
+  ///
+  /// \param[in] port Indicates the port index.
+  /// \param[in] symbol Indicates the symbol index.
+  /// \param[in] subcarrier Indicates the subcarrier index.
+  /// \param[in] value Indicates the RE value.
+  /// \note The test is terminated in case of resource element overwrite.
+  void put(uint8_t port, uint8_t symbol, uint16_t subcarrier, cf_t value)
+  {
+    // Generate key.
+    entry_key_t key{port, symbol, subcarrier};
+
+    // Ensure the resource element does not exist.
+    srsran_assert(entries.count(key) == 0,
+                  "Detected resource grid overwrite for port=%d, symbol=%d and subcarrier=%d.",
+                  port,
+                  symbol,
+                  subcarrier);
+
+    // Write element.
+    entries.emplace(key, value);
+  }
 };
 
+/// \brief Describes a resource grid dummy used for testing classes that handle resource grids but do not use the
+/// interface.
+///
+/// \note The test terminates if any component under test calls any method from the interface.
 class resource_grid_dummy : public resource_grid
 {
+private:
+  /// Throws a assertion failure due to an overridden method call.
+  void failure() const
+  {
+    srsran_assertion_failure(
+        "Components using resource grid dummy are not allowed to call any method from the interface.");
+  }
+
 public:
-  void put(unsigned port, span<const resource_grid_coordinate> coordinates, span<const cf_t> symbols) override {}
+  void put(unsigned port, span<const resource_grid_coordinate> coordinates, span<const cf_t> symbols) override
+  {
+    failure();
+  }
   void put(unsigned port, unsigned l, unsigned k_init, span<const bool> mask, span<const cf_t>& symbol_buffer) override
-  {}
-  void put(unsigned port, unsigned l, unsigned k_init, span<const cf_t> symbols) override {}
-  void get(unsigned port, span<const resource_grid_coordinate> coordinates, span<cf_t> symbols) const override {}
+  {
+    failure();
+  }
+  void put(unsigned port, unsigned l, unsigned k_init, span<const cf_t> symbols) override { failure(); }
+  void get(unsigned port, span<const resource_grid_coordinate> coordinates, span<cf_t> symbols) const override
+  {
+    failure();
+  }
   void get(unsigned port, unsigned l, unsigned k_init, span<const bool> mask, span<cf_t>& symbol_buffer) const override
-  {}
-  void get(unsigned port, unsigned l, unsigned k_init, span<cf_t> symbols) const override {}
-  void set_all_zero() override {}
+  {
+    failure();
+  }
+  void get(unsigned port, unsigned l, unsigned k_init, span<cf_t> symbols) const override { failure(); }
+  void set_all_zero() override { failure(); }
 };
 
 } // namespace srsgnb
