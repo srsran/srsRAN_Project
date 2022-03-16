b611a063e5f7d3f7fbae96ee8c73ba6129fa460b
ofontbach <oriol.font@srs.io>
2022-03-16 14:08:49 +0100

    Adds suggestions

--- include/srsgnb/support/file_vector.h
+++ include/srsgnb/support/file_vector.h
@@ -6,28 +6,30 @@
 namespace srsgnb {
 
 template <typename T>
-class file_vector {
+class file_vector
+{
 private:
-    std::string file_name;
+  std::string file_name;
+
 public:
   // Constructs object. It does not perform any file operation.
-  file_vector(std::string filename) {
-      file_name = filename;
-  }
-  
-  // Open the file and appends in a \c std::vector<T> all the file entries.
-  std::vector<T> read() const {
-    std::ifstream binary_file(file_name, std::ios::in | std::ios::binary);
-    std::vector<T> read_data = {};
+  file_vector(std::string filename) : file_name(std::move(filename)) {}
 
-    while(!binary_file.eof()) {
-        T read_value;
-        binary_file.read(reinterpret_cast<char*>(&read_value),sizeof(T));
-        read_data.push_back(read_value);
+  // Open the file and appends in a \c std::vector<T> all the file entries.
+  std::vector<T> read() const
+  {
+    std::ifstream binary_file(file_name, std::ios::in | std::ios::binary);
+    if (!binary_file.is_open())
+      return {};
+    std::vector<T> read_data;
+
+    while (!binary_file.eof()) {
+      T read_value;
+      binary_file.read(reinterpret_cast<char*>(&read_value), sizeof(T));
+      read_data.push_back(read_value);
     }
     // remove last repeated entry
     read_data.pop_back();
-    binary_file.close();
 
     return read_data;
   }
