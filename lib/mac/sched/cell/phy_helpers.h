3fd160a82ff31d19cc8314b7a8ff32b7021ca91c
Carlo Galiotto <carlo@srs.io>
2022-03-29 14:43:35 +0200

    sched: fix header
    Signed-off-by: Carlo Galiotto <carlo@srs.io>

--- lib/mac/sched/cell/phy_helpers.h
+++ lib/mac/sched/cell/phy_helpers.h
@@ -12,8 +12,13 @@ class interval;
 /// symbol Interval [start, stop).
 using symbol_interval = interval<uint8_t>;
 
+/// Get MSG3 Delay.
+/// \param[in] ul_cfg_common ASN1 BWP-UplinkCommon struct.
+/// \param[in] sl_dci An index giving a combination (jointly encoded) of start symbols and length indicator (SLIV).
+/// \param[out] pusch_delay MSG3 delay.
+/// \param[out] symbols PUSCH OFDM symbol interval where MSG3 is allocated.
 int get_msg3_delay(const asn1::rrc_nr::ul_cfg_common_sib_s& ul_cfg_common,
-                   slot_point                               sl_dci,
+                   const slot_point&                        sl_dci,
                    unsigned&                                pusch_delay,
                    symbol_interval&                         symbols);
 
