117c344ba87d97dd4c309a56cf00565e496e9765
dvdgrgrtt <davidg@srs.io>
2022-04-12 11:53:22 +0200

    PHY: Provide demodulator implementation
    Simple, not optimized implementation that exploits the widely used
    LLR approximation based on minimum-distance constellation points.

---
+++ lib/phy/upper/channel_modulation/demodulation_mapper_impl.cpp
@@ -0,0 +1,111 @@
+#include "demodulation_mapper_impl.h"
+#include "srsgnb/support/srsran_assert.h"
+
+using namespace srsgnb;
+
+static void demodulate_soft_BPSK(span<int8_t> llrs, span<const cf_t> symbols, span<const float> noise_vars)
+{
+  // Maximum (absolute) value considered for quantization. Larger values will be clipped.
+  constexpr float range_limit_float = 200;
+  // Equivalent of range_limit_float in fixed point representation.
+  constexpr float range_limit_int = 64;
+
+  auto demod_sym = [](cf_t z, float n) {
+    float l_value = 2 * M_SQRT2 * (std::real(z) + std::imag(z)) / n;
+    l_value       = (std::abs(l_value) <= range_limit_float) ? l_value : std::copysign(range_limit_float, l_value);
+    return static_cast<int8_t>(std::round(l_value * range_limit_int / range_limit_float));
+  };
+
+  std::transform(symbols.begin(), symbols.end(), noise_vars.begin(), llrs.begin(), demod_sym);
+}
+
+static void demodulate_soft_QPSK(span<int8_t> llrs, span<const cf_t> symbols, span<const float> noise_vars)
+{
+  // Maximum (absolute) value considered for quantization. Larger values will be clipped.
+  constexpr float range_limit_float = 200;
+  // Equivalent of range_limit_float in fixed point representation.
+  constexpr float range_limit_int = 64;
+
+  auto demod_sym = [](float z, float n) {
+    float l_value = 2 * M_SQRT2 * z / n;
+    l_value       = (std::abs(l_value) <= range_limit_float) ? l_value : std::copysign(range_limit_float, l_value);
+    return static_cast<int8_t>(std::round(l_value * range_limit_int / range_limit_float));
+  };
+
+  auto*       llr_it   = llrs.begin();
+  const auto* noise_it = noise_vars.begin();
+  for (const auto this_symbol : symbols) {
+    *llr_it            = demod_sym(std::real(this_symbol), *noise_it);
+    *std::next(llr_it) = demod_sym(std::imag(this_symbol), *noise_it);
+    llr_it             = std::next(llr_it, 2);
+    noise_it           = std::next(noise_it);
+  }
+}
+
+static void demodulate_soft_QAM16(span<int8_t> llrs, span<const cf_t> symbols, span<const float> noise_vars)
+{
+  // Maximum (absolute) value considered for quantization. Larger values will be clipped.
+  constexpr float range_limit_float = 100;
+  // Equivalent of range_limit_float in fixed point representation.
+  constexpr float range_limit_int = 64;
+
+  const float M_SQRT1_10 = 1.0F / std::sqrt(10.0F);
+
+  auto demod_sym_01 = [M_SQRT1_10](float z, float n) {
+    float l_value = 4 * M_SQRT1_10 * z;
+    l_value       = (std::abs(z) <= 2 * M_SQRT1_10) ? l_value : 2 * l_value - std::copysign(0.8F, z);
+    l_value /= n;
+    l_value = (std::abs(l_value) <= range_limit_float) ? l_value : std::copysign(range_limit_float, l_value);
+    return static_cast<int8_t>(std::round(l_value * range_limit_int / range_limit_float));
+  };
+
+  auto demod_sym_23 = [M_SQRT1_10](float z, float n) {
+    float l_value = 0.8F - 4 * M_SQRT1_10 * std::abs(z);
+    l_value /= n;
+    l_value = (std::abs(l_value) <= range_limit_float) ? l_value : std::copysign(range_limit_float, l_value);
+    return static_cast<int8_t>(std::round(l_value * range_limit_int / range_limit_float));
+  };
+
+  auto*       llr_it   = llrs.begin();
+  const auto* noise_it = noise_vars.begin();
+  for (const auto this_symbol : symbols) {
+    *llr_it               = demod_sym_01(std::real(this_symbol), *noise_it);
+    *std::next(llr_it)    = demod_sym_01(std::imag(this_symbol), *noise_it);
+    *std::next(llr_it, 2) = demod_sym_23(std::real(this_symbol), *noise_it);
+    *std::next(llr_it, 3) = demod_sym_23(std::imag(this_symbol), *noise_it);
+    llr_it                = std::next(llr_it, 4);
+    noise_it              = std::next(noise_it);
+  }
+}
+
+void demodulation_mapper_impl::demodulate_soft(span<int8_t>      llrs,
+                                               span<const cf_t>  symbols,
+                                               span<const float> noise_vars,
+                                               modulation_scheme mod)
+{
+  srsran_assert(symbols.size() == noise_vars.size(), "Inputs symbols and noise_vars must have the same length.");
+  unsigned bits_per_symbol = static_cast<unsigned>(mod);
+  srsran_assert(symbols.size() * bits_per_symbol == llrs.size(), "Input and output lengths are incompatible.");
+  srsran_assert(std::all_of(noise_vars.begin(), noise_vars.end(), [](float f) { return f > 0; }),
+                "Input noise_vars must have positive entries.");
+
+  switch (mod) {
+    case modulation_scheme::BPSK:
+      demodulate_soft_BPSK(llrs, symbols, noise_vars);
+      break;
+    case modulation_scheme::QPSK:
+      demodulate_soft_QPSK(llrs, symbols, noise_vars);
+      break;
+    case modulation_scheme::QAM16:
+      demodulate_soft_QAM16(llrs, symbols, noise_vars);
+      break;
+    default:
+      // TODO(david): finish QAM64 and QAM256. Also, double-check quantization.
+      assert(false);
+  }
+}
+
+std::unique_ptr<demodulation_mapper> srsgnb::create_demodulation_mapper()
+{
+  return std::make_unique<demodulation_mapper_impl>();
+}
---
+++ lib/phy/upper/channel_modulation/demodulation_mapper_impl.h
@@ -0,0 +1,32 @@
+/// \file
+/// \brief Demodulator implementation.
+
+#ifndef SRSGNB_LIB_PHY_UPPER_CHANNEL_MODULATION_DEMODULATION_MAPPER_IMPL_H
+#define SRSGNB_LIB_PHY_UPPER_CHANNEL_MODULATION_DEMODULATION_MAPPER_IMPL_H
+
+#include "srsgnb/phy/upper/channel_modulation/demodulation_mapper.h"
+
+namespace srsgnb {
+
+/// \brief Approximate-LLR demodulator.
+///
+/// Implementation of the interface provided by demodulation_mapper. Instead of the defining expression, the
+/// log-likelihood ratios are computed using the widely accepted approximation
+/// \f[
+/// \ell(b_{m+i}) = -\frac{1}{\sigma_m^2} \biggl( \min_{s\in \mathcal{S}_{i,0}} \abs{y_m - s}^2
+///               - \min_{s\in \mathcal{S}_{i,1}} \abs{y_m - s}^2 \biggr)
+/// \f]
+/// where \f$y_m\f$ denotes the noisy modulation symbol, \f$\sigma_m^2\f$ is the equivalent (after equalization) noise
+/// variance and \f$\mathcal{S}_{i,B}\f$ is the set of constellation points for which \f$b_{m+i}=B\in\{0,1\}\f$.
+class demodulation_mapper_impl : public demodulation_mapper
+{
+  // See interface for the documentation.
+  void demodulate_soft(span<int8_t>      llrs,
+                       span<const cf_t>  symbols,
+                       span<const float> noise_vars,
+                       modulation_scheme mod) override;
+};
+
+} // namespace srsgnb
+
+#endif // SRSGNB_LIB_PHY_UPPER_CHANNEL_MODULATION_DEMODULATION_MAPPER_IMPL_H
