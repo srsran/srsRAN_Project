9a6afd2f625cc93e0f316abbd0a132b04d8c3dea
dvdgrgrtt <davidg@srs.io>
2022-04-14 16:51:52 +0200

    DOCS: Improve modulation mapper documentation

--- include/srsgnb/phy/upper/channel_modulation/modulation_mapper.h
+++ include/srsgnb/phy/upper/channel_modulation/modulation_mapper.h
@@ -9,14 +9,25 @@
 
 namespace srsgnb {
 
+/// \brief Modulation mapper public interface.
+///
+/// Maps bits to complex symbols as specified in TS38.211 Section 5.1.
 class modulation_mapper
 {
 public:
+  /// Default destructor.
   virtual ~modulation_mapper() = default;
 
+  /// \brief Maps a sequence of bits  to a sequence of complex symbols according to the given modulation scheme.
+  /// \param[in]  input   The sequence of bits. Each entry corresponds to a single bit.
+  /// \param[out] symbols The resulting sequence of complex-valued symbols.
+  /// \param[in]  scheme  The modulation scheme determining the mapping.
+  /// \remark The length of \c input should be equal to the length of \c symbols times the modulation order (that is,
+  /// the number of bits per modulated symbol).
   virtual void modulate(span<const uint8_t> input, span<cf_t> symbols, modulation_scheme scheme) = 0;
 };
 
+/// Creates a modulation mapper.
 std::unique_ptr<modulation_mapper> create_modulation_mapper();
 
 } // namespace srsgnb
--- lib/phy/upper/channel_modulation/modulation_mapper_impl.h
+++ lib/phy/upper/channel_modulation/modulation_mapper_impl.h
@@ -9,67 +9,60 @@
  * the distribution.
  *
  */
-#ifndef SRSGNB_CHANNEL_MODULATION_MODULATION_MAPPER_IMPL_H_
-#define SRSGNB_CHANNEL_MODULATION_MODULATION_MAPPER_IMPL_H_
+#ifndef SRSGNB_LIB_PHY_UPPER_CHANNEL_MODULATION_MODULATION_MAPPER_IMPL_H
+#define SRSGNB_LIB_PHY_UPPER_CHANNEL_MODULATION_MODULATION_MAPPER_IMPL_H
 
 #include "srsgnb/phy/upper/channel_modulation/modulation_mapper.h"
 #include <map>
 
 namespace srsgnb {
+/// Modulation mapper based on look-up tables.
 class modulation_mapper_impl : public modulation_mapper
 {
 private:
-  /**
-   * @brief Defines a base structure to store the modulation tables
-   */
+  /// Defines a base structure to store the modulation tables.
   struct modulator_table_s {
-    unsigned          Qm;
+    /// Modulation order (i.e., number of bits per symbol).
+    unsigned Qm;
+
+    /// \brief Look-up table.
+    ///
+    /// The entry at position \c i provides the complex symbol corresponding to the binary expansion of \c i.
     std::vector<cf_t> table;
   };
 
-  /**
-   * @brief Modulation table for BPSK
-   */
+  /// Modulation table for BPSK modualtion.
   struct bpsk_table_s : public modulator_table_s {
     bpsk_table_s();
   };
 
-  /**
-   * @brief Modulation table for QPSK
-   */
+  /// Look-up table for QPSK modulation.
   struct qpsk_table_s : public modulator_table_s {
     qpsk_table_s();
   };
 
-  /**
-   * @brief Modulation table for QAM16
-   */
+  /// Look-up table for 16QAM modulation.
   struct qam16_table_s : public modulator_table_s {
     qam16_table_s();
   };
 
-  /**
-   * @brief Modulation table for QAM64
-   */
+  /// Look-up table for 64QAM modulation.
   struct qam64_table_s : public modulator_table_s {
     qam64_table_s();
   };
 
-  /**
-   * @brief Modulation table for QAM256
-   */
+  /// Look-up table for 256QAM modulation.
   struct qam256_table_s : public modulator_table_s {
     qam256_table_s();
   };
 
-  /**
-   * @brief Maps the modulation schemes to modulation tables
-   */
+  /// Map associating modulation schemes to the corresponding look-up tables.
   static const std::map<modulation_scheme, modulator_table_s> modulation_tables;
 
 public:
+  // See interface for the documentation.
   void modulate(span<const uint8_t> input, span<cf_t> symbols, modulation_scheme scheme) override;
 };
 
 } // namespace srsgnb
-#endif // SRSGNB_CHANNEL_MODULATION_MODULATION_MAPPER_IMPL_H_
+#endif // SRSGNB_LIB_PHY_UPPER_CHANNEL_MODULATION_MODULATION_MAPPER_IMPL_H
