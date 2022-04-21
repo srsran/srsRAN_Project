c50564eb7b5119cd7dede95db3968441c9b75c75
ofontbach <oriol.font@srs.io>
2022-04-21 14:27:48 +0200

    Uppercase for constant

--- unittests/phy/upper/channel_processors/pdsch_encoder_test.cpp
+++ unittests/phy/upper/channel_processors/pdsch_encoder_test.cpp
@@ -16,11 +16,11 @@ int main()
     std::vector<uint8_t> expected_codeword = test_case.encoded.read();
 
     // Assert codeword length.
-    constexpr unsigned max_cw_lenght = 40000;
+    constexpr unsigned MAX_CW_LENGTH = 40000;
     unsigned           cw_length =
         test_case.config.nof_ch_symbols * static_cast<unsigned>(test_case.config.mod) * test_case.config.nof_layers;
     TESTASSERT_EQ(cw_length, expected_codeword.size(), "Wrong codeword length.");
-    static_vector<uint8_t, max_cw_lenght> codeword(cw_length);
+    static_vector<uint8_t, MAX_CW_LENGTH> codeword(cw_length);
 
     // Encode the TB.
     std::unique_ptr<pdsch_encoder> pdsch_encoder = create_pdsch_encoder();
