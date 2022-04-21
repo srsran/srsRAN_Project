344c60eab00e5dd84201f4a7f13b5d7cb8ce5fa8
dvdgrgrtt <davidg@srs.io>
2022-04-21 13:05:01 +0200

    PHY: Use TESTASSERT in demodulator test

--- unittests/phy/upper/channel_modulation/demodulation_mapper_test.cpp
+++ unittests/phy/upper/channel_modulation/demodulation_mapper_test.cpp
@@ -6,9 +6,8 @@
 /// also provided by test vectors.
 
 #include "demodulation_mapper_test_data.h"
-#include "srsgnb/adt/static_vector.h"
 #include "srsgnb/phy/upper/channel_modulation/demodulation_mapper.h"
-#include "srsgnb/phy/upper/channel_modulation/modulation_mapper.h"
+#include "srsgnb/support/srsgnb_test.h"
 
 #include <random>
 
@@ -28,21 +27,21 @@ int main()
 
     const unsigned          nof_symbols = test_case.nsymbols;
     const std::vector<cf_t> symbols     = test_case.symbols.read();
-    srsran_assert(symbols.size() == nof_symbols, "Error reading modulated symbols.");
+    TESTASSERT_EQ(symbols.size(), nof_symbols, "Error reading modulated symbols.");
 
     const std::vector<float> noise_var = test_case.noise_var.read();
-    srsran_assert(noise_var.size() == nof_symbols, "Error reading noise variances.");
-    srsran_assert(std::all_of(noise_var.cbegin(), noise_var.cend(), [](float f) { return f > 0; }),
-                  "Noise variances should take positive values.");
+    TESTASSERT_EQ(noise_var.size(), nof_symbols, "Error reading noise variances.");
+    TESTASSERT(std::all_of(noise_var.cbegin(), noise_var.cend(), [](float f) { return f > 0; }),
+               "Noise variances should take positive values.");
 
     const unsigned      nof_bits = nof_symbols * get_bits_per_symbol(mod);
     std::vector<int8_t> soft_bits(nof_bits);
     demodulator->demodulate_soft(soft_bits, symbols, noise_var, mod);
 
     const std::vector<int8_t> soft_bits_true = test_case.soft_bits.read();
-    srsran_assert(soft_bits_true.size() == nof_bits, "Error reading soft bits.");
+    TESTASSERT_EQ(soft_bits_true.size(), nof_bits, "Error reading soft bits.");
 
-    srsran_assert(
+    TESTASSERT(
         std::equal(
             soft_bits.cbegin(), soft_bits.cend(), soft_bits_true.cbegin(), [](int8_t a, int8_t b) { return a == b; }),
         "Soft bits are not sufficiently precise.");
@@ -50,7 +49,7 @@ int main()
     std::vector<uint8_t> hard_bits(nof_bits);
     std::transform(soft_bits.cbegin(), soft_bits.cend(), hard_bits.begin(), [](int8_t a) { return (a > 0) ? 0U : 1U; });
     const std::vector<uint8_t> hard_bits_true = test_case.hard_bits.read();
-    srsran_assert(hard_bits_true.size() == nof_bits, "Error reading hard bits.");
-    srsran_assert(std::equal(hard_bits.cbegin(), hard_bits.cend(), hard_bits_true.cbegin()), "Hard bits do not match.");
+    TESTASSERT_EQ(hard_bits_true.size(), nof_bits, "Error reading hard bits.");
+    TESTASSERT(std::equal(hard_bits.cbegin(), hard_bits.cend(), hard_bits_true.cbegin()), "Hard bits do not match.");
   }
 }
