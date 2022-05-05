237f38429215733503164ff3c9aeed434bbe19f0
dvdgrgrtt <davidg@srs.io>
2022-05-05 17:03:04 +0200

    PHY: Provide short-block detector unit test

---
+++ unittests/phy/upper/channel_coding/short/short_block_detector_test.cpp
@@ -0,0 +1,34 @@
+/// \file
+/// \brief Short-block detector unit test.
+///
+/// Detects short messages from sequences of log-likelihood ratios. Both the LLR sequences and the corresponding
+/// messages (for comparison purposes) are provided as test vectors.
+
+#include "short_block_detector_test_data.h"
+#include "srsgnb/phy/upper/channel_coding/short/short_block_detector.h"
+#include "srsgnb/support/srsgnb_test.h"
+
+using namespace srsgnb;
+int main()
+{
+  std::unique_ptr<short_block_detector> test_detector = create_short_block_detector();
+
+  for (const auto& test_data : short_block_detector_test_data) {
+    unsigned nof_messages     = test_data.nof_messages;
+    unsigned message_length   = test_data.message_length;
+    unsigned codeblock_length = test_data.codeblock_length;
+
+    const std::vector<uint8_t> messages = test_data.messages.read();
+    TESTASSERT_EQ(messages.size(), nof_messages * message_length, "Error reading messages.");
+    const std::vector<int8_t> codeblocks = test_data.codeblocks.read();
+    TESTASSERT_EQ(codeblocks.size(), nof_messages * codeblock_length, "Error reading codeblocks.");
+
+    std::vector<uint8_t> messages_test(messages.size());
+    for (unsigned msg_idx = 0; msg_idx != nof_messages; ++msg_idx) {
+      span<const int8_t> input  = span<const int8_t>(codeblocks).subspan(msg_idx * codeblock_length, codeblock_length);
+      span<uint8_t>      output = span<uint8_t>(messages_test).subspan(msg_idx * message_length, message_length);
+      TESTASSERT(test_detector->detect(output, input), "Meaningless detection.");
+    }
+    TESTASSERT(std::equal(messages_test.cbegin(), messages_test.cend(), messages.cbegin()), "Detection went wrong.");
+  }
+}
---
+++ unittests/phy/upper/channel_coding/short/short_block_detector_test_data.h
@@ -0,0 +1,47 @@
+#ifndef SRSGNB_UNITTESTS_PHY_UPPER_CHANNEL_CODING_SHORT_SHORT_BLOCK_DETECTOR_TEST_DATA_H
+#define SRSGNB_UNITTESTS_PHY_UPPER_CHANNEL_CODING_SHORT_SHORT_BLOCK_DETECTOR_TEST_DATA_H
+
+// This file was generated using the following MATLAB class:
+//   + "srsShortBlockDetectorUnittest.m"
+
+#include "srsgnb/phy/modulation_scheme.h"
+#include "srsgnb/support/file_vector.h"
+
+namespace srsgnb {
+
+struct test_case_t {
+  unsigned             nof_messages     = 0;
+  unsigned             message_length   = 0;
+  unsigned             codeblock_length = 0;
+  modulation_scheme    mod              = {};
+  file_vector<int8_t>  codeblocks;
+  file_vector<uint8_t> messages;
+};
+
+static const std::vector<test_case_t> short_block_detector_test_data = {
+    // clang-format off
+  {10, 1, 1, modulation_scheme::BPSK, {"test_data/short_block_detector_test_input0.dat"}, {"test_data/short_block_detector_test_output0.dat"}},
+  {10, 1, 2, modulation_scheme::QPSK, {"test_data/short_block_detector_test_input1.dat"}, {"test_data/short_block_detector_test_output1.dat"}},
+  {10, 1, 4, modulation_scheme::QAM16, {"test_data/short_block_detector_test_input2.dat"}, {"test_data/short_block_detector_test_output2.dat"}},
+  {10, 1, 6, modulation_scheme::QAM64, {"test_data/short_block_detector_test_input3.dat"}, {"test_data/short_block_detector_test_output3.dat"}},
+  {10, 1, 8, modulation_scheme::QAM256, {"test_data/short_block_detector_test_input4.dat"}, {"test_data/short_block_detector_test_output4.dat"}},
+  {10, 2, 3, modulation_scheme::BPSK, {"test_data/short_block_detector_test_input5.dat"}, {"test_data/short_block_detector_test_output5.dat"}},
+  {10, 2, 6, modulation_scheme::QPSK, {"test_data/short_block_detector_test_input6.dat"}, {"test_data/short_block_detector_test_output6.dat"}},
+  {10, 2, 12, modulation_scheme::QAM16, {"test_data/short_block_detector_test_input7.dat"}, {"test_data/short_block_detector_test_output7.dat"}},
+  {10, 2, 18, modulation_scheme::QAM64, {"test_data/short_block_detector_test_input8.dat"}, {"test_data/short_block_detector_test_output8.dat"}},
+  {10, 2, 24, modulation_scheme::QAM256, {"test_data/short_block_detector_test_input9.dat"}, {"test_data/short_block_detector_test_output9.dat"}},
+  {10, 3, 32, modulation_scheme::BPSK, {"test_data/short_block_detector_test_input10.dat"}, {"test_data/short_block_detector_test_output10.dat"}},
+  {10, 4, 32, modulation_scheme::BPSK, {"test_data/short_block_detector_test_input11.dat"}, {"test_data/short_block_detector_test_output11.dat"}},
+  {10, 5, 32, modulation_scheme::BPSK, {"test_data/short_block_detector_test_input12.dat"}, {"test_data/short_block_detector_test_output12.dat"}},
+  {10, 6, 32, modulation_scheme::BPSK, {"test_data/short_block_detector_test_input13.dat"}, {"test_data/short_block_detector_test_output13.dat"}},
+  {10, 7, 32, modulation_scheme::BPSK, {"test_data/short_block_detector_test_input14.dat"}, {"test_data/short_block_detector_test_output14.dat"}},
+  {10, 8, 32, modulation_scheme::BPSK, {"test_data/short_block_detector_test_input15.dat"}, {"test_data/short_block_detector_test_output15.dat"}},
+  {10, 9, 32, modulation_scheme::BPSK, {"test_data/short_block_detector_test_input16.dat"}, {"test_data/short_block_detector_test_output16.dat"}},
+  {10, 10, 32, modulation_scheme::BPSK, {"test_data/short_block_detector_test_input17.dat"}, {"test_data/short_block_detector_test_output17.dat"}},
+  {10, 11, 32, modulation_scheme::BPSK, {"test_data/short_block_detector_test_input18.dat"}, {"test_data/short_block_detector_test_output18.dat"}},
+    // clang-format on
+};
+
+} // namespace srsgnb
+
+#endif // SRSGNB_UNITTESTS_PHY_UPPER_CHANNEL_CODING_SHORT_SHORT_BLOCK_DETECTOR_TEST_DATA_H
