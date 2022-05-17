1cda070fb1d0b6c781f84c3e365bdad3e4e38dc7
dvdgrgrtt <davidg@srs.io>
2022-05-17 12:25:38 +0200

    PHY: Preliminary PUSCH decoder implementation
    The provided implementation works fine but needs some fine-tuning on the
    management of retransmissions and on the recollection of statistics.

---
+++ lib/phy/upper/channel_processors/pusch_decoder_impl.cpp
@@ -0,0 +1,136 @@
+/*
+ *
+ * Copyright 2013-2022 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "pusch_decoder_impl.h"
+#include "srsgnb/srsvec/bit.h"
+
+using namespace srsgnb;
+
+// Number of bits in one byte.
+static constexpr unsigned BITS_PER_BYTE = 8;
+
+// Maximum accepted transport block size.
+static constexpr unsigned MAX_TBS = 1277992;
+
+std::unique_ptr<pusch_decoder> srsgnb::create_pusch_decoder()
+{
+  std::unique_ptr<ldpc_segmenter>      seg  = create_ldpc_segmenter();
+  std::unique_ptr<ldpc_rate_dematcher> rdem = create_ldpc_rate_dematcher();
+  std::unique_ptr<ldpc_decoder>        dec  = create_ldpc_decoder("generic");
+  pusch_decoder_impl::sch_crc          crcs = {create_crc_calculator(crc_generator_poly::CRC16),
+                                               create_crc_calculator(crc_generator_poly::CRC24A),
+                                               create_crc_calculator(crc_generator_poly::CRC24B)};
+  return std::make_unique<pusch_decoder_impl>(seg, rdem, dec, crcs);
+}
+
+// Select the CRC for the decoder based on the TBS and the number of codeblocks.
+crc_calculator* select_crc(pusch_decoder_impl::sch_crc& crcs, unsigned tbs, unsigned nof_blocks)
+{
+  if (nof_blocks > 1) {
+    return crcs.crc24B.get();
+  }
+  // From here, we have only 1 codeblock.
+  constexpr unsigned MAX_BITS_CRC16 = 3824;
+  if (tbs > MAX_BITS_CRC16) {
+    return crcs.crc24A.get();
+  }
+  // One short codeblock.
+  return crcs.crc16.get();
+}
+
+void pusch_decoder_impl::decode(span<uint8_t>            transport_block,
+                                statistics&              info,
+                                rx_softbuffer*           soft_codeword,
+                                span<const int8_t>       llrs,
+                                const bool               new_data,
+                                const segment_config&    cfg,
+                                const algorithm_details& alg_cfg)
+{
+  // Temporary buffer to store the rate-matched codeblocks (represented by LLRs) and their metadata.
+  static_vector<described_rx_codeblock, MAX_NOF_SEGMENTS> codeblock_llrs = {};
+  // Recall that the TB is in packed format.
+  unsigned tb_size = transport_block.size() * BITS_PER_BYTE;
+  segmenter->segment_rx(codeblock_llrs, llrs, tb_size, cfg);
+
+  unsigned nof_cbs = codeblock_llrs.size();
+  srsran_assert(nof_cbs == soft_codeword->get_nof_codeblocks(), "Wrong number of codeblocks.");
+
+  // BG1 has rate 1/3 and BG2 has rate 1/5.
+  constexpr unsigned INVERSE_BG1_RATE = 3;
+  constexpr unsigned INVERSE_BG2_RATE = 5;
+  unsigned           inverse_rate =
+      (codeblock_llrs[0].second.tb_common.base_graph == ldpc::base_graph_t::BG1) ? INVERSE_BG1_RATE : INVERSE_BG2_RATE;
+
+  unsigned tb_and_crc_size = tb_size;
+  // If only one codeblock is transmitted, the CRC is removed by the decoder. If more than one codeblock is transmitted,
+  // there is an extra CRC of length 24 bits.
+  constexpr unsigned BLOCK_CRC_LENGTH = 24;
+  if (nof_cbs > 1) {
+    tb_and_crc_size += BLOCK_CRC_LENGTH;
+  }
+
+  // Temporary buffer to store the unpacked transport block (and, if applicable, its CRC).
+  static_vector<uint8_t, MAX_TBS + BLOCK_CRC_LENGTH> tmp_tb_bits(tb_and_crc_size);
+
+  // Select CRC calculator for inner codeblock checks.
+  crc_calculator* block_crc = select_crc(crc_set, tb_size, nof_cbs);
+
+  span<bool> cb_crcs   = soft_codeword->get_codeblocks_crc();
+  unsigned   tb_offset = 0;
+  for (unsigned cb_id = 0; cb_id != nof_cbs; ++cb_id) {
+    const auto& cb_llrs = codeblock_llrs[cb_id].first;
+    const auto& cb_meta = codeblock_llrs[cb_id].second;
+    srsran_assert(cb_llrs.size() == cb_meta.cb_specific.rm_length, "Wrong rate-matched codeblock length.");
+
+    // Get codeblock length, without rate matching.
+    unsigned cb_length = cb_meta.cb_specific.full_length;
+    // Compute the message length.
+    unsigned msg_length = cb_length / inverse_rate;
+    // Number of data bits (no CRC, no filler bits - may contain zero-padding).
+    unsigned nof_data_bits = msg_length - cb_meta.cb_specific.nof_crc_bits - cb_meta.cb_specific.nof_filler_bits;
+    if (!cb_crcs[cb_id]) {
+      // Get the LLRs from previous transmissions, if any, or a clean buffer.
+      span<int8_t> codeblock = soft_codeword->get_codeblock_soft_bits(cb_id, cb_length);
+
+      // Dematch the new LLRs and combine them with the ones from previous transmissions.
+      dematcher->rate_dematch(codeblock, cb_llrs, new_data, cb_meta);
+
+      // Temporary buffer to store decoded bits.
+      static_vector<uint8_t, MAX_SEG_LENGTH> message(msg_length);
+
+      // Number of TB bits still "empty".
+      unsigned free_tb_bits = tb_and_crc_size - tb_offset;
+      // Avoid including zero-padding in the TB.
+      unsigned nof_new_bits = std::min(free_tb_bits, nof_data_bits);
+      // Try to decode
+      if (decoder->decode(message, codeblock, block_crc, {cb_meta, alg_cfg})) {
+        // If successful decoding, flag the CRC and copy bits to the TB buffer.
+        cb_crcs[cb_id]             = true;
+        span<uint8_t> this_segment = span<uint8_t>(tmp_tb_bits).subspan(tb_offset, nof_new_bits);
+        std::copy_n(message.begin(), nof_new_bits, this_segment.begin());
+      }
+      tb_offset += nof_new_bits;
+    }
+  }
+  srsran_assert(tb_offset == tb_and_crc_size, "All TB bits should be filled at this point.");
+
+  if (nof_cbs == 1) {
+    // When only one codeblock, the CRC of codeblock and transport block are the same.
+    info.tb_crc_ok = cb_crcs[0];
+  } else {
+    // When more than one codeblock, we need to check the global transport block CRC. Note that there is no need to
+    // compute the CRC if any of the codeblocks was not decoded correctly.
+    info.tb_crc_ok = (std::all_of(cb_crcs.begin(), cb_crcs.end(), [](bool a) { return a; })) &&
+                     (crc_set.crc24A->calculate_bit(tmp_tb_bits) == 0);
+  }
+
+  // Pack the transport block.
+  srsvec::bit_pack(transport_block, span<uint8_t>(tmp_tb_bits).first(tb_size));
+}
---
+++ lib/phy/upper/channel_processors/pusch_decoder_impl.h
@@ -0,0 +1,87 @@
+/*
+ *
+ * Copyright 2013-2022 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#ifndef SRSGNB_LIB_PHY_UPPER_CHANNEL_PROCESSORS_PUSCH_DECODER_IMPL_H
+#define SRSGNB_LIB_PHY_UPPER_CHANNEL_PROCESSORS_PUSCH_DECODER_IMPL_H
+
+#include "srsgnb/phy/upper/channel_coding/ldpc/ldpc_decoder.h"
+#include "srsgnb/phy/upper/channel_coding/ldpc/ldpc_rate_dematcher.h"
+#include "srsgnb/phy/upper/channel_coding/ldpc/ldpc_segmenter.h"
+#include "srsgnb/phy/upper/channel_processors/pusch_decoder.h"
+
+namespace srsgnb {
+
+/// Implementation of the PUSCH decoder.
+class pusch_decoder_impl : public pusch_decoder
+{
+public:
+  /// CRC calculators used in shared channels.
+  struct sch_crc {
+    /// For short TB checksums.
+    std::unique_ptr<crc_calculator> crc16;
+    /// For long TB checksums.
+    std::unique_ptr<crc_calculator> crc24A;
+    /// For segment-specific checksums.
+    std::unique_ptr<crc_calculator> crc24B;
+  };
+
+  /// \brief PUSCH decoder constructor.
+  ///
+  /// Sets up the internal components, namely LDPC segmenter, LDPC rate dematcher, LDPC decoder and all the CRC
+  /// calculators.
+  ///
+  /// \param[in] seg   Pointer to an LDPC segmenter object.
+  /// \param[in] rdem  Pointer to an LDPC rate dematcher object.
+  /// \param[in] dec   Pointer to an LDPC decoder object.
+  /// \param[in] crcs  Structure with pointers to three CRC calculator objects, with generator polynomials of type \c
+  ///                  CRC16, \c CRC24A and \c CRC24B.
+  pusch_decoder_impl(std::unique_ptr<ldpc_segmenter>&      seg,
+                     std::unique_ptr<ldpc_rate_dematcher>& rdem,
+                     std::unique_ptr<ldpc_decoder>&        dec,
+                     sch_crc&                              crcs) :
+    segmenter(std::move(seg)),
+    dematcher(std::move(rdem)),
+    decoder(std::move(dec)),
+    crc_set({std::move(crcs.crc16), std::move(crcs.crc24A), std::move(crcs.crc24B)})
+  {
+    srsran_assert(crc_set.crc16->get_generator_poly() == crc_generator_poly::CRC16, "Wrong TB CRC calculator.");
+    srsran_assert(crc_set.crc24A->get_generator_poly() == crc_generator_poly::CRC24A, "Wrong TB CRC calculator.");
+    srsran_assert(crc_set.crc24B->get_generator_poly() == crc_generator_poly::CRC24B, "Wrong TB CRC calculator.");
+  };
+
+  // See interface for the documentation.
+  void decode(span<uint8_t>            transport_block,
+              statistics&              info,
+              rx_softbuffer*           soft_codeword,
+              span<const int8_t>       llrs,
+              bool                     new_data,
+              const segment_config&    cfg,
+              const algorithm_details& alg_cfg) override;
+
+private:
+  /// Pointer to an LDPC segmenter.
+  std::unique_ptr<ldpc_segmenter> segmenter;
+
+  /// Pointer to an LDPC rate-dematcher.
+  std::unique_ptr<ldpc_rate_dematcher> dematcher;
+
+  /// Pointer to an LDPC decoder.
+  std::unique_ptr<ldpc_decoder> decoder;
+
+  /// \brief Pointer to a CRC calculator for TB-wise checksum.
+  ///
+  /// Only the CRC calculator with generator polynomial crc_generator_poly::CRC24A, used for long transport blocks, is
+  /// needed. Indeed, if a transport block is short enough not to be segmented, the CRC is verified by the decoder.
+  sch_crc crc_set;
+};
+
+} // namespace srsgnb
+
+#endif // SRSGNB_LIB_PHY_UPPER_CHANNEL_PROCESSORS_PUSCH_DECODER_IMPL_H
