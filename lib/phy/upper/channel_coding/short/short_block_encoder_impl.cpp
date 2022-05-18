4b6aa56129c42fdf9f481b15fb319d6009d7af37
dvdgrgrtt <davidg@srs.io>
2022-05-18 07:39:25 +0200

    Add copyright/license header to new files

--- lib/phy/upper/channel_coding/short/short_block_encoder_impl.cpp
+++ lib/phy/upper/channel_coding/short/short_block_encoder_impl.cpp
@@ -1,3 +1,13 @@
+/*
+ *
+ * Copyright 2013-2022 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
 #include "short_block_encoder_impl.h"
 #include "srsgnb/adt/static_vector.h"
 #include "srsgnb/srsvec/binary.h"
