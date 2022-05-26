ba36dbcd76054a96e0ad1aaf37f18822a72a2b76
dvdgrgrtt <davidg@srs.io>
2022-05-26 18:43:53 +0200

    PHY: Improve LLR unit test
    Extend coverage.

--- unittests/phy/upper/log_likelihood_ratio_test.cpp
+++ unittests/phy/upper/log_likelihood_ratio_test.cpp
@@ -37,14 +37,21 @@ int main()
 
   llr0 += llr1;
   TESTASSERT_EQ(log_likelihood_ratio(2), llr0, "Addition assignment not working.");
+  TESTASSERT_EQ(log_likelihood_ratio(0), llr0 - llr1, "Special case 0 not working.");
+  TESTASSERT_EQ(LLR_INFINITY, LLR_INFINITY - llr1, "Special case INFTY + finite not working.");
+  TESTASSERT_EQ(LLR_INFINITY, llr0 + LLR_INFINITY, "Special case finite + INFTY not working.");
+  TESTASSERT_EQ(log_likelihood_ratio(0), LLR_INFINITY - LLR_INFINITY, "Special case INFTY - INFTY not working.");
 
   TESTASSERT_EQ(static_cast<int>(llr0), 2, "Static casting not working.");
   TESTASSERT_EQ(llr0.to_int(), 2, "Static casting not working.");
+  TESTASSERT_EQ(static_cast<log_likelihood_ratio::value_type>(llr0), 2, "Static casting not working.");
 
   llr0 = -100;
   llr1 = 100;
   TESTASSERT_EQ(-LLR_MAX, llr0 - llr1, "Saturation of negative values not working.");
   TESTASSERT_EQ(LLR_INFINITY, log_likelihood_ratio::promotion_sum(LLR_MAX, LLR_MAX), "Promotion sum not working.");
+  TESTASSERT_EQ(
+      LLR_INFINITY, log_likelihood_ratio::promotion_sum(LLR_INFINITY, LLR_MAX), "Promotion sum special not working.");
 
   std::array<log_likelihood_ratio, 4> llr_sequence = {2, -2, 2, -2};
   TESTASSERT(typeid(int) == typeid(log_likelihood_ratio::norm_squared(llr_sequence)),
@@ -62,4 +69,8 @@ int main()
              "dot_prod_llr does not propagate type.");
   TESTASSERT(std::abs(1.1F - log_likelihood_ratio::dot_prod(llr_sequence, rhs, 1.1F)) < .00001,
              "dot_prod_llr not working.");
+
+  fmt::memory_buffer buf;
+  fmt::format_to(buf, "{}", log_likelihood_ratio(1));
+  TESTASSERT_EQ(to_string(buf), "LLR(1)");
 }
