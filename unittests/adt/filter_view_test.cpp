7bcb3da08854928b56ceb9dd8c43697858f07f56
Francisco <francisco.paisana@softwareradiosystems.com>
2022-05-27 12:08:28 +0100

    adt: implement filter view and respective unit test

---
+++ unittests/adt/filter_view_test.cpp
@@ -0,0 +1,55 @@
+/*
+ *
+ * Copyright 2013-2022 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "srsgnb/adt/filter_view.h"
+#include "srsgnb/support/test_utils.h"
+
+using namespace srsgnb;
+
+void test_array_filter()
+{
+  std::array<bool, 10> a{};
+  a[2] = true;
+
+  int count = 0;
+  for (bool v : views::filter(a, [](bool b) { return b; })) {
+    TESTASSERT(v == true);
+    count++;
+  }
+  TESTASSERT_EQ(1, count);
+}
+
+void test_rvalue_filter()
+{
+  int count = 0;
+  for (int v : views::filter(std::vector<int>{1, 2, 3, 4, 5}, [](int i) { return i < 3; })) {
+    TESTASSERT(v < 3);
+    count++;
+  }
+  TESTASSERT_EQ(2, count);
+}
+
+void test_chain_filter()
+{
+  int count = 0;
+  for (int v : views::filter(views::filter(std::vector<int>{1, 2, 3, 4, 5, 6, 7}, [](int i) { return i < 5; }),
+                             [](int i) { return i % 2 == 0; })) {
+    TESTASSERT(v < 5 and v % 2 == 0);
+    count++;
+  }
+  TESTASSERT_EQ(2, count);
+}
+
+int main()
+{
+  test_array_filter();
+  test_rvalue_filter();
+  test_chain_filter();
+}
