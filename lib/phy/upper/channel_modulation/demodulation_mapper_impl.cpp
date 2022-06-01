407690596246453280859aebac42cb4d72bd62d4
dvdgrgrtt <davidg@srs.io>
2022-06-01 16:15:43 +0200

    PHY: Avoid lambdas in demodulation mapper

--- lib/phy/upper/channel_modulation/demodulation_mapper_impl.cpp
+++ lib/phy/upper/channel_modulation/demodulation_mapper_impl.cpp
@@ -14,71 +14,74 @@
 
 using namespace srsgnb;
 
+// Equivalent of RANGE_LIMIT_FLOAT in fixed point representation.
+static constexpr float RANGE_LIMIT_INT = 64;
+
+// Squared root of 1/10.
+static const float M_SQRT1_10 = 1.0F / std::sqrt(10.0F);
+
+static int8_t demod_BPSK_symbol(cf_t z, float noise_var, float range_limit)
+{
+  float l_value = 2.0F * M_SQRT2f32 * (std::real(z) + std::imag(z)) / noise_var;
+  return clip_and_quantize(l_value, range_limit, RANGE_LIMIT_INT);
+};
+
 static void demodulate_soft_BPSK(span<int8_t> llrs, span<const cf_t> symbols, span<const float> noise_vars)
 {
   // Maximum (absolute) value considered for quantization. Larger values will be clipped.
   constexpr float RANGE_LIMIT_FLOAT = 200;
-  // Equivalent of range_limit_float in fixed point representation.
-  constexpr float RANGE_LIMIT_INT = 64;
-
-  auto demod_sym = [](cf_t z, float n) {
-    float l_value = 2.0F * M_SQRT2f32 * (std::real(z) + std::imag(z)) / n;
-    return clip_and_quantize(l_value, RANGE_LIMIT_FLOAT, RANGE_LIMIT_INT);
-  };
-
-  std::transform(symbols.begin(), symbols.end(), noise_vars.begin(), llrs.begin(), demod_sym);
+  std::transform(symbols.begin(), symbols.end(), noise_vars.begin(), llrs.begin(), [](cf_t z, float n) {
+    return demod_BPSK_symbol(z, n, RANGE_LIMIT_FLOAT);
+  });
 }
+
+static int8_t demod_QPSK_symbol(float x, float noise_var, float range_limit)
+{
+  float l_value = 2.0F * M_SQRT2f32 * x / noise_var;
+  return clip_and_quantize(l_value, range_limit, RANGE_LIMIT_INT);
+};
 
 static void demodulate_soft_QPSK(span<int8_t> llrs, span<const cf_t> symbols, span<const float> noise_vars)
 {
   // Maximum (absolute) value considered for quantization. Larger values will be clipped.
   constexpr float RANGE_LIMIT_FLOAT = 200;
-  // Equivalent of range_limit_float in fixed point representation.
-  constexpr float RANGE_LIMIT_INT = 64;
-
-  auto demod_sym = [](float z, float n) {
-    float l_value = 2.0F * M_SQRT2f32 * z / n;
-    return clip_and_quantize(l_value, RANGE_LIMIT_FLOAT, RANGE_LIMIT_INT);
-  };
 
   auto*       llr_it   = llrs.begin();
   const auto* noise_it = noise_vars.begin();
   for (const auto this_symbol : symbols) {
-    *llr_it++ = demod_sym(std::real(this_symbol), *noise_it);
-    *llr_it++ = demod_sym(std::imag(this_symbol), *noise_it);
+    *llr_it++ = demod_QPSK_symbol(std::real(this_symbol), *noise_it, RANGE_LIMIT_FLOAT);
+    *llr_it++ = demod_QPSK_symbol(std::imag(this_symbol), *noise_it, RANGE_LIMIT_FLOAT);
     ++noise_it;
   }
 }
+
+static int8_t demod_16QAM_symbol_01(float x, float noise_var, float range_limit)
+{
+  float l_value = 4 * M_SQRT1_10 * x;
+  l_value       = (std::abs(x) <= 2 * M_SQRT1_10) ? l_value : 2 * l_value - std::copysign(0.8F, x);
+  l_value /= noise_var;
+  return clip_and_quantize(l_value, range_limit, RANGE_LIMIT_INT);
+};
+
+static int8_t demod_16QAM_symbol_23(float x, float noise_var, float range_limit)
+{
+  float l_value = 0.8F - 4 * M_SQRT1_10 * std::abs(x);
+  l_value /= noise_var;
+  return clip_and_quantize(l_value, range_limit, RANGE_LIMIT_INT);
+};
 
 static void demodulate_soft_QAM16(span<int8_t> llrs, span<const cf_t> symbols, span<const float> noise_vars)
 {
   // Maximum (absolute) value considered for quantization. Larger values will be clipped.
   constexpr float RANGE_LIMIT_FLOAT = 100;
-  // Equivalent of range_limit_float in fixed point representation.
-  constexpr float RANGE_LIMIT_INT = 64;
-
-  const float M_SQRT1_10 = 1.0F / std::sqrt(10.0F);
-
-  auto demod_sym_01 = [M_SQRT1_10](float z, float n) {
-    float l_value = 4 * M_SQRT1_10 * z;
-    l_value       = (std::abs(z) <= 2 * M_SQRT1_10) ? l_value : 2 * l_value - std::copysign(0.8F, z);
-    l_value /= n;
-    return clip_and_quantize(l_value, RANGE_LIMIT_FLOAT, RANGE_LIMIT_INT);
-  };
-
-  auto demod_sym_23 = [M_SQRT1_10](float z, float n) {
-    float l_value = 0.8F - 4 * M_SQRT1_10 * std::abs(z);
-    l_value /= n;
-    return clip_and_quantize(l_value, RANGE_LIMIT_FLOAT, RANGE_LIMIT_INT);
-  };
 
   auto*       llr_it   = llrs.begin();
   const auto* noise_it = noise_vars.begin();
   for (const auto this_symbol : symbols) {
-    *llr_it++ = demod_sym_01(std::real(this_symbol), *noise_it);
-    *llr_it++ = demod_sym_01(std::imag(this_symbol), *noise_it);
-    *llr_it++ = demod_sym_23(std::real(this_symbol), *noise_it);
-    *llr_it++ = demod_sym_23(std::imag(this_symbol), *noise_it);
+    *llr_it++ = demod_16QAM_symbol_01(std::real(this_symbol), *noise_it, RANGE_LIMIT_FLOAT);
+    *llr_it++ = demod_16QAM_symbol_01(std::imag(this_symbol), *noise_it, RANGE_LIMIT_FLOAT);
+    *llr_it++ = demod_16QAM_symbol_23(std::real(this_symbol), *noise_it, RANGE_LIMIT_FLOAT);
+    *llr_it++ = demod_16QAM_symbol_23(std::imag(this_symbol), *noise_it, RANGE_LIMIT_FLOAT);
     ++noise_it;
   }
 }
