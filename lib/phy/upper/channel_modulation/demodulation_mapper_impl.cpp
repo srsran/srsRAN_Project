0ac6eea138313ef48151cd7fa48e98474739e92b
dvdgrgrtt <davidg@srs.io>
2022-06-02 12:38:43 +0200

    PHY: Provide demodulation mapper for 256QAM

--- lib/phy/upper/channel_modulation/demodulation_mapper_impl.cpp
+++ lib/phy/upper/channel_modulation/demodulation_mapper_impl.cpp
@@ -19,6 +19,9 @@ static const float M_SQRT1_10 = 1.0F / std::sqrt(10.0F);
 // Square root of 1/42.
 static const float M_SQRT1_42 = 1.0F / std::sqrt(42.0F);
 
+// Square root of 1/170.
+static const float M_SQRT1_170 = 1.0F / std::sqrt(170.0F);
+
 static log_likelihood_ratio demod_BPSK_symbol(cf_t z, float noise_var, float range_limit)
 {
   float l_value = 2.0F * M_SQRT2f32 * (std::real(z) + std::imag(z)) / noise_var;
@@ -168,6 +171,173 @@ demodulate_soft_QAM64(span<log_likelihood_ratio> llrs, span<const cf_t> symbols,
   }
 }
 
+static log_likelihood_ratio demod_256QAM_symbol_01(float x, float noise_var, float range_limit)
+{
+  constexpr unsigned                     NOF_INTERVALS = 16;
+  const std::array<float, NOF_INTERVALS> SLOPE         = {32 * M_SQRT1_170,
+                                                  28 * M_SQRT1_170,
+                                                  24 * M_SQRT1_170,
+                                                  20 * M_SQRT1_170,
+                                                  16 * M_SQRT1_170,
+                                                  12 * M_SQRT1_170,
+                                                  8 * M_SQRT1_170,
+                                                  4 * M_SQRT1_170,
+                                                  4 * M_SQRT1_170,
+                                                  8 * M_SQRT1_170,
+                                                  12 * M_SQRT1_170,
+                                                  16 * M_SQRT1_170,
+                                                  20 * M_SQRT1_170,
+                                                  24 * M_SQRT1_170,
+                                                  28 * M_SQRT1_170,
+                                                  32 * M_SQRT1_170};
+  const std::array<float, NOF_INTERVALS> INTERCEPT     = {112.0F / 85,
+                                                      84.0F / 85,
+                                                      60.0F / 85,
+                                                      40.0F / 85,
+                                                      24.0F / 85,
+                                                      12.0F / 85,
+                                                      4.0F / 85,
+                                                      0.0F,
+                                                      0.0F,
+                                                      -4.0F / 85,
+                                                      -12.0F / 85,
+                                                      -24.0F / 85,
+                                                      -40.0F / 85,
+                                                      -60.0F / 85,
+                                                      -84.0F / 85,
+                                                      -112.0F / 85};
+
+  unsigned idx     = compute_interval_idx(x, 2 * M_SQRT1_170, NOF_INTERVALS);
+  float    l_value = SLOPE[idx] * x + INTERCEPT[idx];
+  l_value /= noise_var;
+  return log_likelihood_ratio::quantize(l_value, range_limit);
+}
+
+static log_likelihood_ratio demod_256QAM_symbol_23(float x, float noise_var, float range_limit)
+{
+  constexpr unsigned                     NOF_INTERVALS = 16;
+  const std::array<float, NOF_INTERVALS> SLOPE         = {16 * M_SQRT1_170,
+                                                  12 * M_SQRT1_170,
+                                                  8 * M_SQRT1_170,
+                                                  4 * M_SQRT1_170,
+                                                  4 * M_SQRT1_170,
+                                                  8 * M_SQRT1_170,
+                                                  12 * M_SQRT1_170,
+                                                  16 * M_SQRT1_170,
+                                                  -16 * M_SQRT1_170,
+                                                  -12 * M_SQRT1_170,
+                                                  -8 * M_SQRT1_170,
+                                                  -4 * M_SQRT1_170,
+                                                  -4 * M_SQRT1_170,
+                                                  -8 * M_SQRT1_170,
+                                                  -12 * M_SQRT1_170,
+                                                  -16 * M_SQRT1_170};
+  const std::array<float, NOF_INTERVALS> INTERCEPT     = {88.0F / 85,
+                                                      60.0F / 85,
+                                                      36.0F / 85,
+                                                      16.0F / 85,
+                                                      16.0F / 85,
+                                                      28.0F / 85,
+                                                      36.0F / 85,
+                                                      40.0F / 85,
+                                                      40.0F / 85,
+                                                      36.0F / 85,
+                                                      28.0F / 85,
+                                                      16.0F / 85,
+                                                      16.0F / 85,
+                                                      36.0F / 85,
+                                                      60.0F / 85,
+                                                      88.0F / 85};
+
+  unsigned idx     = compute_interval_idx(x, 2 * M_SQRT1_170, NOF_INTERVALS);
+  float    l_value = SLOPE[idx] * x + INTERCEPT[idx];
+  l_value /= noise_var;
+  return log_likelihood_ratio::quantize(l_value, range_limit);
+}
+
+static log_likelihood_ratio demod_256QAM_symbol_45(float x, float noise_var, float range_limit)
+{
+  constexpr unsigned                     NOF_INTERVALS = 16;
+  const std::array<float, NOF_INTERVALS> SLOPE         = {8 * M_SQRT1_170,
+                                                  4 * M_SQRT1_170,
+                                                  4 * M_SQRT1_170,
+                                                  8 * M_SQRT1_170,
+                                                  -8 * M_SQRT1_170,
+                                                  -4 * M_SQRT1_170,
+                                                  -4 * M_SQRT1_170,
+                                                  -8 * M_SQRT1_170,
+                                                  8 * M_SQRT1_170,
+                                                  4 * M_SQRT1_170,
+                                                  4 * M_SQRT1_170,
+                                                  8 * M_SQRT1_170,
+                                                  -8 * M_SQRT1_170,
+                                                  -4 * M_SQRT1_170,
+                                                  -4 * M_SQRT1_170,
+                                                  -8 * M_SQRT1_170};
+  const std::array<float, NOF_INTERVALS> INTERCEPT     = {52.0F / 85,
+                                                      24.0F / 85,
+                                                      24.0F / 85,
+                                                      44.0F / 85,
+                                                      -20.0F / 85,
+                                                      -8.0F / 85,
+                                                      -8.0F / 85,
+                                                      -12.0F / 85,
+                                                      -12.0F / 85,
+                                                      -8.0F / 85,
+                                                      -8.0F / 85,
+                                                      -20.0F / 85,
+                                                      44.0F / 85,
+                                                      24.0F / 85,
+                                                      24.0F / 85,
+                                                      52.0F / 85};
+
+  unsigned idx     = compute_interval_idx(x, 2 * M_SQRT1_170, NOF_INTERVALS);
+  float    l_value = SLOPE[idx] * x + INTERCEPT[idx];
+  l_value /= noise_var;
+  return log_likelihood_ratio::quantize(l_value, range_limit);
+}
+
+static log_likelihood_ratio demod_256QAM_symbol_67(float x, float noise_var, float range_limit)
+{
+  constexpr unsigned                     NOF_INTERVALS = 8;
+  const std::array<float, NOF_INTERVALS> SLOPE         = {4 * M_SQRT1_170,
+                                                  -4 * M_SQRT1_170,
+                                                  4 * M_SQRT1_170,
+                                                  -4 * M_SQRT1_170,
+                                                  4 * M_SQRT1_170,
+                                                  -4 * M_SQRT1_170,
+                                                  4 * M_SQRT1_170,
+                                                  -4 * M_SQRT1_170};
+  const std::array<float, NOF_INTERVALS> INTERCEPT     = {
+      28.0F / 85, -20.0F / 85, 12.0F / 85, -4.0F / 85, -4.0F / 85, 12.0F / 85, -20.0F / 85, 28.0F / 85};
+
+  unsigned idx     = compute_interval_idx(x, 4 * M_SQRT1_170, NOF_INTERVALS);
+  float    l_value = SLOPE[idx] * x + INTERCEPT[idx];
+  l_value /= noise_var;
+  return log_likelihood_ratio::quantize(l_value, range_limit);
+}
+
+static void
+demodulate_soft_QAM256(span<log_likelihood_ratio> llrs, span<const cf_t> symbols, span<const float> noise_vars)
+{
+  // Maximum (absolute) value considered for quantization. Larger values will be clipped.
+  constexpr float RANGE_LIMIT_FLOAT = 90;
+
+  auto*       llr_it   = llrs.begin();
+  const auto* noise_it = noise_vars.begin();
+  for (const auto this_symbol : symbols) {
+    *llr_it++ = demod_256QAM_symbol_01(std::real(this_symbol), *noise_it, RANGE_LIMIT_FLOAT);
+    *llr_it++ = demod_256QAM_symbol_01(std::imag(this_symbol), *noise_it, RANGE_LIMIT_FLOAT);
+    *llr_it++ = demod_256QAM_symbol_23(std::real(this_symbol), *noise_it, RANGE_LIMIT_FLOAT);
+    *llr_it++ = demod_256QAM_symbol_23(std::imag(this_symbol), *noise_it, RANGE_LIMIT_FLOAT);
+    *llr_it++ = demod_256QAM_symbol_45(std::real(this_symbol), *noise_it, RANGE_LIMIT_FLOAT);
+    *llr_it++ = demod_256QAM_symbol_45(std::imag(this_symbol), *noise_it, RANGE_LIMIT_FLOAT);
+    *llr_it++ = demod_256QAM_symbol_67(std::real(this_symbol), *noise_it, RANGE_LIMIT_FLOAT);
+    *llr_it++ = demod_256QAM_symbol_67(std::imag(this_symbol), *noise_it, RANGE_LIMIT_FLOAT);
+    ++noise_it;
+  }
+}
+
 void demodulation_mapper_impl::demodulate_soft(span<log_likelihood_ratio> llrs,
                                                span<const cf_t>           symbols,
                                                span<const float>          noise_vars,
@@ -191,8 +361,10 @@ void demodulation_mapper_impl::demodulate_soft(span<log_likelihood_ratio> llrs,
     case modulation_scheme::QAM64:
       demodulate_soft_QAM64(llrs, symbols, noise_vars);
       break;
+    case modulation_scheme::QAM256:
+      demodulate_soft_QAM256(llrs, symbols, noise_vars);
+      break;
     default:
-      // TODO(david): finish QAM64 and QAM256. Also, double-check quantization.
       assert(false);
   }
 }
