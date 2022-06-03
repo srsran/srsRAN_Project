cfc3613d9fe08d240d92dfd2afd574d5c82112ff
Xavier Arteaga <xavier.arteaga@softwareradiosystems.com>
2022-06-03 19:26:12 +0200

    ran: review documentation for PDCCH candidates

--- include/srsgnb/ran/pdcch/pdcch_candidates.h
+++ include/srsgnb/ran/pdcch/pdcch_candidates.h
@@ -28,18 +28,18 @@ using pdcch_candidate_list = static_vector<uint8_t, PDCCH_MAX_NOF_CANDIDATES_SS>
 /// \f$s\f$ associated with the CORESET \f$p\f$ in slot \f$n_{s,f}^\mu\f$ for an active DL BWP of a serving cell
 /// corresponding to the carrier indicator field \f$n_{CI}\f$:
 ///
-/// \f$ n_{CCE} = \left \{ \left ( Y_{p,n_{s,f}^\mu} + \left \lfloor \frac{m_{s,n_{CI}}\cdot N_{CCE,p}}{L\cdot
+/// \f$ n_{CCE} = L\cdot \left \{ \left ( Y_{p,n_{s,f}^\mu} + \left \lfloor \frac{m_{s,n_{CI}}\cdot N_{CCE,p}}{L\cdot
 /// M_{s,max}} \right \rfloor +n_{CI} \right ) mod \left \lfloor N_{CCE,p}/L \right \rfloor \right \}\f$
 ///
 /// Where:
 /// - \f$Y_{p,n_{s,f}^\mu}=0\f$,
 /// - \f$n_{CI}=0\f$,
-/// - \f$m_{s,n_{CI}}\epsilon {0,...,M_{s,n_{CI}}^{(L)}}\f$,
+/// - \f$m_{s,n_{CI}}\epsilon \{0,...,M_{s,n_{CI}}^{(L)}\}\f$,
 struct pdcch_candidates_common_ss_configuration {
   /// Aggregation level \f$L\epsilon\{1,2,4,8,16\}\f$.
   unsigned aggregation_level;
-  /// Number of PDCCH candidates \f$M_{s,max}^{(L)}\f$ the UE is configured to monitor for aggregation level \f$L\f$ of
-  /// a search space set \f$s\f$ for a serving cell corresponding to \f$n_{CI}\f$.
+  /// \brief Number of PDCCH candidates \f$M_{s,max}^{(L)}\f$ the UE is configured to monitor for aggregation level
+  /// \f$L\f$ of a search space set \f$s\f$ for a serving cell corresponding to \f$n_{CI}\f$.
   unsigned nof_candidates;
   /// Number of CCEs contained in CORESET \f$p\f$, \f$N_{CCE,p}\f$.
   unsigned nof_cce_coreset;
@@ -52,7 +52,7 @@ struct pdcch_candidates_common_ss_configuration {
 /// - the number of CCE in the CORESET is zero, or
 /// - the number of candidates times the aggregation level exceeds the number of CCE in the CORESET.
 ///
-/// The number of the resultant candidates is equal to \c nof_candidates.
+/// The number of the resultant candidates is equal to \c config.nof_candidates.
 pdcch_candidate_list pdcch_candidates_common_ss_get(const pdcch_candidates_common_ss_configuration& config);
 
 } // namespace srsgnb
