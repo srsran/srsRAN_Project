9402d2eab79d29dd538f7f626ca1578cd7c1f8be
Pedro Alvarez <pedro.alvarez@softwareradiosystems.com>
2022-06-07 15:20:35 +0100

    rlc: moved oran types to their own file

--- include/srsgnb/rlc/rlc.h
+++ include/srsgnb/rlc/rlc.h
@@ -14,70 +14,9 @@
 #include "srsgnb/adt/byte_buffer.h"
 #include "srsgnb/ran/du_types.h"
 #include "srsgnb/ran/lcid.h"
+#include "srsgnb/rlc/rlc_oran.h"
 
 namespace srsgnb {
-
-/********************************
- * 9.2.5.3 - UE Reconfiguration
- *******************************/
-
-struct rlc_ue_reconfiguration_message {
-  struct lc_ch_to_setup {
-    lcid_t lcid;
-    enum class rlc_mode { AM, UM } mode;
-    // ...
-  };
-
-  du_cell_index_t cell_index;
-  du_ue_index_t   ue_index;
-};
-
-struct rlc_ue_reconfiguration_response_message {
-  du_cell_index_t cell_index;
-  du_ue_index_t   ue_index;
-  bool            result;
-};
-
-/********************************
- * ORAN WP8 9.2.5.5 - UE Delete
- *******************************/
-
-struct rlc_ue_delete_message {
-  du_cell_index_t cell_index;
-  du_ue_index_t   ue_index;
-};
-
-struct rlc_ue_delete_response_message {
-  du_cell_index_t cell_index;
-  bool            result;
-};
-
-/****************************************
- * ORAN WP8 9.2.5.8 - UL RRC Message Transfer
- ***************************************/
-
-struct ul_rrc_transfer_message {
-  du_cell_index_t cell_index;
-  du_ue_index_t   ue_index;
-  lcid_t          lcid;
-  byte_buffer     rrc_msg;
-};
-
-/**************************************************
- * ORAN WP8 9.2.5.11 - UE Reestablishment Request
- *************************************************/
-
-struct rlc_ue_reestablishment_message {
-  du_cell_index_t     cell_index;
-  du_ue_index_t       ue_index;
-  std::vector<lcid_t> lc_ch_ids;
-};
-
-struct rlc_ue_reestablishment_response_message {
-  du_cell_index_t cell_index;
-  du_ue_index_t   ue_index;
-  bool            result;
-};
 
 /// This interface represents the entry point of the receiving side of a RLC entity.
 class rlc_pdu_handler
---
+++ include/srsgnb/rlc/rlc_oran.h
@@ -0,0 +1,85 @@
+/*
+ *
+ * Copyright 2013-2022 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#ifndef SRSGNB_RLC_ORAN_H
+#define SRSGNB_RLC_ORAN_H
+
+#include "srsgnb/adt/byte_buffer.h"
+#include "srsgnb/ran/du_types.h"
+#include "srsgnb/ran/lcid.h"
+#include <vector>
+
+namespace srsgnb {
+
+/********************************
+ * 9.2.5.3 - UE Reconfiguration
+ *******************************/
+
+struct rlc_ue_reconfiguration_message {
+  struct lc_ch_to_setup {
+    lcid_t lcid;
+    enum class rlc_mode { AM, UM } mode;
+    // ...
+  };
+
+  du_cell_index_t cell_index;
+  du_ue_index_t   ue_index;
+};
+
+struct rlc_ue_reconfiguration_response_message {
+  du_cell_index_t cell_index;
+  du_ue_index_t   ue_index;
+  bool            result;
+};
+
+/********************************
+ * ORAN WP8 9.2.5.5 - UE Delete
+ *******************************/
+
+struct rlc_ue_delete_message {
+  du_cell_index_t cell_index;
+  du_ue_index_t   ue_index;
+};
+
+struct rlc_ue_delete_response_message {
+  du_cell_index_t cell_index;
+  bool            result;
+};
+
+/****************************************
+ * ORAN WP8 9.2.5.8 - UL RRC Message Transfer
+ ***************************************/
+
+struct ul_rrc_transfer_message {
+  du_cell_index_t cell_index;
+  du_ue_index_t   ue_index;
+  lcid_t          lcid;
+  byte_buffer     rrc_msg;
+};
+
+/**************************************************
+ * ORAN WP8 9.2.5.11 - UE Reestablishment Request
+ *************************************************/
+
+struct rlc_ue_reestablishment_message {
+  du_cell_index_t     cell_index;
+  du_ue_index_t       ue_index;
+  std::vector<lcid_t> lc_ch_ids;
+};
+
+struct rlc_ue_reestablishment_response_message {
+  du_cell_index_t cell_index;
+  du_ue_index_t   ue_index;
+  bool            result;
+};
+
+} // namespace srsgnb
+
+#endif
