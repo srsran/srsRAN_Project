a4e4ee3045132b9d0f0821157d968c25d74016ff
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2022-06-17 23:58:14 +0100

    f1ap: add helper functions to extract transaction id and f1ap pdu message type text

--- lib/f1_interface/common/f1ap_asn1_utils.h
+++ lib/f1_interface/common/f1ap_asn1_utils.h
@@ -12,25 +12,97 @@
 #define SRSGNB_F1AP_ASN1_UTILS_H
 
 #include "srsgnb/asn1/f1ap.h"
+#include "srsgnb/adt/expected.h"
 
 namespace srsgnb {
 
 /// Get string with F1AP error cause.
 inline const char* get_cause_str(const asn1::f1ap::cause_c& cause)
 {
+  using namespace asn1::f1ap;
   switch (cause.type()) {
-    case asn1::f1ap::cause_c::types_opts::radio_network:
+    case cause_c::types_opts::radio_network:
       return cause.radio_network().to_string();
-    case asn1::f1ap::cause_c::types_opts::transport:
+    case cause_c::types_opts::transport:
       return cause.transport().to_string();
-    case asn1::f1ap::cause_c::types_opts::protocol:
+    case cause_c::types_opts::protocol:
       return cause.protocol().to_string();
-    case asn1::f1ap::cause_c::types_opts::misc:
+    case cause_c::types_opts::misc:
       return cause.misc().to_string();
     default:
       break;
   }
   return "unknown";
+}
+
+/// Extracts message type.
+inline const char* get_message_type_str(const asn1::f1ap::f1_ap_pdu_c& pdu) {
+  switch (pdu.type().value) {
+    case asn1::f1ap::f1_ap_pdu_c::types_opts::init_msg:
+      return pdu.init_msg().value.type().to_string();
+    case asn1::f1ap::f1_ap_pdu_c::types_opts::successful_outcome:
+      return pdu.successful_outcome().value.type().to_string();
+    case asn1::f1ap::f1_ap_pdu_c::types_opts::unsuccessful_outcome:
+      return pdu.unsuccessful_outcome().value.type().to_string();
+    default:
+      break;
+  }
+  srsran_terminate("Invalid F1AP PDU type \"{}\"", pdu.type().to_string());
+}
+
+/// Extracts transaction id of Successful Outcome message.
+inline expected<uint8_t> get_transaction_id(const asn1::f1ap::successful_outcome_s& out) {
+  using namespace asn1::f1ap;
+  switch (out.value.type().value) {
+    case f1_ap_elem_procs_o::successful_outcome_c::types_opts::f1_setup_resp:
+      return out.value.f1_setup_resp()->transaction_id->value;
+      // TODO: Remaining cases.
+    default:
+      break;
+  }
+  return {default_error_t{}};
+}
+
+/// Extracts transaction id of Initiating message.
+inline expected<uint8_t> get_transaction_id(const asn1::f1ap::init_msg_s& out) {
+  using namespace asn1::f1ap;
+  switch (out.value.type().value) {
+    case f1_ap_elem_procs_o::init_msg_c::types_opts::f1_setup_request:
+      return out.value.f1_setup_request()->transaction_id->value;
+      // TODO: Remaining cases.
+    default:
+      break;
+  }
+  return {default_error_t{}};
+}
+
+/// Extracts transaction id of Unsuccessful Outcome message.
+inline expected<uint8_t> get_transaction_id(const asn1::f1ap::unsuccessful_outcome_s& out) {
+  using namespace asn1::f1ap;
+  switch (out.value.type().value) {
+    case f1_ap_elem_procs_o::unsuccessful_outcome_c::types_opts::f1_setup_fail:
+      return out.value.f1_setup_fail()->transaction_id->value;
+      // TODO: Remaining cases.
+    default:
+      break;
+  }
+  return {default_error_t{}};
+}
+
+/// Extracts transaction id of F1AP PDU.
+inline expected<uint8_t> get_transaction_id(const asn1::f1ap::f1_ap_pdu_c& pdu) {
+  using namespace asn1::f1ap;
+  switch (pdu.type().value) {
+    case f1_ap_pdu_c::types_opts::init_msg:
+      return get_transaction_id(pdu.init_msg());
+    case f1_ap_pdu_c::types_opts::successful_outcome:
+    return get_transaction_id(pdu.successful_outcome());
+    case f1_ap_pdu_c::types_opts::unsuccessful_outcome:
+      return get_transaction_id(pdu.unsuccessful_outcome());
+    default:
+      break;
+  }
+  return {default_error_t{}};
 }
 
 } // namespace srsgnb
