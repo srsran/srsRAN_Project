4c451726b82dc0f18ca5bb15ba6b92a7b3e63abf
Xavier Arteaga Martinez <xavier@srs.io>
2022-06-30 09:49:47 +0000

    schduler: initial Type0-PDCCH CSS occasions for multiplexing pattern 1.

---
+++ lib/scheduler/support/pdcch/pdcch_type0_css_occasions.cpp
@@ -0,0 +1,77 @@
+/*
+ *
+ * Copyright 2013-2022 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "pdcch_type0_css_occasions.h"
+
+using namespace srsgnb;
+
+pdcch_type0_css_occasion_pattern1_description
+srsgnb::pdcch_type0_css_occasions_get_pattern1(const pdcch_type0_css_occasion_pattern1_configuration& config)
+{
+  srsran_assert((!config.is_fr2 && config.ss_zero_index < 16) || (config.is_fr2 && config.ss_zero_index < 14),
+                "SearchSpaceZero ({}) must be (0...{}) for {}.",
+                config.ss_zero_index,
+                config.is_fr2 ? 13 : 15,
+                config.is_fr2 ? "FR2" : "FR1");
+
+  static const unsigned N_SYMB_CORESET = UINT32_MAX;
+
+  // TS38.213 Table 13-11 for FR1.
+  static const std::array<pdcch_type0_css_occasion_pattern1_description, 16> TABLE_FR1 = {{
+      {0, 1.0, {0}},
+      {0, 0.5, {0, N_SYMB_CORESET}},
+      {2, 1.0, {0}},
+      {2, 0.5, {0, N_SYMB_CORESET}},
+      {5, 1.0, {0}},
+      {5, 0.5, {0, N_SYMB_CORESET}},
+      {7, 1.0, {0}},
+      {7, 0.5, {0, N_SYMB_CORESET}},
+      {0, 2.0, {0}},
+      {5, 2.0, {0}},
+      {0, 1.0, {1}},
+      {0, 1.0, {2}},
+      {2, 1.0, {1}},
+      {2, 1.0, {2}},
+      {5, 1.0, {1}},
+      {5, 1.0, {2}},
+  }};
+
+  // TS38.213 Table 13-12 for FR2.
+  static const std::array<pdcch_type0_css_occasion_pattern1_description, 14> TABLE_FR2 = {{
+      {0, 1.0, {0}},
+      {0, 0.5, {0, 7}},
+      {2.5, 1.0, {0}},
+      {2.5, 0.5, {0, 7}},
+      {5.0, 1.0, {0}},
+      {5.0, 0.5, {0, 7}},
+      {0.0, 0.5, {0, N_SYMB_CORESET}},
+      {2.5, 0.5, {0, N_SYMB_CORESET}},
+      {5.0, 0.5, {0, N_SYMB_CORESET}},
+      {7.5, 1.0, {0}},
+      {7.5, 0.5, {0, 7}},
+      {7.5, 0.5, {0, N_SYMB_CORESET}},
+      {0.0, 2.0, {0}},
+      {5.0, 2.0, {0}},
+  }};
+
+  // Select occasion from the tables.
+  pdcch_type0_css_occasion_pattern1_description occasion = TABLE_FR1[config.ss_zero_index];
+  if (config.is_fr2) {
+    occasion = TABLE_FR2[config.ss_zero_index];
+  }
+
+  // Substitute the start symbol with the CORESET number of symbols.
+  if (occasion.start_symbol.size() == occasion.MAX_NOF_OCCASIONS_PER_SLOT and
+      occasion.start_symbol.back() == N_SYMB_CORESET) {
+    occasion.start_symbol.back() = config.nof_symb_coreset;
+  }
+
+  return occasion;
+}
---
+++ unittests/scheduler/support/pdcch/pdcch_type0_css_occassions_test.cpp
@@ -0,0 +1,103 @@
+/*
+ *
+ * Copyright 2013-2022 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "lib/scheduler/support/pdcch/pdcch_type0_css_occasions.h"
+#include "srsgnb/support/srsgnb_test.h"
+
+using namespace srsgnb;
+
+static void test_pattern1()
+{
+  for (bool is_fr2 : {true, false}) {
+    for (unsigned ss0_index = 0, ss0_index_end = is_fr2 ? 14 : 16; ss0_index != ss0_index_end; ++ss0_index) {
+      for (unsigned nof_symb_coreset : {1, 2, 3}) {
+        pdcch_type0_css_occasion_pattern1_configuration config;
+        config.is_fr2           = is_fr2;
+        config.ss_zero_index    = ss0_index;
+        config.nof_symb_coreset = nof_symb_coreset;
+
+        pdcch_type0_css_occasion_pattern1_description occasion = pdcch_type0_css_occasions_get_pattern1(config);
+
+        if (is_fr2) {
+          // Assert offset.
+          if (ss0_index == 2 || ss0_index == 3 || ss0_index == 7) {
+            TESTASSERT_EQ(occasion.offset, 2.5);
+          } else if (ss0_index == 4 || ss0_index == 5 || ss0_index == 8 || ss0_index == 13) {
+            TESTASSERT_EQ(occasion.offset, 5);
+          } else if (ss0_index == 9 || ss0_index == 10 || ss0_index == 11) {
+            TESTASSERT_EQ(occasion.offset, 7.5);
+          } else {
+            TESTASSERT_EQ(occasion.offset, 0.0);
+          }
+          // Assert M
+          if (ss0_index == 0 || ss0_index == 2 || ss0_index == 4 || ss0_index == 9) {
+            TESTASSERT_EQ(occasion.M, 1.0);
+          } else if (ss0_index == 12 || ss0_index == 13) {
+            TESTASSERT_EQ(occasion.M, 2.0);
+          } else {
+            TESTASSERT_EQ(occasion.M, 0.5);
+          }
+          // Assert start symbol.
+          if (ss0_index == 1 || ss0_index == 3 || ss0_index == 5 || ss0_index == 10) {
+            TESTASSERT_EQ(occasion.start_symbol.size(), 2);
+            TESTASSERT_EQ(occasion.start_symbol[0], 0);
+            TESTASSERT_EQ(occasion.start_symbol[1], 7);
+          } else if (ss0_index == 6 || ss0_index == 7 || ss0_index == 8 || ss0_index == 11) {
+            TESTASSERT_EQ(occasion.start_symbol.size(), 2);
+            TESTASSERT_EQ(occasion.start_symbol[0], 0);
+            TESTASSERT_EQ(occasion.start_symbol[1], nof_symb_coreset);
+          } else {
+            TESTASSERT_EQ(occasion.start_symbol.size(), 1);
+            TESTASSERT_EQ(occasion.start_symbol[0], 0);
+          }
+        } else {
+          // Assert offset.
+          if (ss0_index == 2 || ss0_index == 3 || ss0_index == 12 || ss0_index == 13) {
+            TESTASSERT_EQ(occasion.offset, 2);
+          } else if (ss0_index == 4 || ss0_index == 5 || ss0_index == 9 || ss0_index == 14 || ss0_index == 15) {
+            TESTASSERT_EQ(occasion.offset, 5);
+          } else if (ss0_index == 6 || ss0_index == 7) {
+            TESTASSERT_EQ(occasion.offset, 7);
+          } else {
+            TESTASSERT_EQ(occasion.offset, 0);
+          }
+          // Assert M
+          if (ss0_index == 1 || ss0_index == 3 || ss0_index == 5 || ss0_index == 7) {
+            TESTASSERT_EQ(occasion.M, 0.5);
+          } else if (ss0_index == 8 || ss0_index == 9) {
+            TESTASSERT_EQ(occasion.M, 2.0);
+          } else {
+            TESTASSERT_EQ(occasion.M, 1.0);
+          }
+          // Assert start symbol.
+          if (ss0_index == 1 || ss0_index == 3 || ss0_index == 5 || ss0_index == 7) {
+            TESTASSERT_EQ(occasion.start_symbol.size(), 2);
+            TESTASSERT_EQ(occasion.start_symbol[0], 0);
+            TESTASSERT_EQ(occasion.start_symbol[1], nof_symb_coreset);
+          } else if (ss0_index == 10 || ss0_index == 12 || ss0_index == 14) {
+            TESTASSERT_EQ(occasion.start_symbol.size(), 1);
+            TESTASSERT_EQ(occasion.start_symbol[0], 1);
+          } else if (ss0_index == 11 || ss0_index == 13 || ss0_index == 15) {
+            TESTASSERT_EQ(occasion.start_symbol.size(), 1);
+            TESTASSERT_EQ(occasion.start_symbol[0], 2);
+          } else {
+            TESTASSERT_EQ(occasion.start_symbol.size(), 1);
+            TESTASSERT_EQ(occasion.start_symbol[0], 0);
+          }
+        }
+      }
+    }
+  }
+}
+
+int main()
+{
+  test_pattern1();
+}
