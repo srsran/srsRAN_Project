81bcc5912402da487c1bd1ae492ec5feec63e167
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2022-07-06 17:13:07 +0100

    sched: improve documentation to verify SearchSpace PDCCH monitoring slots

--- lib/scheduler/pdcch_scheduling/pdcch_config_helpers.h
+++ lib/scheduler/pdcch_scheduling/pdcch_config_helpers.h
@@ -16,16 +16,22 @@
 
 namespace srsgnb {
 
+/// \brief Helper function to check whether a SearchSpace PDCCH is monitored by the UE for a given slot.
+///
+/// \param sl Slot in which the SearchSpace is being monitored.
+/// \param ss_cfg SearchSpace configuration.
+/// \return true if the SearchSpace is being monitored. False, otherwise.
 inline bool is_pdcch_monitoring_active(slot_point sl, const search_space_configuration& ss_cfg)
 {
-  unsigned slot_offset = sl.to_uint() % ss_cfg.monitoring_slot_period;
-  unsigned lower_lim   = ss_cfg.monitoring_slot_offset;
-  unsigned upper_lim   = (lower_lim + ss_cfg.duration) % ss_cfg.monitoring_slot_period;
+  unsigned slot_offset  = sl.to_uint() % ss_cfg.monitoring_slot_period;
+  unsigned window_start = ss_cfg.monitoring_slot_offset;
+  unsigned window_end   = (window_start + ss_cfg.duration) % ss_cfg.monitoring_slot_period;
 
-  if (lower_lim < upper_lim) {
-    return slot_offset >= lower_lim and slot_offset < upper_lim;
+  // Checks whether slot_offset falls [window_start, window_end), taking into account the wrap-around of "window_end".
+  if (window_start < window_end) {
+    return slot_offset >= window_start and slot_offset < window_end;
   }
-  return slot_offset >= lower_lim or slot_offset < upper_lim;
+  return slot_offset >= window_start or slot_offset < window_end;
 }
 
 } // namespace srsgnb
