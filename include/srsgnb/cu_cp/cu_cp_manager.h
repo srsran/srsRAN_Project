92c5947499f60e6fc9f3b736b09fe71d303f3515
Fabian Eckermann <fabian@srs.io>
2022-07-08 15:52:33 +0200

    cu_cp: Improve the documentation of the CU-CP manager.

--- include/srsgnb/cu_cp/cu_cp_manager.h
+++ include/srsgnb/cu_cp/cu_cp_manager.h
@@ -37,24 +37,48 @@ class cu_cp_manager_du_handler
 public:
   virtual ~cu_cp_manager_du_handler() = default;
 
-  /// \brief Handles a remove request. The corresponding DU processor will be removed.
-  /// \param[in] du_index The index of the DU processor to delete.
+  /// \brief Handles a remove request. The corresponding DU processor object will be removed.
+  /// \param[in] du_index The index of the DU processor object to delete.
   virtual void handle_du_remove_request(const du_index_t du_index) = 0;
 };
 
 class cu_cp_manager_du_interface
 {
 public:
-  virtual ~cu_cp_manager_du_interface()                     = default;
-  virtual void          add_du()                            = 0;
-  virtual void          remove_du(du_index_t du_index)      = 0;
-  virtual du_index_t    get_next_du_index()                 = 0;
-  virtual du_processor* find_du(du_index_t du_index)        = 0;
+  virtual ~cu_cp_manager_du_interface() = default;
+
+  /// \brief Adds a DU processor object to the CU-CP manager.
+  virtual void add_du() = 0;
+
+  /// \brief Removes the specified DU processor object from the CU-CP manager.
+  /// \param[in] du_index The index of the DU processor to delete.
+  virtual void remove_du(du_index_t du_index) = 0;
+
+  /// \brief Get the next available index from the DU processor database.
+  /// \return The DU index.
+  virtual du_index_t get_next_du_index() = 0;
+
+  /// \brief Find a DU object.
+  /// \param[in] du_index The index of the DU processor object.
+  /// \return The DU processor object.
+  virtual du_processor* find_du(du_index_t du_index) = 0;
+
+  /// \brief Find a DU object.
+  /// \param[in] packed_nr_cell_id The cell id of a DU processor object.
+  /// \return The DU processor object.
   virtual du_processor* find_du(uint64_t packed_nr_cell_id) = 0;
 
+  /// \brief Get the number of DUs connected to the CU-CP manager.
+  /// \return The number of DUs.
   virtual size_t get_nof_dus() const = 0;
+
+  /// \brief Get the number of UEs connected to the all DUs of the CU-CP manager.
+  /// \return The number of UEs.
   virtual size_t get_nof_ues() const = 0;
 
+  /// \brief Get the F1C message handler interface of the DU processor object.
+  /// \param[in] du_index The index of the DU processor object.
+  /// \return The F1C message handler interface of the DU processor object.
   virtual f1c_message_handler* get_f1c_message_handler(const du_index_t du_index) = 0;
 };
 
