3ce98cb3841f2965cc863e712f1c51a4ae7291f3
Robert Falkenberg <robert.falkenberg@srs.io>
2022-07-28 13:48:27 +0200

    rlc,am: tx entity basic tx test

--- unittests/rlc/rlc_am_test.cpp
+++ unittests/rlc/rlc_am_test.cpp
@@ -53,6 +53,48 @@ public:
   void on_buffer_state_update(unsigned bsr) override {}
 };
 
+///
+/// \brief Obtains full RLC AMD PDUs from generated SDUs that are passed through an RLC AM entity
+/// \param[out] out_pdus Pre-allocated array of size n_pdus for the resulting RLC AMD PDUs
+/// \param[in] n_pdus Length of the out_pdus array
+/// \param[in] rlc RLC entity to produce RLC AMD PDUs
+/// \param[in] sn_size Sequence number size
+/// \param[in] sdu_size Size of SDU that is passed through RLC AM entity
+///
+void tx_full_pdus(byte_buffer_slice_chain* out_pdus,
+                  uint32_t                 n_pdus,
+                  rlc_am_entity&           rlc,
+                  rlc_am_sn_size           sn_size,
+                  uint32_t                 sdu_size = 1)
+{
+  rlc_tx_sdu_handler*     rlc_tx_upper = rlc.get_tx_sdu_handler();
+  rlc_tx_pdu_transmitter* rlc_tx_lower = rlc.get_tx_pdu_transmitter();
+
+  // Push "n_pdus" SDUs into RLC
+  for (uint32_t i = 0; i < n_pdus; i++) {
+    byte_buffer sdu_buf = byte_buffer();
+    for (uint32_t k = 0; k < sdu_size; ++k) {
+      sdu_buf.append(i + k);
+    }
+
+    // write SDU into upper end
+    rlc_sdu sdu = {i, std::move(sdu_buf)};
+    rlc_tx_upper->handle_sdu(std::move(sdu));
+  }
+
+  uint32_t header_size         = sn_size == rlc_am_sn_size::size12bits ? 2 : 3;
+  uint32_t data_pdu_size       = header_size + sdu_size;
+  uint32_t expect_buffer_state = n_pdus * data_pdu_size;
+
+  // Read "n_pdus" PDUs from RLC
+  for (uint32_t i = 0; i < n_pdus; i++) {
+    TESTASSERT_EQ(expect_buffer_state - i * data_pdu_size, rlc_tx_lower->get_buffer_state());
+    out_pdus[i] = rlc_tx_lower->pull_pdu(data_pdu_size);
+    TESTASSERT_EQ(data_pdu_size, out_pdus[i].length());
+  }
+  TESTASSERT_EQ(0, rlc_tx_lower->get_buffer_state());
+}
+
 void test_instantiation(rlc_am_sn_size sn_size)
 {
   test_delimit_logger delimiter{"RLC AM ({} bit) - Instantiation", to_number(sn_size)};
@@ -83,6 +125,23 @@ void test_instantiation(rlc_am_sn_size sn_size)
   buffer_state = rlc2_tx_lower->get_buffer_state();
   TESTASSERT_EQ(0, buffer_state);
 }
+
+void test_tx_full_pdu(rlc_am_sn_size sn_size)
+{
+  test_delimit_logger delimiter{"RLC AM ({} bit) - TX full PDUs", to_number(sn_size)};
+  rlc_am_config       config;
+  config.tx = std::make_unique<rlc_tx_am_config>(rlc_tx_am_config{sn_size, 45, 4, 4, 25});
+  config.rx = std::make_unique<rlc_rx_am_config>(rlc_rx_am_config{sn_size, 35, 8});
+  timer_manager  timers;
+  rlc_test_frame tester1;
+  rlc_am_entity  rlc1(du_ue_index_t::MIN_DU_UE_INDEX, lcid_t::LCID_SRB0, config, tester1, tester1, tester1, timers);
+
+  constexpr uint32_t n_pdus = 5;
+
+  byte_buffer_slice_chain pdus[n_pdus];
+  tx_full_pdus(pdus, n_pdus, rlc1, sn_size);
+}
+
 } // namespace srsgnb
 
 int main()
@@ -96,5 +155,6 @@ int main()
                                                             srsgnb::rlc_am_sn_size::size18bits};
   for (srsgnb::rlc_am_sn_size sn_size : sn_sizes) {
     srsgnb::test_instantiation(sn_size);
+    srsgnb::test_tx_full_pdu(sn_size);
   }
 }
