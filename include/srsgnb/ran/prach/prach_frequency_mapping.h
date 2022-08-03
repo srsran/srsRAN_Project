19bc13a3549b93e8e2fc75eb5ad110d551693edb
Xavier Arteaga <xavier@srs.io>
2022-08-03 09:16:18 +0200

    ran: review PRACH frequency mapping docs

--- include/srsgnb/ran/prach/prach_frequency_mapping.h
+++ include/srsgnb/ran/prach/prach_frequency_mapping.h
@@ -17,8 +17,8 @@ namespace srsgnb {
 
 /// Collects PRACH frequency mapping information as per TS38.211 Section 5.3.2 and Section 6.3.3.2.
 struct prach_frequency_mapping_information {
-  /// Number of resource blocks per PRACH occasion in frequency-domain, with a \f$\Delta f\f$ for PUSCH granularity.
-  /// Parameter \f$N_{RB}^{RA}\f$.
+  /// Number of resource blocks per PRACH occasion in the frequency domain, relative to the PUSCH subcarrier spacing
+  /// (\f$\Delta f\f$).
   unsigned nof_rb_ra;
   /// Parameter \f$\bar{k}\f$.
   unsigned k_bar;
@@ -27,11 +27,11 @@ struct prach_frequency_mapping_information {
 /// Reserved PRACH frequency-domain mapping information.
 static constexpr prach_frequency_mapping_information PRACH_FREQUENCY_MAPPING_INFORMATION_RESERVED = {0, 0};
 
-/// \brief Gets PRACH frequency mapping information contained in TS38.211 Table 6.3.3.2-1.
-/// \param[in] prach_scs_Hz Parameter \f$\Delta f_{RA}\f$ for PRACH.
-/// \param[in] pusch_scs_Hz Parameter \f$\Delta f\f$ for PUSCH.
-/// \return The corresponding PRACH frequency mapping information if the input parameters combination is valid.
-/// Otherwise, \c PRACH_FREQUENCY_MAPPING_INFORMATION_RESERVED.
+/// \brief Gets the PRACH frequency mapping information contained in TS38.211 Table 6.3.3.2-1.
+/// \param[in] prach_scs_Hz PRACH subcarrier spacing (parameter \f$\Delta f_{RA}\f$).
+/// \param[in] pusch_scs_Hz PUSCH subcarrier spacing (parameter \f$\Delta f\f$).
+/// \return The requested PRACH frequency mapping information if the combination of input parameters combination is
+/// valid, \c PRACH_FREQUENCY_MAPPING_INFORMATION_RESERVED otherwise.
 prach_frequency_mapping_information prach_frequency_mapping_get(prach_subcarrier_spacing prach_scs,
                                                                 subcarrier_spacing       pusch_scs);
 
