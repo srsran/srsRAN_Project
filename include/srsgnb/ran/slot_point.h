6b89f487bd07299027b0244caf3fe44f1e053a50
Xavier Arteaga <xavier@srs.io>
2022-08-05 08:41:41 +0200

    ran: make slot_point const methods constexpr.

--- include/srsgnb/ran/slot_point.h
+++ include/srsgnb/ran/slot_point.h
@@ -44,14 +44,14 @@ public:
   constexpr slot_point() : numerology_val(NOF_NUMEROLOGIES), count_val(0) {}
 
   /// Takes a numerology and total count value.
-  slot_point(uint32_t numerology, uint32_t count) : numerology_val(numerology), count_val(count)
+  constexpr slot_point(uint32_t numerology, uint32_t count) : numerology_val(numerology), count_val(count)
   {
     srsgnb_assert(numerology < NOF_NUMEROLOGIES, "Invalid numerology idx={} passed", numerology);
     srsgnb_assert(count < nof_slots_per_system_frame(), "Invalid slot count={} passed", count);
   }
 
   /// Takes a numerology, SFN and slot index in radio frame.
-  slot_point(uint32_t numerology, uint32_t sfn_val, uint32_t slot_radio_frame_idx_) :
+  constexpr slot_point(uint32_t numerology, uint32_t sfn_val, uint32_t slot_radio_frame_idx_) :
     numerology_val(numerology), count_val(slot_radio_frame_idx_ + sfn_val * nof_slots_per_frame())
   {
     srsgnb_assert(numerology < NOF_NUMEROLOGIES, "Invalid numerology idx={} passed", numerology);
@@ -63,7 +63,7 @@ public:
   }
 
   /// Takes a numerology, SFN, subframe index and slot index within subframe.
-  slot_point(uint32_t numerology, uint32_t sfn_val, uint32_t subframe_index_, uint32_t slot_subframe_index_) :
+  constexpr slot_point(uint32_t numerology, uint32_t sfn_val, uint32_t subframe_index_, uint32_t slot_subframe_index_) :
     numerology_val(numerology),
     count_val(slot_subframe_index_ + subframe_index_ * nof_slots_per_subframe() + sfn_val * nof_slots_per_frame())
   {
@@ -80,61 +80,61 @@ public:
   void clear() { numerology_val = NOF_NUMEROLOGIES; }
 
   /// Determines whether slot_point was correctly initialized.
-  bool valid() const { return numerology_val < NOF_NUMEROLOGIES; }
+  constexpr bool valid() const { return numerology_val < NOF_NUMEROLOGIES; }
 
   /// Numerology index (0..4).
-  uint32_t numerology() const { return numerology_val; }
+  constexpr uint32_t numerology() const { return numerology_val; }
 
   /// Slot index in radio frame.
   /// Value: SCS=15kHz: (0..9), SCS=30kHz: (0..19), SCS=60kHz: (0..39), SCS=120kHz: (0..79), SCS=240kHz: (0..159)
-  uint32_t slot_index() const { return static_cast<uint32_t>(count_val) % nof_slots_per_frame(); }
+  constexpr uint32_t slot_index() const { return static_cast<uint32_t>(count_val) % nof_slots_per_frame(); }
 
   /// Radio Frame Number. Value: (0..1023).
-  uint32_t sfn() const { return static_cast<uint32_t>(count_val) / nof_slots_per_frame(); }
+  constexpr uint32_t sfn() const { return static_cast<uint32_t>(count_val) / nof_slots_per_frame(); }
 
   /// Number of slots per system frame.
-  uint32_t nof_slots_per_system_frame() const { return nof_slots_per_frame() * NOF_SFNS; }
+  constexpr uint32_t nof_slots_per_system_frame() const { return nof_slots_per_frame() * NOF_SFNS; }
 
   /// Number of slots present in a subframe. Depends on numerology.
-  uint32_t nof_slots_per_subframe() const { return 1U << static_cast<uint32_t>(numerology_val); }
+  constexpr uint32_t nof_slots_per_subframe() const { return 1U << static_cast<uint32_t>(numerology_val); }
 
   /// Number of slots present in a frame. Depends on numerology.
-  uint32_t nof_slots_per_frame() const { return nof_slots_per_subframe() * NOF_SUBFRAMES_PER_FRAME; }
+  constexpr uint32_t nof_slots_per_frame() const { return nof_slots_per_subframe() * NOF_SUBFRAMES_PER_FRAME; }
 
   /// Subframe index in a radio frame. Value: (0..9).
-  uint32_t subframe_index() const { return slot_index() / nof_slots_per_subframe(); }
+  constexpr uint32_t subframe_index() const { return slot_index() / nof_slots_per_subframe(); }
 
   /// Conversion of slot_point to system slot.
-  uint32_t system_slot() const { return count_val; }
-  uint32_t to_uint() const { return count_val; }
-  explicit operator uint32_t() const { return count_val; }
+  constexpr uint32_t system_slot() const { return count_val; }
+  constexpr uint32_t to_uint() const { return count_val; }
+  constexpr explicit operator uint32_t() const { return count_val; }
 
   /// Get Half Radio Frame size in number of slots
-  uint32_t nof_hrf_slots() const { return nof_slots_per_frame() / 2; }
+  constexpr uint32_t nof_hrf_slots() const { return nof_slots_per_frame() / 2; }
 
   /// Checks if slot is in second half of the radio frame.
-  bool is_odd_hrf() const { return subframe_index() >= (NOF_SUBFRAMES_PER_FRAME / 2); }
+  constexpr bool is_odd_hrf() const { return subframe_index() >= (NOF_SUBFRAMES_PER_FRAME / 2); }
 
   /// Get MOD(slot index, Nof slots in Half Frame)
-  uint32_t hrf_slot_index() const { return static_cast<uint32_t>(count_val) % nof_hrf_slots(); }
+  constexpr uint32_t hrf_slot_index() const { return static_cast<uint32_t>(count_val) % nof_hrf_slots(); }
 
   /// Get MOD(slot index, Nof slots in a subframe)
-  uint32_t subframe_slot_index() const { return static_cast<uint32_t>(count_val) % nof_slots_per_subframe(); }
+  constexpr uint32_t subframe_slot_index() const { return static_cast<uint32_t>(count_val) % nof_slots_per_subframe(); }
 
   /// Equality comparison of two slot_point objects. Two slot points are equal if their numerology, SFN and slot index
   /// have the same value.
-  bool operator==(const slot_point& other) const
+  constexpr bool operator==(const slot_point& other) const
   {
     return other.count_val == count_val and other.numerology_val == numerology_val;
   }
 
   /// Inequality comparison of two slot_point objects
-  bool operator!=(const slot_point& other) const { return not(*this == other); }
+  constexpr bool operator!=(const slot_point& other) const { return not(*this == other); }
 
   /// Checks if "lhs" slot point is lower than "rhs". It assumes that both "lhs" and "rhs" use the same numerology
   /// This comparison accounts for the wrap-around of both slot index and SFNs. The ambiguity range of the comparison
   /// is equal to half of the total number of slots in a hyperframe.
-  bool operator<(const slot_point& other) const
+  constexpr bool operator<(const slot_point& other) const
   {
     srsgnb_assert(numerology() == other.numerology(), "Comparing slots of different numerologies");
     int v = static_cast<int>(other.count_val) - static_cast<int>(count_val);
@@ -145,13 +145,13 @@ public:
   }
 
   /// Other lower/higher comparisons that build on top of operator== and operator<.
-  bool operator<=(const slot_point& other) const { return (*this == other) or (*this < other); }
-  bool operator>=(const slot_point& other) const { return not(*this < other); }
-  bool operator>(const slot_point& other) const { return (*this != other) and *this >= other; }
+  constexpr bool operator<=(const slot_point& other) const { return (*this == other) or (*this < other); }
+  constexpr bool operator>=(const slot_point& other) const { return not(*this < other); }
+  constexpr bool operator>(const slot_point& other) const { return (*this != other) and *this >= other; }
 
   /// Implements the subtraction of two slot points.
   /// @return a signed integer that is positive if "lhs" is higher than "rhs", zero if equal, and negative otherwise.
-  slot_difference operator-(const slot_point& other) const
+  constexpr slot_difference operator-(const slot_point& other) const
   {
     int a = static_cast<int>(count_val) - static_cast<int>(other.count_val);
     if (a >= (int)nof_slots_per_system_frame() / 2) {
@@ -199,7 +199,7 @@ public:
   slot_point& operator-=(int jump) { return this->operator+=(-jump); }
 
   /// Determines whether this slot_point fits within provided slot point range.
-  bool is_in_interval(slot_point begin, slot_point end) const { return (*this >= begin and *this < end); }
+  constexpr bool is_in_interval(slot_point begin, slot_point end) const { return (*this >= begin and *this < end); }
 
   /// Tag for numerology used: {0: 15 kHz, 1: 30 kHz, 2: 60 kHz, 3: 120 kHz, 4: 240 kHz}.
   uint32_t numerology_val : 3;
