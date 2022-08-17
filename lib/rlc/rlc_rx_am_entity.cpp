f4c6ac03bbaa22ea33e225812900310ae897e69f
Robert Falkenberg <robert.falkenberg@srs.io>
2022-08-17 11:39:33 +0200

    rlc_rx_am: add state variables and formatter; move methods to cpp file

--- lib/rlc/rlc_rx_am_entity.cpp
+++ lib/rlc/rlc_rx_am_entity.cpp
@@ -20,3 +20,40 @@ rlc_rx_am_entity::rlc_rx_am_entity(du_ue_index_t                     du_index,
   rlc_rx_entity(du_index, lcid, upper_dn), cfg(config), mod(cardinality(to_number(cfg.sn_field_length)))
 {
 }
+
+// Interfaces for lower layers
+void rlc_rx_am_entity::handle_pdu(byte_buffer_slice buf)
+{
+  metrics_add_pdus(1, buf.length());
+  logger.log_info("Rx PDU ({} B)", buf.length());
+  metrics_add_sdus(1, buf.length());
+  if (rlc_am_status_pdu::is_control_pdu(buf.view())) {
+    rlc_am_status_pdu status_pdu(cfg.sn_field_length);
+    if (status_pdu.unpack(buf.view())) {
+      logger.log_debug(buf.begin(), buf.end(), "Successfully unpacked control PDU ({} B)", buf.length());
+      status_handler->handle_status_pdu(std::move(status_pdu));
+    } else {
+      logger.log_error(buf.begin(), buf.end(), "Failed to unpack control PDU ({} B)", buf.length());
+    }
+  } else {
+    // TODO: handle data PDU
+  }
+}
+
+rlc_am_status_pdu rlc_rx_am_entity::get_status_pdu()
+{
+  // TODO
+  return rlc_am_status_pdu(cfg.sn_field_length);
+}
+
+uint32_t rlc_rx_am_entity::get_status_pdu_length()
+{
+  // TODO
+  return 0;
+}
+
+bool rlc_rx_am_entity::status_report_required()
+{
+  // TODO
+  return false;
+}
--- lib/rlc/rlc_rx_am_entity.h
+++ lib/rlc/rlc_rx_am_entity.h
@@ -17,11 +17,38 @@
 
 namespace srsgnb {
 
+/// \brief Rx state variables
+/// Ref: 3GPP TS 38.322 version 16.2.0 Section 7.1
+struct rlc_rx_am_state {
+  /// \brief RX_Next – Receive state variable
+  /// This state variable holds the value of the SN following the last in-sequence completely received RLC SDU, and it
+  /// serves as the lower edge of the receiving window. It is initially set to 0, and is updated whenever the AM RLC
+  /// entity receives an RLC SDU with SN = RX_Next.
+  uint32_t rx_next = 0;
+
+  /// \brief RX_Next_Status_Trigger – t-Reassembly state variable
+  /// This state variable holds the value of the SN following the SN of the RLC SDU which triggered t-Reassembly.
+  uint32_t rx_next_status_trigger = 0;
+
+  /// \brief RX_Highest_Status – Maximum STATUS transmit state variable
+  /// This state variable holds the highest possible value of the SN which can be indicated by "ACK_SN" when a STATUS
+  /// PDU needs to be constructed. It is initially set to 0.
+  uint32_t rx_highest_status = 0;
+
+  /// \brief RX_Next_Highest – Highest received state variable
+  /// This state variable holds the value of the SN following the SN of the RLC SDU with the highest SN among received
+  /// RLC SDUs. It is initially set to 0.
+  uint32_t rx_next_highest = 0;
+};
+
 class rlc_rx_am_entity : public rlc_rx_entity, public rlc_rx_am_status_provider
 {
 private:
   // Config storage
   const rlc_rx_am_config cfg;
+
+  // Rx state variables
+  rlc_rx_am_state st;
 
   // Status handler
   rlc_tx_am_status_handler* status_handler = nullptr;
@@ -40,42 +67,35 @@ public:
   void set_status_handler(rlc_tx_am_status_handler* status_handler_) { status_handler = status_handler_; }
 
   // Interfaces for lower layers
-  void handle_pdu(byte_buffer_slice buf) override
-  {
-    metrics_add_pdus(1, buf.length());
-    logger.log_info("Rx PDU ({} B)", buf.length());
-    metrics_add_sdus(1, buf.length());
-    if (rlc_am_status_pdu::is_control_pdu(buf.view())) {
-      rlc_am_status_pdu status_pdu(cfg.sn_field_length);
-      if (status_pdu.unpack(buf.view())) {
-        logger.log_debug(buf.begin(), buf.end(), "Successfully unpacked control PDU ({} B)", buf.length());
-        status_handler->handle_status_pdu(std::move(status_pdu));
-      } else {
-        logger.log_error(buf.begin(), buf.end(), "Failed to unpack control PDU ({} B)", buf.length());
-      }
-    } else {
-      // TODO: handle data PDU
-    }
-  }
+  void handle_pdu(byte_buffer_slice buf) override;
 
   // Status provider for RX entity
-  rlc_am_status_pdu get_status_pdu() override
-  {
-    // TODO
-    return rlc_am_status_pdu(cfg.sn_field_length);
-  }
-
-  uint32_t get_status_pdu_length() override
-  {
-    // TODO
-    return 0;
-  }
-
-  bool status_report_required() override
-  {
-    // TODO
-    return false;
-  }
+  rlc_am_status_pdu get_status_pdu() override;
+  uint32_t          get_status_pdu_length() override;
+  bool              status_report_required() override;
 };
 
 } // namespace srsgnb
+
+namespace fmt {
+template <>
+struct formatter<srsgnb::rlc_rx_am_state> {
+  template <typename ParseContext>
+  auto parse(ParseContext& ctx) -> decltype(ctx.begin())
+  {
+    return ctx.begin();
+  }
+
+  template <typename FormatContext>
+  auto format(const srsgnb::rlc_rx_am_state& st, FormatContext& ctx) -> decltype(std::declval<FormatContext>().out())
+  {
+    return format_to(ctx.out(),
+                     "rx_next={}, rx_next_status_trigger={}, rx_highest_status={}, rx_next_highest={}",
+                     st.rx_next,
+                     st.rx_next_status_trigger,
+                     st.rx_highest_status,
+                     st.rx_next_highest);
+  }
+};
+
+} // namespace fmt
