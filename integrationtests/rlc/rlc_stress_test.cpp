c98594c3027c492d90a2aac9027c5fcc02740874
Pedro Alvarez <pedro.alvarez@softwareradiosystems.com>
2022-08-31 16:07:12 +0100

    rlc,stresstest: added basic command line parsing to stress test

--- integrationtests/rlc/rlc_stress_test.cpp
+++ integrationtests/rlc/rlc_stress_test.cpp
@@ -11,44 +11,42 @@
  */
 
 #include "rlc_stress_test.h"
-#include "srsgnb/rlc/rlc_config.h"
-#include <cassert>
-#include <cstdlib>
-#include <iostream>
-#include <pthread.h>
+#include "srsgnb/rlc/rlc_factory.h"
 #include <random>
+
+using namespace srsgnb;
 
 void stress_test(stress_test_args args)
 {
-  // auto log_sink =
-  //     (args.log_filename == "stdout") ? srslog::create_stdout_sink() : srslog::create_file_sink(args.log_filename);
-  // if (!log_sink) {
-  //   return;
-  // }
-  // srslog::log_channel* chan = srslog::create_log_channel("main_channel", *log_sink);
-  // if (!chan) {
-  //  return;
-  //}
-  // srslog::set_default_sink(*log_sink);
+  auto log_sink =
+      (args.log_filename == "stdout") ? srslog::create_stdout_sink() : srslog::create_file_sink(args.log_filename);
+  if (!log_sink) {
+    return;
+  }
+  srslog::log_channel* chan = srslog::create_log_channel("main_channel", *log_sink);
+  if (!chan) {
+    return;
+  }
+  srslog::set_default_sink(*log_sink);
 
-  // srsgnb::rlc_config cnfg = {};
-  // if (args.mode == "UM6") {
-  //   cnfg_ = srsran::rlc_config_t::default_rlc_um_nr_config(6);
-  // } else if (args.mode == "UM12") {
-  //   cnfg_ = srsran::rlc_config_t::default_rlc_um_nr_config(12);
-  // } else if (args.mode == "AM12") {
-  //   cnfg_                       = srsran::rlc_config_t::default_rlc_am_nr_config(12);
-  //   cnfg_.am_nr.max_retx_thresh = args.max_retx;
-  // } else if (args.mode == "AM18") {
-  //   cnfg_                       = srsran::rlc_config_t::default_rlc_am_nr_config(18);
-  //   cnfg_.am_nr.max_retx_thresh = args.max_retx;
-  // } else {
-  //   std::cout << "Unsupported RLC mode " << args.mode << ", exiting." << std::endl;
-  //   exit(-1);
-  // }
+  //  rlc_config cnfg = {};
+  //  if (args.mode == "TM") {
+  //    cnfg.mode = srsgnb::rlc_mode::tm;
+  //  } else if (args.mode == "UM") {
+  //    cnfg.mode = srsgnb::rlc_mode::um_bidir;
+  //  } else if (args.mode == "AM") {
+  //    cnfg.mode = srsgnb::rlc_mode::am;
+  //  } else {
+  //    fprintf(stderr, "Unsupported RLC mode %s, exiting.\n", args.mode.c_str());
+  //    exit(-1);
+  //  }
 
-  // Print and analyse metrics
-  // TODO
+  // RLC 1
+  // rlc_entity_creation_message msg;
+  // msg.config                      = cnfg;
+  // std::unique_ptr<rlc_entity> rlc = create_rlc_entity(msg);
+  //  Print and analyse metrics
+  //  TODO
 }
 
 int main(int argc, char** argv)
@@ -56,11 +54,14 @@ int main(int argc, char** argv)
   // srsgnb_debug_handle_crash(argc, argv);
 
   stress_test_args args = {};
-  parse_args(args, argc, argv);
+  if (not parse_args(args, argc, argv)) {
+    fprintf(stderr, "Could not parse command line options\n");
+    return -1;
+  }
 
   srslog::init();
 
   stress_test(args);
 
-  exit(0);
+  return 0;
 }
--- integrationtests/rlc/rlc_stress_test.h
+++ integrationtests/rlc/rlc_stress_test.h
@@ -11,29 +11,58 @@
 #pragma once
 
 #include "srsgnb/srslog/srslog.h"
-#include <cassert>
-#include <cstdlib>
-#include <iostream>
-#include <pthread.h>
-#include <random>
+#include <getopt.h>
 
 struct stress_test_args {
-  std::string mode = "UM";
+  std::string mode         = "UM";
+  std::string sn_size      = "12";
+  std::string log_filename = "stdout";
 };
 
-void parse_args(stress_test_args& args, int argc, char* argv[]) {}
-
-class mac_dummy
+bool parse_args(stress_test_args& args, int argc, char* argv[])
 {
-public:
-  explicit mac_dummy(stress_test_args args_) {}
-};
+  static const struct option long_options[] = {{"help", no_argument, nullptr, 'h'},
+                                               {"mode", required_argument, nullptr, 'm'},
+                                               {"sn_size", required_argument, nullptr, 's'},
+                                               {"log_filename", required_argument, nullptr, 'l'},
+                                               {nullptr, 0, nullptr, 0}};
 
-class rlc_tester
-{
-public:
-  rlc_tester() {}
+  static const char usage[] = "Usage: rlc_stress_test [options]\n"
+                              "\n"
+                              "  -h, --help                 Show help message and quit.\n"
+                              "  -m, --mode <RLC mode>      Specify TM, UM, or AM.\n"
+                              "  -s, --sn_size <SN size>    Specify 6bit, 12bit, or 18bit. Ignored in TM.\n"
+                              "  --log_filename <filename>  Specify log filename. Use 'stdout' to print to console.\n"
+                              "\n";
+  // Parse arguments
+  while (true) {
+    int option_index = 0;
+    int c            = getopt_long(argc, argv, "hm:s:l:", long_options, &option_index);
+    if (c == -1) {
+      break;
+    }
 
-private:
-};
+    switch (c) {
+      case 'h':
+        fprintf(stdout, "%s", usage);
+        break;
+      case 'm':
+        args.mode = std::string(optarg);
+        fprintf(stdout, "RLC Mode %s\n", args.mode.c_str());
+        break;
+      case 's':
+        args.sn_size = std::string(optarg);
+        fprintf(stdout, "RLC SN Size %s\n", args.log_filename.c_str());
+        break;
+      case 'l':
+        args.log_filename = std::string(optarg);
+        fprintf(stdout, "Log filename %s\n", args.log_filename.c_str());
+        break;
+      default:
+        fprintf(stderr, "error parsing arguments\n");
+        return false;
+    }
+  }
+  return true;
+}
 
