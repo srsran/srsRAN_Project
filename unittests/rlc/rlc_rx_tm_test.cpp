921e0ecb679cd258a7e21569030f340dae5dfe57
Robert Falkenberg <robert.falkenberg@srs.io>
2022-08-31 11:17:00 +0200

    rlc_tm_test: migrate to gtest; split tx and rx tests

---
+++ unittests/rlc/rlc_rx_tm_test.cpp
@@ -0,0 +1,126 @@
+/*
+ *
+ * Copyright 2013-2022 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "../../lib/rlc/rlc_rx_tm_entity.h"
+#include <gtest/gtest.h>
+#include <queue>
+
+using namespace srsgnb;
+
+/// Mocking class of the surrounding layers invoked by the RLC TM Rx entity.
+class rlc_rx_tm_test_frame : public rlc_rx_upper_layer_data_notifier
+{
+public:
+  std::queue<byte_buffer_slice> sdu_queue;
+  uint32_t                      sdu_counter = 0;
+
+  // rlc_rx_upper_layer_data_notifier interface
+  void on_new_sdu(byte_buffer_slice sdu) override
+  {
+    sdu_queue.push(std::move(sdu));
+    sdu_counter++;
+  }
+};
+
+/// Fixture class for RLC TM Rx tests
+class rlc_rx_am_test : public ::testing::Test
+{
+protected:
+  void SetUp() override
+  {
+    // init test's logger
+    srslog::init();
+    logger.set_level(srslog::basic_levels::debug);
+
+    // init RLC logger
+    srslog::fetch_basic_logger("RLC", false).set_level(srslog::basic_levels::debug);
+    srslog::fetch_basic_logger("RLC", false).set_hex_dump_max_size(-1);
+
+    init();
+  }
+
+  void TearDown() override
+  {
+    // flush logger after each test
+    srslog::flush();
+  }
+
+  /// \brief Initializes fixture according to size sequence number size
+  void init()
+  {
+    logger.info("Creating RLC Rx TM entity");
+
+    // Create test frame
+    tester = std::make_unique<rlc_rx_tm_test_frame>();
+
+    // Create RLC AM TX entity
+    rlc = std::make_unique<rlc_rx_tm_entity>(du_ue_index_t::MIN_DU_UE_INDEX, lcid_t::LCID_SRB0, *tester);
+  }
+
+  /// \brief Creates a byte_buffer serving as SDU for RLC
+  ///
+  /// The produced SDU contains an incremental sequence of bytes starting with the value given by first_byte,
+  /// i.e. if first_byte = 0xfc, the SDU will be 0xfc 0xfe 0xff 0x00 0x01 ...
+  /// \param sdu_size Size of the SDU
+  /// \param first_byte Value of the first byte
+  /// \return the produced SDU as a byte_buffer
+  byte_buffer create_sdu(uint32_t sdu_size, uint8_t first_byte = 0) const
+  {
+    byte_buffer sdu_buf;
+    for (uint32_t k = 0; k < sdu_size; ++k) {
+      sdu_buf.append(first_byte + k);
+    }
+    return sdu_buf;
+  }
+
+  srslog::basic_logger&                 logger = srslog::fetch_basic_logger("TEST", false);
+  std::unique_ptr<rlc_rx_tm_test_frame> tester;
+  std::unique_ptr<rlc_rx_tm_entity>     rlc;
+};
+
+TEST_F(rlc_rx_am_test, create_new_entity)
+{
+  EXPECT_EQ(tester->sdu_counter, 0);
+}
+
+TEST_F(rlc_rx_am_test, test_rx)
+{
+  const uint32_t sdu_size = 4;
+  uint32_t       count    = 0;
+
+  // write first PDU into lower end
+  byte_buffer       pdu_buf = create_sdu(sdu_size, count);
+  byte_buffer_slice pdu     = {pdu_buf.deep_copy()};
+  rlc->handle_pdu(std::move(pdu));
+
+  count++;
+
+  // write second PDU into lower end
+  byte_buffer pdu_buf2 = create_sdu(sdu_size, count);
+  pdu                  = {pdu_buf2.deep_copy()};
+  rlc->handle_pdu(std::move(pdu));
+
+  // read first SDU from tester
+  EXPECT_EQ(tester->sdu_counter, 2);
+  byte_buffer_slice sdu = tester->sdu_queue.front();
+  EXPECT_TRUE(std::equal(sdu.begin(), sdu.end(), pdu_buf.begin(), pdu_buf.end()));
+  tester->sdu_queue.pop();
+
+  // read second SDU from tester
+  byte_buffer_slice sdu2 = tester->sdu_queue.front();
+  EXPECT_TRUE(std::equal(sdu2.begin(), sdu2.end(), pdu_buf2.begin(), pdu_buf2.end()));
+  tester->sdu_queue.pop();
+}
+
+int main(int argc, char** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ unittests/rlc/rlc_tx_tm_test.cpp
@@ -0,0 +1,164 @@
+/*
+ *
+ * Copyright 2013-2022 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "../../lib/rlc/rlc_tx_tm_entity.h"
+#include <gtest/gtest.h>
+#include <queue>
+
+using namespace srsgnb;
+
+/// Mocking class of the surrounding layers invoked by the RLC TM Tx entity.
+class rlc_tx_tm_test_frame : public rlc_tx_upper_layer_data_notifier,
+                             public rlc_tx_upper_layer_control_notifier,
+                             public rlc_tx_lower_layer_notifier
+{
+public:
+  std::queue<byte_buffer_slice> sdu_queue;
+  uint32_t                      sdu_counter = 0;
+
+  // rlc_tx_upper_layer_data_notifier interface
+  void on_delivered_sdu(uint32_t pdcp_count) override {}
+
+  // rlc_tx_upper_layer_control_notifier interface
+  void on_protocol_failure() override {}
+  void on_max_retx() override {}
+
+  // rlc_tx_buffer_state_update_notifier interface
+  void on_buffer_state_update(unsigned bsr) override {}
+};
+
+/// Fixture class for RLC TM Tx tests
+class rlc_tx_am_test : public ::testing::Test
+{
+protected:
+  void SetUp() override
+  {
+    // init test's logger
+    srslog::init();
+    logger.set_level(srslog::basic_levels::debug);
+
+    // init RLC logger
+    srslog::fetch_basic_logger("RLC", false).set_level(srslog::basic_levels::debug);
+    srslog::fetch_basic_logger("RLC", false).set_hex_dump_max_size(-1);
+
+    init();
+  }
+
+  void TearDown() override
+  {
+    // flush logger after each test
+    srslog::flush();
+  }
+
+  /// \brief Initializes fixture according to size sequence number size
+  void init()
+  {
+    logger.info("Creating RLC Tx TM entity");
+
+    // Create test frame
+    tester = std::make_unique<rlc_tx_tm_test_frame>();
+
+    // Create RLC AM TX entity
+    rlc = std::make_unique<rlc_tx_tm_entity>(
+        du_ue_index_t::MIN_DU_UE_INDEX, lcid_t::LCID_SRB0, *tester, *tester, *tester);
+  }
+
+  /// \brief Creates a byte_buffer serving as SDU for RLC
+  ///
+  /// The produced SDU contains an incremental sequence of bytes starting with the value given by first_byte,
+  /// i.e. if first_byte = 0xfc, the SDU will be 0xfc 0xfe 0xff 0x00 0x01 ...
+  /// \param sdu_size Size of the SDU
+  /// \param first_byte Value of the first byte
+  /// \return the produced SDU as a byte_buffer
+  byte_buffer create_sdu(uint32_t sdu_size, uint8_t first_byte = 0) const
+  {
+    byte_buffer sdu_buf;
+    for (uint32_t k = 0; k < sdu_size; ++k) {
+      sdu_buf.append(first_byte + k);
+    }
+    return sdu_buf;
+  }
+
+  srslog::basic_logger&                 logger = srslog::fetch_basic_logger("TEST", false);
+  std::unique_ptr<rlc_tx_tm_test_frame> tester;
+  std::unique_ptr<rlc_tx_tm_entity>     rlc;
+};
+
+TEST_F(rlc_tx_am_test, create_new_entity)
+{
+  EXPECT_EQ(rlc->get_buffer_state(), 0);
+}
+
+TEST_F(rlc_tx_am_test, test_tx)
+{
+  const uint32_t sdu_size = 4;
+  uint32_t       count    = 0;
+
+  EXPECT_EQ(rlc->get_buffer_state(), 0);
+
+  byte_buffer sdu_buf = create_sdu(sdu_size, count);
+  rlc_sdu sdu = {/* pdcp_count = */ count, sdu_buf.deep_copy()}; // no std::move - keep local copy for later comparison
+
+  // write SDU into upper end
+  rlc->handle_sdu(std::move(sdu));
+  EXPECT_EQ(rlc->get_buffer_state(), sdu_size);
+
+  byte_buffer_slice_chain pdu;
+
+  // read PDU from lower end
+  pdu = rlc->pull_pdu(sdu_size);
+  EXPECT_EQ(pdu.length(), sdu_size);
+  EXPECT_TRUE(std::equal(pdu.begin(), pdu.end(), sdu_buf.begin(), sdu_buf.end()));
+  EXPECT_EQ(rlc->get_buffer_state(), 0);
+
+  // read another PDU from lower end but there is nothing to read
+  pdu = rlc->pull_pdu(sdu_size);
+  EXPECT_EQ(pdu.length(), 0);
+  EXPECT_EQ(rlc->get_buffer_state(), 0);
+
+  // write another SDU into upper end
+  count++;
+  sdu_buf = create_sdu(sdu_size, count);
+  sdu     = {/* pdcp_count = */ count, sdu_buf.deep_copy()}; // no std::move - keep local copy for later comparison
+  rlc->handle_sdu(std::move(sdu));
+  EXPECT_EQ(rlc->get_buffer_state(), sdu_size);
+
+  // read PDU from lower end with insufficient space for the whole SDU
+  pdu = rlc->pull_pdu(sdu_size - 1);
+  EXPECT_EQ(pdu.length(), 0);
+  EXPECT_EQ(rlc->get_buffer_state(), sdu_size);
+
+  // write another SDU into upper end
+  count++;
+  byte_buffer sdu_buf2 = create_sdu(sdu_size, count);
+  sdu = {/* pdcp_count = */ count, sdu_buf2.deep_copy()}; // no std::move - keep local copy for later comparison
+
+  // write SDU into upper end
+  rlc->handle_sdu(std::move(sdu));
+  EXPECT_EQ(rlc->get_buffer_state(), 2 * sdu_size);
+
+  // read first PDU from lower end with oversized space
+  pdu = rlc->pull_pdu(3 * sdu_size);
+  EXPECT_EQ(pdu.length(), sdu_size);
+  EXPECT_TRUE(std::equal(pdu.begin(), pdu.end(), sdu_buf.begin(), sdu_buf.end()));
+  EXPECT_EQ(rlc->get_buffer_state(), sdu_size);
+
+  // read second PDU from lower end with oversized space
+  pdu = rlc->pull_pdu(3 * sdu_size);
+  EXPECT_EQ(pdu.length(), sdu_size);
+  EXPECT_TRUE(std::equal(pdu.begin(), pdu.end(), sdu_buf2.begin(), sdu_buf2.end()));
+  EXPECT_EQ(rlc->get_buffer_state(), 0);
+}
+
+int main(int argc, char** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
