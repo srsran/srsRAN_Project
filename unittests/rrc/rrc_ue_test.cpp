1b02664c82f254b3943457e5e475386736e393b2
Fabian Eckermann <fabian@srs.io>
2022-08-31 11:30:56 +0200

    cu_cp,rrc_ue: Move RRC UE dummies to test helper.

--- unittests/rrc/rrc_ue_test.cpp
+++ unittests/rrc/rrc_ue_test.cpp
@@ -9,104 +9,14 @@
  */
 
 #include "srsgnb/adt/byte_buffer.h"
-#include "srsgnb/cu_cp/ue_context.h"
 #include "srsgnb/rrc/rrc_du_factory.h"
 #include "srsgnb/support/async/async_task_loop.h"
 #include "srsgnb/support/test_utils.h"
+#include "test_helpers.h"
 #include <gtest/gtest.h>
 
 using namespace srsgnb;
 using namespace srs_cu_cp;
-
-class dummy_tx_pdu_handler
-{
-public:
-  dummy_tx_pdu_handler() = default;
-  void handle_pdu(byte_buffer_slice pdu) { last_pdu = std::move(pdu); }
-
-  byte_buffer_slice last_pdu;
-};
-
-class dummy_rrc_pdu_notifier : public rrc_pdu_notifier
-{
-public:
-  dummy_rrc_pdu_notifier(dummy_tx_pdu_handler& handler_) : handler(handler_) {}
-
-  void on_new_pdu(const rrc_pdu_message& msg) override { handler.handle_pdu(byte_buffer_slice{std::move(msg.pdu)}); }
-
-private:
-  dummy_tx_pdu_handler& handler;
-};
-
-class dummy_du_processor_rrc_ue_interface : public du_processor_rrc_ue_interface
-{
-public:
-  dummy_du_processor_rrc_ue_interface(ue_context& ue_ctxt_) : ue_ctxt(ue_ctxt_){};
-
-  void create_srb(const srb_creation_message& msg) override
-  {
-    // set notifier to a known value (i.e. nullptr) to be able to check if it was called
-    ue_ctxt.srbs[srb_id_to_uint(msg.srb_id)].rrc_tx_notifier.reset();
-    last_srb = msg;
-  }
-
-  void handle_ue_context_release_command(const ue_context_release_command_message& msg) override
-  {
-    last_ue_ctxt_rel_cmd = msg;
-  }
-
-  ue_context_release_command_message last_ue_ctxt_rel_cmd;
-  srb_creation_message               last_srb;
-
-private:
-  ue_context& ue_ctxt;
-};
-
-class dummy_rrc_ue_du_processor_adapter : public rrc_ue_du_processor_notifier
-{
-public:
-  void connect(dummy_du_processor_rrc_ue_interface& du_processor_rrc_ue_)
-  {
-    du_processor_rrc_ue_handler = &du_processor_rrc_ue_;
-  }
-
-  void on_create_srb(const srb_creation_message& msg) override { du_processor_rrc_ue_handler->create_srb(msg); }
-
-  void on_ue_context_release_command(const ue_context_release_command_message& msg) override
-  {
-    du_processor_rrc_ue_handler->handle_ue_context_release_command(msg);
-  }
-
-private:
-  dummy_du_processor_rrc_ue_interface* du_processor_rrc_ue_handler = nullptr;
-};
-
-class dummy_rrc_ue_ngap_adapter : public rrc_ue_nas_notifier
-{
-public:
-  void on_initial_ue_message(const initial_ue_message& msg) override { logger.info("Received Initial UE Message"); }
-
-  void on_ul_nas_transport_message(const ul_nas_transport_message& msg) override
-  {
-    logger.info("Received UL NAS Transport message");
-  }
-
-private:
-  srslog::basic_logger& logger = srslog::fetch_basic_logger("TEST");
-};
-
-struct dummy_ue_task_scheduler : public rrc_ue_task_scheduler {
-public:
-  void           schedule_async_task(async_task<void>&& task) override { ctrl_loop.schedule(std::move(task)); }
-  unique_timer   make_unique_timer() override { return timer_db.create_unique_timer(); }
-  timer_manager& get_timer_manager() override { return timer_db; }
-
-  void tick_timer() { timer_db.tick_all(); }
-
-private:
-  async_task_sequencer ctrl_loop{16};
-  timer_manager        timer_db;
-};
 
 /// Fixture class RRC Setup tests preparation
 class rrc_setup : public ::testing::Test
--- unittests/rrc/test_helpers.h
+++ unittests/rrc/test_helpers.h
@@ -16,6 +16,97 @@
 #include "srsgnb/rrc/rrc_ue.h"
 
 namespace srsgnb {
+namespace srs_cu_cp {
+
+class dummy_tx_pdu_handler
+{
+public:
+  dummy_tx_pdu_handler() = default;
+  void handle_pdu(byte_buffer_slice pdu) { last_pdu = std::move(pdu); }
+
+  byte_buffer_slice last_pdu;
+};
+
+class dummy_rrc_pdu_notifier : public rrc_pdu_notifier
+{
+public:
+  dummy_rrc_pdu_notifier(dummy_tx_pdu_handler& handler_) : handler(handler_) {}
+
+  void on_new_pdu(const rrc_pdu_message& msg) override { handler.handle_pdu(byte_buffer_slice{std::move(msg.pdu)}); }
+
+private:
+  dummy_tx_pdu_handler& handler;
+};
+
+class dummy_du_processor_rrc_ue_interface : public du_processor_rrc_ue_interface
+{
+public:
+  dummy_du_processor_rrc_ue_interface(ue_context& ue_ctxt_) : ue_ctxt(ue_ctxt_){};
+
+  void create_srb(const srb_creation_message& msg) override
+  {
+    // set notifier to a known value (i.e. nullptr) to be able to check if it was called
+    ue_ctxt.srbs[srb_id_to_uint(msg.srb_id)].rrc_tx_notifier.reset();
+    last_srb = msg;
+  }
+
+  void handle_ue_context_release_command(const ue_context_release_command_message& msg) override
+  {
+    last_ue_ctxt_rel_cmd = msg;
+  }
+
+  ue_context_release_command_message last_ue_ctxt_rel_cmd;
+  srb_creation_message               last_srb;
+
+private:
+  ue_context& ue_ctxt;
+};
+
+class dummy_rrc_ue_du_processor_adapter : public rrc_ue_du_processor_notifier
+{
+public:
+  void connect(dummy_du_processor_rrc_ue_interface& du_processor_rrc_ue_)
+  {
+    du_processor_rrc_ue_handler = &du_processor_rrc_ue_;
+  }
+
+  void on_create_srb(const srb_creation_message& msg) override { du_processor_rrc_ue_handler->create_srb(msg); }
+
+  void on_ue_context_release_command(const ue_context_release_command_message& msg) override
+  {
+    du_processor_rrc_ue_handler->handle_ue_context_release_command(msg);
+  }
+
+private:
+  dummy_du_processor_rrc_ue_interface* du_processor_rrc_ue_handler = nullptr;
+};
+
+class dummy_rrc_ue_ngap_adapter : public rrc_ue_nas_notifier
+{
+public:
+  void on_initial_ue_message(const initial_ue_message& msg) override { logger.info("Received Initial UE Message"); }
+
+  void on_ul_nas_transport_message(const ul_nas_transport_message& msg) override
+  {
+    logger.info("Received UL NAS Transport message");
+  }
+
+private:
+  srslog::basic_logger& logger = srslog::fetch_basic_logger("TEST");
+};
+
+struct dummy_ue_task_scheduler : public rrc_ue_task_scheduler {
+public:
+  void           schedule_async_task(async_task<void>&& task) override { ctrl_loop.schedule(std::move(task)); }
+  unique_timer   make_unique_timer() override { return timer_db.create_unique_timer(); }
+  timer_manager& get_timer_manager() override { return timer_db; }
+
+  void tick_timer() { timer_db.tick_all(); }
+
+private:
+  async_task_sequencer ctrl_loop{16};
+  timer_manager        timer_db;
+};
 
 /// Reusable notifier class that a) stores the received msg for test inspection and b)
 /// calls the registered msg handler (if any). The handler can be added upon construction
@@ -40,4 +131,5 @@ private:
   srslog::basic_logger& logger;
 };
 
+} // namespace srs_cu_cp
 } // namespace srsgnb
