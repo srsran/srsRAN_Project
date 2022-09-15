362cb3a4dfc2dbf48d6e59eea918c4328567c7be
Carlo Galiotto <carlo@srs.io>
2022-09-15 12:38:12 +0200

    du: fix unit tests
    [ci skip]
    
    Signed-off-by: Carlo Galiotto <carlo@srs.io>

--- include/srsgnb/du/du_cell_config_helpers.h
+++ include/srsgnb/du/du_cell_config_helpers.h
@@ -216,6 +216,7 @@ inline ul_config_common make_default_ul_config_common(const du_cell_config_defau
   cfg.init_ul_bwp.pusch_cfg_common->pusch_td_alloc_list[0].map_type = sch_mapping_type::typeA;
   cfg.init_ul_bwp.pusch_cfg_common->pusch_td_alloc_list[0].symbols  = {0, 14};
   // TODO: Set these parameters according to some reliable source.
+  cfg.init_ul_bwp.pucch_cfg_common.emplace();
   cfg.init_ul_bwp.pucch_cfg_common->pucch_resource_common = 11;
   cfg.init_ul_bwp.pucch_cfg_common->pucch_group_hopping   = pucch_config_common::pucch_group_hop_opt::neither;
   cfg.init_ul_bwp.pucch_cfg_common->p0_nominal            = -90;
