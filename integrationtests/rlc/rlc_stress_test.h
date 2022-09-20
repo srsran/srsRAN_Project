44c802736b6cd5097b702c478420e0d2bd56d3cb
Robert Falkenberg <robert.falkenberg@srs.io>
2022-09-20 12:56:47 +0200

    rlc,stresstest: add basic traffic test

--- integrationtests/rlc/rlc_stress_test.h
+++ integrationtests/rlc/rlc_stress_test.h
@@ -14,11 +14,23 @@
 #include "srsgnb/rlc/rlc_tx.h"
 #include "srsgnb/srslog/srslog.h"
 #include <getopt.h>
+#include <random>
 
 namespace srsgnb {
+
 struct stress_test_args {
-  std::string mode         = "UM12";
-  std::string log_filename = "stdout";
+  std::string mode               = "UM12";
+  int32_t     sdu_size           = -1;
+  float       pdu_drop_rate      = 0.1;
+  float       pdu_cut_rate       = 0.0;
+  float       pdu_duplicate_rate = 0.0;
+  uint32_t    avg_opp_size       = 1505;
+  bool        random_opp         = true;
+  uint32_t    seed               = 0;
+  uint32_t    nof_pdu_tti        = 1;
+  std::string log_filename       = "stdout";
+  uint32_t    min_sdu_size       = 5;
+  uint32_t    max_sdu_size       = 9000;
 };
 
 bool parse_args(stress_test_args& args, int argc, char* argv[])
@@ -62,13 +74,39 @@ bool parse_args(stress_test_args& args, int argc, char* argv[])
   return true;
 }
 
-class pdcp_rx_dummy : public rlc_rx_upper_layer_data_notifier
+class pdcp_traffic_sink : public rlc_rx_upper_layer_data_notifier
 {
-  void on_new_sdu(byte_buffer_slice_chain pdu) final {}
+  srslog::basic_logger& logger;
+
+public:
+  pdcp_traffic_sink() : logger(srslog::fetch_basic_logger("PDCP_RX", false)) {}
+
+  // rlc_rx_upper_layer_data_notifier interface
+  void on_new_sdu(byte_buffer_slice_chain pdu) final;
 };
 
-class pdcp_tx_dummy : public rlc_tx_upper_layer_data_notifier, public rlc_tx_upper_layer_control_notifier
+class pdcp_traffic_source : public rlc_tx_upper_layer_data_notifier, public rlc_tx_upper_layer_control_notifier
 {
+  stress_test_args& args;
+  uint32_t          pdcp_sn  = 0;
+  uint32_t          sdu_size = 0;
+  uint8_t           payload  = 0x0; // increment for each SDU
+
+  std::mt19937                    rgen;
+  std::uniform_int_distribution<> int_dist;
+
+  rlc_tx_upper_layer_data_interface* rlc_tx_upper = nullptr;
+
+public:
+  pdcp_traffic_source(stress_test_args& args) :
+    args(args), rgen(args.seed), int_dist(args.min_sdu_size, args.max_sdu_size)
+  {
+  }
+
+  void set_rlc_tx_upper(rlc_tx_upper_layer_data_interface* rlc_tx_upper) { this->rlc_tx_upper = rlc_tx_upper; }
+
+  void send_pdu();
+
   // rlc_tx_upper_layer_data_notifier interface
   void on_delivered_sdu(uint32_t pdcp_count) final {}
 
@@ -77,10 +115,32 @@ class pdcp_tx_dummy : public rlc_tx_upper_layer_data_notifier, public rlc_tx_upp
   void on_max_retx() final {}
 };
 
-class mac_tx_dummy : public rlc_tx_lower_layer_notifier
+class mac_dummy : public rlc_tx_lower_layer_notifier
 {
+  stress_test_args&     args;
+  srslog::basic_logger& logger;
+
+  std::mt19937                          rgen;
+  std::uniform_real_distribution<float> real_dist;
+
+  std::atomic<unsigned>                bsr;
+  std::vector<byte_buffer_slice_chain> pdu_list;
+
+  rlc_tx_lower_layer_interface* rlc_tx_lower = nullptr;
+  rlc_rx_lower_layer_interface* rlc_rx_lower = nullptr;
+
+public:
+  mac_dummy(stress_test_args& args) :
+    args(args), logger(srslog::fetch_basic_logger("MAC", false)), rgen(args.seed), bsr(0){};
+
+  void run_tx_tti();
+  void run_rx_tti();
+
   // rlc_tx_lower_layer_notifier interface
-  void on_buffer_state_update(unsigned bsr) final {}
+  void on_buffer_state_update(unsigned bsr) final { this->bsr.store(bsr, std::memory_order_relaxed); }
+
+  void set_rlc_tx_lower(rlc_tx_lower_layer_interface* rlc_tx_lower) { this->rlc_tx_lower = rlc_tx_lower; }
+  void set_rlc_rx_lower(rlc_rx_lower_layer_interface* rlc_rx_lower) { this->rlc_rx_lower = rlc_rx_lower; }
 };
 
 } // namespace srsgnb
