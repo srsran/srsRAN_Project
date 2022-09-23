d61bac74122405d75d056456101d0743e51bc4de
Joaquim Broquetas <joaquim.broquetas@srs.io>
2022-09-23 17:49:11 +0200

    phy: update equalizer and pusch processor test data

--- unittests/phy/upper/equalization/channel_equalizer_test_data.h
+++ unittests/phy/upper/equalization/channel_equalizer_test_data.h
@@ -10,7 +10,7 @@
 
 #pragma once
 
-// This file was generated using the following MATLAB class on 19-Sep-2022:
+// This file was generated using the following MATLAB class on 23-Sep-2022:
 //   + "srsChEqualizerUnittest.m"
 
 #include "srsgnb/adt/complex.h"
@@ -42,12 +42,12 @@ struct test_case_t {
 
 static const std::vector<test_case_t> channel_equalizer_test_data = {
     // clang-format off
-{  {25, 14, 1, {"test_data/channel_equalizer_test_output_eq_symbols0.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_output_eq_noise_vars0.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_check_tx_symbols0.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_input_rx_symbols0.dat"}},  {25, 14, 1, 1, 0.1, {"test_data/channel_equalizer_test_input_ch_estimates0.dat"}}, 1.2 , "MMSE" },
-{  {25, 14, 1, {"test_data/channel_equalizer_test_output_eq_symbols2.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_output_eq_noise_vars2.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_check_tx_symbols2.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_input_rx_symbols2.dat"}},  {25, 14, 1, 1, 0.1, {"test_data/channel_equalizer_test_input_ch_estimates2.dat"}}, 1.2 , "ZF" },
-{  {25, 14, 1, {"test_data/channel_equalizer_test_output_eq_symbols4.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_output_eq_noise_vars4.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_check_tx_symbols4.dat"}},  {25, 14, 2, {"test_data/channel_equalizer_test_input_rx_symbols4.dat"}},  {25, 14, 2, 1, 0.1, {"test_data/channel_equalizer_test_input_ch_estimates4.dat"}}, 1.2 , "MMSE" },
-{  {25, 14, 1, {"test_data/channel_equalizer_test_output_eq_symbols6.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_output_eq_noise_vars6.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_check_tx_symbols6.dat"}},  {25, 14, 2, {"test_data/channel_equalizer_test_input_rx_symbols6.dat"}},  {25, 14, 2, 1, 0.1, {"test_data/channel_equalizer_test_input_ch_estimates6.dat"}}, 1.2 , "ZF" },
-{  {25, 14, 2, {"test_data/channel_equalizer_test_output_eq_symbols8.dat"}},  {25, 14, 2, {"test_data/channel_equalizer_test_output_eq_noise_vars8.dat"}},  {25, 14, 2, {"test_data/channel_equalizer_test_check_tx_symbols8.dat"}},  {25, 14, 2, {"test_data/channel_equalizer_test_input_rx_symbols8.dat"}},  {25, 14, 2, 2, 0.1, {"test_data/channel_equalizer_test_input_ch_estimates8.dat"}}, 1.2 , "MMSE" },
-{  {25, 14, 2, {"test_data/channel_equalizer_test_output_eq_symbols10.dat"}},  {25, 14, 2, {"test_data/channel_equalizer_test_output_eq_noise_vars10.dat"}},  {25, 14, 2, {"test_data/channel_equalizer_test_check_tx_symbols10.dat"}},  {25, 14, 2, {"test_data/channel_equalizer_test_input_rx_symbols10.dat"}},  {25, 14, 2, 2, 0.1, {"test_data/channel_equalizer_test_input_ch_estimates10.dat"}}, 1.2 , "ZF" },
+{  {25, 14, 1, {"test_data/channel_equalizer_test_output_eq_symbols0.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_output_eq_noise_vars0.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_check_tx_symbols0.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_input_rx_symbols0.dat"}},  {25, 14, 1, 1, 0.1, {"test_data/channel_equalizer_test_input_ch_estimates0.dat"}}, 1.2, "MMSE"},
+{  {25, 14, 1, {"test_data/channel_equalizer_test_output_eq_symbols2.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_output_eq_noise_vars2.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_check_tx_symbols2.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_input_rx_symbols2.dat"}},  {25, 14, 1, 1, 0.1, {"test_data/channel_equalizer_test_input_ch_estimates2.dat"}}, 1.2, "ZF"},
+{  {25, 14, 1, {"test_data/channel_equalizer_test_output_eq_symbols4.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_output_eq_noise_vars4.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_check_tx_symbols4.dat"}},  {25, 14, 2, {"test_data/channel_equalizer_test_input_rx_symbols4.dat"}},  {25, 14, 2, 1, 0.1, {"test_data/channel_equalizer_test_input_ch_estimates4.dat"}}, 1.2, "MMSE"},
+{  {25, 14, 1, {"test_data/channel_equalizer_test_output_eq_symbols6.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_output_eq_noise_vars6.dat"}},  {25, 14, 1, {"test_data/channel_equalizer_test_check_tx_symbols6.dat"}},  {25, 14, 2, {"test_data/channel_equalizer_test_input_rx_symbols6.dat"}},  {25, 14, 2, 1, 0.1, {"test_data/channel_equalizer_test_input_ch_estimates6.dat"}}, 1.2, "ZF"},
+{  {25, 14, 2, {"test_data/channel_equalizer_test_output_eq_symbols8.dat"}},  {25, 14, 2, {"test_data/channel_equalizer_test_output_eq_noise_vars8.dat"}},  {25, 14, 2, {"test_data/channel_equalizer_test_check_tx_symbols8.dat"}},  {25, 14, 2, {"test_data/channel_equalizer_test_input_rx_symbols8.dat"}},  {25, 14, 2, 2, 0.1, {"test_data/channel_equalizer_test_input_ch_estimates8.dat"}}, 1.2, "MMSE"},
+{  {25, 14, 2, {"test_data/channel_equalizer_test_output_eq_symbols10.dat"}},  {25, 14, 2, {"test_data/channel_equalizer_test_output_eq_noise_vars10.dat"}},  {25, 14, 2, {"test_data/channel_equalizer_test_check_tx_symbols10.dat"}},  {25, 14, 2, {"test_data/channel_equalizer_test_input_rx_symbols10.dat"}},  {25, 14, 2, 2, 0.1, {"test_data/channel_equalizer_test_input_ch_estimates10.dat"}}, 1.2, "ZF"},
     // clang-format on
 };
 
