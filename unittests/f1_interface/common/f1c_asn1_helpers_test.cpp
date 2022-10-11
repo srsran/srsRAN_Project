72f64d0ea7bb64a62f06882328aea4b5634ad256
Andre Puschmann <andre@softwareradiosystems.com>
2022-10-11 13:37:15 +0200

    f1c_asn1_helpers_test: convert to Gtest and add testcase for PLMN hex

--- unittests/f1_interface/common/f1c_asn1_helpers_test.cpp
+++ unittests/f1_interface/common/f1c_asn1_helpers_test.cpp
@@ -10,17 +10,14 @@
 
 #include "lib/f1_interface/common/asn1_helpers.h"
 #include "srsgnb/asn1/f1ap.h"
-#include "srsgnb/f1_interface/common/f1c_common.h"
 #include "srsgnb/ran/nr_cgi.h"
-#include "srsgnb/support/test_utils.h"
+#include <gtest/gtest.h>
 
 using namespace srsgnb;
 
 /// Test PLMN decoding
-void test_ngi_converter()
+TEST(f1c_asn1_helpers_test, test_ngi_converter)
 {
-  test_delimit_logger delimiter{"Test NGI converter"};
-
   // use known a PLMN
   asn1::f1ap::nrcgi_s asn1_cgi;
   asn1_cgi.plmn_id.from_string("02f899"); // 208.99
@@ -28,16 +25,8 @@ void test_ngi_converter()
   // convert to internal NGI representation
   nr_cell_global_identity ngi = cgi_from_asn1(asn1_cgi);
 
-  TESTASSERT_EQ(0xf208, ngi.mcc);   // BCD-encoded MCC
-  TESTASSERT_EQ(0xff99, ngi.mnc);   // BCD-encoded MNC
-  TESTASSERT_EQ("20899", ngi.plmn); // human-readable PLMN
-}
-
-int main()
-{
-  srslog::fetch_basic_logger("TEST").set_level(srslog::basic_levels::debug);
-
-  srslog::init();
-
-  test_ngi_converter();
+  ASSERT_EQ(0xf208, ngi.mcc);        // BCD-encoded MCC
+  ASSERT_EQ(0xff99, ngi.mnc);        // BCD-encoded MNC
+  ASSERT_EQ("20899", ngi.plmn);      // human-readable PLMN
+  ASSERT_EQ("02f899", ngi.plmn_hex); // hex-encoded PLMN (like above)
 }
