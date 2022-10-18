55b159a13b0b0918580b52f0d6e4f6e198c03516
Pedro Alvarez <pedro.alvarez@softwareradiosystems.com>
2022-10-18 15:30:26 +0100

    pdcp: added TX test helper to share code between test fixtures

--- unittests/pdcp/pdcp_tx_metrics_test.h
+++ unittests/pdcp/pdcp_tx_metrics_test.h
@@ -12,6 +12,7 @@
 
 #include "../../lib/pdcp/pdcp_entity_tx.h"
 #include "pdcp_test_vectors.h"
+#include "pdcp_tx_test_helpers.h"
 #include "srsgnb/pdcp/pdcp_config.h"
 #include "srsgnb/support/timers.h"
 #include <gtest/gtest.h>
@@ -19,25 +20,11 @@
 
 namespace srsgnb {
 
-/// Mocking class of the surrounding layers invoked by the PDCP.
-class pdcp_tx_metrics_test_frame : public pdcp_tx_lower_notifier, public pdcp_tx_upper_control_notifier
-{
-public:
-  std::queue<byte_buffer> pdu_queue   = {};
-  uint32_t                pdu_counter = 0;
-
-  /// PDCP TX upper layer control notifier
-  void on_max_hfn_reached() final {}
-  void on_protocol_failure() final {}
-
-  /// PDCP TX lower layer data notifier
-  void on_new_pdu(byte_buffer pdu) final { pdu_queue.push(std::move(pdu)); }
-  void on_discard_pdu(uint32_t count) final {}
-};
-
 /// Fixture class for PDCP tests
 /// It requires TEST_P() and INSTANTIATE_TEST_SUITE_P() to create/spawn tests for each supported SN size
-class pdcp_tx_metrics_test : public ::testing::Test, public ::testing::WithParamInterface<pdcp_sn_size>
+class pdcp_tx_metrics_test : public pdcp_tx_test_helper,
+                             public ::testing::Test,
+                             public ::testing::WithParamInterface<pdcp_sn_size>
 {
 protected:
   void SetUp() override
@@ -56,78 +43,5 @@ protected:
     // flush logger after each test
     srslog::flush();
   }
-
-  /// \brief Initializes fixture according to size sequence number size
-  /// \param sn_size_ size of the sequence number
-  void init(pdcp_sn_size sn_size_, pdcp_discard_timer discard_timer = pdcp_discard_timer::ms10)
-  {
-    logger.info("Creating PDCP TX ({} bit)", to_number(sn_size_));
-
-    sn_size     = sn_size_;
-    pdu_hdr_len = pdcp_data_pdu_header_size(sn_size); // Round up division
-    mac_hdr_len = 4;                                  // MAC-I size
-
-    // Set TX config
-    config.sn_size       = sn_size;
-    config.rb_type       = pdcp_rb_type::drb;
-    config.rlc_mode      = pdcp_rlc_mode::am;
-    config.discard_timer = discard_timer;
-
-    // Set security keys
-    sec_cfg.k_128_rrc_int = k_128_int;
-    sec_cfg.k_128_up_int  = k_128_int;
-    sec_cfg.k_128_rrc_enc = k_128_enc;
-    sec_cfg.k_128_up_enc  = k_128_enc;
-
-    // Set encription/integrity algorithms
-    sec_cfg.integ_algo  = integrity_algorithm::nia1;
-    sec_cfg.cipher_algo = ciphering_algorithm::nea1;
-
-    // Create RLC entities
-    pdcp_tx = std::make_unique<pdcp_entity_tx>(0, LCID_SRB1, config, test_frame, test_frame, timers);
-  }
-
-  /// \brief Helper to advance the timers
-  /// \param nof_tick Number of ticks to advance timers
-  void tick_all(uint32_t nof_ticks)
-  {
-    for (uint32_t i = 0; i < nof_ticks; i++) {
-      timers.tick_all();
-    }
-  }
-
-  /// \brief Gets expected PDU based on the COUNT being tested
-  /// \param count COUNT being tested
-  /// \param exp_pdu Expected PDU that is set to the correct test vector
-  void get_expected_pdu(uint32_t count, byte_buffer& exp_pdu)
-  {
-    ASSERT_EQ(true, get_pdu_test_vector(sn_size, count, exp_pdu));
-  }
-
-  srslog::basic_logger& logger = srslog::fetch_basic_logger("TEST", false);
-
-  pdcp_sn_size                sn_size = GetParam();
-  uint32_t                    pdu_hdr_len;
-  uint32_t                    mac_hdr_len;
-  pdcp_config::pdcp_tx_config config = {};
-  timer_manager               timers;
-  pdcp_tx_metrics_test_frame  test_frame = {};
-
-  // 12 bit test PDUs
-  byte_buffer buf_count0_snlen12{pdu1_count0_snlen12};       // [HFN | SN] 0000 0000 0000 0000 0000 | 0000 0000 0000
-  byte_buffer buf_count2048_snlen12{pdu1_count2048_snlen12}; // [HFN | SN] 0000 0000 0000 0000 0000 | 0001 0000 0000
-  byte_buffer buf_count4096_snlen12{pdu1_count4096_snlen12}; // [HFN | SN] 0000 0000 0000 0000 0001 | 0000 0000 0000
-  byte_buffer buf_count4294967295_snlen12{pdu1_count4294967295_snlen12}; // All 1's
-
-  // 18 bit test PDUs
-  byte_buffer buf_count0_snlen18{pdu1_count0_snlen18};           // [HFN | SN] 0000 0000 0000 00|00 0000 0000 0000 0000
-  byte_buffer buf_count131072_snlen18{pdu1_count131072_snlen18}; // [HFN | SN] 0000 0000 0000 00|10 0000 0000 0000 0000
-  byte_buffer buf_count262144_snlen18{pdu1_count262144_snlen18}; // [HFN | SN] 0000 0000 0000 01|00 0000 0000 0000 0000
-  byte_buffer buf_count4294967295_snlen18{pdu1_count4294967295_snlen18}; // All 1's
-
-  // Security configuration
-  sec_128_as_config sec_cfg = {};
-
-  std::unique_ptr<pdcp_entity_tx> pdcp_tx;
 };
 } // namespace srsgnb
