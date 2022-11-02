b7f483da0bf4d92c0fd2489c2989ae234f316241
Supreeth Herle <supreeth.herle@srs.io>
2022-11-02 15:06:33 +0100

    adt: add header to use variant under own namespace

---
+++ include/srsgnb/adt/variant.h
@@ -0,0 +1,20 @@
+/*
+ *
+ * Copyright 2013-2022 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#pragma once
+
+#include "external/variant/variant.hpp"
+
+namespace srsgnb {
+
+template <typename... Types>
+using variant = mpark::variant<Types...>;
+
+} // namespace srsgnb
