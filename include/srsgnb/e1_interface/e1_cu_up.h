4c9b482c595829ed012e9aa76073479273a7c630
Andre Puschmann <andre@softwareradiosystems.com>
2022-11-04 16:21:48 +0100

    e1: add initial e1 interface definition

---
+++ include/srsgnb/e1_interface/e1_cu_up.h
@@ -0,0 +1,59 @@
+/*
+ *
+ * Copyright 2013-2022 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#pragma once
+
+#include "e1_common.h"
+#include "srsgnb/asn1/e1ap.h"
+
+namespace srsgnb {
+namespace srs_cu_up {
+
+/// \brief CU-CP initiated E1 setup request.
+struct cu_cp_e1_setup_request_message {
+  asn1::e1ap::gnb_cu_cp_e1_setup_request_s request;
+};
+
+/// \brief Response to CU-CP initiated E1 setup request.
+struct cu_cp_e1_setup_response_message {
+  bool                                  success;
+  asn1::e1ap::gnb_cu_cp_e1_setup_resp_s response;
+  asn1::e1ap::gnb_cu_cp_e1_setup_fail_s failure;
+};
+
+/// Handle E1 interface management procedures as defined in TS 38.463 section 8.2.4.
+class e1_connection_manager
+{
+public:
+  virtual ~e1_connection_manager() = default;
+
+  /// \brief Creates and transmits the CU-CP initiated E1 Setup outcome to the CU-CP.
+  virtual void handle_cu_cp_e1_setup_response(const cu_cp_e1_setup_response_message& msg) = 0;
+};
+
+/// Methods used by E1 to notify the CU-CP (manager).
+class e1_cu_cp_notifier
+{
+public:
+  virtual ~e1_cu_cp_notifier() = default;
+
+  /// \brief Notifies about the reception of a E1 Setup Request message.
+  virtual void on_e1_setup_request_received(const cu_cp_e1_setup_request_message& msg) = 0;
+};
+
+/// Combined entry point for E1 handling.
+class e1_interface : public e1_message_handler, public e1_event_handler, public e1_connection_manager
+{
+public:
+  virtual ~e1_interface() = default;
+};
+
+} // namespace srs_cu_up
+} // namespace srsgnb
