b8e0ba960fbcc7b875e303844b9ecd4f7fa096a7
Supreeth Herle <supreeth.herle@srs.io>
2022-11-17 17:05:06 +0100

    test: modify test case to check multiple entries addition to pucch resource

--- unittests/du_manager/serving_cell_config_converter_test.cpp
+++ unittests/du_manager/serving_cell_config_converter_test.cpp
@@ -402,6 +402,18 @@ TEST(serving_cell_config_converter_test, test_ue_custom_pucch_cfg_conversion)
   res_basic.format_1.time_domain_occ      = 0;
   dest_pucch_cfg.pucch_res_list.push_back(res_basic);
 
+  pucch_resource res_basic2{.res_id                 = 3,
+                            .starting_prb           = 40,
+                            .second_hop_prb         = 50,
+                            .intraslot_freq_hopping = true,
+                            .format                 = pucch_format::FORMAT_3};
+  res_basic.format                        = pucch_format::FORMAT_3;
+  res_basic.format_1.initial_cyclic_shift = 0;
+  res_basic.format_1.nof_symbols          = 2;
+  res_basic.format_1.starting_sym_idx     = 7;
+  res_basic.format_1.time_domain_occ      = 0;
+  dest_pucch_cfg.pucch_res_list.push_back(res_basic2);
+
   // Remove first element.
   dest_pucch_cfg.pucch_res_list.erase(dest_pucch_cfg.pucch_res_list.begin());
 
@@ -453,7 +465,11 @@ TEST(serving_cell_config_converter_test, test_ue_custom_pucch_cfg_conversion)
     ASSERT_EQ(rrc_sp_cell_cfg_ded.ul_cfg.init_ul_bwp.pucch_cfg.setup().res_set_to_add_mod_list.size(), 1);
     ASSERT_EQ(rrc_sp_cell_cfg_ded.ul_cfg.init_ul_bwp.pucch_cfg.setup().res_set_to_release_list.size(), 1);
 
-    ASSERT_EQ(rrc_sp_cell_cfg_ded.ul_cfg.init_ul_bwp.pucch_cfg.setup().res_to_add_mod_list.size(), 1);
+    ASSERT_EQ(rrc_sp_cell_cfg_ded.ul_cfg.init_ul_bwp.pucch_cfg.setup().res_to_add_mod_list.size(), 2);
+    auto& pucch_res1 = rrc_sp_cell_cfg_ded.ul_cfg.init_ul_bwp.pucch_cfg.setup().res_to_add_mod_list[0];
+    ASSERT_EQ(pucch_res1.pucch_res_id, 2);
+    auto& pucch_res2 = rrc_sp_cell_cfg_ded.ul_cfg.init_ul_bwp.pucch_cfg.setup().res_to_add_mod_list[1];
+    ASSERT_EQ(pucch_res2.pucch_res_id, 3);
     ASSERT_EQ(rrc_sp_cell_cfg_ded.ul_cfg.init_ul_bwp.pucch_cfg.setup().res_to_release_list.size(), 1);
 
     ASSERT_EQ(rrc_sp_cell_cfg_ded.ul_cfg.init_ul_bwp.pucch_cfg.setup().sched_request_res_to_add_mod_list.size(), 1);
