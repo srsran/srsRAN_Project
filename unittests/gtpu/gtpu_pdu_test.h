fee55d8127fe409951b7ca1da6682ec575a83a40
Pedro Alvarez <pedro.alvarez@softwareradiosystems.com>
2022-11-17 17:54:36 +0000

    gtpu: added RX PDU test

--- unittests/gtpu/gtpu_pdu_test.h
+++ unittests/gtpu/gtpu_pdu_test.h
@@ -11,6 +11,8 @@
 #pragma once
 
 #include "../../lib/gtpu/gtpu_pdu.h"
+#include "srsgnb/gtpu/gtpu_dl.h"
+#include "srsgnb/gtpu/gtpu_ul.h"
 #include "srsgnb/support/timers.h"
 #include <gtest/gtest.h>
 
@@ -22,6 +24,15 @@ const uint8_t gtpu_ping_vec[] = {
     0x61, 0x63, 0x00, 0x00, 0x00, 0x00, 0x1a, 0x20, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x11, 0x12, 0x13, 0x14,
     0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27,
     0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37};
+
+class gtpu_test_dl : public gtpu_dl_lower_layer_notifier
+{
+  void on_new_pdu(byte_buffer buf) final {}
+};
+class gtpu_test_ul : public gtpu_ul_upper_layer_notifier
+{
+  void on_new_sdu(byte_buffer buf) final {}
+};
 
 /// Fixture class for GTP-U PDU tests
 class gtpu_test : public ::testing::Test
@@ -40,8 +51,8 @@ protected:
     logger.set_level(srslog::basic_levels::debug);
 
     // init GTPU logger
-    srslog::fetch_basic_logger("GTPU", false).set_level(srslog::basic_levels::debug);
-    srslog::fetch_basic_logger("GTPU", false).set_hex_dump_max_size(100);
+    gtpu_logger.set_level(srslog::basic_levels::debug);
+    gtpu_logger.set_hex_dump_max_size(100);
   }
 
   void TearDown() override
--- unittests/gtpu/gtpu_test.cpp
+++ unittests/gtpu/gtpu_test.cpp
@@ -27,6 +27,24 @@ TEST_F(gtpu_test, entity_creation)
   ASSERT_NE(gtpu, nullptr);
 };
 
+TEST_F(gtpu_test, rx_sdu)
+{
+  srsgnb::test_delimit_logger delimiter("GTP-U entity creation test");
+
+  gtpu_test_dl                 gtpu_dl = {};
+  gtpu_test_ul                 gtpu_ul = {};
+  gtpu_entity_creation_message msg     = {};
+  msg.cfg.dl.src_teid                  = 0x1;
+  msg.dl_lower                         = &gtpu_dl;
+  msg.ul_upper                         = &gtpu_ul;
+  std::unique_ptr<gtpu_entity>   gtpu  = create_gtpu_entity(msg);
+  byte_buffer                    orig_vec{gtpu_ping_vec};
+  gtpu_dl_upper_layer_interface* tx = gtpu->get_dl_upper_layer_interface();
+  tx->handle_sdu(std::move(orig_vec));
+
+  ASSERT_NE(gtpu, nullptr);
+};
+
 int main(int argc, char** argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
