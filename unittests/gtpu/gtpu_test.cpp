4a05915cb19fbf65fce55af9d6103ea73e80ab8b
Pedro Alvarez <pedro.alvarez@softwareradiosystems.com>
2022-11-18 12:53:17 +0000

    gtpu: merged pdu packing test into the main gtpu_test.cpp

--- unittests/gtpu/gtpu_test.cpp
+++ unittests/gtpu/gtpu_test.cpp
@@ -8,7 +8,7 @@
  *
  */
 
-#include "gtpu_pdu_test.h"
+#include "gtpu_test.h"
 #include "srsgnb/gtpu/gtpu_factory.h"
 #include "srsgnb/support/test_utils.h"
 #include "srsgnb/support/timers.h"
@@ -16,6 +16,50 @@
 #include <queue>
 
 using namespace srsgnb;
+
+/// \brief Test correct packing/unpacking of GTPU PDU
+TEST_F(gtpu_test, pack_unpack)
+{
+  srsgnb::test_delimit_logger delimiter("GTP-U unpack/pack test");
+  byte_buffer                 orig_vec{gtpu_ping_vec};
+  byte_buffer                 tst_vec{gtpu_ping_vec};
+  byte_buffer                 tst_vec_no_header{tst_vec.begin() + 8, tst_vec.end()};
+  gtpu_header                 hdr;
+
+  // Unpack SDU
+  logger.info(orig_vec.begin(), orig_vec.end(), "Original SDU");
+  bool read_ok = gtpu_read_and_strip_header(hdr, tst_vec, gtpu_logger);
+  ASSERT_EQ(read_ok, true);
+  logger.info(tst_vec.begin(), tst_vec.end(), "Unpacked PDU");
+
+  // Check flags
+  ASSERT_EQ(hdr.flags.version, GTPU_FLAGS_VERSION_V1);
+  ASSERT_EQ(hdr.flags.protocol_type, GTPU_FLAGS_GTP_PROTOCOL);
+  ASSERT_EQ(hdr.flags.ext_hdr, false);
+  ASSERT_EQ(hdr.flags.seq_number, false);
+  ASSERT_EQ(hdr.flags.n_pdu, false);
+
+  // Check message type
+  ASSERT_EQ(hdr.message_type, GTPU_MSG_DATA_PDU);
+
+  // Check length
+  ASSERT_EQ(hdr.length, tst_vec_no_header.length());
+
+  // Check TEID
+  ASSERT_EQ(hdr.teid, 1);
+
+  // Check PDU after striping header
+  ASSERT_EQ(tst_vec.length(), tst_vec_no_header.length());
+  ASSERT_EQ(tst_vec, tst_vec_no_header);
+
+  logger.info("Unpacked GTP-U header: {}", hdr);
+  byte_buffer repack_buf = tst_vec_no_header.deep_copy();
+
+  gtpu_write_header(repack_buf, hdr, gtpu_logger);
+  logger.info(repack_buf.begin(), repack_buf.end(), "Repackaged GTP-U packet");
+  ASSERT_EQ(repack_buf.length(), orig_vec.length());
+  ASSERT_EQ(repack_buf, orig_vec);
+}
 
 /// \brief Test correct creation of GTP-U entity
 TEST_F(gtpu_test, entity_creation)
@@ -27,6 +71,7 @@ TEST_F(gtpu_test, entity_creation)
   ASSERT_NE(gtpu, nullptr);
 };
 
+/// \brief Test correct reception of GTP-U packet
 TEST_F(gtpu_test, rx_sdu)
 {
   srsgnb::test_delimit_logger delimiter("GTP-U entity creation test");
@@ -43,6 +88,7 @@ TEST_F(gtpu_test, rx_sdu)
   rx->handle_sdu(std::move(orig_vec));
 };
 
+/// \brief Test correct transmission of GTP-U packet
 TEST_F(gtpu_test, tx_pdu)
 {
   srsgnb::test_delimit_logger delimiter("GTP-U entity creation test");
