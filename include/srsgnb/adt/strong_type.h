15a94202cc5ef5215d6ec3140529ebf3ef7e02f6
faluco <borja@srs.io>
2022-12-12 14:04:15 +0100

    adt: Implement a class for strong typing arithmetic types. This class also allows the user to specify which operators should be overloaded for the strong type.

---
+++ include/srsgnb/adt/strong_type.h
@@ -0,0 +1,405 @@
+/*
+ *
+ * Copyright 2013-2022 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#pragma once
+
+#include <type_traits>
+
+namespace srsgnb {
+
+namespace detail {
+
+template <typename Property, typename T>
+using strong_property_t = typename Property::template strong_property<T>;
+
+/// Base class that defines the equality operators for two different types.
+template <typename T, typename U>
+class strong_equality_with_type
+{
+public:
+  friend constexpr bool operator==(const T& lhs, const U& rhs) { return lhs.value() == rhs; }
+  friend constexpr bool operator==(const U& lhs, const T& rhs) { return lhs == rhs.value(); }
+  friend constexpr bool operator!=(const T& lhs, const U& rhs) { return lhs.value() != rhs; }
+  friend constexpr bool operator!=(const U& lhs, const T& rhs) { return lhs != rhs.value(); }
+};
+
+/// Base class that defines the comparison operators for two different types.
+template <typename T, typename U>
+class strong_comparison_with_type
+{
+public:
+  friend constexpr bool operator<(const T& lhs, const U& rhs) { return lhs.value() < rhs; }
+  friend constexpr bool operator<(const U& lhs, const T& rhs) { return lhs < rhs.value(); }
+  friend constexpr bool operator>(const T& lhs, const U& rhs) { return lhs.value() > rhs; }
+  friend constexpr bool operator>(const U& lhs, const T& rhs) { return lhs > rhs.value(); }
+  friend constexpr bool operator<=(const T& lhs, const U& rhs) { return lhs.value() <= rhs; }
+  friend constexpr bool operator<=(const U& lhs, const T& rhs) { return lhs <= rhs.value(); }
+  friend constexpr bool operator>=(const T& lhs, const U& rhs) { return lhs.value() >= rhs; }
+  friend constexpr bool operator>=(const U& lhs, const T& rhs) { return lhs >= rhs.value(); }
+};
+
+} // namespace detail
+
+/// Tag structure to construct an uninitialized strong_type object.
+struct strong_uninit_t {};
+static constexpr strong_uninit_t strong_uninit{};
+
+/// \brief Template class for strong typing arithmetic types.
+///
+/// This template class wraps an arithmetic type and allows enabling the common operators through the Properties
+/// template argument pack.
+template <typename T, typename Tag, typename... Properties>
+class strong_type : public detail::strong_property_t<Properties, strong_type<T, Tag, Properties...>>...
+{
+  static_assert(std::is_arithmetic<T>::value, "T should be an arithmetic type");
+  T val;
+
+public:
+  using value_type = T;
+  using tag_type   = Tag;
+
+  /// Special member function definitions.
+  explicit constexpr strong_type(strong_uninit_t u) {}
+  explicit constexpr strong_type(T val_) : val(val_) {}
+  constexpr strong_type(const strong_type&) = default;
+  constexpr strong_type& operator=(const strong_type&) = default;
+
+  /// Accessor for the underlying value of the strong type.
+  constexpr T&       value() { return val; }
+  constexpr const T& value() const { return val; }
+};
+
+struct strong_equality {
+  template <typename T>
+  class strong_property;
+};
+
+/// Equality operator definitions for strong types.
+template <typename T, typename Tag, typename... Properties>
+class strong_equality::strong_property<strong_type<T, Tag, Properties...>>
+{
+  using base_type = strong_type<T, Tag, Properties...>;
+
+public:
+  friend constexpr bool operator==(const base_type& lhs, const base_type& rhs) { return lhs.value() == rhs.value(); }
+  friend constexpr bool operator!=(const base_type& lhs, const base_type& rhs) { return lhs.value() != rhs.value(); }
+};
+
+/// Equality operator definitions for strong types and the list of types in the template argument pack.
+template <typename... Ts>
+struct strong_equality_with {
+  static_assert(std::is_arithmetic<Ts...>::value, "All Ts should be arithmetic types");
+
+  template <typename T>
+  class strong_property : public detail::strong_equality_with_type<T, Ts>...
+  {};
+};
+
+struct strong_comparison {
+  template <typename T>
+  class strong_property;
+};
+
+/// Comparison operator definitions for strong types.
+template <typename T, typename Tag, typename... Properties>
+class strong_comparison::strong_property<strong_type<T, Tag, Properties...>>
+{
+  using base_type = strong_type<T, Tag, Properties...>;
+
+public:
+  friend constexpr bool operator<(const base_type& lhs, const base_type& rhs) { return lhs.value() < rhs.value(); }
+  friend constexpr bool operator>(const base_type& lhs, const base_type& rhs) { return lhs.value() > rhs.value(); }
+  friend constexpr bool operator<=(const base_type& lhs, const base_type& rhs) { return lhs.value() <= rhs.value(); }
+  friend constexpr bool operator>=(const base_type& lhs, const base_type& rhs) { return lhs.value() >= rhs.value(); }
+};
+
+/// Comparison operator definitions for strong types and the list of types in the template argument pack.
+template <typename... Ts>
+struct strong_comparison_with {
+  static_assert(std::is_arithmetic<Ts...>::value, "All Ts should be arithmetic types");
+
+  template <typename T>
+  class strong_property : public detail::strong_comparison_with_type<T, Ts>...
+  {};
+};
+
+struct strong_increment_decrement {
+  template <typename T>
+  class strong_property;
+};
+
+/// Increment and decrement operator definitions for strong types.
+template <typename T, typename Tag, typename... Properties>
+class strong_increment_decrement::strong_property<strong_type<T, Tag, Properties...>>
+{
+  using base_type = strong_type<T, Tag, Properties...>;
+
+public:
+  friend constexpr base_type& operator++(base_type& v)
+  {
+    ++v.value();
+    return v;
+  }
+
+  friend constexpr base_type operator++(base_type& v, int)
+  {
+    auto tmp = v;
+    ++v;
+    return tmp;
+  }
+
+  friend constexpr base_type& operator--(base_type& v)
+  {
+    --v.value();
+    return v;
+  }
+
+  friend constexpr base_type operator--(base_type& v, int)
+  {
+    auto tmp = v;
+    --v;
+    return tmp;
+  }
+};
+
+struct strong_arithmetic {
+  template <typename T>
+  class strong_property;
+};
+
+/// Arithmetic operator definitions for strong types.
+template <typename T, typename Tag, typename... Properties>
+class strong_arithmetic::strong_property<strong_type<T, Tag, Properties...>>
+  : public strong_comparison::strong_property<strong_type<T, Tag, Properties...>>,
+    public strong_equality::strong_property<strong_type<T, Tag, Properties...>>
+{
+  using base_type = strong_type<T, Tag, Properties...>;
+
+public:
+  friend constexpr base_type operator-(const base_type& v) { return base_type(-v.value()); }
+
+  friend constexpr base_type& operator+=(base_type& lhs, const base_type& rhs)
+  {
+    lhs.value() += rhs.value();
+    return lhs;
+  }
+
+  friend constexpr base_type& operator-=(base_type& lhs, const base_type& rhs)
+  {
+    lhs.value() -= rhs.value();
+    return lhs;
+  }
+
+  friend constexpr base_type& operator*=(base_type& lhs, const base_type& rhs)
+  {
+    lhs.value() *= rhs.value();
+    return lhs;
+  }
+
+  friend constexpr base_type& operator/=(base_type& lhs, const base_type& rhs)
+  {
+    lhs.value() /= rhs.value();
+    return lhs;
+  }
+
+  friend constexpr base_type operator+(base_type lhs, const base_type& rhs)
+  {
+    lhs += rhs;
+    return lhs;
+  }
+
+  friend constexpr base_type operator-(base_type lhs, const base_type& rhs)
+  {
+    lhs -= rhs;
+    return lhs;
+  }
+
+  friend constexpr base_type operator*(base_type lhs, const base_type& rhs)
+  {
+    lhs *= rhs;
+    return lhs;
+  }
+
+  friend constexpr base_type operator/(base_type lhs, const base_type& rhs)
+  {
+    lhs /= rhs;
+    return lhs;
+  }
+};
+
+struct strong_arithmetic_with_underlying_type {
+  template <typename T>
+  class strong_property;
+};
+
+/// Increment and decrement operator definitions for strong types and its underlying type.
+template <typename T, typename Tag, typename... Properties>
+class strong_arithmetic_with_underlying_type::strong_property<strong_type<T, Tag, Properties...>>
+{
+  using base_type = strong_type<T, Tag, Properties...>;
+
+public:
+  friend constexpr base_type& operator+=(base_type& lhs, T rhs)
+  {
+    lhs.value() += rhs;
+    return lhs;
+  }
+
+  friend constexpr base_type& operator-=(base_type& lhs, T rhs)
+  {
+    lhs.value() -= rhs;
+    return lhs;
+  }
+
+  friend constexpr base_type& operator*=(base_type& lhs, T rhs)
+  {
+    lhs.value() *= rhs;
+    return lhs;
+  }
+
+  friend constexpr base_type& operator/=(base_type& lhs, T rhs)
+  {
+    lhs.value() /= rhs;
+    return lhs;
+  }
+
+  friend constexpr base_type operator+(base_type lhs, T rhs)
+  {
+    lhs += rhs;
+    return lhs;
+  }
+
+  friend constexpr base_type operator+(T lhs, base_type rhs)
+  {
+    rhs += lhs;
+    return rhs;
+  }
+
+  friend constexpr base_type operator-(base_type lhs, T rhs)
+  {
+    lhs -= rhs;
+    return lhs;
+  }
+
+  friend constexpr base_type operator-(T lhs, base_type rhs)
+  {
+    lhs -= rhs.value();
+    return base_type(lhs);
+  }
+
+  friend constexpr base_type operator*(base_type lhs, T rhs)
+  {
+    lhs *= rhs;
+    return lhs;
+  }
+
+  friend constexpr base_type operator*(T lhs, base_type rhs)
+  {
+    rhs += lhs;
+    return rhs;
+  }
+
+  friend constexpr base_type operator/(base_type lhs, T rhs)
+  {
+    lhs /= rhs;
+    return lhs;
+  }
+
+  friend constexpr base_type operator/(T lhs, base_type rhs)
+  {
+    lhs /= rhs.value();
+    return base_type(lhs);
+  }
+};
+
+struct strong_bitwise {
+  template <typename T>
+  class strong_property;
+};
+
+/// Bitwise operator definitions for strong types.
+template <typename T, typename Tag, typename... Properties>
+class strong_bitwise::strong_property<strong_type<T, Tag, Properties...>>
+{
+  using base_type = strong_type<T, Tag, Properties...>;
+
+public:
+  friend constexpr base_type& operator&=(base_type& lhs, const base_type& rhs)
+  {
+    lhs.value() &= rhs.value();
+    return lhs;
+  }
+
+  friend constexpr base_type& operator|=(base_type& lhs, const base_type& rhs)
+  {
+    lhs.value() |= rhs.value();
+    return lhs;
+  }
+
+  friend constexpr base_type& operator^=(base_type& lhs, const base_type& rhs)
+  {
+    lhs.value() ^= rhs.value();
+    return lhs;
+  }
+
+  template <typename U>
+  friend constexpr base_type& operator<<=(base_type& lhs, U shift)
+  {
+    static_assert(std::is_integral<U>::value, "U should be an integral type");
+    lhs.value() <<= shift;
+    return lhs;
+  }
+
+  template <typename U>
+  friend constexpr base_type& operator>>=(base_type& lhs, U shift)
+  {
+    static_assert(std::is_integral<U>::value, "U should be an integral type");
+    lhs.value() >>= shift;
+    return lhs;
+  }
+
+  friend constexpr base_type operator~(const base_type& v) { return base_type(~v.value()); }
+
+  friend constexpr base_type operator&(base_type lhs, base_type rhs)
+  {
+    lhs &= rhs;
+    return lhs;
+  }
+
+  friend constexpr base_type operator|(base_type lhs, base_type rhs)
+  {
+    lhs |= rhs;
+    return lhs;
+  }
+
+  friend constexpr base_type operator^(base_type lhs, base_type rhs)
+  {
+    lhs ^= rhs;
+    return lhs;
+  }
+
+  template <typename U>
+  friend constexpr base_type operator<<(base_type lhs, U shift)
+  {
+    static_assert(std::is_integral<U>::value, "U should be an integral type");
+    lhs <<= shift;
+    return lhs;
+  }
+
+  template <typename U>
+  friend constexpr base_type operator>>(base_type lhs, U shift)
+  {
+    static_assert(std::is_integral<U>::value, "U should be an integral type");
+    lhs >>= shift;
+    return lhs;
+  }
+};
+
+} // namespace srsgnb
---
+++ unittests/adt/strong_type_test.cpp
@@ -0,0 +1,124 @@
+/*
+ *
+ * Copyright 2013-2022 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "srsgnb/adt/strong_type.h"
+#include <gtest/gtest.h>
+
+using namespace srsgnb;
+
+TEST(strong_type_test, construction)
+{
+  using strong_int = strong_type<int, struct strong_int_tag>;
+
+  strong_int a{3};
+  ASSERT_EQ(a.value(), 3);
+}
+
+TEST(strong_type_test, equality)
+{
+  using strong_int = strong_type<int, struct strong_int_tag, strong_equality>;
+
+  strong_int a{3};
+  strong_int b{4};
+  ASSERT_NE(a, b);
+
+  strong_int c{3};
+  strong_int d{3};
+  ASSERT_EQ(c, d);
+}
+
+TEST(strong_type_test, equality_with_type)
+{
+  using strong_int = strong_type<int, struct strong_int_tag, strong_equality_with<int>>;
+
+  strong_int a{3};
+  ASSERT_EQ(a, 3);
+
+  strong_int b{3};
+  ASSERT_NE(b, 4);
+}
+
+TEST(strong_type_test, comparison)
+{
+  using strong_int = strong_type<int, struct strong_int_tag, strong_comparison>;
+
+  strong_int a{3};
+  strong_int b{4};
+  ASSERT_LT(a, b);
+  ASSERT_GT(b, a);
+  ASSERT_LE(a, b);
+  ASSERT_GE(b, a);
+}
+
+TEST(strong_type_test, comparison_with_type)
+{
+  using strong_int = strong_type<int, struct strong_int_tag, strong_comparison_with<int>>;
+
+  strong_int a{3};
+  ASSERT_LT(a, 4);
+  ASSERT_LT(2, a);
+  ASSERT_GT(a, 2);
+  ASSERT_GT(4, a);
+  ASSERT_LE(a, 3);
+  ASSERT_LE(3, a);
+  ASSERT_GE(a, 3);
+  ASSERT_GE(3, a);
+}
+
+TEST(strong_type_test, incr_decr)
+{
+  using strong_int = strong_type<int, struct strong_int_tag, strong_increment_decrement>;
+
+  strong_int a{3};
+  ++a;
+  ASSERT_EQ(a.value(), 4);
+  ASSERT_EQ(a++.value(), 4);
+  ASSERT_EQ(a.value(), 5);
+  --a;
+  ASSERT_EQ(a.value(), 4);
+  ASSERT_EQ(a--.value(), 4);
+  ASSERT_EQ(a.value(), 3);
+}
+
+TEST(strong_type_test, arithmetic)
+{
+  using strong_int = strong_type<int, struct strong_int_tag, strong_arithmetic>;
+
+  strong_int a{3};
+  strong_int b{4};
+  ASSERT_EQ((a + b).value(), 7);
+  ASSERT_EQ((a - b).value(), -1);
+  ASSERT_EQ((a * b).value(), 12);
+  ASSERT_EQ((a / b).value(), 0);
+}
+
+TEST(strong_type_test, arithmetic_with_underlying_type)
+{
+  using strong_int = strong_type<int, struct strong_int_tag, strong_arithmetic_with_underlying_type>;
+
+  strong_int a{3};
+  ASSERT_EQ((a + 1).value(), 4);
+  ASSERT_EQ((a - 1).value(), 2);
+  ASSERT_EQ((a * 3).value(), 9);
+  ASSERT_EQ((a / 3).value(), 1);
+}
+
+TEST(strong_type_test, bitwise)
+{
+  using strong_int = strong_type<int, struct strong_int_tag, strong_bitwise>;
+
+  strong_int a{2};
+  strong_int b{1};
+  ASSERT_EQ((a | b).value(), 3);
+  ASSERT_EQ((a & b).value(), 0);
+  ASSERT_EQ((a ^ b).value(), 3);
+  ASSERT_EQ((a >> 1).value(), 1);
+  ASSERT_EQ((a << 1).value(), 4);
+}
