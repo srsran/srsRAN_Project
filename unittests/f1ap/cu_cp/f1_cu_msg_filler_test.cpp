a4a2d76bd1db3aca4c5de57233ae106485265ee8
Andre Puschmann <andre@softwareradiosystems.com>
2022-12-22 10:04:24 +0100

    f1ap: add test for message filler for UE context modification request

---
+++ unittests/f1ap/cu_cp/f1_cu_msg_filler_test.cpp
@@ -0,0 +1,103 @@
+/*
+ *
+ * Copyright 2013-2022 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "lib/cu_cp/helpers/rrc_ue_f1ap_asn1_helpers.h"
+#include <gtest/gtest.h>
+
+using namespace asn1;
+using namespace srsgnb;
+using namespace srs_cu_cp;
+
+#define JSON_OUTPUT 1
+
+class f1ap_cu_msg_filler_test : public ::testing::Test
+{
+protected:
+  void SetUp() override
+  {
+    srslog::fetch_basic_logger("ASN1").set_level(srslog::basic_levels::debug);
+    srslog::fetch_basic_logger("ASN1").set_hex_dump_max_size(-1);
+
+    test_logger.set_level(srslog::basic_levels::debug);
+    test_logger.set_hex_dump_max_size(-1);
+
+    srslog::init();
+
+    // Start the log backend.
+    srslog::init();
+  }
+
+  void TearDown() override
+  {
+    // flush logger after each test
+    srslog::flush();
+  }
+
+  void verify_f1ap_ue_context_modifcation(const asn1::f1ap::ue_context_mod_request_s& msg)
+  {
+    // set required fields to have full PDU
+    asn1::f1ap::f1_ap_pdu_c pdu;
+    pdu.set_init_msg();
+    pdu.init_msg().load_info_obj(ASN1_F1AP_ID_UE_CONTEXT_MOD);
+    pdu.init_msg().value.ue_context_mod_request()                            = msg;
+    pdu.init_msg().value.ue_context_mod_request()->gnb_cu_ue_f1_ap_id->value = 1;
+    pdu.init_msg().value.ue_context_mod_request()->gnb_du_ue_f1_ap_id->value = 2;
+
+    // verify succesful packing
+    srsgnb::byte_buffer tx_buffer;
+    asn1::bit_ref       bref(tx_buffer);
+    ASSERT_EQ(pdu.pack(bref), asn1::SRSASN_SUCCESS);
+
+    std::vector<uint8_t> bytes{tx_buffer.begin(), tx_buffer.end()};
+#if JSON_OUTPUT
+    asn1::json_writer json_writer1;
+    pdu.to_json(json_writer1);
+    test_logger.info(
+        bytes.data(), bytes.size(), "F1AP PDU unpacked ({} B): \n {}", bytes.size(), json_writer1.to_string().c_str());
+#endif
+
+    ASSERT_EQ(test_pack_unpack_consistency(pdu), asn1::SRSASN_SUCCESS);
+  }
+
+  srslog::basic_logger& test_logger = srslog::fetch_basic_logger("TEST");
+};
+
+rrc_ue_ue_context_modification_request_message generate_ue_context_mod_request()
+{
+  rrc_ue_ue_context_modification_request_message msg           = {};
+  rrc_ue_drb_setup_message                       drb_setup_msg = {};
+  drb_setup_msg.drb_id                                         = 1;
+  drb_setup_msg.rlc                                            = rlc_mode::am;
+
+  cu_cp_gtp_tunnel gtp_tunnel        = {};
+  gtp_tunnel.gtp_teid                = 1;
+  gtp_tunnel.transport_layer_address = "";
+  drb_setup_msg.gtp_tunnels.push_back(gtp_tunnel);
+
+  qos_flow_setup_request_item mapped_flow = {};
+  mapped_flow.qos_flow_id                 = 1;
+  drb_setup_msg.qos_flows_mapped_to_drb.push_back(mapped_flow);
+
+  msg.rrc_ue_drb_setup_msgs.push_back(drb_setup_msg);
+
+  return msg;
+}
+
+// Test correct filling and generation of F1AP ASN1 messages for UE context modficication request.
+TEST_F(f1ap_cu_msg_filler_test, when_context_mod_req_valid_then_valid_asn1_msg_generated)
+{
+  rrc_ue_ue_context_modification_request_message msg = generate_ue_context_mod_request();
+
+  f1ap_ue_context_modification_request_message f1ap_ue_context_mod_request;
+  fill_f1ap_ue_context_modification_request(f1ap_ue_context_mod_request, msg);
+
+  // pack full F1AP PDU and verify correctnes
+  verify_f1ap_ue_context_modifcation(f1ap_ue_context_mod_request.msg);
+}
