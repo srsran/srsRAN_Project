bcf8963b178e2d71fe6e7b649e108bf64279dabb
Andre Puschmann <andre@softwareradiosystems.com>
2022-12-22 10:00:43 +0100

    rrc_ue_f1ap_asn1_helpers: fix conversion for DRB setup structs from flat type to asn1::f1ap

--- lib/cu_cp/helpers/rrc_ue_f1ap_asn1_helpers.h
+++ lib/cu_cp/helpers/rrc_ue_f1ap_asn1_helpers.h
@@ -11,6 +11,8 @@
 #pragma once
 
 #include "../converters/f1ap_asn1_converters.h"
+#include "srsgnb/f1ap/cu_cp/f1ap_cu.h"
+#include "srsgnb/rrc/rrc_ue.h"
 
 namespace srsgnb {
 namespace srs_cu_cp {
@@ -19,13 +21,33 @@ inline void fill_f1ap_ue_context_modification_request(f1ap_ue_context_modificati
                                                       const rrc_ue_ue_context_modification_request_message& msg)
 {
   // drb to be setup mod list
-  f1c_request.msg->drbs_to_be_setup_mod_list_present = true;
+  if (msg.rrc_ue_drb_setup_msgs.size() > 0) {
+    f1c_request.msg->drbs_to_be_setup_mod_list_present = true;
+  }
 
   for (auto drb_to_be_setup : msg.rrc_ue_drb_setup_msgs) {
     asn1::protocol_ie_single_container_s<asn1::f1ap::drbs_to_be_setup_mod_item_ies_o> f1ap_setup_item;
     auto& f1ap_drb_to_setup_item = f1ap_setup_item->drbs_to_be_setup_mod_item();
 
     f1ap_drb_to_setup_item.drbid = drb_to_be_setup.drb_id;
+    switch (drb_to_be_setup.rlc) {
+      case rlc_mode::am:
+        f1ap_drb_to_setup_item.rlc_mode.value = asn1::f1ap::rlc_mode_opts::rlc_am;
+        break;
+      case rlc_mode::um_bidir:
+        f1ap_drb_to_setup_item.rlc_mode.value = asn1::f1ap::rlc_mode_opts::rlc_um_bidirectional;
+        break;
+      case rlc_mode::um_unidir_dl:
+        f1ap_drb_to_setup_item.rlc_mode.value = asn1::f1ap::rlc_mode_opts::rlc_um_unidirectional_dl;
+        break;
+      case rlc_mode::um_unidir_ul:
+        f1ap_drb_to_setup_item.rlc_mode.value = asn1::f1ap::rlc_mode_opts::rlc_um_unidirectional_ul;
+        break;
+      case rlc_mode::tm:
+        // TM not supported for DRBs
+        report_fatal_error("Invalid RLC mode {}", drb_to_be_setup.rlc);
+        break;
+    }
 
     // Add uLUPTNLInformation_ToBeSetup
     for (auto cu_cp_gtp_tunnel_item : drb_to_be_setup.gtp_tunnels) {
@@ -35,16 +57,30 @@ inline void fill_f1ap_ue_context_modification_request(f1ap_ue_context_modificati
       f1ap_drb_to_setup_item.uluptnl_info_to_be_setup_list.push_back(uluptnl_item);
     }
 
-    // TODO: Add qos information
+    // Add qos information
     f1ap_drb_to_setup_item.qo_sinfo.set_choice_ext();
     auto& choice_ext = f1ap_drb_to_setup_item.qo_sinfo.choice_ext();
-
     choice_ext.load_info_obj(ASN1_F1AP_ID_DRB_INFO);
-    // auto& drb_info = choice_ext.value().drb_info();
 
-    // TODO: Add rlc mode
+    auto& drb_info = choice_ext.value().drb_info();
+    drb_info.drb_qos.qo_s_characteristics.set_non_dynamic_minus5_qi();
+    drb_info.drb_qos.qo_s_characteristics.non_dynamic_minus5_qi().five_qi = drb_to_be_setup.qos_info.five_qi;
+    drb_info.drb_qos.ngra_nalloc_retention_prio.prio_level                = drb_to_be_setup.qos_info.prio_level_arp;
+    drb_info.drb_qos.ngra_nalloc_retention_prio.pre_emption_cap =
+        asn1::f1ap::pre_emption_cap_opts::shall_not_trigger_pre_emption;
+    drb_info.drb_qos.ngra_nalloc_retention_prio.pre_emption_vulnerability.value =
+        asn1::f1ap::pre_emption_vulnerability_opts::not_pre_emptable;
+    drb_info.snssai.sst.from_number(drb_to_be_setup.s_nssai.sst);
+    if (drb_to_be_setup.s_nssai.sd.has_value()) {
+      drb_info.snssai.sd.from_number(drb_to_be_setup.s_nssai.sd.value());
+    }
 
-    // TODO: Add optional values (ulcfg, dupl_activation, ie_exts)
+    for (const auto& qos_flow : drb_to_be_setup.qos_flows_mapped_to_drb) {
+      asn1::f1ap::flows_mapped_to_drb_item_s new_item;
+      new_item.qo_sflow_id               = qos_flow.qos_flow_id;
+      new_item.qo_sflow_level_qos_params = drb_info.drb_qos;
+      drb_info.flows_mapped_to_drb_list.push_back(new_item);
+    }
 
     f1c_request.msg->drbs_to_be_setup_mod_list.value.push_back(f1ap_setup_item);
   }
