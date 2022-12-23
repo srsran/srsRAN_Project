c2843abf18498ecce86a16a6ce015f3958095d16
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2022-12-23 13:29:09 +0100

    sched: optimize uci_sr_scheduling_test

--- unittests/scheduler/uci_and_pucch/uci_sr_scheduling_test.cpp
+++ unittests/scheduler/uci_and_pucch/uci_sr_scheduling_test.cpp
@@ -10,6 +10,7 @@
 
 #include "unittests/scheduler/test_utils/scheduler_test_suite.h"
 #include "unittests/scheduler/test_utils/uci_test_utils.h"
+#include "srsgnb/support/test_utils.h"
 #include <gtest/gtest.h>
 #include <random>
 
@@ -36,11 +37,13 @@ static expected_output_params make_expected_output_params_sr_harq()
 }
 
 // Class to test PUCCH schedule with SR occasions only.
-class test_pucch_output_sr_only : public ::testing::TestWithParam<std::tuple<sr_periodicity, unsigned>>
+class test_pucch_output_sr_only : public ::testing::TestWithParam<sr_periodicity>
 {
 public:
   test_pucch_output_sr_only() :
-    t_bench{pucch_res_common, n_cces, std::get<0>(GetParam()), std::get<1>(GetParam())},
+    sr_period(GetParam()),
+    sr_offset(test_rgen::uniform_int<unsigned>(0, sr_periodicity_to_slot(GetParam()) - 1)),
+    t_bench{pucch_res_common, n_cces, sr_period, sr_offset},
     pucch_expected(build_pucch_info(&t_bench.cell_cfg.ul_cfg_common.init_ul_bwp.generic_params,
                                     static_cast<unsigned>(t_bench.cell_cfg.pci),
                                     expected_output_sr.format,
@@ -54,9 +57,11 @@ public:
 
 protected:
   // Parameters that are passed by the routing to run the tests.
-  unsigned   pucch_res_common{11};
-  unsigned   n_cces{0};
-  test_bench t_bench;
+  sr_periodicity sr_period;
+  unsigned       sr_offset;
+  unsigned       pucch_res_common{11};
+  unsigned       n_cces{0};
+  test_bench     t_bench;
   // This contains the list of output expected parameters for SR PUCCH PDU.
   expected_output_params expected_output_sr;
   // Expected SR PUCCH PDU that would be passed to PHY.
@@ -64,11 +69,13 @@ protected:
 };
 
 // Class to test PUCCH schedule with SR occasions & HARQ-ACK.
-class test_pucch_output_sr_harq : public ::testing::TestWithParam<std::tuple<sr_periodicity, unsigned>>
+class test_pucch_output_sr_harq : public ::testing::TestWithParam<sr_periodicity>
 {
 public:
   test_pucch_output_sr_harq() :
-    t_bench{pucch_res_common, n_cces, std::get<0>(GetParam()), std::get<1>(GetParam())},
+    sr_period(GetParam()),
+    sr_offset(test_rgen::uniform_int<unsigned>(0, sr_periodicity_to_slot(GetParam()) - 1)),
+    t_bench{pucch_res_common, n_cces, sr_period, sr_offset},
     expected_output_sr{make_expected_output_params_sr_harq()},
     pucch_expected(build_pucch_info(&t_bench.cell_cfg.ul_cfg_common.init_ul_bwp.generic_params,
                                     static_cast<unsigned>(t_bench.cell_cfg.pci),
@@ -83,6 +90,8 @@ public:
 
 protected:
   // Parameters that are passed by the routing to run the tests.
+  sr_periodicity sr_period;
+  unsigned       sr_offset;
   unsigned       pucch_res_common{11};
   unsigned       n_cces{0};
   const unsigned sl_point_harq_delay{0};
@@ -96,23 +105,18 @@ protected:
 // Tests the PUCCH scheduler with SR opportunities only.
 TEST_P(test_pucch_output_sr_only, test_pucch_sr_only)
 {
-  sr_periodicity sr_period         = std::get<0>(GetParam());
-  unsigned       sr_offset         = std::get<1>(GetParam());
-  const unsigned NOF_SLOTS_TO_TEST = 1921;
+  const unsigned NOF_SLOTS_TO_TEST = sr_periodicity_to_slot(sr_period) * 4;
 
-  // Skip test if sr_period <= sr_offset.
-  if (sr_periodicity_to_slot(sr_period) > sr_offset) {
-    for (; t_bench.sl_tx.to_uint() < NOF_SLOTS_TO_TEST; t_bench.slot_indication(++t_bench.sl_tx)) {
-      // t_bench.pucch_alloc.slot_indication(t_bench.sl_tx);
-      t_bench.uci_sched.run_slot(t_bench.res_grid, t_bench.sl_tx);
+  for (; t_bench.sl_tx.to_uint() < NOF_SLOTS_TO_TEST; t_bench.slot_indication(++t_bench.sl_tx)) {
+    // t_bench.pucch_alloc.slot_indication(t_bench.sl_tx);
+    t_bench.uci_sched.run_slot(t_bench.res_grid, t_bench.sl_tx);
 
-      if ((t_bench.sl_tx - sr_offset).to_uint() % sr_periodicity_to_slot(sr_period) == 0) {
-        ASSERT_EQ(1, t_bench.res_grid[0].result.ul.pucchs.size());
-        ASSERT_TRUE(assess_ul_pucch_info(pucch_expected, t_bench.res_grid[0].result.ul.pucchs.back()));
-        ASSERT_TRUE(assert_ul_resource_grid_filled(t_bench.cell_cfg, t_bench.res_grid, 0));
-      } else {
-        ASSERT_TRUE(t_bench.res_grid[0].result.ul.pucchs.empty());
-      }
+    if ((t_bench.sl_tx - sr_offset).to_uint() % sr_periodicity_to_slot(sr_period) == 0) {
+      ASSERT_EQ(1, t_bench.res_grid[0].result.ul.pucchs.size());
+      ASSERT_TRUE(assess_ul_pucch_info(pucch_expected, t_bench.res_grid[0].result.ul.pucchs.back()));
+      ASSERT_TRUE(assert_ul_resource_grid_filled(t_bench.cell_cfg, t_bench.res_grid, 0));
+    } else {
+      ASSERT_TRUE(t_bench.res_grid[0].result.ul.pucchs.empty());
     }
   }
 }
@@ -120,61 +124,52 @@ TEST_P(test_pucch_output_sr_only, test_pucch_sr_only)
 // Tests the PUCCH scheduler with SR opportunities and HARQ-ACK.
 TEST_P(test_pucch_output_sr_harq, test_pucch_sr_harq)
 {
-  sr_periodicity sr_period         = std::get<0>(GetParam());
-  unsigned       sr_offset         = std::get<1>(GetParam());
-  const unsigned NOF_SLOTS_TO_TEST = 1921;
+  const unsigned NOF_SLOTS_TO_TEST = sr_periodicity_to_slot(sr_period) * 4;
 
-  // Skip test if sr_period <= sr_offset.
-  if (sr_periodicity_to_slot(sr_period) > sr_offset) {
-    for (; t_bench.sl_tx.to_uint() < NOF_SLOTS_TO_TEST; t_bench.slot_indication(++t_bench.sl_tx)) {
-      t_bench.pucch_alloc.alloc_common_pucch_harq_ack_ue(
-          t_bench.res_grid, t_bench.get_main_ue().crnti, t_bench.k0, sl_point_harq_delay, t_bench.dci_info);
-      t_bench.uci_sched.run_slot(t_bench.res_grid, t_bench.sl_tx);
+  for (; t_bench.sl_tx.to_uint() < NOF_SLOTS_TO_TEST; t_bench.slot_indication(++t_bench.sl_tx)) {
+    t_bench.pucch_alloc.alloc_common_pucch_harq_ack_ue(
+        t_bench.res_grid, t_bench.get_main_ue().crnti, t_bench.k0, sl_point_harq_delay, t_bench.dci_info);
+    t_bench.uci_sched.run_slot(t_bench.res_grid, t_bench.sl_tx);
 
-      if ((t_bench.sl_tx - sr_offset).to_uint() % sr_periodicity_to_slot(sr_period) == 0) {
-        ASSERT_EQ(2, t_bench.res_grid[0].result.ul.pucchs.size());
-        ASSERT_TRUE(assess_ul_pucch_info(pucch_expected, t_bench.res_grid[0].result.ul.pucchs.back()));
-        ASSERT_TRUE(assert_ul_resource_grid_filled(t_bench.cell_cfg, t_bench.res_grid, sl_point_harq_delay));
-      } else {
-        ASSERT_EQ(1, t_bench.res_grid[0].result.ul.pucchs.size());
-      }
+    if ((t_bench.sl_tx - sr_offset).to_uint() % sr_periodicity_to_slot(sr_period) == 0) {
+      ASSERT_EQ(2, t_bench.res_grid[0].result.ul.pucchs.size());
+      ASSERT_TRUE(assess_ul_pucch_info(pucch_expected, t_bench.res_grid[0].result.ul.pucchs.back()));
+      ASSERT_TRUE(assert_ul_resource_grid_filled(t_bench.cell_cfg, t_bench.res_grid, sl_point_harq_delay));
+    } else {
+      ASSERT_EQ(1, t_bench.res_grid[0].result.ul.pucchs.size());
     }
   }
 }
 
-INSTANTIATE_TEST_SUITE_P(
-    test_pucch_output_for_dci,
-    test_pucch_output_sr_only,
-    testing::Combine(testing::Values(sr_periodicity::sl_1,
-                                     sr_periodicity::sl_2,
-                                     sr_periodicity::sl_4,
-                                     sr_periodicity::sl_5,
-                                     sr_periodicity::sl_8,
-                                     sr_periodicity::sl_16,
-                                     sr_periodicity::sl_20,
-                                     sr_periodicity::sl_40,
-                                     sr_periodicity::sl_80,
-                                     sr_periodicity::sl_160,
-                                     sr_periodicity::sl_320,
-                                     sr_periodicity::sl_640),
-                     testing::Values(0, 1, 2, 3, 4, 7, 13, 15, 19, 23, 39, 43, 79, 91, 159, 243, 319, 473, 639)));
+INSTANTIATE_TEST_SUITE_P(test_pucch_output_for_dci,
+                         test_pucch_output_sr_only,
+                         testing::Values(sr_periodicity::sl_1,
+                                         sr_periodicity::sl_2,
+                                         sr_periodicity::sl_4,
+                                         sr_periodicity::sl_5,
+                                         sr_periodicity::sl_8,
+                                         sr_periodicity::sl_16,
+                                         sr_periodicity::sl_20,
+                                         sr_periodicity::sl_40,
+                                         sr_periodicity::sl_80,
+                                         sr_periodicity::sl_160,
+                                         sr_periodicity::sl_320,
+                                         sr_periodicity::sl_640));
 
-INSTANTIATE_TEST_SUITE_P(
-    test_pucch_output_for_dci,
-    test_pucch_output_sr_harq,
-    testing::Combine(testing::Values(sr_periodicity::sl_1,
-                                     sr_periodicity::sl_2,
-                                     sr_periodicity::sl_4,
-                                     sr_periodicity::sl_5,
-                                     sr_periodicity::sl_8,
-                                     sr_periodicity::sl_16,
-                                     sr_periodicity::sl_20,
-                                     sr_periodicity::sl_40,
-                                     sr_periodicity::sl_80,
-                                     sr_periodicity::sl_160,
-                                     sr_periodicity::sl_320,
-                                     sr_periodicity::sl_640),
-                     testing::Values(0, 1, 2, 3, 4, 7, 13, 15, 19, 23, 39, 43, 79, 91, 159, 243, 319, 473, 639)));
+INSTANTIATE_TEST_SUITE_P(test_pucch_output_for_dci,
+                         test_pucch_output_sr_harq,
+                         testing::Values(sr_periodicity::sl_1,
+                                         sr_periodicity::sl_2,
+                                         sr_periodicity::sl_4,
+                                         sr_periodicity::sl_5,
+                                         sr_periodicity::sl_8,
+                                         sr_periodicity::sl_16,
+                                         sr_periodicity::sl_20,
+                                         sr_periodicity::sl_40,
+                                         sr_periodicity::sl_80,
+                                         sr_periodicity::sl_160,
+                                         sr_periodicity::sl_320,
+                                         sr_periodicity::sl_640));
 
 int main(int argc, char** argv)
 {
