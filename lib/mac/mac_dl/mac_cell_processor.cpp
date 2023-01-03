c858bb8f276885b6bb041c24c3bb28fc8bece8a6
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2023-01-03 13:44:50 +0100

    mac: fix computation of maximum slot value in mac cell processor

--- lib/mac/mac_dl/mac_cell_processor.cpp
+++ lib/mac/mac_dl/mac_cell_processor.cpp
@@ -31,7 +31,7 @@ mac_cell_processor::mac_cell_processor(const mac_cell_creation_request& cell_cfg
   // The PDU pool has to be large enough to fit the maximum number of PDUs per slot for all possible K0 values.
   pdu_pool(dl_sch_pdu::MAX_PDU_LENGTH * MAX_DL_PDUS_PER_SLOT,
            MAX_K0_DELAY,
-           get_nof_slots_per_subframe(cell_cfg.scs_common) * NOF_SFNS),
+           get_nof_slots_per_subframe(cell_cfg.scs_common) * NOF_SFNS * NOF_SUBFRAMES_PER_FRAME),
   ssb_helper(cell_cfg_req_),
   sib_assembler(cell_cfg_req_.bcch_dl_sch_payload),
   rar_assembler(pdu_pool),
