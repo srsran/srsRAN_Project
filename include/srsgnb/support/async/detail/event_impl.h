59028caa318a4d0f24a32b6ca41574e8bc35b912
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2023-01-09 15:31:03 +0100

    adt: improve async manual_event documentation

--- include/srsgnb/support/async/detail/event_impl.h
+++ include/srsgnb/support/async/detail/event_impl.h
@@ -27,6 +27,63 @@ void flush_awaiter_list(AwaiterType* current)
   }
 }
 
+/// Base class implementation with functionality that is common to manual_event<R> and manual_event_flag
+/// Awaiters for the event are stored in an intrusive linked list, where each awaiter is stored in the embedded
+/// memory buffer of the coroutine frame
+class manual_event_common
+{
+public:
+  struct awaiter_common {
+    explicit awaiter_common(manual_event_common& event_) : event(&event_) {}
+    bool await_ready() const { return event->is_set(); }
+    void await_suspend(coro_handle<> c)
+    {
+      // Store suspending coroutine.
+      suspended_handle = c;
+
+      // enqueue awaiter
+      next = static_cast<awaiter_common*>(std::exchange(event->state, static_cast<void*>(this)));
+    }
+
+    manual_event_common* event;
+    awaiter_common*      next;
+    coro_handle<>        suspended_handle;
+  };
+
+  manual_event_common()                                             = default;
+  manual_event_common(const manual_event_common& event_)            = delete;
+  manual_event_common& operator=(const manual_event_common& event_) = delete;
+  ~manual_event_common() {}
+
+  /// Resets event state
+  void reset()
+  {
+    if (is_set()) {
+      state = nullptr;
+    }
+  }
+
+  /// Check if event value is set
+  bool is_set() const { return state == this; }
+
+protected:
+  // Event state. Can be in 3 states:
+  // - this - event is set
+  // - nullptr - event is not set and with no awaiters pending
+  // - other - state is not set. Points to first element in linked list of awaiters
+  void* state = nullptr;
+
+  /// triggers all awaiting coroutines after event is set
+  void flush()
+  {
+    if (is_set()) {
+      return;
+    }
+    auto old_state = std::exchange(state, this);
+    flush_awaiter_list(static_cast<awaiter_common*>(old_state));
+  }
+};
+
 } // namespace detail
 
 } // namespace srsgnb
--- include/srsgnb/support/async/manual_event.h
+++ include/srsgnb/support/async/manual_event.h
@@ -16,86 +16,14 @@
 
 namespace srsgnb {
 
-namespace detail {
-
-/// Base class implementation with functionality that is common to manual_event<R> and manual_event_flag
-/// Awaiters for the event are stored in an intrusive linked list, where each awaiter is stored in the embedded
-/// memory buffer of the coroutine frame
-class manual_event_common
-{
-public:
-  struct awaiter_common {
-    explicit awaiter_common(manual_event_common& event_) : event(&event_) {}
-    bool await_ready() const { return event->is_set(); }
-    void await_suspend(coro_handle<> c)
-    {
-      // Store suspending coroutine.
-      suspended_handle = c;
-
-      // enqueue awaiter
-      next = static_cast<awaiter_common*>(std::exchange(event->state, static_cast<void*>(this)));
-    }
-
-    manual_event_common* event;
-    awaiter_common*      next;
-    coro_handle<>        suspended_handle;
-  };
-
-  manual_event_common()                                             = default;
-  manual_event_common(const manual_event_common& event_)            = delete;
-  manual_event_common& operator=(const manual_event_common& event_) = delete;
-  ~manual_event_common() {}
-
-  /// Resets event state
-  void reset()
-  {
-    if (is_set()) {
-      state = nullptr;
-    }
-  }
-
-  /// Check if event value is set
-  bool is_set() const { return state == this; }
-
-protected:
-  // Event state. Can be in 3 states:
-  // - this - event is set
-  // - nullptr - event is not set and with no awaiters pending
-  // - other - state is not set. Points to first element in linked list of awaiters
-  void* state = nullptr;
-
-  /// triggers all awaiting coroutines after event is set
-  void flush()
-  {
-    if (is_set()) {
-      return;
-    }
-    auto old_state = std::exchange(state, this);
-    flush_awaiter_list(static_cast<awaiter_common*>(old_state));
-  }
-};
-
-} // namespace detail
-
-/// @brief Awaitable type that can be manually set to one of the states: "set" or "unset"
-/// Coroutines will automatically resume when they await on a manual_event_flag that is in "set" state
-class manual_event_flag : public detail::manual_event_common
-{
-public:
-  using result_type = void;
-  struct awaiter_type : public awaiter_common {
-    using awaiter_common::awaiter_common;
-    void await_resume() {}
-  };
-
-  void set() { flush(); }
-
-  /// awaiter interface
-  awaiter_type get_awaiter() { return awaiter_type{*this}; }
-};
-
-/// @brief Similar to "manual_event_flag", but the data passed to .set() is propagated to awaiting coroutine
-/// @tparam Data type of data passed to awaiting coroutine
+/// \brief Awaitable type that can be manually set to one of the states:
+/// - "unset" state - Coroutines that await on an "unset" manual_event get suspended. They are only resumed when the
+/// manual_event transitions to "set" state via the manual_event::set method.
+/// - "set" state - Coroutines that await on a "set" manual_event do not get suspended and get automatically resumed.
+/// Once "set", the awaiting coroutines have to option to extract the \c Data that was stored in the manual_event.
+/// Multiple coroutines can simultaneously await this awaitable when in "unset" state. All the awaiting coroutines
+/// will be resumed once the awaitable is set.
+/// \tparam Data type of data passed to awaiting coroutine.
 template <typename Data>
 class manual_event : public detail::manual_event_common
 {
@@ -129,5 +57,24 @@ public:
 private:
   Data data; /// value set
 };
+
+/// \brief Specialization of manual_event<Data> for the case that the Data is void.
+template <>
+class manual_event<void> : public detail::manual_event_common
+{
+public:
+  using result_type = void;
+  struct awaiter_type : public awaiter_common {
+    using awaiter_common::awaiter_common;
+    void await_resume() {}
+  };
+
+  void set() { flush(); }
+
+  /// awaiter interface
+  awaiter_type get_awaiter() { return awaiter_type{*this}; }
+};
+
+using manual_event_flag = manual_event<void>;
 
 } // namespace srsgnb
