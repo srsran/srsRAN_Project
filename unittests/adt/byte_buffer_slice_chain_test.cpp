9fdf27ad53bbc88ff19eeb39e35f72641c3e7649
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2023-01-11 19:25:56 +0100

    adt: convert byte_buffer_chain unit test to gtest

--- unittests/adt/byte_buffer_slice_chain_test.cpp
+++ unittests/adt/byte_buffer_slice_chain_test.cpp
@@ -10,33 +10,34 @@
 
 #include "srsgnb/adt/byte_buffer_slice_chain.h"
 #include "srsgnb/support/test_utils.h"
+#include <gtest/gtest.h>
 
 using namespace srsgnb;
 
-void test_empty_slice_chain()
+TEST(byte_buffer_slice_chain_test, empty_chain)
 {
   byte_buffer_slice_chain buf;
 
-  TESTASSERT(buf.empty());
-  TESTASSERT_EQ(0, buf.length());
-  TESTASSERT(buf.begin() == buf.end());
-  TESTASSERT(buf == std::vector<uint8_t>{});
+  ASSERT_TRUE(buf.empty());
+  ASSERT_EQ(0, buf.length());
+  ASSERT_EQ(buf.begin(), buf.end());
+  ASSERT_EQ(buf, std::vector<uint8_t>{});
 
   buf.push_back(byte_buffer{});
   buf.push_front(byte_buffer{});
 
-  TESTASSERT(buf.empty());
-  TESTASSERT(buf.begin() == buf.end());
+  ASSERT_TRUE(buf.empty());
+  ASSERT_EQ(buf.begin(), buf.end());
 
   byte_buffer buf_copy = buf.deep_copy();
-  TESTASSERT(buf_copy.empty());
-  TESTASSERT(buf_copy.begin() == buf_copy.end());
+  ASSERT_TRUE(buf_copy.empty());
+  ASSERT_EQ(buf_copy.begin(), buf_copy.end());
 }
 
-void test_slice_chain_payload()
+TEST(byte_buffer_slice_chain_test, chain_payload)
 {
   byte_buffer_slice_chain buf;
-  TESTASSERT(buf.empty());
+  ASSERT_TRUE(buf.empty());
 
   byte_buffer other_buffer{1, 2, 3, 4, 5};
   byte_buffer other_buffer2{6, 7, 8};
@@ -46,78 +47,78 @@ void test_slice_chain_payload()
   buf.push_back(other_buffer2.copy());
 
   // Test length/empty methods.
-  TESTASSERT(not buf.empty());
-  TESTASSERT_EQ(buf_concat.length(), buf.length());
+  ASSERT_FALSE(buf.empty());
+  ASSERT_EQ(buf_concat.length(), buf.length());
 
   // Test operator[].
   for (unsigned i = 0; i < buf_concat.length(); ++i) {
-    TESTASSERT_EQ(buf_concat[i], buf[i]);
+    ASSERT_EQ(buf_concat[i], buf[i]);
   }
 
   // Test iterator.
-  TESTASSERT(buf.begin() != buf.end());
+  ASSERT_NE(buf.begin(), buf.end());
   unsigned count = 0;
   for (uint8_t v : buf) {
-    TESTASSERT_EQ(buf_concat[(count++)], v);
+    ASSERT_EQ(buf_concat[(count++)], v);
   }
-  TESTASSERT_EQ(buf_concat.length(), count);
+  ASSERT_EQ(buf_concat.length(), count);
 
   // Test comparison.
-  TESTASSERT(buf == buf_concat);
+  ASSERT_EQ(buf, buf_concat);
 
   // Test copy
   byte_buffer buf_copy = buf.deep_copy();
-  TESTASSERT_EQ(buf_copy.length(), buf_concat.length());
-  TESTASSERT(buf_copy == buf_concat);
+  ASSERT_EQ(buf_copy.length(), buf_concat.length());
+  ASSERT_EQ(buf_copy, buf_concat);
 }
 
-void test_slice_chain_push_front()
+TEST(byte_buffer_slice_chain_test, push_front)
 {
   std::vector<uint8_t>    vec = {1, 2, 3};
   byte_buffer_slice_chain buf;
   byte_buffer             buf2{vec};
 
-  TESTASSERT(buf.empty());
+  ASSERT_TRUE(buf.empty());
 
   // Set header using a span of bytes.
   buf.push_front(buf2.deep_copy());
-  TESTASSERT(not buf.empty());
-  TESTASSERT_EQ(3, buf.length());
-  TESTASSERT(buf == vec);
+  ASSERT_FALSE(buf.empty());
+  ASSERT_EQ(3, buf.length());
+  ASSERT_EQ(buf, vec);
   for (unsigned i = 0; i < vec.size(); ++i) {
-    TESTASSERT_EQ(vec[i], buf[i]);
+    ASSERT_EQ(vec[i], buf[i]);
   }
-  TESTASSERT(buf.begin() != buf.end());
+  ASSERT_NE(buf.begin(), buf.end());
   unsigned count = 0;
   for (uint8_t v : buf) {
-    TESTASSERT_EQ(vec[count++], v);
+    ASSERT_EQ(vec[count++], v);
   }
-  TESTASSERT_EQ(vec.size(), count);
+  ASSERT_EQ(vec.size(), count);
 
   // Set header avoiding ref-count increment and avoiding deep copy.
   buf.clear();
   buf.push_front(std::move(buf2));
-  TESTASSERT(buf == vec);
-  TESTASSERT(buf2.empty());
+  ASSERT_EQ(buf, vec);
+  ASSERT_TRUE(buf2.empty());
 
   // Set header by ref-count increment, avoiding deep copy.
   buf.clear();
   buf2 = vec;
   buf.push_front(buf2.copy());
-  TESTASSERT(buf == vec);
+  ASSERT_EQ(buf, vec);
   *buf2.begin() = 5;
-  TESTASSERT(buf != vec);
+  ASSERT_NE(buf, vec);
 
   // Set header by deep copy.
   buf.clear();
   buf2 = vec;
   buf.push_front(buf2.deep_copy());
-  TESTASSERT(buf == vec);
+  ASSERT_EQ(buf, vec);
   *buf2.begin() = 5;
-  TESTASSERT(buf == vec);
+  ASSERT_EQ(buf, vec);
 }
 
-void test_slice_chain_header_and_payload()
+TEST(byte_buffer_slice_chain_test, chain_header_and_payload)
 {
   byte_buffer_slice_chain buf;
 
@@ -125,31 +126,31 @@ void test_slice_chain_header_and_payload()
   byte_buffer payload      = {4, 5, 6};
 
   buf.push_front(header_bytes.deep_copy());
-  TESTASSERT_EQ(header_bytes.length(), buf.length());
+  ASSERT_EQ(header_bytes.length(), buf.length());
   buf.push_back(payload.copy());
-  TESTASSERT_EQ(header_bytes.length() + payload.length(), buf.length());
+  ASSERT_EQ(header_bytes.length() + payload.length(), buf.length());
 
   // Test comparison.
   std::vector<uint8_t> all_bytes(header_bytes.begin(), header_bytes.end());
   all_bytes.insert(all_bytes.end(), payload.begin(), payload.end());
-  TESTASSERT(buf != payload);
-  TESTASSERT(buf == all_bytes);
+  ASSERT_NE(buf, payload);
+  ASSERT_EQ(buf, all_bytes);
 
   // Test operator[].
   for (unsigned i = 0; i < all_bytes.size(); ++i) {
-    TESTASSERT_EQ(all_bytes[i], buf[i]);
+    ASSERT_EQ(all_bytes[i], buf[i]);
   }
 
   // Test iterator.
-  TESTASSERT(buf.begin() != buf.end());
+  ASSERT_NE(buf.begin(), buf.end());
   unsigned count = 0;
   for (uint8_t v : buf) {
-    TESTASSERT_EQ(all_bytes[count++], v);
+    ASSERT_EQ(all_bytes[count++], v);
   }
-  TESTASSERT_EQ(all_bytes.size(), count);
+  ASSERT_EQ(all_bytes.size(), count);
 }
 
-void test_slice_chain_payload_lifetime()
+TEST(byte_buffer_slice_chain_test, payload_lifetime)
 {
   byte_buffer_slice_chain buf;
 
@@ -169,10 +170,10 @@ void test_slice_chain_payload_lifetime()
   }
   // Note: header and payload went out of scope, but that shouldnt affect the rlc buffer content.
 
-  TESTASSERT(buf == all_bytes);
+  ASSERT_EQ(buf, all_bytes);
 }
 
-void test_slice_chain_formatter()
+TEST(byte_buffer_slice_chain_test, slice_chain_formatter)
 {
   byte_buffer          pdu, pdu2;
   std::vector<uint8_t> bytes = {1, 2, 3, 4, 15, 16, 255};
@@ -184,19 +185,9 @@ void test_slice_chain_formatter()
   chain.push_back(std::move(pdu2), 0, 2);
 
   for (auto& b : chain) {
-    TESTASSERT(b > 0);
+    ASSERT_TRUE(b > 0);
   }
 
   std::string result = fmt::format("{}", chain);
-  TESTASSERT_EQ("04 0f 01 02", result);
-}
-
-int main()
-{
-  test_empty_slice_chain();
-  test_slice_chain_payload();
-  test_slice_chain_push_front();
-  test_slice_chain_header_and_payload();
-  test_slice_chain_payload_lifetime();
-  test_slice_chain_formatter();
+  ASSERT_EQ("04 0f 01 02", result);
 }
