5db20ecd4191c48eb860e40b4834ba452706cf5f
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2023-01-11 18:43:42 +0100

    adt: new byte buffer segment design without byte_buffer-wide control block

--- include/srsgnb/adt/detail/byte_buffer_segment.h
+++ include/srsgnb/adt/detail/byte_buffer_segment.h
@@ -218,6 +218,183 @@ private:
   uint8_t*                          payload_data_;
   uint8_t*                          payload_data_end_;
 };
+class byte_buffer_segment_v2
+{
+public:
+  constexpr static size_t SEGMENT_SIZE     = 1024;
+  constexpr static size_t DEFAULT_HEADROOM = 16;
+
+  using value_type     = uint8_t;
+  using iterator       = uint8_t*;
+  using const_iterator = const uint8_t*;
+
+  byte_buffer_segment_v2(size_t headroom = DEFAULT_HEADROOM)
+  {
+    // Members initialized here instead of the initialization list to avoid
+    // GCC 12.1.0 to warn about uninitialized use of buffer [-Werror=uninitialized]
+    payload_data_     = buffer.data() + headroom;
+    payload_data_end_ = buffer.data() + headroom;
+  }
+  byte_buffer_segment_v2(const byte_buffer_segment_v2& other) noexcept
+  {
+    // Members initialized here instead of the initialization list to avoid
+    // GCC 12.1.0 to warn about uninitialized use of buffer [-Werror=uninitialized]
+    payload_data_     = buffer.data() + other.headroom();
+    payload_data_end_ = buffer.data() + other.tailroom_start();
+    std::copy(other.begin(), other.end(), begin());
+  }
+  byte_buffer_segment_v2(byte_buffer_segment_v2&& other) noexcept
+  {
+    // Members initialized here instead of the initialization list to avoid
+    // GCC 12.1.0 to warn about uninitialized use of buffer [-Werror=uninitialized]
+    payload_data_     = buffer.data() + other.headroom();
+    payload_data_end_ = buffer.data() + other.tailroom_start();
+    std::copy(other.begin(), other.end(), begin());
+  }
+  byte_buffer_segment_v2& operator=(const byte_buffer_segment_v2& other) noexcept
+  {
+    if (this != &other) {
+      payload_data_     = buffer.data() + other.headroom();
+      payload_data_end_ = buffer.data() + other.tailroom_start();
+      std::copy(other.begin(), other.end(), begin());
+    }
+    return *this;
+  }
+  byte_buffer_segment_v2& operator=(byte_buffer_segment_v2&& other) noexcept
+  {
+    payload_data_     = buffer.data() + other.headroom();
+    payload_data_end_ = buffer.data() + other.tailroom_start();
+    std::copy(other.begin(), other.end(), begin());
+    return *this;
+  }
+
+  static size_t capacity() { return SEGMENT_SIZE; }
+
+  /// Checks whether segment has no payload.
+  bool empty() const { return begin() == end(); }
+
+  /// Returns how much space in bytes there is at the head of the segment.
+  size_t headroom() const { return data() - buffer.data(); }
+
+  /// Returns size in bytes of segment.
+  size_t length() const { return end() - begin(); }
+
+  /// Returns how much space in bytes there is at the tail of the segment.
+  size_t tailroom() const { return buffer.end() - end(); }
+
+  /// Appends a span of bytes at the tail of the segment.
+  void append(span<const uint8_t> bytes) { append(bytes.begin(), bytes.end()); }
+
+  /// Appends a iterator range of bytes at the tail of the segment.
+  template <typename It>
+  void append(It it_begin, It it_end)
+  {
+    static_assert(std::is_same<std::decay_t<decltype(*it_begin)>, uint8_t>::value, "Invalid value_type");
+    static_assert(
+        std::is_same<typename std::iterator_traits<It>::iterator_category, std::random_access_iterator_tag>::value,
+        "Only random access iterators allowed.");
+    srsgnb_sanity_check((size_t)(it_end - it_begin) <= tailroom(), "There is not enough tailroom for append.");
+    payload_data_end_ = std::copy(it_begin, it_end, end());
+  }
+
+  /// Appends single byte at the tail of the segment.
+  void append(uint8_t byte)
+  {
+    srsgnb_assert(tailroom() >= 1, "There is not enough tailroom space.");
+    buffer[tailroom_start()] = byte;
+    payload_data_end_++;
+  }
+
+  /// Prepends segment with provided span of bytes.
+  void prepend(span<const uint8_t> bytes)
+  {
+    srsgnb_assert(headroom() >= bytes.size(), "There is not enough headroom space.");
+    payload_data_ -= bytes.size();
+    std::copy(bytes.begin(), bytes.end(), begin());
+  }
+
+  /// Reserves headroom space in segment.
+  /// \param nof_bytes Number of bytes to reserve.
+  void reserve_prepend(size_t nof_bytes)
+  {
+    srsgnb_assert(headroom() >= nof_bytes, "There is not enough headroom space.");
+    payload_data_ -= nof_bytes;
+  }
+
+  /// Removes "nof_bytes" from the head of the segment.
+  void trim_head(size_t nof_bytes)
+  {
+    srsgnb_assert(nof_bytes <= length(), "There is not enough headroom space.");
+    payload_data_ += nof_bytes;
+  }
+
+  /// Removes "nof_bytes" from the tail of the segment.
+  void trim_tail(size_t nof_bytes)
+  {
+    srsgnb_assert(nof_bytes <= length(), "There is not enough headroom space.");
+    payload_data_end_ -= nof_bytes;
+  }
+
+  /// Resizes payload of segment.
+  void resize(size_t nof_bytes)
+  {
+    srsgnb_assert(nof_bytes <= capacity() - headroom(), "There is not enough space for provided size");
+    payload_data_end_ = payload_data_ + nof_bytes;
+  }
+
+  uint8_t& operator[](size_t idx)
+  {
+    srsgnb_assert(idx < length(), "Out-of-bound access");
+    return *(begin() + idx);
+  }
+  const uint8_t& operator[](size_t idx) const
+  {
+    srsgnb_assert(idx < length(), "Out-of-bound access");
+    return *(begin() + idx);
+  }
+
+  const uint8_t* data() const { return begin(); }
+  uint8_t*       data() { return begin(); }
+
+  uint8_t& back()
+  {
+    srsgnb_assert(not empty(), "back() called for empty segment.");
+    return *(payload_data_end_ - 1);
+  }
+  const uint8_t& back() const
+  {
+    srsgnb_assert(not empty(), "back() called for empty segment.");
+    return *(payload_data_end_ - 1);
+  }
+
+  iterator       begin() { return payload_data_; }
+  iterator       end() { return payload_data_end_; }
+  const_iterator begin() const { return payload_data_; }
+  const_iterator end() const { return payload_data_end_; }
+
+  std::unique_ptr<byte_buffer_segment_v2>&       next() { return next_segment; }
+  const std::unique_ptr<byte_buffer_segment_v2>& next() const { return next_segment; }
+
+  template <typename Container>
+  bool operator==(const Container& other) const
+  {
+    static_assert(std::is_same<std::decay_t<decltype(*other.begin())>, uint8_t>::value, "Invalid value_type");
+    return std::equal(begin(), end(), other.begin(), other.end());
+  }
+  template <typename Container>
+  bool operator!=(const Container& other) const
+  {
+    return !(*this == other);
+  }
+
+private:
+  size_t tailroom_start() const { return headroom() + length(); }
+
+  uint8_t*                                payload_data_;
+  uint8_t*                                payload_data_end_;
+  std::unique_ptr<byte_buffer_segment_v2> next_segment;
+  std::array<uint8_t, SEGMENT_SIZE>       buffer;
+};
 
 /// Pool of byte buffer segments.
 struct byte_buffer_pool_tag {};
