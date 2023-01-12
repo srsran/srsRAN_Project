a1fb6bfce1318614fbd0258fddce66b6e369a7b1
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2023-01-12 18:38:56 +0100

    sched: wrote unit test to verify the correct activation of logical channels during UE reconfigurations

--- unittests/scheduler/ue_scheduling/ue_configuration_test.cpp
+++ unittests/scheduler/ue_scheduling/ue_configuration_test.cpp
@@ -1,5 +1,5 @@
 
-#include "lib/scheduler/ue_scheduling/ue_configuration.h"
+#include "lib/scheduler/ue_scheduling/ue.h"
 #include "unittests/scheduler/test_utils/config_generators.h"
 #include "srsgnb/support/srsgnb_test.h"
 #include <gtest/gtest.h>
@@ -52,4 +52,44 @@ TEST(ue_configuration, configuration_valid_on_reconfiguration)
   TESTASSERT(ue_cfg.find_coreset(to_coreset_id(2)) != nullptr);
   TESTASSERT_EQ(2, ue_cfg.coreset(to_coreset_id(2)).id);
   TESTASSERT(ue_cfg.coreset(to_coreset_id(2)) == ue_cell_reconf.serv_cell_cfg.init_dl_bwp.pdcch_cfg->coresets.back());
+}
+
+TEST(ue_configuration, when_reconfiguration_is_received_then_ue_updates_logical_channel_states)
+{
+  // Test Preamble.
+  scheduler_expert_config                  expert_cfg = config_helpers::make_default_scheduler_expert_config();
+  sched_cell_configuration_request_message msg        = test_helpers::make_default_sched_cell_configuration_request();
+  cell_configuration                       cell_cfg{msg};
+  sched_ue_creation_request_message        ue_create_msg = test_helpers::create_default_sched_ue_creation_request();
+  ue                                       u{expert_cfg.ue, cell_cfg, ue_create_msg};
+
+  // Pass Reconfiguration to UE with an new Logical Channel.
+  sched_ue_reconfiguration_message recfg{};
+  recfg.ue_index = ue_create_msg.ue_index;
+  recfg.crnti    = ue_create_msg.crnti;
+  recfg.cfg      = ue_create_msg.cfg;
+  recfg.cfg.lc_config_list.push_back(config_helpers::create_default_logical_channel_config(uint_to_lcid(4)));
+  u.handle_reconfiguration_request(recfg);
+  dl_buffer_state_indication_message ind{recfg.ue_index, recfg.crnti, uint_to_lcid(4), 0};
+
+  // Verify that DL buffer state indications affect newly active logical channels.
+  for (const auto& lc : recfg.cfg.lc_config_list) {
+    if (lc.lcid == uint_to_lcid(0)) {
+      // LCID0 is a special case.
+      continue;
+    }
+    ind.lcid = lc.lcid;
+    ind.bs   = 10;
+    u.handle_dl_buffer_state_indication(ind);
+    ASSERT_TRUE(u.pending_dl_newtx_bytes());
+    ind.bs = 0;
+    u.handle_dl_buffer_state_indication(ind);
+    ASSERT_FALSE(u.pending_dl_newtx_bytes());
+  }
+
+  // Verify that inactive logical channels do not affect pending bytes.
+  ind.lcid = uint_to_lcid(6);
+  ind.bs   = 10;
+  u.handle_dl_buffer_state_indication(ind);
+  ASSERT_FALSE(u.pending_dl_newtx_bytes());
 }
