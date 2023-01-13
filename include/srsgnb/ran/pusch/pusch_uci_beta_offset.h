22c50b0579d979eb74e8a97054e8f5cc37795d72
Carlo Galiotto <carlo@srs.io>
2023-01-13 17:00:21 +0100

    sched: move beta_offset mapper to new file
    Signed-off-by: Carlo Galiotto <carlo@srs.io>

---
+++ include/srsgnb/ran/pusch/pusch_uci_beta_offset.h
@@ -0,0 +1,35 @@
+/*
+ *
+ * Copyright 2013-2022 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#pragma once
+
+namespace srsgnb {
+
+/// \brief Maps the integer \c beta_offset value for HARQ-ACK reporting into the corresponding float value.
+///
+/// The integer \c beta_offset value is passed by the \c PUSCH-Config, TS 38.331, and the mapping into the corresponding
+/// float value is determined as per Table 9.3-1, TS 38.213.
+/// \param[in] beta_uint_val is the integer value as per \c BetaOffsets, TS 38.331.
+/// \remark The caller must ensure \c beta_uint_val < 16, so as not to incur in reserved values, as per Table 9.3-1,
+/// TS 38.213.
+/// \return The corresponding float value as per Table 9.3-1, TS 38.213.
+float beta_harq_ack_to_float(unsigned beta_uint_val);
+
+/// \brief Maps the integer \c beta_offset value for CSI reporting into the corresponding float value.
+///
+/// The integer \c beta_offset value is passed by the \c PUSCH-Config, TS 38.331, and the mapping into the corresponding
+/// float value is determined as per Table 9.3-2, TS 38.213.
+/// \param[in] beta_uint_val is the integer value as per \c BetaOffsets, TS 38.331.
+/// \remark The caller must ensure \c beta_uint_val < 19, so as not to incur in reserved values, as per Table 9.3-2,
+/// TS 38.213.
+/// \return The corresponding float value as per Table 9.3-2, TS 38.213.
+float beta_csi_to_float(unsigned beta_uint_val);
+
+} // namespace srsgnb
