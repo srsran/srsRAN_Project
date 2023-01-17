c86adb3fd067a10ddaaaf893ce7887b7c2224b1c
Fabian Eckermann <fabian@srs.io>
2023-01-17 17:11:08 +0100

    cu_cp,f1ap: Fix f1ap ue context indexing

--- include/srsgnb/f1ap/common/f1ap_types.h
+++ include/srsgnb/f1ap/common/f1ap_types.h
@@ -18,11 +18,8 @@ namespace srsgnb {
 
 /// \brief GNB-CU-UE-F1AP-ID used to identify the UE in the F1AP-CU.
 /// \remark See TS 38.473 Section 9.3.1.4: GNB-CU-UE-F1AP-ID valid values: (0..2^32-1)
-enum class gnb_cu_ue_f1ap_id_t : uint64_t {
-  min     = 0,
-  max     = std::numeric_limits<uint32_t>::max(),
-  invalid = 0x1ffffffff
-};
+constexpr static uint64_t MAX_NOF_CU_F1AP_UES = ((uint64_t)1 << 32);
+enum class gnb_cu_ue_f1ap_id_t : uint64_t { min = 0, max = MAX_NOF_CU_F1AP_UES - 1, invalid = 0x1ffffffff };
 
 constexpr inline uint64_t gnb_cu_ue_f1ap_id_to_uint(gnb_cu_ue_f1ap_id_t id)
 {
@@ -30,18 +27,15 @@ constexpr inline uint64_t gnb_cu_ue_f1ap_id_to_uint(gnb_cu_ue_f1ap_id_t id)
 }
 
 /// Convert integer to GNB-CU-UE-F1AP-ID type.
-constexpr inline gnb_cu_ue_f1ap_id_t int_to_gnb_cu_ue_f1ap_id(std::underlying_type_t<gnb_cu_ue_f1ap_id_t> idx)
+constexpr inline gnb_cu_ue_f1ap_id_t int_to_gnb_cu_ue_f1ap_id(uint64_t idx)
 {
   return static_cast<gnb_cu_ue_f1ap_id_t>(idx);
 }
 
 /// \brief GNB-DU-UE-F1AP-ID used to identify the UE in the F1AP-DU.
 /// \remark See TS 38.473 Section 9.3.1.5: GNB-DU-UE-F1AP-ID valid values: (0..2^32-1)
-enum class gnb_du_ue_f1ap_id_t : uint64_t {
-  min     = 0,
-  max     = std::numeric_limits<uint32_t>::max(),
-  invalid = 0x1ffffffff
-};
+constexpr static uint64_t MAX_NOF_DU_F1AP_UES = ((uint64_t)1 << 32);
+enum class gnb_du_ue_f1ap_id_t : uint64_t { min = 0, max = MAX_NOF_DU_F1AP_UES - 1, invalid = 0x1ffffffff };
 
 constexpr inline uint64_t gnb_du_ue_f1ap_id_to_uint(gnb_du_ue_f1ap_id_t id)
 {
@@ -49,7 +43,7 @@ constexpr inline uint64_t gnb_du_ue_f1ap_id_to_uint(gnb_du_ue_f1ap_id_t id)
 }
 
 /// Convert integer to GNB-DU-UE-F1AP-ID type.
-constexpr inline gnb_du_ue_f1ap_id_t int_to_gnb_du_ue_f1ap_id(std::underlying_type_t<gnb_du_ue_f1ap_id_t> idx)
+constexpr inline gnb_du_ue_f1ap_id_t int_to_gnb_du_ue_f1ap_id(uint64_t idx)
 {
   return static_cast<gnb_du_ue_f1ap_id_t>(idx);
 }
--- lib/f1ap/cu_cp/ue_context/f1ap_cu_ue_context.h
+++ lib/f1ap/cu_cp/ue_context/f1ap_cu_ue_context.h
@@ -13,6 +13,7 @@
 #include "f1ap_cu_ue_transaction_manager.h"
 #include "srsgnb/adt/slotted_array.h"
 #include "srsgnb/f1ap/cu_cp/f1ap_cu.h"
+#include <unordered_map>
 
 namespace srsgnb {
 namespace srs_cu_cp {
@@ -36,48 +37,59 @@ class f1ap_ue_context_list
 public:
   f1ap_ue_context_list(timer_manager& timers_) : timers(timers_) {}
 
-  bool contains(gnb_cu_ue_f1ap_id_t cu_ue_id) const { return ues.contains(gnb_cu_ue_f1ap_id_to_uint(cu_ue_id)); }
+  bool contains(gnb_cu_ue_f1ap_id_t cu_ue_id) const { return ues.find(cu_ue_id) != ues.end(); }
 
   /// \brief Checks whether a UE with the given UE index exists.
   /// \param[in] ue_index The UE index used to find the UE.
   /// \return The CU UE ID.
-  bool contains(ue_index_t ue_idx) const
-  {
-    return std::any_of(ues.begin(), ues.end(), [ue_idx](const f1ap_ue_context& u) { return u.ue_index == ue_idx; });
-  }
+  bool contains(ue_index_t ue_idx) const { return ue_index_to_ue_f1ap_id.find(ue_idx) != ue_index_to_ue_f1ap_id.end(); }
 
-  f1ap_ue_context&       operator[](gnb_cu_ue_f1ap_id_t cu_ue_id) { return ues[gnb_cu_ue_f1ap_id_to_uint(cu_ue_id)]; }
-  const f1ap_ue_context& operator[](gnb_cu_ue_f1ap_id_t cu_ue_id) const
-  {
-    return ues[gnb_cu_ue_f1ap_id_to_uint(cu_ue_id)];
-  }
-  f1ap_ue_context& operator[](ue_index_t ue_idx)
-  {
-    auto it = std::find_if(ues.begin(), ues.end(), [ue_idx](const f1ap_ue_context& u) { return u.ue_index == ue_idx; });
-    return *it;
-  }
+  f1ap_ue_context& operator[](gnb_cu_ue_f1ap_id_t cu_ue_id) { return ues.at(cu_ue_id); }
+  f1ap_ue_context& operator[](ue_index_t ue_idx) { return ues.at(ue_index_to_ue_f1ap_id.at(ue_idx)); }
 
   f1ap_ue_context& add_ue(ue_index_t ue_idx, gnb_cu_ue_f1ap_id_t cu_ue_id)
   {
-    ues.emplace(gnb_cu_ue_f1ap_id_to_uint(cu_ue_id), ue_idx, cu_ue_id, timers);
-    return ues[gnb_cu_ue_f1ap_id_to_uint(cu_ue_id)];
+    ues.emplace(
+        std::piecewise_construct, std::forward_as_tuple(cu_ue_id), std::forward_as_tuple(ue_idx, cu_ue_id, timers));
+    ue_index_to_ue_f1ap_id.emplace(ue_idx, cu_ue_id);
+    return ues.at(cu_ue_id);
   }
 
-  void remove_ue(gnb_cu_ue_f1ap_id_t cu_ue_id) { ues.erase(gnb_cu_ue_f1ap_id_to_uint(cu_ue_id)); }
+  void remove_ue(gnb_cu_ue_f1ap_id_t cu_ue_id)
+  {
+    ue_index_to_ue_f1ap_id.erase(ues.at(cu_ue_id).ue_index);
+    ues.erase(cu_ue_id);
+  }
 
   size_t size() const { return ues.size(); }
 
   /// \brief Get the next available GNB-CU-F1AP-UE-ID.
   gnb_cu_ue_f1ap_id_t next_gnb_cu_ue_f1ap_id() const
   {
-    unsigned i = ues.find_first_empty(0);
-    return i == MAX_NOF_UES ? gnb_cu_ue_f1ap_id_t::invalid : int_to_gnb_cu_ue_f1ap_id(i);
+    if (ue_index_to_ue_f1ap_id.size() == MAX_NOF_UES) {
+      return gnb_cu_ue_f1ap_id_t::invalid;
+    }
+
+    for (unsigned it = 0; it < gnb_cu_ue_f1ap_id_to_uint(gnb_cu_ue_f1ap_id_t::max); it++) {
+      // Only iterate over ue_index_to_ue_f1ap_id (size=MAX NOF UEs)
+      // to avoid iterating over all possible values of gnb_cu_ue_f1ap_id_t (size=2^32-1)
+      auto it2 = std::find_if(ue_index_to_ue_f1ap_id.begin(), ue_index_to_ue_f1ap_id.end(), [it](auto& u) {
+        return u.second == int_to_gnb_cu_ue_f1ap_id(it);
+      });
+
+      if (it2 == ue_index_to_ue_f1ap_id.end()) {
+        return int_to_gnb_cu_ue_f1ap_id(it);
+      }
+    }
+
+    return gnb_cu_ue_f1ap_id_t::invalid;
   }
 
 private:
   timer_manager& timers;
 
-  slotted_array<f1ap_ue_context, MAX_NOF_UES> ues; // indexed by gnb_cu_ue_f1ap_id
+  std::unordered_map<gnb_cu_ue_f1ap_id_t, f1ap_ue_context> ues;                    // indexed by gnb_cu_ue_f1ap_id
+  std::unordered_map<ue_index_t, gnb_cu_ue_f1ap_id_t>      ue_index_to_ue_f1ap_id; // indexed by ue_index
 };
 
 } // namespace srs_cu_cp
