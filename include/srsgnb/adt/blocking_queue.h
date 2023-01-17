c25afb03174d03dd953ce1832fb5f952f376abdc
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2023-01-17 15:06:33 +0100

    adt: improve documentation of blocking_queue

--- include/srsgnb/adt/blocking_queue.h
+++ include/srsgnb/adt/blocking_queue.h
@@ -34,6 +34,9 @@ class base_blocking_queue
   using T = typename CircBuffer::value_type;
 
 public:
+  /// \brief Creates a blocking_queue.
+  /// \param push_func_ Callable to be called on every inserted element.
+  /// \param pop_func_ Callable to be called on every popped element.
   template <typename... Args>
   base_blocking_queue(PushingFunc push_func_, PoppingFunc pop_func_, Args&&... args) :
     push_func(push_func_), pop_func(pop_func_), circ_buffer(std::forward<Args>(args)...)
@@ -44,6 +47,7 @@ public:
   base_blocking_queue& operator=(const base_blocking_queue&) = delete;
   base_blocking_queue& operator=(base_blocking_queue&&)      = delete;
 
+  /// \brief Sets queue state to "stopped" and awake any threads currently blocked waiting (either pushing or popping).
   void stop()
   {
     std::unique_lock<std::mutex> lock(mutex);
@@ -65,30 +69,72 @@ public:
     }
   }
 
-  bool          try_push(const T& t) { return push_(t, false); }
+  /// \brief Try to push new element to queue.
+  /// \return If the queue is full or inactive, returns false. Otherwise, it returns true.
+  bool try_push(const T& t) { return push_(t, false); }
+
+  /// \brief Try to push new r-value element to queue.
+  /// \return If the queue is full or inactive, returns error type storing the element that failed to be pushed.
+  /// Otherwise, it returns success type.
   error_type<T> try_push(T&& t) { return push_(std::move(t), false); }
+
+  /// \brief Tries to push all elements in a range into the queue.
+  /// \return Returns number of inserted elements.
   template <typename It>
   unsigned try_push(It b, It e)
   {
     return push_(b, e, false);
   }
-  unsigned      try_push(span<T> t) { return try_push(t.begin(), t.end()); }
-  bool          push_blocking(const T& t) { return push_(t, true); }
+
+  /// \brief Tries to push all elements in a span into the queue.
+  /// \return Returns number of inserted elements.
+  unsigned try_push(span<T> t) { return try_push(t.begin(), t.end()); }
+
+  /// \brief Pushes an element into the queue. If the queue is full, this call *blocks* waiting for another thread
+  /// to pop an element from the queue or set the queue as inactive.
+  /// \return Returns true if element was pushed. False, otherwise.
+  bool push_blocking(const T& t) { return push_(t, true); }
+
+  /// \brief Pushes an r-value element into the queue. If the queue is full, this call *blocks* waiting for another
+  /// thread to pop an element from the queue or set the queue as inactive.
+  /// \return If the queue is inactive, returns error type storing the element that failed to be pushed.
+  /// Otherwise, it returns success type.
   error_type<T> push_blocking(T&& t) { return push_(std::move(t), true); }
+
+  /// \brief Pushes all elements in a range into the queue. If the queue becomes full, this call *blocks* waiting
+  /// for space to become available or the queue to become inactive.
+  /// \return Returns number of inserted elements.
   template <typename It>
   unsigned push_blocking(It b, It e)
   {
     return push_(b, e, true);
   }
+
+  /// \brief Pushes all elements in a span into the queue. If the queue becomes full, this call *blocks* waiting
+  /// for space to become available or the queue to become inactive.
+  /// \return Returns number of inserted elements.
   unsigned push_blocking(span<T> t) { return push_blocking(t.begin(), t.end()); }
-  bool     try_pop(T& obj) { return pop_(obj, false); }
+
+  /// \brief Tries to pop one object from the queue.
+  /// \return If queue is empty or inactive, returns false. Otherwise, returns true.
+  bool try_pop(T& obj) { return pop_(obj, false); }
+
+  /// \brief Tries to pop a range of elements from the queue.
+  /// \return Number of popped elements.
   template <typename It>
   unsigned try_pop(It b, It e)
   {
     return pop_(b, e, false);
   }
+
+  /// \brief Tries to pop a range of elements from the queue into a span.
+  /// \return Number of popped elements.
   unsigned try_pop(span<T> t) { return try_pop(t.begin(), t.end()); }
-  T        pop_blocking(bool* success = nullptr)
+
+  /// \brief Pops element from the queue. If queue is empty, this call *blocks* waiting for another thread to push an
+  /// new element to the queue or that the queue is set to inactive.
+  /// \return Popped element.
+  T pop_blocking(bool* success = nullptr)
   {
     T    obj{};
     bool ret = pop_(obj, true);
@@ -97,13 +143,33 @@ public:
     }
     return obj;
   }
+
+  /// \brief Pops a range of elements from the queue. If queue is empty, this call *blocks* waiting for another thread
+  /// to push a new element to the queue or that the queue is set to inactive.
+  /// \param[out] b First element that is to be popped from the queue.
+  /// \param[out] e End of the range of elements to be popped from the queue.
+  /// \return Number of popped elements. This number must be lower than std::distance(b, e).
   template <typename It>
   unsigned pop_blocking(It b, It e)
   {
     return pop_(b, e, true);
   }
+
+  /// \brief Pops a span of elements from the queue. If queue is empty, this call *blocks* waiting for another thread
+  /// to push a new element to the queue or that the queue is set to inactive.
+  /// \param[out] t Span of elements popped from the queue. The size of this span defines the maximum number of elements
+  /// to be popped.
+  /// \return Number of actually popped elements.
   unsigned pop_blocking(span<T> t) { return pop_blocking(t.begin(), t.end()); }
+
+  /// \brief Pops an element from the queue. If the queue is empty, this call *blocks* waiting an element to be pushed
+  /// to the queue or that the queue becomes inactive. This wait is bounded by \c until.
+  /// \param[out] obj Popped element.
+  /// \param[in] until Maximum time point to wait for a pushed element.
+  /// \return True if element was successfully popped. False, otherwise.
   bool pop_wait_until(T& obj, const std::chrono::steady_clock::time_point& until) { return pop_(obj, true, &until); }
+
+  /// \brief Clear all elements of the queue.
   void clear()
   {
     T obj;
@@ -111,31 +177,42 @@ public:
     }
   }
 
+  /// \brief Returns the number of elements stored in the queue.
   size_t size() const
   {
     std::lock_guard<std::mutex> lock(mutex);
     return circ_buffer.size();
   }
+
+  /// \brief Checks whether the queue is empty.
   bool empty() const
   {
     std::lock_guard<std::mutex> lock(mutex);
     return circ_buffer.empty();
   }
+
+  /// \brief Checks whether the queue is full.
   bool full() const
   {
     std::lock_guard<std::mutex> lock(mutex);
     return circ_buffer.full();
   }
+
+  /// \brief Checks the maximum number of elements of the queue.
   size_t max_size() const
   {
     std::lock_guard<std::mutex> lock(mutex);
     return circ_buffer.max_size();
   }
+
+  /// \brief Checks whether the queue is inactive.
   bool is_stopped() const
   {
     std::lock_guard<std::mutex> lock(mutex);
     return not active;
   }
+
+  /// \brief Apply provided callable on first position of the queue.
   template <typename F>
   bool try_call_on_front(const F& f)
   {
@@ -147,6 +224,7 @@ public:
     return false;
   }
 
+  /// \brief Apply provided callable on elements of the queue, until the callable returns false.
   template <typename F>
   bool apply_first(const F& func)
   {
