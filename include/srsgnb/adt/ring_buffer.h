585cf636c0a95b9a952d3b1eff70657627eb5673
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2023-01-17 14:45:29 +0100

    adt: refactor ring_buffer_impl template interface and other minor refactors

--- include/srsgnb/adt/ring_buffer.h
+++ include/srsgnb/adt/ring_buffer.h
@@ -24,6 +24,19 @@ namespace srsgnb {
 
 namespace detail {
 
+template <typename Container>
+struct rebind_buffer_storage;
+
+template <typename T>
+struct rebind_buffer_storage<std::vector<T>> {
+  using type = std::vector<type_storage<T>>;
+};
+
+template <typename T, size_t N>
+struct rebind_buffer_storage<std::array<T, N>> {
+  using type = std::array<type_storage<T>, N>;
+};
+
 template <typename Container, bool ForcePower2Size>
 class ring_buffer_storage;
 
@@ -37,44 +50,45 @@ class ring_buffer_storage<std::array<T, N>, ForcePower2Size>
 
 public:
   constexpr ring_buffer_storage() = default;
-  ring_buffer_storage(size_t /**/, size_t rpos_, size_t count_) : rpos(rpos_), count(count_) {}
+  ring_buffer_storage(size_t /**/, unsigned rpos_, unsigned count_) : rpos(rpos_), count(count_) {}
   ring_buffer_storage(ring_buffer_storage&& other) noexcept :
     rpos(std::exchange(other.rpos, 0)), count(std::exchange(other.count, 0))
   {
     static_assert(std::is_move_constructible<T>::value, "T must be move-constructible");
-    for (size_t i = 0; i != count; ++i) {
-      size_t idx = advance_pos_impl(rpos, i);
-      buffer[idx].emplace(std::move(other.buffer[idx].get()));
-      other.buffer[idx].destroy();
+    for (unsigned i = 0; i != count; ++i) {
+      unsigned idx = advance_pos(rpos, i);
+      data[idx].emplace(std::move(other.data[idx].get()));
+      other.data[idx].destroy();
     }
   }
   ring_buffer_storage& operator=(ring_buffer_storage&& other) noexcept
   {
-    for (size_t i = 0; i != count; ++i) {
-      buffer[advance_pos_impl(rpos, i)].destroy();
+    for (unsigned i = 0; i != count; ++i) {
+      data[advance_pos(rpos, i)].destroy();
     }
     rpos  = std::exchange(other.rpos, 0);
     count = std::exchange(other.count, 0);
-    for (size_t i = 0; i < count; ++i) {
-      size_t idx = advance_pos_impl(rpos, i);
-      buffer[idx].emplace(std::move(other.buffer[idx].get()));
-      other.buffer[idx].destroy();
+    for (unsigned i = 0; i < count; ++i) {
+      unsigned idx = advance_pos(rpos, i);
+      data[idx].emplace(std::move(other.data[idx].get()));
+      other.data[idx].destroy();
     }
     return *this;
   }
 
   static constexpr size_t max_size() noexcept { return N; }
 
-  static unsigned advance_pos_impl(unsigned pos, unsigned n) noexcept { return (pos + n) % max_size(); }
+  /// \brief Advance \c pos by \c n positions accounting for wrap-around.
+  static unsigned advance_pos(unsigned pos, unsigned n) noexcept { return (pos + n) % max_size(); }
 
-  void set_size(size_t sz)
+  void set_size(unsigned sz)
   {
     // do nothing.
   }
 
-  size_t           rpos  = 0; ///< Reading position
-  size_t           count = 0; ///< Number of elements in the buffer
-  std::array<T, N> buffer;
+  unsigned         rpos  = 0; ///< Reading position
+  unsigned         count = 0; ///< Number of elements in the buffer
+  std::array<T, N> data;
 };
 
 /// \brief Specialization of internal ring_buffer data storage for the case a std::vector is used.
@@ -83,20 +97,22 @@ template <typename T>
 class ring_buffer_storage<std::vector<T>, false>
 {
 public:
-  explicit ring_buffer_storage(size_t sz = 0) : buffer(sz) {}
-  ring_buffer_storage(size_t max_size_, size_t rpos_, size_t count_) : rpos(rpos_), count(count_), buffer(max_size_) {}
+  explicit ring_buffer_storage(unsigned sz = 0) : data(sz) {}
+  ring_buffer_storage(size_t max_size_, unsigned rpos_, unsigned count_) : rpos(rpos_), count(count_), data(max_size_)
+  {
+  }
   ring_buffer_storage(ring_buffer_storage&& other) noexcept :
-    rpos(std::exchange(other.rpos, 0)), count(std::exchange(other.count, 0)), buffer(std::move(other.buffer))
+    rpos(std::exchange(other.rpos, 0)), count(std::exchange(other.count, 0)), data(std::move(other.data))
   {
   }
   ring_buffer_storage& operator=(ring_buffer_storage&& other) noexcept
   {
     for (unsigned i = 0; i != count; ++i) {
-      buffer[this->advance_pos_impl(rpos, i)].destroy();
+      data[this->advance_pos(rpos, i)].destroy();
     }
-    rpos   = std::exchange(other.rpos, 0);
-    count  = std::exchange(other.count, 0);
-    buffer = std::move(other.buffer);
+    rpos  = std::exchange(other.rpos, 0);
+    count = std::exchange(other.count, 0);
+    data  = std::move(other.data);
     return *this;
   }
 
@@ -104,22 +120,23 @@ public:
   {
     std::swap(rpos, other.rpos);
     std::swap(count, other.count);
-    std::swap(buffer, other.buffer);
+    std::swap(data, other.data);
   }
 
-  size_t max_size() const noexcept { return buffer.size(); }
+  size_t max_size() const noexcept { return data.size(); }
 
-  void set_size(size_t sz)
+  void set_size(unsigned sz)
   {
-    srsgnb_assert(count == 0 or sz == buffer.size(), "Dynamic resizes not supported when circular buffer is not empty");
-    buffer.resize(sz);
+    srsgnb_assert(count == 0 or sz == data.size(), "Dynamic resizes not supported when ring buffer is not empty");
+    data.resize(sz);
   }
 
-  unsigned advance_pos_impl(unsigned pos, unsigned n) const noexcept { return (pos + n) % max_size(); }
+  /// \brief Advance \c pos by \c n positions accounting for wrap-around.
+  unsigned advance_pos(unsigned pos, unsigned n) const noexcept { return (pos + n) % max_size(); }
 
-  size_t         rpos  = 0; ///< Reading position
-  size_t         count = 0; ///< Number of elements in the buffer
-  std::vector<T> buffer;    ///< Container where elements are stored
+  unsigned       rpos  = 0; ///< Reading position
+  unsigned       count = 0; ///< Number of elements in the buffer
+  std::vector<T> data;      ///< Container where elements are stored
 };
 
 /// \brief Specialization of internal ring_buffer data storage for the case a std::vector is used.
@@ -130,24 +147,26 @@ class ring_buffer_storage<std::vector<T>, true> : private ring_buffer_storage<st
 {
   using super_type = ring_buffer_storage<std::vector<T>, false>;
 
-  static size_t to_next_pow2(size_t sz) { return pow(2, ceil(log(sz) / log(2))); }
+  static unsigned to_next_pow2(unsigned sz) { return pow(2, ceil(log(sz) / log(2))); }
 
 public:
-  using super_type::buffer;
   using super_type::count;
+  using super_type::data;
   using super_type::max_size;
   using super_type::rpos;
   using super_type::super_type;
   using super_type::swap;
-  explicit ring_buffer_storage(size_t sz) : super_type(to_next_pow2(sz)) {}
+  explicit ring_buffer_storage(unsigned sz) : super_type(to_next_pow2(sz)) {}
 
-  void set_size(size_t sz)
+  void set_size(unsigned sz)
   {
     // Round up to closest power of 2.
     super_type::set_size(to_next_pow2(sz));
   }
 
-  unsigned advance_pos_impl(unsigned pos, unsigned n) const noexcept
+  /// \brief Advance \c pos by \c n positions accounting for wrap-around. Leverages the fact that max_size() is a power
+  /// of 2.
+  unsigned advance_pos(unsigned pos, unsigned n) const noexcept
   {
     // x mod y == x * (y - 1), when y is power of 2.
     return (pos + n) & (this->max_size() - 1);
@@ -160,22 +179,27 @@ public:
 /// - it provides an iterator interface to iterate over added elements in the buffer
 /// - not thread-safe.
 /// \tparam Container underlying container type used as buffer (e.g. std::array<T, N> or std::vector<T>).
-template <typename Container, bool ForcePower2Size>
-class base_ring_buffer
+template <typename T, typename Container, bool ForcePower2Size>
+class ring_buffer_impl
 {
-  using buffer_type = ring_buffer_storage<Container, ForcePower2Size>;
-  using T           = typename Container::value_type::value_type;
+  static_assert(std::is_same<T, typename Container::value_type>::value, "Invalid container value_type");
+  static_assert(std::is_same<Container, std::vector<T>>::value or is_std_array<Container>::value,
+                "Invalid container type. Only std::vector and std::array are supported");
+
+  constexpr static bool has_static_size = is_std_array<Container>::value;
+
+  using raw_container    = typename detail::rebind_buffer_storage<Container>::type;
+  using buffer_type      = ring_buffer_storage<raw_container, ForcePower2Size>;
+  using ring_buffer_type = ring_buffer_impl<T, Container, ForcePower2Size>;
 
   /// Iterator implementation.
   /// \tparam DataType can be T or const T.
   template <typename DataType>
   class iterator_impl
   {
-    using parent_type = std::conditional_t<std::is_same<DataType, T>::value,
-                                           base_ring_buffer<Container, ForcePower2Size>,
-                                           const base_ring_buffer<Container, ForcePower2Size>>;
+    using parent_type = std::conditional_t<std::is_same<DataType, T>::value, ring_buffer_type, const ring_buffer_type>;
 
-    iterator_impl(parent_type& parent_, size_t ring_idx_) : parent(&parent_), ring_idx(ring_idx_)
+    iterator_impl(parent_type& parent_, unsigned ring_idx_) : parent(&parent_), ring_idx(ring_idx_)
     {
       srsgnb_assert(ring_idx <= parent->size(), "Invalid iterator position");
     }
@@ -226,7 +250,7 @@ class base_ring_buffer
     difference_type operator-(const iterator_impl<DataType>& other) const { return ring_idx - other.ring_idx; }
 
   private:
-    friend class base_ring_buffer<Container, ForcePower2Size>;
+    friend class ring_buffer_impl<T, Container, ForcePower2Size>;
 
     void assert_idx_within_bounds()
     {
@@ -256,12 +280,12 @@ class base_ring_buffer
     reference get()
     {
       assert_idx_within_bounds();
-      return parent->buffer.buffer[parent->buffer.advance_pos_impl(parent->buffer.rpos, ring_idx)].get();
+      return parent->buffer.data[parent->buffer.advance_pos(parent->buffer.rpos, ring_idx)].get();
     }
     reference get() const
     {
       assert_idx_within_bounds();
-      return parent->buffer.buffer[parent->buffer.advance_pos_impl(parent->buffer.rpos, ring_idx)].get();
+      return parent->buffer.data[parent->buffer.advance_pos(parent->buffer.rpos, ring_idx)].get();
     }
 
     parent_type* parent;
@@ -269,45 +293,49 @@ class base_ring_buffer
   };
 
 public:
-  using index_type      = std::size_t;
+  using index_type      = unsigned;
   using value_type      = std::remove_const_t<T>;
   using difference_type = typename Container::difference_type;
-  using size_type       = std::size_t;
+  using size_type       = unsigned;
 
   static constexpr bool max_size_is_power_of_2 = ForcePower2Size;
 
   using iterator       = iterator_impl<T>;
   using const_iterator = iterator_impl<const T>;
 
-  constexpr base_ring_buffer() = default;
-  ~base_ring_buffer() { clear(); }
-  explicit base_ring_buffer(size_type sz) : buffer(sz) {}
-  base_ring_buffer(const base_ring_buffer& other) noexcept :
+  constexpr ring_buffer_impl() = default;
+  ~ring_buffer_impl() { clear(); }
+  template <typename Dummy                                                                                  = T,
+            std::enable_if_t<not ring_buffer_impl<Dummy, Container, ForcePower2Size>::has_static_size, int> = 0>
+  explicit ring_buffer_impl(size_type sz) : buffer(sz)
+  {
+  }
+  ring_buffer_impl(const ring_buffer_impl& other) noexcept :
     buffer(other.max_size(), other.buffer.rpos, other.buffer.count)
   {
     static_assert(std::is_copy_constructible<T>::value, "T must be copy-constructible");
     for (unsigned i = 0; i != buffer.count; ++i) {
-      unsigned n = buffer.advance_pos_impl(buffer.rpos, i);
-      buffer.buffer[n].emplace(other.buffer.buffer[n].get());
+      unsigned n = buffer.advance_pos(buffer.rpos, i);
+      buffer.data[n].emplace(other.buffer.data[n].get());
     }
   }
-  base_ring_buffer(base_ring_buffer&& other) noexcept = default;
-  base_ring_buffer& operator=(const base_ring_buffer& other) noexcept
+  ring_buffer_impl(ring_buffer_impl&& other) noexcept = default;
+  ring_buffer_impl& operator=(const ring_buffer_impl& other) noexcept
   {
     static_assert(std::is_copy_constructible<T>::value, "T must be copy-constructible");
     if (this != &other) {
       clear();
-      buffer.set_size(other.buffer.buffer.size());
+      buffer.set_size(other.buffer.data.size());
       buffer.rpos  = other.buffer.rpos;
       buffer.count = other.buffer.count;
       for (unsigned i = 0; i != buffer.count; ++i) {
-        unsigned n = buffer.advance_pos_impl(buffer.rpos, i);
-        buffer.buffer[n].emplace(other.buffer.buffer[n].get());
+        unsigned n = buffer.advance_pos(buffer.rpos, i);
+        buffer.data[n].emplace(other.buffer.data[n].get());
       }
     }
     return *this;
   }
-  base_ring_buffer& operator=(base_ring_buffer&& other) noexcept = default;
+  ring_buffer_impl& operator=(ring_buffer_impl&& other) noexcept = default;
 
   /// \brief Checks the maximum number of elements that can be stored in the ring_buffer.
   constexpr size_t max_size() const noexcept { return buffer.max_size(); }
@@ -319,14 +347,21 @@ public:
   constexpr bool empty() const noexcept { return buffer.count == 0; }
 
   /// \brief Checks the number of elements currently stored in the ring_buffer.
-  constexpr size_t size() const noexcept { return buffer.count; }
+  constexpr size_type size() const noexcept { return buffer.count; }
+
+  template <typename Dummy                                                                                  = T,
+            std::enable_if_t<not ring_buffer_impl<Dummy, Container, ForcePower2Size>::has_static_size, int> = 0>
+  void set_size(size_type sz) noexcept
+  {
+    buffer.set_size(sz);
+  }
 
   /// \brief Push an element to the ring buffer. Asserts if ring_buffer is full.
   template <typename U, std::enable_if_t<std::is_convertible<U, T>::value, int> = 0>
   void push(U&& u) noexcept
   {
     srsgnb_assert(not full(), "Circular buffer is full.");
-    buffer.buffer[get_wpos()].emplace(std::forward<U>(u));
+    buffer.data[get_wpos()].emplace(std::forward<U>(u));
     buffer.count++;
   }
 
@@ -379,8 +414,8 @@ public:
   void pop()
   {
     srsgnb_assert(not empty(), "Cannot call pop() in empty circular buffer");
-    buffer.buffer[buffer.rpos].destroy();
-    buffer.rpos = buffer.advance_pos_impl(buffer.rpos, 1);
+    buffer.data[buffer.rpos].destroy();
+    buffer.rpos = buffer.advance_pos(buffer.rpos, 1);
     buffer.count--;
   }
 
@@ -394,7 +429,7 @@ public:
       if (empty()) {
         break;
       }
-      *it = std::move(buffer.buffer[buffer.rpos].get());
+      *it = std::move(buffer.data[buffer.rpos].get());
       pop();
     }
     return orig_size - size();
@@ -408,19 +443,19 @@ public:
   T& top()
   {
     srsgnb_assert(not empty(), "Cannot call top() in empty circular buffer");
-    return buffer.buffer[buffer.rpos].get();
+    return buffer.data[buffer.rpos].get();
   }
   const T& top() const
   {
     srsgnb_assert(not empty(), "Cannot call top() in empty circular buffer");
-    return buffer.buffer[buffer.rpos].get();
+    return buffer.data[buffer.rpos].get();
   }
 
   /// \brief Clears the content of the ring_buffer.
   void clear()
   {
     for (unsigned i = 0; i != buffer.count; ++i) {
-      buffer.buffer[buffer.advance_pos_impl(buffer.rpos, i)].destroy();
+      buffer.data[buffer.advance_pos(buffer.rpos, i)].destroy();
     }
     buffer.rpos  = 0;
     buffer.count = 0;
@@ -430,12 +465,12 @@ public:
   T& operator[](index_type i)
   {
     srsgnb_assert(i < size(), "Out-of-bounds access to circular buffer ({} >= {})", i, buffer.count);
-    return buffer.buffer[buffer.advance_pos_impl(buffer.rpos, i)].get();
+    return buffer.data[buffer.advance_pos(buffer.rpos, i)].get();
   }
   const T& operator[](index_type i) const
   {
     srsgnb_assert(i < size(), "Out-of-bounds access to circular buffer ({} >= {})", i, buffer.count);
-    return buffer.buffer[buffer.advance_pos_impl(buffer.rpos, i)].get();
+    return buffer.data[buffer.advance_pos(buffer.rpos, i)].get();
   }
 
   iterator       begin() { return iterator(*this, 0); }
@@ -443,11 +478,11 @@ public:
   iterator       end() { return iterator(*this, buffer.count); }
   const_iterator end() const { return const_iterator(*this, buffer.count); }
 
-  bool operator==(const base_ring_buffer& other) const
+  bool operator==(const ring_buffer_impl& other) const
   {
     return std::equal(begin(), end(), other.begin(), other.end());
   }
-  bool operator!=(const base_ring_buffer& other) const { return not(*this == other); }
+  bool operator!=(const ring_buffer_impl& other) const { return not(*this == other); }
 
   /// \brief Apply predicate callable to elements until the predicate returns true.
   /// \param func Predicate functor.
@@ -464,12 +499,21 @@ public:
   }
 
 protected:
-  size_t get_wpos() const { return buffer.advance_pos_impl(buffer.rpos, buffer.count); }
+  size_type get_wpos() const { return buffer.advance_pos(buffer.rpos, buffer.count); }
 
   buffer_type buffer;
 };
 
 } // namespace detail
+
+/// Ring buffer with data storage via a std::vector<T>.
+/// - size can be defined at construction or via set_size.
+/// - not thread-safe.
+/// \tparam T value type stored by buffer
+/// \tparam RoundUpSizeToPowerOf2 Whether the ring buffer size gets round up to the next power of 2. When set to true,
+/// the compiler is able to avoid the % operation and use bitwise and instead.
+template <typename T, bool RoundUpSizeToPowerOf2 = true>
+using ring_buffer = detail::ring_buffer_impl<T, std::vector<T>, RoundUpSizeToPowerOf2>;
 
 /// \brief Ring buffer with fixed, embedded data storage via a std::array<T, N>. It can contain up to N elements.
 /// - Given that the buffer size is known at compile-time, the circular iteration over the buffer may be more optimized
@@ -478,28 +522,6 @@ protected:
 /// \tparam T value type stored by buffer.
 /// \tparam N Maximum capacity of the ring buffer.
 template <typename T, size_t N>
-class static_ring_buffer : public detail::base_ring_buffer<std::array<detail::type_storage<T>, N>, false>
-{
-  using super_type = detail::base_ring_buffer<std::array<detail::type_storage<T>, N>, false>;
-
-public:
-  using super_type::super_type;
-};
-
-/// Ring buffer with storage via a std::vector<T>.
-/// - size can be defined at start-time.
-/// - not thread-safe.
-/// \tparam T value type stored by buffer
-/// \tparam ForcePower2Size Whether the ring buffer size gets round up to the next power of 2.
-template <typename T, bool ForcePower2Size = false>
-class ring_buffer : public detail::base_ring_buffer<std::vector<detail::type_storage<T>>, ForcePower2Size>
-{
-  using super_type = detail::base_ring_buffer<std::vector<detail::type_storage<T>>, ForcePower2Size>;
-
-public:
-  using super_type::super_type;
-
-  void set_size(size_t sz) { this->buffer.set_size(sz); }
-};
+using static_ring_buffer = detail::ring_buffer_impl<T, std::array<T, N>, false>;
 
 } // namespace srsgnb
