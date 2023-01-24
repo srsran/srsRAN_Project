dcb4647ade30a412eb49027436702467c442442b
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2023-01-24 18:38:15 +0100

    support: implement os thread priority add and subtract operations

--- include/srsgnb/support/unique_thread.h
+++ include/srsgnb/support/unique_thread.h
@@ -27,31 +27,68 @@ struct os_thread_realtime_priority_tag {};
 class os_thread_realtime_priority
   : public strong_type<int, struct os_thread_realtime_priority_tag, strong_equality, strong_comparison>
 {
+  static int max_posix_prio()
+  {
+    // Subtract one to the priority offset to avoid scheduling threads with the highest priority that could contend with
+    // OS critical tasks.
+    static int value = sched_get_priority_max(SCHED_FIFO) - 1;
+    return value;
+  }
+  static int min_posix_prio()
+  {
+    static int value = sched_get_priority_min(SCHED_FIFO);
+    return value;
+  }
+
 public:
   /// Creates non-realtime thread priority
   os_thread_realtime_priority() : strong_type(0) {}
 
-  /// Creates realtime thread priority value based on priority.
-  /// \param[in] ratio Priority value between 0 (min) and 1 (max).
-  explicit os_thread_realtime_priority(float ratio) : strong_type(ratio_to_prio(ratio)) {}
-
-  static os_thread_realtime_priority max() { return os_thread_realtime_priority{1.0f}; }
-  static os_thread_realtime_priority min() { return os_thread_realtime_priority{0.0f}; }
+  /// Returns a strong-type for OS thread priority set to non-realtime.
   static os_thread_realtime_priority no_realtime() { return os_thread_realtime_priority{}; }
 
+  /// Returns a strong-type for OS thread priority set to max priority.
+  static os_thread_realtime_priority max() { return os_thread_realtime_priority{max_posix_prio()}; }
+
+  /// Returns a strong-type for OS thread priority set to min priority.
+  static os_thread_realtime_priority min() { return os_thread_realtime_priority{min_posix_prio()}; }
+
+  /// Increase thread priority by \c prio_diff.
+  os_thread_realtime_priority& operator+=(int prio_diff)
+  {
+    *this = os_thread_realtime_priority{value() + prio_diff};
+    return *this;
+  }
+  os_thread_realtime_priority operator+(int prio_diff) const
+  {
+    os_thread_realtime_priority ret{*this};
+    return ret += prio_diff;
+  }
+
+  /// Decrease thread priority by \c prio_diff.
+  os_thread_realtime_priority& operator-=(int prio_diff)
+  {
+    *this = os_thread_realtime_priority{value() - prio_diff};
+    return *this;
+  }
+  os_thread_realtime_priority operator-(int prio_diff) const
+  {
+    os_thread_realtime_priority ret{*this};
+    return ret -= prio_diff;
+  }
+
+  /// Returns Posix OS thread priority value within {0, ..., sched_get_priority_max(SCHED_FIFO)}.
   int native() const { return strong_type::value(); }
 
   int native_sched_policy() const { return *this == no_realtime() ? SCHED_OTHER : SCHED_FIFO; }
 
 private:
-  static int ratio_to_prio(float ratio)
+  /// Creates real-time thread priority value based on POSIX priority.
+  /// \param[in] posix_prio Posix priority value between sched_get_priority_min(SCHED_FIFO) and
+  /// sched_get_priority_max(SCHED_FIFO).
+  explicit os_thread_realtime_priority(int posix_prio) :
+    strong_type(std::max(min_posix_prio(), std::min(max_posix_prio(), posix_prio)))
   {
-    srsgnb_assert(ratio >= 0 and ratio <= 1, "Invalid percentage={}", ratio);
-    int min = sched_get_priority_min(SCHED_FIFO);
-    // Subtract one to the priority offset to avoid scheduling threads with the highest priority that could contend with
-    // OS critical tasks.
-    int max = sched_get_priority_max(SCHED_FIFO) - 1;
-    return (int)std::round(ratio * (max - min)) + min;
   }
 };
 
