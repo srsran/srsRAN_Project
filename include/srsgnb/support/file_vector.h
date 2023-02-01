49bce1fce692256de4ca74f4883ca83383910db1
Oriol Font-Bach <oriol@srs.io>
2023-02-01 11:32:10 +0000

    PHY: extends file_vector class to add write support, as required by FPGA testing

--- include/srsgnb/support/file_vector.h
+++ include/srsgnb/support/file_vector.h
@@ -10,28 +10,52 @@
 
 #pragma once
 
+#include "srsgnb/adt/span.h"
 #include "srsgnb/support/error_handling.h"
 #include <fstream>
 #include <vector>
 
 namespace srsgnb {
 
+/// \c file_vector open modes.
+enum class openmode {
+  /// Opens the file in read-only mode.
+  read_only,
+  /// Opens the file in write-only mode.
+  write_only,
+  /// \brief Opens the file in read-write mode.
+  ///
+  /// Any previous content of the file will be lost.
+  read_write
+};
+
 template <typename T>
+/// \brief Simple binary file input-output interface.
+///
+/// An object of this file allows reading and writing values of type \c T from and to a binary file.
 class file_vector
 {
 private:
   std::string file_name;
+  openmode    op;
 
 public:
-  // Constructs object. It does not perform any file operation.
-  file_vector(std::string filename) : file_name(std::move(filename)) {}
+  /// \brief Constructor.
+  ///
+  /// Sets the name and the mode (default is \e read-only) of the binary file. No operations are performed on the file.
+  file_vector(std::string filename, openmode op_ = openmode::read_only) : file_name(std::move(filename)), op(op_) {}
 
-  /// Get file name.
+  /// Returns the file name.
   const std::string& get_file_name() const { return file_name; }
 
-  // Open the file and appends in a \c std::vector<T> all the file entries.
+  /// \brief Reads the file.
+  ///
+  /// The contents of the file are dumped to a \c std::vector<T>. The file is closed when the operation is completed.
+  /// \warning An exception arises if the file cannot be opened or if the write-only open mode was specified at
+  /// creation.
   std::vector<T> read() const
   {
+    report_fatal_error_if_not(op != openmode::write_only, "The file_vector was created with write-only permissions.");
     std::ifstream binary_file(file_name, std::ios::in | std::ios::binary);
 
     report_fatal_error_if_not(binary_file.is_open(), "Error opening file '{}'. {}.", file_name, strerror(errno));
@@ -43,6 +67,23 @@ public:
     }
 
     return read_data;
+  }
+
+  /// \brief Writes data to the file.
+  ///
+  /// The sequence of \c T values viewed by \c write_data are written to the file.
+  /// If the specified file does not exist, it is created. If it exists, previous contents will be erased.
+  /// The file is closed when the operation is completed.
+  ///
+  /// \warning An exception arises if the file cannot be opened or if the read-only open mode was specified at creation.
+  void write(span<const T> write_data) const
+  {
+    report_fatal_error_if_not(op != openmode::read_only, "The file_vector was created with read-only permissions.");
+    std::ofstream binary_file(file_name, std::ios::out | std::ios::binary);
+
+    report_fatal_error_if_not(binary_file.is_open(), "Error opening file '{}'. {}.", file_name, strerror(errno));
+
+    binary_file.write(reinterpret_cast<const char*>(write_data.begin()), write_data.size() * sizeof(T));
   }
 };
 
