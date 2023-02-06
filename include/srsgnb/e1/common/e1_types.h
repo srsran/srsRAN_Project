808b5e4a1c17d3f95c976aca95ddaa2512f4fc2c
Fabian Eckermann <fabian@srs.io>
2023-02-06 10:51:28 +0100

    e1ap: Move common types to e1_types

--- include/srsgnb/e1/common/e1_types.h
+++ include/srsgnb/e1/common/e1_types.h
@@ -10,6 +10,15 @@
 
 #pragma once
 
+#include "srsgnb/adt/optional.h"
+#include "srsgnb/adt/slotted_array.h"
+#include "srsgnb/asn1/e1ap/e1ap.h"
+#include "srsgnb/ran/cause.h"
+#include "srsgnb/ran/cu_types.h"
+#include "srsgnb/ran/lcid.h"
+#include "srsgnb/ran/up_transport_layer_info.h"
+#include "srsgnb/rlc/rlc_config.h"
+#include "srsgnb/security/security.h"
 #include <cstdint>
 #include <limits>
 #include <type_traits>
@@ -47,5 +56,307 @@ constexpr inline gnb_cu_up_ue_e1ap_id_t int_to_gnb_cu_up_ue_e1ap_id(uint64_t idx
 {
   return static_cast<gnb_cu_up_ue_e1ap_id_t>(idx);
 }
+
+struct e1ap_security_ind {
+  std::string           integrity_protection_ind;
+  std::string           confidentiality_protection_ind;
+  optional<std::string> maximum_ipdatarate;
+};
+
+struct e1ap_cell_group_info_item {
+  uint8_t               cell_group_id;
+  optional<std::string> ul_cfg;
+  optional<std::string> dl_tx_stop;
+  optional<std::string> rat_type;
+};
+
+struct e1ap_packet_error_rate {
+  uint8_t per_scalar;
+  uint8_t per_exponent;
+};
+
+struct e1ap_dynamic_5qi_descriptor {
+  uint8_t                qos_prio_level;
+  uint16_t               packet_delay_budget;
+  e1ap_packet_error_rate packet_error_rate;
+  optional<uint16_t>     five_qi;
+  optional<std::string>  delay_crit;
+  optional<uint16_t>     averaging_win;
+  optional<uint16_t>     max_data_burst_volume;
+};
+
+struct e1ap_non_dynamic_5qi_descriptor {
+  uint16_t           five_qi;
+  optional<uint8_t>  qos_prio_level;
+  optional<uint16_t> averaging_win;
+  optional<uint16_t> max_data_burst_volume;
+};
+
+struct e1ap_qos_characteristics {
+  optional<e1ap_dynamic_5qi_descriptor>     dyn_5qi;
+  optional<e1ap_non_dynamic_5qi_descriptor> non_dyn_5qi;
+};
+
+struct e1ap_ng_ran_alloc_and_retention_prio {
+  uint8_t     prio_level;
+  std::string pre_emption_cap;
+  std::string pre_emption_vulnerability;
+};
+
+struct e1ap_gbr_qos_flow_info {
+  uint64_t           max_flow_bit_rate_dl;
+  uint64_t           max_flow_bit_rate_ul;
+  uint64_t           guaranteed_flow_bit_rate_dl;
+  uint64_t           guaranteed_flow_bit_rate_ul;
+  optional<uint16_t> max_packet_loss_rate_dl;
+  optional<uint16_t> max_packet_loss_rate_ul;
+};
+
+struct e1ap_qos_flow_level_qos_params {
+  e1ap_qos_characteristics             qos_characteristics;
+  e1ap_ng_ran_alloc_and_retention_prio ng_ran_alloc_retention_prio;
+  optional<e1ap_gbr_qos_flow_info>     gbr_qos_flow_info;
+  optional<std::string>                reflective_qos_attribute;
+  optional<std::string>                add_qos_info;
+  optional<uint8_t>                    paging_policy_ind;
+  optional<std::string>                reflective_qos_ind;
+};
+
+struct e1ap_qos_flow_qos_param_item {
+  qos_flow_id_t                  qos_flow_id = qos_flow_id_t::invalid;
+  e1ap_qos_flow_level_qos_params qos_flow_level_qos_params;
+  optional<std::string>          qos_flow_map_ind;
+};
+
+struct e1ap_qos_flow_map_item {
+  qos_flow_id_t         qos_flow_id = qos_flow_id_t::invalid;
+  optional<std::string> qos_flow_map_ind;
+};
+
+struct e1ap_data_forwarding_info_request {
+  std::string                                              data_forwarding_request;
+  slotted_id_vector<qos_flow_id_t, e1ap_qos_flow_map_item> qos_flows_forwarded_on_fwd_tunnels;
+};
+
+struct e1ap_pdcp_count {
+  uint32_t pdcp_sn;
+  uint64_t hfn;
+};
+
+struct e1ap_drb_status_transfer {
+  e1ap_pdcp_count    count_value;
+  optional<uint64_t> receive_statusof_pdcpsdu;
+};
+
+struct e1ap_pdcp_sn_status_info {
+  e1ap_drb_status_transfer pdcp_status_transfer_ul;
+  e1ap_pdcp_count          pdcp_status_transfer_dl;
+};
+
+struct e1ap_rohc {
+  uint16_t       max_cid;
+  uint16_t       rohc_profiles;
+  optional<bool> continue_rohc;
+};
+
+struct e1ap_rohc_params {
+  optional<e1ap_rohc> rohc;
+  optional<e1ap_rohc> ul_only_rohc;
+};
+
+struct e1ap_pdcp_config {
+  uint8_t                    pdcp_sn_size_ul;
+  uint8_t                    pdcp_sn_size_dl;
+  srsgnb::rlc_mode           rlc_mod;
+  optional<e1ap_rohc_params> rohc_params;
+  optional<uint16_t>         t_reordering_timer;
+  optional<int16_t>          discard_timer;
+  optional<int32_t>          ul_data_split_thres;
+  optional<bool>             pdcp_dupl;
+  optional<bool>             pdcp_reest;
+  optional<bool>             pdcp_data_recovery;
+  optional<std::string>      dupl_activation;
+  optional<bool>             out_of_order_delivery;
+};
+
+struct e1ap_drb_to_setup_item_ng_ran {
+  drb_id_t                                                       drb_id = drb_id_t::invalid;
+  sdap_config_t                                                  sdap_cfg;
+  e1ap_pdcp_config                                               pdcp_cfg;
+  std::vector<e1ap_cell_group_info_item>                         cell_group_info = {};
+  slotted_id_vector<qos_flow_id_t, e1ap_qos_flow_qos_param_item> qos_flow_info_to_be_setup;
+  optional<e1ap_data_forwarding_info_request>                    drb_data_forwarding_info_request;
+  optional<uint16_t>                                             drb_inactivity_timer;
+  optional<e1ap_pdcp_sn_status_info>                             pdcp_sn_status_info;
+};
+
+struct e1ap_pdu_session_res_to_setup_item {
+  pdu_session_id_t                                           pdu_session_id = pdu_session_id_t::invalid;
+  std::string                                                pdu_session_type;
+  s_nssai_t                                                  snssai;
+  up_transport_layer_info                                    ng_ul_up_tnl_info;
+  e1ap_security_ind                                          security_ind;
+  slotted_id_vector<drb_id_t, e1ap_drb_to_setup_item_ng_ran> drb_to_setup_list_ng_ran;
+
+  optional<uint64_t>                          pdu_session_res_dl_ambr;
+  optional<e1ap_data_forwarding_info_request> pdu_session_data_forwarding_info_request;
+  optional<uint16_t>                          pdu_session_inactivity_timer;
+  optional<up_transport_layer_info>           existing_allocated_ng_dl_up_tnl_info;
+  optional<uint16_t>                          network_instance;
+};
+
+struct e1ap_security_algorithm {
+  srsgnb::security::ciphering_algorithm           ciphering_algo;
+  optional<srsgnb::security::integrity_algorithm> integrity_protection_algorithm;
+};
+
+struct e1ap_up_security_key {
+  byte_buffer encryption_key;
+  byte_buffer integrity_protection_key;
+};
+
+struct e1ap_security_info {
+  e1ap_security_algorithm security_algorithm;
+  e1ap_up_security_key    up_security_key;
+};
+
+struct e1ap_up_params_item {
+  up_transport_layer_info up_tnl_info;
+  uint8_t                 cell_group_id;
+};
+
+struct e1ap_qos_flow_item {
+  qos_flow_id_t qos_flow_id = qos_flow_id_t::invalid;
+};
+
+struct e1ap_qos_flow_failed_item {
+  qos_flow_id_t qos_flow_id = qos_flow_id_t::invalid;
+  cause_t       cause;
+};
+
+struct e1ap_data_forwarding_info {
+  optional<up_transport_layer_info> ul_data_forwarding;
+  optional<up_transport_layer_info> dl_data_forwarding;
+};
+
+struct e1ap_drb_setup_item_ng_ran {
+  drb_id_t                                                    drb_id                 = drb_id_t::invalid;
+  std::vector<e1ap_up_params_item>                            ul_up_transport_params = {};
+  slotted_id_vector<qos_flow_id_t, e1ap_qos_flow_item>        flow_setup_list;
+  slotted_id_vector<qos_flow_id_t, e1ap_qos_flow_failed_item> flow_failed_list;
+  optional<e1ap_data_forwarding_info>                         drb_data_forwarding_info_resp;
+};
+
+struct e1ap_drb_failed_item_ng_ran {
+  drb_id_t drb_id = drb_id_t::invalid;
+  cause_t  cause;
+};
+
+struct e1ap_pdu_session_resource_setup_modification_item {
+  pdu_session_id_t                                         pdu_session_id = pdu_session_id_t::invalid;
+  up_transport_layer_info                                  ng_dl_up_tnl_info;
+  slotted_id_vector<drb_id_t, e1ap_drb_setup_item_ng_ran>  drb_setup_list_ng_ran;
+  slotted_id_vector<drb_id_t, e1ap_drb_failed_item_ng_ran> drb_failed_list_ng_ran;
+  optional<security_result_t>                              security_result;
+  optional<e1ap_data_forwarding_info>                      pdu_session_data_forwarding_info_resp;
+  optional<bool>                                           ng_dl_up_unchanged;
+};
+
+struct e1ap_pdu_session_resource_failed_item {
+  pdu_session_id_t pdu_session_id = pdu_session_id_t::invalid;
+  cause_t          cause;
+};
+
+struct e1ap_crit_diagnostics_item {
+  std::string iecrit;
+  uint32_t    ie_id;
+  std::string type_of_error;
+};
+
+struct e1ap_crit_diagnostics {
+  std::vector<e1ap_crit_diagnostics_item> ies_crit_diagnostics = {};
+  optional<uint16_t>                      proc_code;
+  optional<std::string>                   trigger_msg;
+  optional<std::string>                   proc_crit;
+  optional<uint16_t>                      transaction_id;
+};
+
+struct e1ap_drb_to_setup_mod_item_ng_ran {
+  drb_id_t                                                       drb_id = drb_id_t::invalid;
+  sdap_config_t                                                  sdap_cfg;
+  e1ap_pdcp_config                                               pdcp_cfg;
+  std::vector<e1ap_cell_group_info_item>                         cell_group_info = {};
+  slotted_id_vector<qos_flow_id_t, e1ap_qos_flow_qos_param_item> flow_map_info;
+  optional<e1ap_data_forwarding_info_request>                    drb_data_forwarding_info_request;
+  optional<uint16_t>                                             drb_inactivity_timer;
+  optional<e1ap_pdcp_sn_status_info>                             pdcp_sn_status_info;
+};
+
+struct e1ap_pdu_session_res_to_setup_mod_item {
+  pdu_session_id_t                                               pdu_session_id = pdu_session_id_t::invalid;
+  std::string                                                    pdu_session_type;
+  s_nssai_t                                                      snssai;
+  e1ap_security_ind                                              security_ind;
+  optional<uint64_t>                                             pdu_session_res_ambr;
+  up_transport_layer_info                                        ng_ul_up_tnl_info;
+  optional<e1ap_data_forwarding_info_request>                    pdu_session_data_forwarding_info_request;
+  optional<uint16_t>                                             pdu_session_inactivity_timer;
+  slotted_id_vector<drb_id_t, e1ap_drb_to_setup_mod_item_ng_ran> drb_to_setup_mod_list_ng_ran;
+};
+
+struct e1ap_drb_to_modify_item_ng_ran {
+  drb_id_t                                                       drb_id = drb_id_t::invalid;
+  optional<sdap_config_t>                                        sdap_cfg;
+  optional<pdcp_config_t>                                        pdcp_cfg;
+  optional<e1ap_data_forwarding_info>                            drb_data_forwarding_info;
+  optional<std::string>                                          pdcp_sn_status_request;
+  std::vector<e1ap_up_params_item>                               dl_up_params         = {};
+  std::vector<e1ap_cell_group_info_item>                         cell_group_to_add    = {};
+  std::vector<e1ap_cell_group_info_item>                         cell_group_to_modify = {};
+  std::vector<e1ap_cell_group_info_item>                         cell_group_to_rem    = {};
+  slotted_id_vector<qos_flow_id_t, e1ap_qos_flow_qos_param_item> flow_map_info;
+  optional<uint16_t>                                             drb_inactivity_timer;
+};
+
+struct e1ap_pdu_session_res_to_modify_item {
+  pdu_session_id_t                                            pdu_session_id = pdu_session_id_t::invalid;
+  optional<e1ap_security_ind>                                 security_ind;
+  optional<uint64_t>                                          pdu_session_res_dl_ambr;
+  optional<up_transport_layer_info>                           ng_ul_up_tnl_info;
+  optional<e1ap_data_forwarding_info_request>                 pdu_session_data_forwarding_info_request;
+  optional<e1ap_data_forwarding_info>                         pdu_session_data_forwarding_info;
+  optional<uint16_t>                                          pdu_session_inactivity_timer;
+  optional<uint16_t>                                          network_instance;
+  slotted_id_vector<drb_id_t, e1ap_drb_to_setup_item_ng_ran>  drb_to_setup_list_ng_ran;
+  slotted_id_vector<drb_id_t, e1ap_drb_to_modify_item_ng_ran> drb_to_modify_list_ng_ran;
+  std::vector<drb_id_t>                                       drb_to_rem_list_ng_ran = {};
+
+  slotted_id_vector<drb_id_t, e1ap_drb_to_setup_mod_item_ng_ran> drb_to_setup_mod_list_ng_ran;
+};
+
+struct e1ap_ng_ran_bearer_context_mod_request {
+  slotted_id_vector<pdu_session_id_t, e1ap_pdu_session_res_to_setup_mod_item> pdu_session_res_to_setup_mod_list;
+  slotted_id_vector<pdu_session_id_t, e1ap_pdu_session_res_to_modify_item>    pdu_session_res_to_modify_list;
+  std::vector<pdu_session_id_t>                                               pdu_session_res_to_rem_list = {};
+};
+
+struct e1ap_drb_modified_item_ng_ran {
+  drb_id_t                                                    drb_id                 = drb_id_t::invalid;
+  std::vector<e1ap_up_params_item>                            ul_up_transport_params = {};
+  slotted_id_vector<qos_flow_id_t, e1ap_qos_flow_item>        flow_setup_list;
+  slotted_id_vector<qos_flow_id_t, e1ap_qos_flow_failed_item> flow_failed_list;
+  optional<e1ap_pdcp_sn_status_info>                          pdcp_sn_status_info;
+};
+
+struct e1ap_pdu_session_resource_modified_item {
+  pdu_session_id_t                                           pdu_session_id = pdu_session_id_t::invalid;
+  up_transport_layer_info                                    ng_dl_up_tnl_info;
+  slotted_id_vector<drb_id_t, e1ap_drb_setup_item_ng_ran>    drb_setup_list_ng_ran;
+  slotted_id_vector<drb_id_t, e1ap_drb_failed_item_ng_ran>   drb_failed_list_ng_ran;
+  slotted_id_vector<drb_id_t, e1ap_drb_modified_item_ng_ran> drb_modified_list_ng_ran;
+  slotted_id_vector<drb_id_t, e1ap_drb_failed_item_ng_ran>   drb_failed_to_modify_list_ng_ran;
+  optional<security_result_t>                                security_result;
+  optional<e1ap_data_forwarding_info>                        pdu_session_data_forwarding_info_resp;
+};
 
 } // namespace srsgnb
