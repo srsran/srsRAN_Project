db79097f949e058022f3181a39a8d50571682a1e
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2023-02-09 17:48:30 +0100

    sched: reduce the number of available k1 values in TDD

--- lib/scheduler/config/serving_cell_config_factory.cpp
+++ lib/scheduler/config/serving_cell_config_factory.cpp
@@ -454,7 +454,7 @@ uplink_config srsgnb::config_helpers::make_default_ue_uplink_config(const cell_c
     pucch_cfg.dl_data_to_ul_ack = {4};
   } else {
     // TDD
-    pucch_cfg.dl_data_to_ul_ack = {4, 5, 6, 7, 8};
+    pucch_cfg.dl_data_to_ul_ack = {4, 5, 6, 7};
   }
 
   // > PUSCH config.
