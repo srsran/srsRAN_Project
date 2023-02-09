59bf370870d16e61f11d97e5c8321998753ece2b
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2023-02-09 10:45:20 +0100

    mac: reduction of the size of ring buffer pool used by rar pdu assembler

--- lib/mac/mac_dl/mac_cell_processor.cpp
+++ lib/mac/mac_dl/mac_cell_processor.cpp
@@ -31,8 +31,8 @@ mac_cell_processor::mac_cell_processor(const mac_cell_creation_request& cell_cfg
   slot_exec(slot_exec_),
   ctrl_exec(ctrl_exec_),
   phy_cell(phy_notifier_),
-  // The PDU pool has to be large enough to fit the maximum number of PDUs per slot for all possible K0 values.
-  pdu_pool(MAX_DL_PDU_LENGTH * MAX_DL_PDUS_PER_SLOT,
+  // The PDU pool has to be large enough to fit the maximum number of RARs per slot for all possible K0 values.
+  pdu_pool(MAX_DL_PDU_LENGTH,
            MAX_K0_DELAY,
            get_nof_slots_per_subframe(cell_cfg.scs_common) * NOF_SFNS * NOF_SUBFRAMES_PER_FRAME),
   ssb_helper(cell_cfg_req_),
