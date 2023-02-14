2a6d92b72a85f5f4f2eee8b79647184248a28a21
Xavier Arteaga Martinez <xavier@softwareradiosystems.com>
2023-02-14 18:56:34 +0100

    phy: revert lower PHY task defer

--- lib/phy/lower/lower_phy_impl.cpp
+++ lib/phy/lower/lower_phy_impl.cpp
@@ -257,10 +257,13 @@ void lower_phy_impl::process_symbol()
 
 void lower_phy_impl::realtime_process_loop(task_executor& realtime_task_executor)
 {
+  // Process symbol.
+  process_symbol();
+
   // Feedbacks the task if no stop has been signaled.
-  while (state_fsm.is_running()) {
-    // Process symbol.
-    process_symbol();
+  if (state_fsm.is_running()) {
+    realtime_task_executor.defer([this, &realtime_task_executor]() { realtime_process_loop(realtime_task_executor); });
+    return;
   }
 
   // Notify the stop of the asynchronous operation.
