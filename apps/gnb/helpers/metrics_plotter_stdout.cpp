c7bea9c1fd8b4e2c112a2d5fa98f8d72dd4e7ecc
Andre Puschmann <andre@srs.io>
2023-02-16 10:16:58 +0100

    gnb,metrics: print stats header only if user is attached

--- apps/gnb/helpers/metrics_plotter_stdout.cpp
+++ apps/gnb/helpers/metrics_plotter_stdout.cpp
@@ -29,7 +29,7 @@ void metrics_plotter_stdout::report_metrics(span<const scheduler_ue_metrics> ue_
     return;
   }
 
-  if (++nof_lines > 10) {
+  if (++nof_lines > 10 && !ue_metrics.empty()) {
     nof_lines = 0;
     print_header();
   }
