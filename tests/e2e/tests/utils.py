b02da26648b56cd317052d57545068abb2840bca
Alfredo Saez <alfredo.saez@srs.io>
2023-02-16 11:12:59 +0100

    ci: adjust paths to new tests folder

--- tests/e2e/tests/utils.py
+++ tests/e2e/tests/utils.py
@@ -1,6 +1,13 @@
+#
+# Copyright 2013-2023 Software Radio Systems Limited
+#
+# By using this file, you agree to the terms and conditions set
+# forth in the LICENSE file which can be found at the top level of
+# the distribution.
+#
+
 import logging
 import os
-import pathlib
 from collections import defaultdict
 from contextlib import contextmanager, suppress
 from pprint import pformat
