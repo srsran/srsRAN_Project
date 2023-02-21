4bb23f45357f2b874156967aa3ebb435201d6be8
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2023-02-21 16:28:32 +0100

    du_manager: extend unit tests to verify that SR PUCCH resources for different UEs do not collide in the case of TDD

--- tests/unittests/du_manager/du_ran_resource_manager_test.cpp
+++ tests/unittests/du_manager/du_ran_resource_manager_test.cpp
@@ -16,13 +16,18 @@
 using namespace srsran;
 using namespace srs_du;
 
-class du_ran_resource_manager_tester : public ::testing::Test
+struct params {
+  duplex_mode duplx_mode;
+};
+
+class du_ran_resource_manager_tester : public ::testing::TestWithParam<params>
 {
 protected:
-  du_ran_resource_manager_tester() :
-    cell_cfg_list({config_helpers::make_default_du_cell_config()}),
+  du_ran_resource_manager_tester(const cell_config_builder_params& params =
+                                     {.dl_arfcn = GetParam().duplx_mode == duplex_mode::FDD ? 365000U : 520002U}) :
+    cell_cfg_list({config_helpers::make_default_du_cell_config(params)}),
     qos_cfg_list(config_helpers::make_default_du_qos_config_list()),
-    default_ue_cell_cfg(config_helpers::create_default_initial_ue_serving_cell_config()),
+    default_ue_cell_cfg(config_helpers::create_default_initial_ue_serving_cell_config(params)),
     res_mng(std::make_unique<du_ran_resource_manager_impl>(cell_cfg_list, qos_cfg_list))
   {
   }
@@ -54,7 +59,7 @@ protected:
   slotted_array<ue_ran_resource_configurator, MAX_NOF_DU_UES> ues;
 };
 
-TEST_F(du_ran_resource_manager_tester, when_ue_resource_config_is_created_then_pcell_is_configured)
+TEST_P(du_ran_resource_manager_tester, when_ue_resource_config_is_created_then_pcell_is_configured)
 {
   du_ue_index_t                 ue_idx1 = to_du_ue_index(0);
   ue_ran_resource_configurator& ue_res  = create_ue(ue_idx1);
@@ -69,7 +74,7 @@ TEST_F(du_ran_resource_manager_tester, when_ue_resource_config_is_created_then_p
   ASSERT_FALSE(ue_res->mcg_cfg.scheduling_request_config.empty());
 }
 
-TEST_F(du_ran_resource_manager_tester, when_srb1_is_added_then_ue_resource_config_is_updated)
+TEST_P(du_ran_resource_manager_tester, when_srb1_is_added_then_ue_resource_config_is_updated)
 {
   du_ue_index_t                 ue_idx1 = to_du_ue_index(0);
   ue_ran_resource_configurator& ue_res  = create_ue(ue_idx1);
@@ -82,18 +87,32 @@ TEST_F(du_ran_resource_manager_tester, when_srb1_is_added_then_ue_resource_confi
   ASSERT_EQ(ue_res->rlc_bearers[0].rlc_cfg.mode, rlc_mode::am);
 }
 
-TEST_F(du_ran_resource_manager_tester, when_multiple_ues_are_created_then_they_use_different_sr_offsets)
+TEST_P(du_ran_resource_manager_tester, when_multiple_ues_are_created_then_they_use_different_sr_offsets)
 {
-  unsigned sr_period = get_config_sr_period();
+  unsigned sr_period            = get_config_sr_period();
+  unsigned slots_per_frame      = NOF_SUBFRAMES_PER_FRAME * get_nof_slots_per_subframe(cell_cfg_list[0].scs_common);
+  unsigned nof_avail_sr_offsets = sr_period;
+  if (cell_cfg_list[0].tdd_ul_dl_cfg_common.has_value()) {
+    nof_avail_sr_offsets = 0;
+    for (unsigned i = 0; i != sr_period; ++i) {
+      if (has_active_tdd_ul_symbols(*cell_cfg_list[0].tdd_ul_dl_cfg_common, i % slots_per_frame)) {
+        nof_avail_sr_offsets++;
+      }
+    }
+  }
 
   // > Created UEs have unique SR offsets.
   std::set<unsigned> sr_offsets;
-  for (unsigned i = 0; i != sr_period; ++i) {
+  for (unsigned i = 0; i != nof_avail_sr_offsets; ++i) {
     ue_ran_resource_configurator& ue_res = create_ue(to_du_ue_index(i));
     ASSERT_FALSE(ue_res.empty());
     const auto& sr_res_list = ue_res->cells[0].serv_cell_cfg.ul_config->init_ul_bwp.pucch_cfg->sr_res_list;
     ASSERT_FALSE(sr_res_list.empty());
     ASSERT_EQ(sr_periodicity_to_slot(sr_res_list[0].period), sr_period);
+    if (cell_cfg_list[0].tdd_ul_dl_cfg_common.has_value()) {
+      ASSERT_TRUE(
+          has_active_tdd_ul_symbols(*cell_cfg_list[0].tdd_ul_dl_cfg_common, sr_res_list[0].offset % slots_per_frame));
+    }
     ASSERT_EQ(sr_offsets.count(sr_res_list[0].offset), 0);
     sr_offsets.insert(sr_res_list[0].offset);
   }
@@ -106,7 +125,7 @@ TEST_F(du_ran_resource_manager_tester, when_multiple_ues_are_created_then_they_u
   }
 
   // Removing one UE, should make one SR offset available.
-  du_ue_index_t ue_idx_to_rem = to_du_ue_index(test_rgen::uniform_int<unsigned>(0, sr_period - 1));
+  du_ue_index_t ue_idx_to_rem = to_du_ue_index(test_rgen::uniform_int<unsigned>(0, nof_avail_sr_offsets - 1));
   unsigned      rem_sr_offset =
       ues[ue_idx_to_rem]->cells[0].serv_cell_cfg.ul_config->init_ul_bwp.pucch_cfg->sr_res_list[0].offset;
   ues.erase(ue_idx_to_rem);
@@ -114,3 +133,7 @@ TEST_F(du_ran_resource_manager_tester, when_multiple_ues_are_created_then_they_u
   ASSERT_FALSE(ue_res.empty());
   ASSERT_EQ(rem_sr_offset, ue_res->cells[0].serv_cell_cfg.ul_config->init_ul_bwp.pucch_cfg->sr_res_list[0].offset);
 }
+
+INSTANTIATE_TEST_SUITE_P(du_ran_resource_manager_tester,
+                         du_ran_resource_manager_tester,
+                         ::testing::Values(params{duplex_mode::FDD}, params{duplex_mode::TDD}));
