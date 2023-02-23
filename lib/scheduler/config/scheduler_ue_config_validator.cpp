27fda5b5fc79c0d077c54185c07756445e2995cf
Carlo Galiotto <carlo@srs.io>
2023-02-23 12:27:04 +0100

    sched: add validator for PUCCH scheduler cfg
    Signed-off-by: Carlo Galiotto <carlo@srs.io>

--- lib/scheduler/config/scheduler_ue_config_validator.cpp
+++ lib/scheduler/config/scheduler_ue_config_validator.cpp
@@ -32,6 +32,84 @@ using namespace config_validators;
 
 error_type<std::string> srsran::config_validators::validate_pucch_cfg(const sched_ue_creation_request_message& msg)
 {
+  VERIFY(msg.cfg.cells.front().serv_cell_cfg.ul_config.has_value() and
+             msg.cfg.cells.front().serv_cell_cfg.ul_config.value().init_ul_bwp.pucch_cfg.has_value(),
+         "Missing configuration for uplinkConfig or pucch-Config in spCellConfig");
+
+  const auto& pucch_cfg = msg.cfg.cells.front().serv_cell_cfg.ul_config.value().init_ul_bwp.pucch_cfg.value();
+
+  VERIFY(pucch_cfg.format_1_common_param.has_value(), "Missing PUCCH-format1 parameters in PUCCH-Config");
+  VERIFY(pucch_cfg.format_2_common_param.has_value(), "Missing PUCCH-format2 parameters in PUCCH-Config");
+
+  // Verify that the PUCCH resources IDs of each PUCCH resource set point at a corresponding item in the PUCCH reource
+  // list.
+  VERIFY(pucch_cfg.pucch_res_set.size() >= 2, "At least 2 PUCCH resource sets need to be configured in PUCCH-Config");
+  VERIFY(pucch_cfg.pucch_res_set[0].pucch_res_set_id == 0 and pucch_cfg.pucch_res_set[1].pucch_res_set_id == 1,
+         "PUCCH resouce sets 0 and 1 are expected to have PUCCH-ResourceSetId 0 and 1, respectively");
+  VERIFY((not pucch_cfg.pucch_res_set[0].pucch_res_id_list.empty()) and
+             (not pucch_cfg.pucch_res_set[1].pucch_res_id_list.empty()),
+         "PUCCH resouce sets 0 and 1 are expected to have a non-empty set of PUCCH resource id");
+  for (size_t pucch_res_set_idx = 0; pucch_res_set_idx != 2; ++pucch_res_set_idx) {
+    for (auto res_idx : pucch_cfg.pucch_res_set[pucch_res_set_idx].pucch_res_id_list) {
+      const auto* it = std::find_if(pucch_cfg.pucch_res_list.begin(),
+                                    pucch_cfg.pucch_res_list.end(),
+                                    [res_idx](const pucch_resource& res) { return res_idx == res.res_id; });
+      VERIFY(it != pucch_cfg.pucch_res_list.end(),
+             "PUCCH res. index={} in PUCCH res. set id={} not found in the PUCCH resource list",
+             res_idx,
+             pucch_res_set_idx);
+    }
+  }
+
+  // Check PUCCH Formats for each PUCCH Resource Set.
+  for (auto res_idx : pucch_cfg.pucch_res_set[0].pucch_res_id_list) {
+    VERIFY(pucch_cfg.pucch_res_list[res_idx].format == pucch_format::FORMAT_1,
+           "Only PUCCH Resource Format 1 expected in PUCCH resource set 0.");
+  }
+  for (auto res_idx : pucch_cfg.pucch_res_set[1].pucch_res_id_list) {
+    VERIFY(pucch_cfg.pucch_res_list[res_idx].format == pucch_format::FORMAT_2,
+           "Only PUCCH Resource Format 2 expected in PUCCH resource set 1.");
+  }
+
+  // Verify the PUCCH resource id that indicated in the SR resource config exists in the PUCCH resource list.
+  VERIFY(not pucch_cfg.sr_res_list.empty(), "SchedulingRequestResourceConfig list is empty");
+  const auto* sr_pucch_res_id = std::find_if(pucch_cfg.pucch_res_list.begin(),
+                                             pucch_cfg.pucch_res_list.end(),
+                                             [sr_pucch_res_idx = pucch_cfg.sr_res_list.front().pucch_res_id](
+                                                 const pucch_resource& res) { return sr_pucch_res_idx == res.res_id; });
+  VERIFY(sr_pucch_res_id != pucch_cfg.pucch_res_list.end(),
+         "PUCCH res. index={} given in SR resource config not found in the PUCCH resource list",
+         pucch_cfg.sr_res_list.front().pucch_res_id);
+  VERIFY(pucch_cfg.pucch_res_list[pucch_cfg.sr_res_list.front().pucch_res_id].format == pucch_format::FORMAT_1,
+         "PUCCH resource used for SR is expected to be Format 1");
+
+  // Verify that the PUCCH setting used for CSI report have been configured properly.
+  if (msg.cfg.cells.front().serv_cell_cfg.csi_meas_cfg.has_value()) {
+    const auto& csi_cfg = msg.cfg.cells.front().serv_cell_cfg.csi_meas_cfg.value();
+    VERIFY(not csi_cfg.csi_report_cfg_list.empty() and
+               variant_holds_alternative<csi_report_config::periodic_or_semi_persistent_report_on_pucch>(
+                   csi_cfg.csi_report_cfg_list.front().report_cfg_type) and
+               not variant_get<csi_report_config::periodic_or_semi_persistent_report_on_pucch>(
+                       csi_cfg.csi_report_cfg_list.front().report_cfg_type)
+                       .pucch_csi_res_list.empty(),
+           "PUCCH-CSI-ResourceList has not been configured in the CSI-reportConfig",
+           pucch_cfg.sr_res_list.front().pucch_res_id);
+
+    const auto& csi = variant_get<csi_report_config::periodic_or_semi_persistent_report_on_pucch>(
+                          csi_cfg.csi_report_cfg_list.front().report_cfg_type)
+                          .pucch_csi_res_list.front()
+                          .pucch_res_id;
+    // Verify the PUCCH resource id that indicated in the CSI resource config exists in the PUCCH resource list.
+    const auto* csi_pucch_res_id = std::find_if(pucch_cfg.pucch_res_list.begin(),
+                                                pucch_cfg.pucch_res_list.end(),
+                                                [csi](const pucch_resource& res) { return csi == res.res_id; });
+    VERIFY(csi_pucch_res_id != pucch_cfg.pucch_res_list.end(),
+           "PUCCH res. index={} given in PUCCH-CSI-resourceList not found in the PUCCH resource list",
+           csi);
+    VERIFY(pucch_cfg.pucch_res_list[csi].format == pucch_format::FORMAT_2,
+           "PUCCH resource used for CSI is expected to be Format 2");
+  }
+
   for (const cell_config_dedicated& cell : msg.cfg.cells) {
     if (cell.serv_cell_cfg.ul_config.has_value()) {
       const auto& init_ul_bwp = cell.serv_cell_cfg.ul_config.value().init_ul_bwp;
@@ -43,7 +121,7 @@ error_type<std::string> srsran::config_validators::validate_pucch_cfg(const sche
       }
     }
   }
-  // TODO: Validate other parameters.
+
   return {};
 }
 
@@ -190,6 +268,9 @@ error_type<std::string> srsran::config_validators::validate_csi_meas_cfg(const s
 error_type<std::string>
 srsran::config_validators::validate_sched_ue_creation_request_message(const sched_ue_creation_request_message& msg)
 {
+  // Verify the list of ServingCellConfig contains spCellConfig.
+  VERIFY(not msg.cfg.cells.empty(), "Empty list of ServingCellConfig");
+
   HANDLE_CODE(validate_pdsch_cfg(msg));
 
   HANDLE_CODE(validate_pucch_cfg(msg));
