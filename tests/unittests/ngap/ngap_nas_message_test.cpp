edc597a8cca082a037a344cdea0ac9ac5dd3081b
Fabian Eckermann <fabian@srs.io>
2023-03-13 11:13:03 +0100

    cu_cp,ngap: Add unittest for max amf ue id handling

--- tests/unittests/ngap/ngap_nas_message_test.cpp
+++ tests/unittests/ngap/ngap_nas_message_test.cpp
@@ -109,3 +109,34 @@ TEST_F(ngap_nas_message_routine_test, when_ue_present_and_amf_set_ul_nas_transpo
   // Check that AMF notifier was called with right type
   ASSERT_TRUE(was_ul_nas_transport_forwarded());
 }
+
+/// Test AMF UE ID handling
+TEST_F(ngap_nas_message_routine_test, when_amf_ue_id_is_max_size_then_its_not_cropped)
+{
+  // Test preamble
+  ue_index_t ue_index = uint_to_ue_index(
+      test_rgen::uniform_int<uint64_t>(ue_index_to_uint(ue_index_t::min), ue_index_to_uint(ue_index_t::max)));
+  this->start_procedure(ue_index);
+
+  auto& ue     = test_ues.at(ue_index);
+  ue.amf_ue_id = amf_ue_id_t::max;
+
+  // Inject DL NAS transport message from AMF
+  ngap_message dl_nas_transport = generate_downlink_nas_transport_message(ue.amf_ue_id.value(), ue.ran_ue_id.value());
+  ngap->handle_message(dl_nas_transport);
+
+  // Check that RRC notifier was called
+  ASSERT_TRUE(was_dl_nas_transport_forwarded());
+
+  ngap_ue* ngap_ue = ue_mng.find_ngap_ue(ue_index);
+  ASSERT_EQ(ngap_ue->get_amf_ue_id(), ue.amf_ue_id);
+
+  ngap_ul_nas_transport_message ul_nas_transport = generate_ul_nas_transport_message(ue_index);
+  ngap->handle_ul_nas_transport_message(ul_nas_transport);
+
+  // Check that AMF notifier was called with right type
+  ASSERT_TRUE(was_ul_nas_transport_forwarded());
+
+  ASSERT_EQ(msg_notifier.last_ngap_msg.pdu.init_msg().value.ul_nas_transport()->amf_ue_ngap_id.value.value,
+            amf_ue_id_to_uint(amf_ue_id_t::max));
+}
