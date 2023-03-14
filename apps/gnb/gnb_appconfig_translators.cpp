689b5ff8fbeae7f8a25c81170f78f0c1cf5b491f
Andre Puschmann <andre@srs.io>
2023-03-14 10:30:10 +0100

    gnb: use band_helper to set lower_phy n_ta_offset according to band

--- apps/gnb/gnb_appconfig_translators.cpp
+++ apps/gnb/gnb_appconfig_translators.cpp
@@ -233,7 +233,7 @@ lower_phy_configuration srsran::generate_ru_config(const gnb_appconfig& config)
 
     out_cfg.srate = sampling_rate::from_MHz(config.rf_driver_cfg.srate_MHz);
 
-    out_cfg.ta_offset = n_ta_offset::n0;
+    out_cfg.ta_offset = band_helper::get_ta_offset(config.common_cell_cfg.band.value());
     if (config.rf_driver_cfg.time_alignment_calibration.has_value()) {
       // Selects the user specific value.
       out_cfg.time_alignment_calibration = config.rf_driver_cfg.time_alignment_calibration.value();
