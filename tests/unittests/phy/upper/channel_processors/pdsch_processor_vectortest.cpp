6b7cf86769d75e451f3c475602532af4a398900f
Joaquim Broquetas <joaquim.broquetas@srs.io>
2023-03-15 12:54:27 +0100

    phy: fix RE pattern exclusion mask

--- tests/unittests/phy/upper/channel_processors/pdsch_processor_vectortest.cpp
+++ tests/unittests/phy/upper/channel_processors/pdsch_processor_vectortest.cpp
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2013-2022 Software Radio Systems Limited
+ * Copyright 2021-2023 Software Radio Systems Limited
  *
  * By using this file, you agree to the terms and conditions set
  * forth in the LICENSE file which can be found at the top level of
@@ -31,11 +31,20 @@ using PdschProcessorParams = test_case_t;
 class PdschProcessorFixture : public ::testing::TestWithParam<PdschProcessorParams>
 {
 protected:
-  std::unique_ptr<pdsch_processor>     pdsch_proc;
+  // PDSCH processor factory.
+  static std::shared_ptr<pdsch_processor_factory> pdsch_proc_factory;
+  // PDSCH processor.
+  std::unique_ptr<pdsch_processor> pdsch_proc;
+  // PDSCH validator.
   std::unique_ptr<pdsch_pdu_validator> pdu_validator;
 
-  void SetUp() override
+  static void SetUpTestSuite()
   {
+    // Do nothing if the PDSCH processor factory was previously created.
+    if (pdsch_proc_factory) {
+      return;
+    }
+
     std::shared_ptr<crc_calculator_factory> crc_calculator_factory = create_crc_calculator_factory_sw("auto");
     ASSERT_NE(crc_calculator_factory, nullptr);
 
@@ -55,10 +64,7 @@ protected:
     pdsch_encoder_factory_config.segmenter_factory                      = ldpc_segmenter_tx_factory;
     std::shared_ptr<pdsch_encoder_factory> pdsch_encoder_factory =
         create_pdsch_encoder_factory_sw(pdsch_encoder_factory_config);
-    ASSERT_NE(ldpc_encoder_factory, nullptr);
-
-    std::shared_ptr<polar_factory> polar_code_factory = create_polar_factory_sw();
-    ASSERT_NE(polar_code_factory, nullptr);
+    ASSERT_NE(pdsch_encoder_factory, nullptr);
 
     std::shared_ptr<channel_modulation_factory> modulator_factory = create_channel_modulation_sw_factory();
     ASSERT_NE(modulator_factory, nullptr);
@@ -70,39 +76,31 @@ protected:
         create_dmrs_pdsch_processor_factory_sw(prg_factory);
     ASSERT_NE(dmrs_pdsch_factory, nullptr);
 
-    std::shared_ptr<dmrs_pdcch_processor_factory> dmrs_pdcch_factory =
-        create_dmrs_pdcch_processor_factory_sw(prg_factory);
-    ASSERT_NE(dmrs_pdcch_factory, nullptr);
-
-    std::shared_ptr<pdcch_modulator_factory> pdcch_modulator_factory =
-        create_pdcch_modulator_factory_sw(modulator_factory, prg_factory);
-    ASSERT_NE(pdcch_modulator_factory, nullptr);
-
     std::shared_ptr<pdsch_modulator_factory> pdsch_modulator_factory =
         create_pdsch_modulator_factory_sw(modulator_factory, prg_factory);
     ASSERT_NE(pdsch_modulator_factory, nullptr);
 
-    std::shared_ptr<pdcch_encoder_factory> pdcch_encoder_factory =
-        create_pdcch_encoder_factory_sw(crc_calculator_factory, polar_code_factory);
-    ASSERT_NE(pdcch_encoder_factory, nullptr);
-
-    std::shared_ptr<pdcch_processor_factory> pdcch_proc_factory =
-        create_pdcch_processor_factory_sw(pdcch_encoder_factory, pdcch_modulator_factory, dmrs_pdcch_factory);
-    ASSERT_NE(pdcch_proc_factory, nullptr);
-
-    std::shared_ptr<pdsch_processor_factory> pdsch_proc_factory =
+    pdsch_proc_factory =
         create_pdsch_processor_factory_sw(pdsch_encoder_factory, pdsch_modulator_factory, dmrs_pdsch_factory);
-    ASSERT_NE(pdsch_proc_factory, nullptr);
+    ASSERT_NE(pdsch_proc_factory, nullptr) << "Cannot create PDSCH processor factory.";
+  }
+
+  void SetUp() override
+  {
+    // Assert PDSCH processor factory.
+    ASSERT_NE(pdsch_proc_factory, nullptr) << "Invalid PDSCH processor factory.";
 
     // Create actual PDSCH processor.
     pdsch_proc = pdsch_proc_factory->create();
-    ASSERT_NE(pdsch_proc, nullptr);
+    ASSERT_NE(pdsch_proc, nullptr) << "Cannot create PDSCH processor";
 
     // Create actual PDSCH processor validator.
     pdu_validator = pdsch_proc_factory->create_validator();
-    ASSERT_NE(pdu_validator, nullptr);
+    ASSERT_NE(pdu_validator, nullptr) << "Cannot create PDSCH validator";
   }
 };
+
+std::shared_ptr<pdsch_processor_factory> PdschProcessorFixture::pdsch_proc_factory = nullptr;
 
 TEST_P(PdschProcessorFixture, PdschProcessorVectortest)
 {
@@ -114,7 +112,7 @@ TEST_P(PdschProcessorFixture, PdschProcessorVectortest)
 
   // Read input data as a bit-packed transport block.
   std::vector<uint8_t> transport_block = test_case.sch_data.read();
-  TESTASSERT(!transport_block.empty(), "Failed to load transport block.");
+  ASSERT_FALSE(transport_block.empty()) << "Failed to load transport block.";
 
   // Prepare transport blocks view.
   static_vector<span<const uint8_t>, pdsch_processor::MAX_NOF_TRANSPORT_BLOCKS> transport_blocks;
