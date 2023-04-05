d5d90d709d778f2ec2f66bbaece8866a7ed0fa37
Robert Falkenberg <robert.falkenberg@srs.io>
2023-04-05 08:30:05 +0200

    rlc_tx_am: verify minimum grant_len for status PDU
    This commit verifies the minimum grant_len for a status PDU
    before resetting the status_report_required flag.

--- lib/rlc/rlc_tx_am_entity.cpp
+++ lib/rlc/rlc_tx_am_entity.cpp
@@ -86,7 +86,11 @@ byte_buffer_slice_chain rlc_tx_am_entity::pull_pdu(uint32_t grant_len)
 
   // TX STATUS if requested
   if (status_provider->status_report_required()) {
-    rlc_am_status_pdu status_pdu = status_provider->get_status_pdu();
+    if (grant_len < rlc_am_nr_status_pdu_sizeof_header_ack_sn) {
+      logger.log_debug("Cannot fit status PDU into small grant_len={}.", grant_len);
+      return {};
+    }
+    rlc_am_status_pdu status_pdu = status_provider->get_status_pdu(); // this also resets status_report_required
 
     if (status_pdu.get_packed_size() > grant_len) {
       if (not status_pdu.trim(grant_len)) {
