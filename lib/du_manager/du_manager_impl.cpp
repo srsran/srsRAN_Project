a99ae981cf4803b579a16da005d933ae4cff06d2
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2023-04-14 17:50:01 +0200

    du_manager: reduce du stop wait to 10msec

--- lib/du_manager/du_manager_impl.cpp
+++ lib/du_manager/du_manager_impl.cpp
@@ -96,7 +96,7 @@ void du_manager_impl::stop()
       logger.error("Unable to stop DU Manager.");
       return;
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
   }
 }
 
