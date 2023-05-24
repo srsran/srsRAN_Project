fb4da5d60caba41e0a8d91ea51380c9567af7730
Supreeth Herle <supreeth.herle@srs.io>
2023-05-24 16:35:59 +0200

    sched: make sure to select right SS based on DCI format used for retransmission

--- lib/scheduler/ue_scheduling/ue_cell_grid_allocator.cpp
+++ lib/scheduler/ue_scheduling/ue_cell_grid_allocator.cpp
@@ -64,10 +64,17 @@ bool ue_cell_grid_allocator::allocate_dl_grant(const ue_pdsch_grant& grant)
     logger.warning("Failed to allocate PDSCH. Cause: SearchSpace not valid for active BWP.");
     return false;
   }
-  const search_space_configuration& ss_cfg = *ss_info->cfg;
+  const search_space_configuration& ss_cfg                  = *ss_info->cfg;
+  const dci_dl_rnti_config_type     ss_supported_dci_format = ss_info->get_crnti_dl_dci_format();
 
   const dci_dl_rnti_config_type dci_type =
-      h_dl.empty() ? ss_info->get_crnti_dl_dci_format() : h_dl.last_alloc_params().dci_cfg_type;
+      h_dl.empty() ? ss_supported_dci_format : h_dl.last_alloc_params().dci_cfg_type;
+
+  // In case of re-transmission DCI format must remain same and therefore its necessary to find the SS which support
+  // that DCI format.
+  if (dci_type != ss_supported_dci_format) {
+    return false;
+  }
 
   // See 3GPP TS 38.213, clause 10.1,
   // A UE monitors PDCCH candidates in one or more of the following search spaces sets
