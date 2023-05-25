8b5422ee15293dd28e580fd566b71c05eea90c72
yagoda <justin.tallon@softwareradiosystems.com>
2023-05-25 15:03:00 +0200

    apps: passing deep copy of ue metrics in metrics hub

--- apps/gnb/helpers/metrics_hub.cpp
+++ apps/gnb/helpers/metrics_hub.cpp
@@ -22,9 +22,11 @@ metrics_hub::metrics_hub(task_executor* du_executor_)
 }
 void metrics_hub::report_metrics(span<const scheduler_ue_metrics> ue_metrics)
 {
-  du_executor->execute([this, ue_metrics]() {
+  std::vector<scheduler_ue_metrics> ue_metrics_copy(ue_metrics.begin(), ue_metrics.end());
+  du_executor->execute([this, ue_metrics_copy]() {
     for (auto& subscriber : subscribers) {
-      subscriber->report_metrics(ue_metrics);
+      span<const scheduler_ue_metrics> _ue_metrics(&ue_metrics_copy[0], ue_metrics_copy.size());
+      subscriber->report_metrics(_ue_metrics);
     }
   });
 }
