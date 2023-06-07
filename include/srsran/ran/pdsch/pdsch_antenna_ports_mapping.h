52e58d657c1bd0919ac224792d032180194251b1
Supreeth Herle <supreeth.herle@srs.io>
2023-06-07 08:22:40 +0200

    ran: improve comments for PDSCH antenna ports mapping

--- include/srsran/ran/pdsch/pdsch_antenna_ports_mapping.h
+++ include/srsran/ran/pdsch/pdsch_antenna_ports_mapping.h
@@ -19,32 +19,35 @@ namespace srsran {
 struct pdsch_antenna_ports_mapping {
   /// Number of DMRS CDM group(s) without data.
   unsigned nof_dmrs_cdm_groups_without_data;
-  /// DMRS ports.
+  /// Contains list of DMRS ports used.
+  /// E.g. if the value is {0, 3}, then the DMRS ports (1000 + 0) = 1000 and (1000 + 3) = 1003 are used.
   std::vector<unsigned> dmrs_ports;
-  /// Number of front-load symbols.
+  /// Number of DMRS front-load symbols. Number of front-load symbols equates to maxLength in DMRS-DownlinkConfig whose
+  /// default value is Len1 i.e. 1 symbol.
   unsigned nof_front_load_symbols = 1;
 };
 
-/// \brief Returns the PDSCH antenna ports mapping corresponding to the row in Tables 7.3.1.2.2-1/2/3/4, TS 38.212.
+/// \brief Returns the PDSCH antenna ports mapping from TS 38.212, tables 7.3.1.2.2-1/2/3/4 based on input
+/// configuration.
 /// \param[in] nof_layers Number of layers.
 /// \param[in] nof_dl_antenna_ports Number of DL antenna ports supported by gNB.
 /// \param[in] dmrs_cfg_type PDSCH DMRS configuration type.
 /// \param[in] dmrs_max_len PDSCH DMRS maximum number of OFDM symbols.
-/// \return The PDSCH antenna ports mapping corresponding to the row in Tables 7.3.1.2.2-1/2/3/4, TS 38.212 based on
-///         DMRS configuration, nof. layers etc. If the given input configuration does not match any of the rows in the
-///         table, the function triggers an asserts. But if input configuration matches more than one row in the table,
-///         the first matching row information is returned.
+/// \return The PDSCH antenna ports mapping from TS 38.212, tables 7.3.1.2.2-1/2/3/4 based on DMRS configuration, nof.
+///         layers etc. If the given input configuration does not match any of the rows in the table, the function
+///         triggers an asserts. But if input configuration matches more than one row in the table, the first matching
+///         row information is returned.
 pdsch_antenna_ports_mapping get_pdsch_antenna_port_mapping(unsigned         nof_layers,
                                                            unsigned         nof_dl_antenna_ports,
                                                            dmrs_config_type dmrs_cfg_type,
                                                            dmrs_max_length  dmrs_max_len);
 
-/// \brief Returns the PDSCH antenna ports mapping row index in Tables 7.3.1.2.2-1/2/3/4, TS 38.212.
+/// \brief Returns the PDSCH antenna ports mapping row index in TS 38.212, tables 7.3.1.2.2-1/2/3/4.
 /// \param[in] nof_layers Number of layers.
 /// \param[in] nof_dl_antenna_ports Number of DL antenna ports supported by gNB.
 /// \param[in] dmrs_cfg_type PDSCH DMRS configuration type.
 /// \param[in] dmrs_max_len PDSCH DMRS maximum number of OFDM symbols.
-/// \return The PDSCH antenna ports mapping row index in Tables 7.3.1.2.2-1/2/3/4, TS 38.212 based on DMRS
+/// \return The PDSCH antenna ports mapping row index from TS 38.212, tables 7.3.1.2.2-1/2/3/4 based on DMRS
 ///         configuration, nof. layers etc. If the given input configuration does not match any of the rows in the
 ///         table, the function triggers an asserts. But if input configuration matches more than one row in the
 ///         table,the first matching row index is returned.
