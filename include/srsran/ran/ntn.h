50abf0668249f2d5613f4dac1711775061219cb0
yagoda <justin.tallon@softwareradiosystems.com>
2023-06-12 14:37:40 +0200

    ntn,config: fixing types in NTN config

--- include/srsran/ran/ntn.h
+++ include/srsran/ran/ntn.h
@@ -15,33 +15,33 @@
 namespace srsran {
 
 struct position_velocity_t {
-  int32_t position_x;
-  int32_t position_y;
-  int32_t position_z;
-  int32_t velocity_vx;
-  int32_t velocity_vy;
-  int32_t velocity_vz;
+  int position_x;
+  int position_y;
+  int position_z;
+  int velocity_vx;
+  int velocity_vy;
+  int velocity_vz;
 };
 
 struct ta_common_t {
-  uint32_t ta_common;
-  int32_t  ta_common_drift;
-  uint16_t ta_common_drift_variant;
+  unsigned ta_common;
+  int  ta_common_drift;
+  unsigned ta_common_drift_variant;
 };
 
 struct epoch_time_t {
-  uint64_t sfn;
-  uint32_t subframe_number;
+  unsigned sfn;
+  unsigned subframe_number;
 };
 
 struct ntn_config {
   // sib 19 values
   std::string reference_location = "0";
-  uint32_t    distance_threshold = 0;
+  unsigned    distance_threshold = 0;
   // ntn-config values
   epoch_time_t        epoch_time            = {0, 0};
-  int32_t             cell_specific_koffset = 0;
-  uint32_t            k_mac                 = 0;
+  unsigned            cell_specific_koffset = 0;
+  unsigned            k_mac                 = 0;
   position_velocity_t ephemeris_info        = {1, 2, 3, 1, 1, 1};
   ta_common_t         ta_info               = {0, 0, 0};
 };
