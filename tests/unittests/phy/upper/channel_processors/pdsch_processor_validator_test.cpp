ccaf0c0a6b0d1539cfbb3a4338067c616871ea83
Joaquim Broquetas <joaquim.broquetas@srs.io>
2023-06-14 18:03:54 +0200

    phy: DM-RS PDSCH processor 4 layers

--- tests/unittests/phy/upper/channel_processors/pdsch_processor_validator_test.cpp
+++ tests/unittests/phy/upper/channel_processors/pdsch_processor_validator_test.cpp
@@ -122,12 +122,6 @@ const std::vector<test_case_t> pdsch_processor_validator_test_data = {
      R"(Only contiguous allocation is currently supported\.)"},
     {[] {
        pdsch_processor::pdu_t pdu = base_pdu;
-       pdu.precoding              = precoding_configuration::make_wideband(make_identity(4));
-       return pdu;
-     },
-     R"(Only 1 or 2 layers are currently supported\. 4 layers requested\.)"},
-    {[] {
-       pdsch_processor::pdu_t pdu = base_pdu;
        pdu.tbs_lbrm_bytes         = 0;
        return pdu;
      },
