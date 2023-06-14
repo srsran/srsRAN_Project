efae9807ada18497aaedf819dcc4d4bfbb37b870
Fabian Eckermann <fabian@srs.io>
2023-06-14 09:54:24 +0200

    ran: Small five qi fixes

--- include/srsran/ran/five_qi.h
+++ include/srsran/ran/five_qi.h
@@ -16,18 +16,18 @@
 namespace srsran {
 
 // See TS 38.463: Five QI valid values: (0..255)
-constexpr static uint16_t MAX_FIVEQI = 256;
+constexpr static uint16_t MAX_FIVEQI = 255;
 
 /// \brief Five QI.
-enum class five_qi_t : uint16_t { min = 0, max = MAX_FIVEQI - 1, invalid = MAX_FIVEQI };
+enum class five_qi_t : uint16_t { min = 0, max = MAX_FIVEQI, invalid = MAX_FIVEQI + 1 };
 
-/// Convert Five QItype to integer.
+/// Convert Five QI type to integer.
 constexpr inline uint16_t five_qi_to_uint(five_qi_t five_qi)
 {
   return static_cast<uint16_t>(five_qi);
 }
 
-/// Convert integer to QoS Flow ID type.
+/// Convert integer to Five QI type.
 constexpr inline five_qi_t uint_to_five_qi(uint16_t five_qi)
 {
   srsran_assert(
