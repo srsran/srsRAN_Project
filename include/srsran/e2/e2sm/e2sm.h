7664d61c58f964666f7ddcb52335191a9f1fe4c9
yagoda <justin.tallon@softwareradiosystems.com>
2023-06-20 17:14:45 +0200

    e2ap,e2sm: fixing comments in header files

--- include/srsran/e2/e2sm/e2sm.h
+++ include/srsran/e2/e2sm/e2sm.h
@@ -16,14 +16,14 @@ class e2sm_handler
 {
 public:
   virtual ~e2sm_handler() = default;
-  /// @brief Handle the packed E2SM-KPM Action Definition.
-  /// @param buf
-  /// @return  Returns the unpacked E2SM-KPM Action Definition.
+  /// \brief Handle the packed E2SM-KPM Action Definition.
+  /// \param[in] buf
+  /// \return Returns the unpacked E2SM-KPM Action Definition.
   virtual asn1::e2sm_kpm::e2_sm_kpm_action_definition_s
   handle_packed_e2sm_kpm_action_definition(const srsran::byte_buffer& buf) = 0;
-  /// @brief Handle the packed E2SM-KPM Event Trigger Definition.
-  /// @param buf
-  /// @return Returns the unpacked E2SM-KPM Event Trigger Definition.
+  /// \brief Handle the packed E2SM-KPM Event Trigger Definition.
+  /// \param[in] buf
+  /// \return Returns the unpacked E2SM-KPM Event Trigger Definition.
   virtual asn1::e2sm_kpm::e2_sm_kpm_event_trigger_definition_s
   handle_packed_event_trigger_definition(const srsran::byte_buffer& buf) = 0;
 };
@@ -32,12 +32,12 @@ class e2sm_interface
 {
 public:
   virtual ~e2sm_interface() = default;
-  /// @brief Handle the E2SM-KPM Action Definition.
-  /// @param action_definition
-  /// @return Returns the packed resultant Indication Message.
+  /// \brief Handle the E2SM-KPM Action Definition.
+  /// \param[in] action_definition
+  /// \return Returns the packed resultant Indication Message.
   virtual srsran::byte_buffer handle_action(const srsran::byte_buffer& action_definition) = 0;
-  /// @brief get the indication header associated with the action id.
-  /// @param action_id
-  /// @return Returns the indication header.
+  /// \brief get the indication header associated with the action id.
+  /// \param[in] action_id
+  /// \return Returns the indication header.
   virtual srsran::byte_buffer get_indication_header(uint32_t action_id) = 0;
 };
