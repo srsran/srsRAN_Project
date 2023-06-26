0d5921c02bb5c727edcdc52efbdf89954cddeb31
Andre Puschmann <andre@srs.io>
2023-06-26 16:43:41 +0200

    cu_cp: add simple validator for measurement-related types

---
+++ include/srsran/cu_cp/meas_types_validators.h
@@ -0,0 +1,23 @@
+/*
+ *
+ * Copyright 2021-2023 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#pragma once
+
+#include "meas_types.h"
+
+namespace srsran {
+namespace srs_cu_cp {
+
+/// Validates the provided config(s).
+bool validate_config(const cu_cp_meas_trigger_quant_offset& config);
+bool validate_config(const cu_cp_cond_event_a3& config);
+
+} // namespace srs_cu_cp
+} // namespace srsran
---
+++ lib/cu_cp/meas_types_validators.cpp
@@ -0,0 +1,37 @@
+/*
+ *
+ * Copyright 2021-2023 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "srsran/cu_cp/meas_types_validators.h"
+
+using namespace srsran;
+using namespace srs_cu_cp;
+
+#define LOG_CHAN ("CU-CP")
+
+bool srsran::srs_cu_cp::validate_config(const cu_cp_meas_trigger_quant_offset& config)
+{
+  if (not config.rsrp.has_value() and not config.rsrq.has_value() and not config.sinr.has_value()) {
+    srslog::fetch_basic_logger(LOG_CHAN).error("Either RSRP/RSRQ or SINR need to be configured.");
+    return false;
+  }
+  return true;
+}
+
+bool srsran::srs_cu_cp::validate_config(const cu_cp_cond_event_a3& event)
+{
+  if (not validate_config(event.a3_offset)) {
+    srslog::fetch_basic_logger(LOG_CHAN).error("A3 offset config not valid.");
+    return false;
+  }
+
+  // TODO: add range checks for hysteresis and time to trigger
+
+  return true;
+}
