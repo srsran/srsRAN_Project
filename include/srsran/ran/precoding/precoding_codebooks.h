c21b71e9814ca29ab82f85d2fd3f5fb0c52b0838
Joaquim Broquetas <joaquim.broquetas@srs.io>
2023-06-26 09:23:19 +0200

    phy, ran: four ports precoding codebooks

--- include/srsran/ran/precoding/precoding_codebooks.h
+++ include/srsran/ran/precoding/precoding_codebooks.h
@@ -28,7 +28,7 @@ precoding_weight_matrix make_single_port();
 /// \remark An assertion is triggered if \c i_port is equal to or greater than \c nof_ports.
 precoding_weight_matrix make_one_layer_one_port(unsigned nof_ports, unsigned i_port);
 
-// \brief Constructs a precoding weight matrix for the one layer&ndash;one port mapping.
+/// \brief Constructs a precoding weight matrix for the one layer&ndash;one port mapping.
 ///
 /// The precoding maps each layer to the port with the same identifier.
 ///
@@ -64,5 +64,73 @@ precoding_weight_matrix make_one_layer_two_ports(unsigned i_codebook);
 /// \param[in] i_codebook Codebook identifier.
 /// \return A precoding weight matrix for two layers and two ports.
 precoding_weight_matrix make_two_layer_two_ports(unsigned i_codebook);
+
+/// \brief Constructs a precoding weight matrix for one layer mapped into four transmit ports.
+///
+/// All weights are derived from TS38.214 Table 5.2.2.2.1-5, which describes one layer CSI reporting using a Type 1,
+/// single panel codebook. For one layer mapped to four antenna ports, the precoding uses the same beam for both antenna
+/// polarizations. The beam can be selected from eight possible beams, and the phase shift between cross-polarized
+/// antenna elements can be selected in \f$\pi/2\f$-rad increments, starting from 0 rad. The generated precoding weights
+/// for ports 0 and 1 correspond to the antenna elements of the first polarization, while ports 2 and 3 correspond to
+/// the antenna elements of the second polarization.
+///
+/// \param[in] beam_azimuth_id Beam selector in the horizontal plane. Corresponds to parameter \f$i_{1,1}\f$ in TS38.214
+///                            Section 5.2.2.2.1. Values: {0, 1, ..., 7}
+/// \param[in] pol_shift_id    Polarization phase shift selector. Corresponds to parameter \f$i_2\f$ in TS38.214
+///                            Section 5.2.2.2.1. Values: {0, 1, ..., 3}
+/// \return A precoding weight matrix for one layer and four ports.
+precoding_weight_matrix make_one_layer_four_ports_type1_sp_mode1(unsigned beam_azimuth_id, unsigned pol_shift_id);
+
+/// \brief Constructs a precoding weight matrix for two layers mapped into four transmit ports.
+///
+/// All weights are derived from TS38.214 Table 5.2.2.2.1-6, which describes two layer CSI reporting using a Type 1,
+/// single panel codebook. For two layers mapped to four antenna ports, the precoding uses one beam for each layer.
+/// The first beam can be selected from eight possible beams, and the second beam is specified by applying an offset to
+/// the direction of the first one, in \f$\pi\f$-rad increments, starting from 0 rad. The phase shift between
+/// cross-polarized antenna elements can be selected in \f$\pi/2\f$-rad increments, starting from 0 rad. The generated
+/// precoding weights for ports 0 and 1 correspond to the antenna elements of the first polarization, while ports 2 and
+/// 3 correspond to the antenna elements of the second polarization.
+///
+/// \param[in] beam_azimuth_id First beam selector in the horizontal plane. Corresponds to parameter \f$i_{1,1}\f$ in
+///                            TS38.214 Section 5.2.2.2.1. Values: {0, 1, ..., 7}
+/// \param[in] beam_offset_id  Second beam offset selector. Corresponds to parameter \f$i_{1,3}\f$ in TS38.214 Section
+///                            5.2.2.2.1. Values: {0, 1}
+/// \param[in] pol_shift_id    Polarization phase shift selector. Corresponds to parameter \f$i_2\f$ in TS38.214 Section
+///                            Section 5.2.2.2.1. Values: {0, 1}
+/// \return A precoding weight matrix for two layers and four ports.
+precoding_weight_matrix
+make_two_layer_four_ports_type1_sp_mode1(unsigned beam_azimuth_id, unsigned beam_offset_id, unsigned pol_shift_id);
+
+/// \brief Constructs a precoding weight matrix for three layers mapped into four transmit ports.
+///
+/// All weights are derived from TS38.214 Table 5.2.2.2.1-7, which describes three layer CSI reporting using a Type 1,
+/// single panel codebook. For three layers mapped to four antenna ports, the precoding uses a different combination of
+/// beam offsets and polarization phase shifts for each layer. The first beam can be selected from eight possible beams,
+/// and the phase shift between cross-polarized antenna elements can be selected in \f$\pi/2\f$-rad increments, starting
+/// from 0 rad. The generated precoding weights for ports 0 and 1 correspond to the antenna elements of the first
+/// polarization, while ports 2 and 3 correspond to the antenna elements of the second polarization.
+///
+/// \param[in] beam_azimuth_id First beam selector in the horizontal plane. Corresponds to parameter \f$i_{1,1}\f$ in
+///                            TS38.214 Section 5.2.2.2.1. Values: {0, 1, ..., 7}
+/// \param[in] pol_shift_id    Polarization phase shift selector. Corresponds to parameter \f$i_2\f$ in TS38.214 Section
+///                            Section 5.2.2.2.1. Values: {0, 1}
+/// \return A precoding weight matrix for three layers and four ports.
+precoding_weight_matrix make_three_layer_four_ports_type1_sp(unsigned beam_azimuth_id, unsigned pol_shift_id);
+
+/// \brief Constructs a precoding weight matrix for three layers mapped into four transmit ports.
+///
+/// All weights are derived from TS38.214 Table 5.2.2.2.1-8, which describes four layer CSI reporting using a Type 1,
+/// single panel codebook. For four layers mapped to four antenna ports, the precoding uses a different combination of
+/// beam offsets and polarization phase shifts for each layer. The first beam can be selected from eight possible beams,
+/// and the polarization phase shift can be selected in \f$\pi/2\f$-rad increments, starting from 0 rad. The generated
+/// precoding weights for ports 0 and 1 correspond to the antenna elements of the first polarization, while ports 2 and
+/// 3 correspond to the antenna elements of the second polarization.
+///
+/// \param[in] beam_azimuth_id First beam selector in the horizontal plane. Corresponds to parameter \f$i_{1,1}\f$ in
+///                            TS38.214 Section 5.2.2.2.1. Values: {0, 1, ..., 7}
+/// \param[in] pol_shift_id    Polarization phase shift selector. Corresponds to parameter \f$i_2\f$ in TS38.214 Section
+///                            Section 5.2.2.2.1. Values: {0, 1}
+/// \return A precoding weight matrix for four layers and four ports.
+precoding_weight_matrix make_four_layer_four_ports_type1_sp(unsigned beam_azimuth_id, unsigned pol_shift_id);
 
 } // namespace srsran
--- lib/ran/precoding/precoding_codebooks.cpp
+++ lib/ran/precoding/precoding_codebooks.cpp
@@ -138,3 +138,363 @@ precoding_weight_matrix srsran::make_two_layer_two_ports(unsigned i_codebook)
 
   return result;
 }
+
+// Creates horizontal beam coefficients, given a phase increment between adjacent elements, an initial phase, and a
+// scaling factor.
+static void create_horizontal_beam(span<cf_t> beam, float phase_increment_rad, float initial_phase_rad, float scaling)
+{
+  // Number of beam coefficients.
+  unsigned nof_elements = beam.size();
+
+  // Phasor to increment the phase of each beam coefficient.
+  cf_t phase_increment = std::polar(1.0F, phase_increment_rad);
+
+  // Initial beam coefficient.
+  cf_t beam_coefficient = std::polar(scaling, initial_phase_rad);
+
+  for (unsigned i_element = 0; i_element != nof_elements; ++i_element) {
+    beam[i_element] = beam_coefficient;
+
+    // Increment phase;
+    beam_coefficient *= phase_increment;
+  }
+}
+
+precoding_weight_matrix srsran::make_one_layer_four_ports_type1_sp_mode1(unsigned beam_azimuth_id,
+                                                                         unsigned pol_shift_id)
+{
+  // Beam oversampling factor in the horizontal plane, from TS38.214 Section 5.2.2.2.1.
+  static constexpr unsigned O_1 = 4;
+  // Number of cross-polarized antenna elements, from TS38.214 Section 5.2.2.2.1.
+  static constexpr unsigned N_1 = 2;
+  // Number of possible horizontal beams to choose from.
+  static constexpr unsigned nof_beams = O_1 * N_1;
+  // Number of possible polarization phase shifts to choose from.
+  static constexpr unsigned nof_pol_shifts = 4;
+
+  static constexpr interval<unsigned, false> beam_azimuth_range(0, nof_beams);
+  static constexpr interval<unsigned, false> pol_phase_shift_range(0, nof_pol_shifts);
+
+  srsran_assert(beam_azimuth_range.contains(beam_azimuth_id),
+                "The given beam azimuth identifier i1_1 (i.e., {}) is out of the range {}",
+                beam_azimuth_id,
+                beam_azimuth_range);
+
+  srsran_assert(pol_phase_shift_range.contains(pol_shift_id),
+                "The given polarization phase shift i2 (i.e., {}) is out of the range {}",
+                pol_shift_id,
+                pol_phase_shift_range);
+
+  // Precoding weight matrix for one layer mapped into four antenna ports.
+  precoding_weight_matrix result(1, 4);
+
+  // Power normalization factor. It is equal to the square root of the number of antenna ports.
+  static constexpr float scaling = 0.5F;
+
+  // Phase increment for each beam coefficient. This defines the direction of the beam in the horizontal plane.
+  float phase_increment_rad = 2.0F * M_PI * static_cast<float>(beam_azimuth_id) / static_cast<float>(nof_beams);
+  // Polarization phase shift. This defines the relative phase between the cross-polarized antenna elements.
+  float pol_phase_shift_rad = M_PI_2 * static_cast<float>(pol_shift_id);
+
+  std::array<cf_t, N_1> beam;
+
+  // Beam for the first polarization.
+  create_horizontal_beam(beam, phase_increment_rad, 0.0F, scaling);
+
+  // Set weights for the first polarization.
+  result.set_coefficient(beam[0], 0, 0);
+  result.set_coefficient(beam[1], 0, 1);
+
+  // Beam for the second polarization.
+  create_horizontal_beam(beam, phase_increment_rad, pol_phase_shift_rad, scaling);
+
+  // Set weights for the second polarization.
+  result.set_coefficient(beam[0], 0, 2);
+  result.set_coefficient(beam[1], 0, 3);
+
+  return result;
+}
+
+precoding_weight_matrix srsran::make_two_layer_four_ports_type1_sp_mode1(unsigned beam_azimuth_id,
+                                                                         unsigned beam_offset_id,
+                                                                         unsigned pol_shift_id)
+{
+  // Beam oversampling factor in the horizontal plane, from TS38.214 Section 5.2.2.2.1.
+  static constexpr unsigned O_1 = 4;
+  // Number of cross-polarized antenna elements, from TS38.214 Section 5.2.2.2.1.
+  static constexpr unsigned N_1 = 2;
+  // Number of possible horizontal beams to choose from.
+  static constexpr unsigned nof_beams = O_1 * N_1;
+  // Number of possible polarization phase shifts to choose from.
+  static constexpr unsigned nof_pol_shifts = 2;
+
+  static constexpr interval<unsigned, false> beam_azimuth_range(0, nof_beams);
+  static constexpr interval<unsigned, false> beam_offset_range(0, 2);
+  static constexpr interval<unsigned, false> pol_phase_shift_range(0, nof_pol_shifts);
+
+  srsran_assert(beam_azimuth_range.contains(beam_azimuth_id),
+                "The given beam azimuth identifier i1_1 (i.e., {}) is out of the range {}",
+                beam_azimuth_id,
+                beam_azimuth_range);
+
+  srsran_assert(beam_offset_range.contains(beam_offset_id),
+                "The given beam offset identifier i1_3 (i.e., {}) is out of the range {}",
+                beam_offset_id,
+                beam_offset_range);
+
+  srsran_assert(pol_phase_shift_range.contains(pol_shift_id),
+                "The given polarization phase shift i2 (i.e., {}) is out of the range {}",
+                pol_shift_id,
+                pol_phase_shift_range);
+
+  // Precoding weight matrix for two layers mapped into four antenna ports.
+  precoding_weight_matrix result(2, 4);
+
+  // Power normalization factor. It is equal to the square root of the number of ports times the number of layers.
+  static constexpr float scaling = 0.5F * M_SQRT1_2;
+
+  // Select k1 as per TS38.214 Section 5.2.2.2.1-3.
+  unsigned k1 = (beam_offset_id == 0) ? 0 : O_1;
+
+  // Phase increment for each beam coefficient. This defines the direction of the beam in the horizontal plane.
+  float phase_increment_rad = static_cast<float>(beam_azimuth_id) * 2.0F * M_PI / static_cast<float>(nof_beams);
+  // Polarization phase shift. This defines the relative phase between the cross-polarized antenna elements.
+  float pol_phase_shift_rad = M_PI_2 * static_cast<float>(pol_shift_id);
+
+  std::array<cf_t, N_1> beam;
+
+  // Layer 0.
+
+  // Beam for the first polarization.
+  create_horizontal_beam(beam, phase_increment_rad, 0.0F, scaling);
+
+  // Set weights for the first polarization.
+  result.set_coefficient(beam[0], 0, 0);
+  result.set_coefficient(beam[1], 0, 1);
+
+  // Beam for the second polarization.
+  create_horizontal_beam(beam, phase_increment_rad, pol_phase_shift_rad, scaling);
+
+  // Set weights for the second polarization.
+  result.set_coefficient(beam[0], 0, 2);
+  result.set_coefficient(beam[1], 0, 3);
+
+  // Layer 1. Recalculate phase increment for adjacent beam antenna elements.
+  phase_increment_rad = static_cast<float>(beam_azimuth_id + k1) * 2.0F * M_PI / static_cast<float>(nof_beams);
+
+  // Beam for the first polarization.
+  create_horizontal_beam(beam, phase_increment_rad, 0.0F, scaling);
+
+  // Set weights for the first polarization.
+  result.set_coefficient(beam[0], 1, 0);
+  result.set_coefficient(beam[1], 1, 1);
+
+  // Beam for the second polarization.
+  create_horizontal_beam(beam, phase_increment_rad, pol_phase_shift_rad + M_PI, scaling);
+
+  // Set weights for the second polarization.
+  result.set_coefficient(beam[0], 1, 2);
+  result.set_coefficient(beam[1], 1, 3);
+
+  return result;
+}
+
+precoding_weight_matrix srsran::make_three_layer_four_ports_type1_sp(unsigned beam_azimuth_id, unsigned pol_shift_id)
+{
+  // Beam oversampling factor in the horizontal plane, from TS38.214 Section 5.2.2.2.1.
+  static constexpr unsigned O_1 = 4;
+  // Number of cross-polarized antenna elements, from TS38.214 Section 5.2.2.2.1.
+  static constexpr unsigned N_1 = 2;
+  // Number of possible horizontal beams to choose from.
+  static constexpr unsigned nof_beams = O_1 * N_1;
+  // Number of possible polarization phase shifts to choose from.
+  static constexpr unsigned nof_pol_shifts = 2;
+
+  static constexpr interval<unsigned, false> beam_azimuth_range(0, nof_beams);
+  static constexpr interval<unsigned, false> pol_phase_shift_range(0, nof_pol_shifts);
+
+  srsran_assert(beam_azimuth_range.contains(beam_azimuth_id),
+                "The given beam azimuth identifier i1_1 (i.e., {}) is out of the range {}",
+                beam_azimuth_id,
+                beam_azimuth_range);
+
+  srsran_assert(pol_phase_shift_range.contains(pol_shift_id),
+                "The given polarization phase shift i2 (i.e., {}) is out of the range {}",
+                pol_shift_id,
+                pol_phase_shift_range);
+
+  // Precoding weight matrix for three layers mapped into four antenna ports.
+  precoding_weight_matrix result(3, 4);
+
+  // Power normalization factor. It is equal to the square root of the number of ports times the number of layers.
+  float scaling = 1.0F / std::sqrt(12.0F);
+
+  // Select k1 as per TS38.214 Section 5.2.2.2.1-4.
+  static constexpr unsigned k1 = O_1;
+
+  // Phase increment for each beam coefficient. This defines the direction of the beam in the horizontal plane.
+  float phase_increment_rad = static_cast<float>(beam_azimuth_id) * 2.0F * M_PI / static_cast<float>(nof_beams);
+  // Polarization phase shift. This defines the relative phase between the cross-polarized antenna elements.
+  float pol_phase_shift_rad = M_PI_2 * static_cast<float>(pol_shift_id);
+
+  std::array<cf_t, N_1> beam;
+
+  // Layer 0.
+
+  // Beam for the first polarization.
+  create_horizontal_beam(beam, phase_increment_rad, 0.0F, scaling);
+
+  // Set weights for the first polarization.
+  result.set_coefficient(beam[0], 0, 0);
+  result.set_coefficient(beam[1], 0, 1);
+
+  // Beam for the second polarization.
+  create_horizontal_beam(beam, phase_increment_rad, pol_phase_shift_rad, scaling);
+
+  // Set weights for the second polarization.
+  result.set_coefficient(beam[0], 0, 2);
+  result.set_coefficient(beam[1], 0, 3);
+
+  // Layer 2. The phase increment for adjacent beam antenna elements is the same as layer 0.
+
+  // Beam for the first polarization.
+  create_horizontal_beam(beam, phase_increment_rad, 0.0F, scaling);
+
+  // Set weights for the first polarization.
+  result.set_coefficient(beam[0], 2, 0);
+  result.set_coefficient(beam[1], 2, 1);
+
+  // Beam for the second polarization.
+  create_horizontal_beam(beam, phase_increment_rad, pol_phase_shift_rad + M_PI, scaling);
+
+  // Set weights for the second polarization.
+  result.set_coefficient(beam[0], 2, 2);
+  result.set_coefficient(beam[1], 2, 3);
+
+  // Layer 1. Recalculate phase increment for adjacent beam antenna elements.
+  phase_increment_rad = static_cast<float>(beam_azimuth_id + k1) * 2.0F * M_PI / static_cast<float>(nof_beams);
+
+  // Beam for the first polarization.
+  create_horizontal_beam(beam, phase_increment_rad, 0.0F, scaling);
+
+  // Set weights for the first polarization.
+  result.set_coefficient(beam[0], 1, 0);
+  result.set_coefficient(beam[1], 1, 1);
+
+  // Beam for the second polarization.
+  create_horizontal_beam(beam, phase_increment_rad, pol_phase_shift_rad, scaling);
+
+  // Set weights for the second polarization.
+  result.set_coefficient(beam[0], 1, 2);
+  result.set_coefficient(beam[1], 1, 3);
+
+  return result;
+}
+
+precoding_weight_matrix srsran::make_four_layer_four_ports_type1_sp(unsigned beam_azimuth_id, unsigned pol_shift_id)
+{
+  // Beam oversampling factor in the horizontal plane, from TS38.214 Section 5.2.2.2.1.
+  static constexpr unsigned O_1 = 4;
+  // Number of cross-polarized antenna elements, from TS38.214 Section 5.2.2.2.1.
+  static constexpr unsigned N_1 = 2;
+  // Number of possible horizontal beams to choose from.
+  static constexpr unsigned nof_beams = O_1 * N_1;
+  // Number of possible polarization phase shifts to choose from.
+  static constexpr unsigned nof_pol_shifts = 2;
+
+  static constexpr interval<unsigned, false> beam_azimuth_range(0, nof_beams);
+  static constexpr interval<unsigned, false> pol_phase_shift_range(0, nof_pol_shifts);
+
+  srsran_assert(beam_azimuth_range.contains(beam_azimuth_id),
+                "The given beam azimuth identifier i1_1 (i.e., {}) is out of the range {}",
+                beam_azimuth_id,
+                beam_azimuth_range);
+
+  srsran_assert(pol_phase_shift_range.contains(pol_shift_id),
+                "The given polarization phase shift i2 (i.e., {}) is out of the range {}",
+                pol_shift_id,
+                pol_phase_shift_range);
+
+  // Precoding weight matrix for three layers mapped into four antenna ports.
+  precoding_weight_matrix result(4, 4);
+
+  // Power normalization factor. It is equal to the square root of the number of ports times the number of layers.
+  static constexpr float scaling = 1.0F / 4.0F;
+
+  // Select k1 as per TS38.214 Section 5.2.2.2.1-4.
+  static constexpr unsigned k1 = O_1;
+
+  // Phase increment for each beam coefficient. This defines the direction of the beam in the horizontal plane.
+  float phase_increment_rad = static_cast<float>(beam_azimuth_id) * 2.0F * M_PI / static_cast<float>(nof_beams);
+  // Polarization phase shift. This defines the relative phase between the cross-polarized antenna elements.
+  float pol_phase_shift_rad = M_PI_2 * static_cast<float>(pol_shift_id);
+
+  std::array<cf_t, N_1> beam;
+
+  // Layer 0.
+
+  // Beam for the first polarization.
+  create_horizontal_beam(beam, phase_increment_rad, 0.0F, scaling);
+
+  // Set weights for the first polarization.
+  result.set_coefficient(beam[0], 0, 0);
+  result.set_coefficient(beam[1], 0, 1);
+
+  // Beam for the second polarization.
+  create_horizontal_beam(beam, phase_increment_rad, pol_phase_shift_rad, scaling);
+
+  // Set weights for the second polarization.
+  result.set_coefficient(beam[0], 0, 2);
+  result.set_coefficient(beam[1], 0, 3);
+
+  // Layer 2. The phase increment for adjacent beam antenna elements is the same as layer 0.
+
+  // Beam for the first polarization.
+  create_horizontal_beam(beam, phase_increment_rad, 0.0F, scaling);
+
+  // Set weights for the first polarization.
+  result.set_coefficient(beam[0], 2, 0);
+  result.set_coefficient(beam[1], 2, 1);
+
+  // Beam for the second polarization.
+  create_horizontal_beam(beam, phase_increment_rad, pol_phase_shift_rad + M_PI, scaling);
+
+  // Set weights for the second polarization.
+  result.set_coefficient(beam[0], 2, 2);
+  result.set_coefficient(beam[1], 2, 3);
+
+  // Layer 1. Recalculate phase increment for adjacent beam antenna elements.
+  phase_increment_rad = static_cast<float>(beam_azimuth_id + k1) * 2.0F * M_PI / static_cast<float>(nof_beams);
+
+  // Beam for the first polarization.
+  create_horizontal_beam(beam, phase_increment_rad, 0.0F, scaling);
+
+  // Set weights for the first polarization.
+  result.set_coefficient(beam[0], 1, 0);
+  result.set_coefficient(beam[1], 1, 1);
+
+  // Beam for the second polarization.
+  create_horizontal_beam(beam, phase_increment_rad, pol_phase_shift_rad, scaling);
+
+  // Set weights for the second polarization.
+  result.set_coefficient(beam[0], 1, 2);
+  result.set_coefficient(beam[1], 1, 3);
+
+  // Layer 3. The phase increment for adjacent beam antenna elements is the same as layer 1.
+
+  // Beam for the first polarization.
+  create_horizontal_beam(beam, phase_increment_rad, 0.0F, scaling);
+
+  // Set weights for the first polarization.
+  result.set_coefficient(beam[0], 3, 0);
+  result.set_coefficient(beam[1], 3, 1);
+
+  // Beam for the second polarization.
+  create_horizontal_beam(beam, phase_increment_rad, pol_phase_shift_rad + M_PI, scaling);
+
+  // Set weights for the second polarization.
+  result.set_coefficient(beam[0], 3, 2);
+  result.set_coefficient(beam[1], 3, 3);
+
+  return result;
+}
