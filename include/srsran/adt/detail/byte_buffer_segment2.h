136302277774ccf4e38d7de35c3e5e8e86b2718d
Francisco Paisana <francisco.paisana@softwareradiosystems.com>
2023-06-30 17:57:57 +0200

    adt: new byte_buffer_segment design without simple span and no next pointer

---
+++ include/srsran/adt/detail/byte_buffer_segment2.h
@@ -0,0 +1,176 @@
+/*
+ *
+ * Copyright 2021-2023 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#pragma once
+
+#include "srsran/adt/span.h"
+#include "srsran/support/srsran_assert.h"
+#include <array>
+#include <cstdint>
+
+namespace srsran {
+
+/// \brief Span of bytes (non owning) divided into three parts [ HEADROOM | PAYLOAD | TAILROOM ].
+/// Bytes can be added in the HEADROOM region via prepend() or in the TAILROOM via append().
+class byte_buffer_segment2
+{
+public:
+  using value_type     = uint8_t;
+  using iterator       = uint8_t*;
+  using const_iterator = const uint8_t*;
+
+  /// \brief Initialize a byte_buffer_segment with no headroom, tailroom and payload.
+  byte_buffer_segment2() = default;
+
+  /// \brief Initialize byte_buffer_segment with a given buffer and headroom size. The payload size is set to 0 and the
+  /// tailroom size will be equal to the given buffer size minus the headroom size.
+  ///
+  /// \param buffer_ Buffer where bytes will be stored.
+  /// \param headroom_ Initial size of the headroom.
+  explicit byte_buffer_segment2(span<uint8_t> buffer_, size_t headroom_) :
+    buffer(buffer_), payload(buffer_.subspan(headroom_, 0))
+  {
+  }
+
+  /// \brief Returns the total number of bytes that the segment can store, i.e. the sum of the payload, headroom and
+  /// tailroom sizes.
+  size_t capacity() const { return buffer.size(); }
+
+  /// Checks whether segment has no payload.
+  bool empty() const { return begin() == end(); }
+
+  /// Returns how much space in bytes there is at the head of the segment.
+  size_t headroom() const { return begin() - buffer.begin(); }
+
+  /// Returns size in bytes of segment.
+  size_t length() const { return payload.size(); }
+
+  /// Returns how much space in bytes there is at the tail of the segment.
+  size_t tailroom() const { return buffer.end() - end(); }
+
+  /// \brief Get the last byte of the byte_buffer_segment payload.
+  uint8_t& back()
+  {
+    srsran_assert(not empty(), "back() called for empty segment.");
+    return payload.back();
+  }
+  const uint8_t& back() const
+  {
+    srsran_assert(not empty(), "back() called for empty segment.");
+    return payload.back();
+  }
+
+  /// \brief Get a pointer to the byte_buffer_segment payload.
+  const uint8_t* data() const { return begin(); }
+  uint8_t*       data() { return begin(); }
+
+  /// Index-based access to the payload of the byte_buffer_segment.
+  uint8_t& operator[](size_t idx)
+  {
+    srsran_assert(idx < length(), "Out-of-bound access");
+    return *(begin() + idx);
+  }
+  const uint8_t& operator[](size_t idx) const
+  {
+    srsran_assert(idx < length(), "Out-of-bound access");
+    return *(begin() + idx);
+  }
+
+  /// Resizes payload of segment. If the new size is larger than the old size, the new bytes are uninitialized.
+  void resize(size_t nof_bytes)
+  {
+    srsran_assert(nof_bytes <= capacity() - headroom(), "There is not enough space for provided size");
+    payload = span<uint8_t>{payload.begin(), payload.begin() + nof_bytes};
+  }
+
+  /// Appends a span of bytes at the tail of the segment.
+  void append(span<const uint8_t> bytes) { append(bytes.begin(), bytes.end()); }
+
+  /// Appends a iterator range of bytes at the tail of the segment.
+  template <typename It>
+  void append(It it_begin, It it_end)
+  {
+    static_assert(std::is_same<std::decay_t<decltype(*it_begin)>, uint8_t>::value, "Invalid value_type");
+    static_assert(
+        std::is_same<typename std::iterator_traits<It>::iterator_category, std::random_access_iterator_tag>::value,
+        "Only random access iterators allowed.");
+    srsran_assert((size_t)(it_end - it_begin) <= tailroom(), "There is not enough tailroom for append.");
+    iterator new_end = std::copy(it_begin, it_end, end());
+    payload          = span<uint8_t>{payload.begin(), new_end};
+  }
+
+  /// Appends single byte at the tail of the segment.
+  void append(uint8_t byte)
+  {
+    srsran_assert(tailroom() >= 1, "There is not enough tailroom space.");
+    payload        = span<uint8_t>{payload.begin(), payload.end() + 1};
+    payload.back() = byte;
+  }
+
+  /// Prepends segment with provided span of bytes.
+  void prepend(span<const uint8_t> bytes)
+  {
+    srsran_assert(headroom() >= bytes.size(), "There is not enough headroom space.");
+    payload = span<uint8_t>{payload.begin() - bytes.size(), payload.end()};
+    std::copy(bytes.begin(), bytes.end(), begin());
+  }
+
+  /// Reserves headroom space in segment.
+  /// \param nof_bytes Number of bytes to reserve.
+  void reserve_prepend(size_t nof_bytes)
+  {
+    srsran_assert(headroom() >= nof_bytes, "There is not enough headroom space.");
+    payload = span<uint8_t>{payload.begin() - nof_bytes, payload.end()};
+  }
+
+  /// Removes "nof_bytes" from the head of the segment.
+  void trim_head(size_t nof_bytes)
+  {
+    srsran_assert(nof_bytes <= length(), "There is not enough headroom space.");
+    payload = span<uint8_t>{payload.begin() + nof_bytes, payload.end()};
+  }
+
+  /// Removes "nof_bytes" from the tail of the segment.
+  void trim_tail(size_t nof_bytes)
+  {
+    srsran_assert(nof_bytes <= length(), "There is not enough headroom space.");
+    payload = span<uint8_t>{payload.begin(), payload.end() - nof_bytes};
+  }
+
+  /// \brief Returns iterator pointing to the start of the payload.
+  iterator       begin() { return payload.begin(); }
+  const_iterator begin() const { return payload.begin(); }
+
+  /// \brief Returns iterator pointing to the end of the payload.
+  iterator       end() { return payload.end(); }
+  const_iterator end() const { return payload.end(); }
+
+  /// \brief Checks if the payload of a segment is equal to the content of another range.
+  template <typename Container>
+  bool operator==(const Container& other) const
+  {
+    static_assert(std::is_same<std::decay_t<decltype(*other.begin())>, uint8_t>::value, "Invalid value_type");
+    return std::equal(begin(), end(), other.begin(), other.end());
+  }
+  template <typename Container>
+  bool operator!=(const Container& other) const
+  {
+    return !(*this == other);
+  }
+
+private:
+  // Range of bytes spanning the beginning of the headroom and end of the tailroom.
+  span<const uint8_t> buffer;
+
+  // Range of bytes where the payload is stored.
+  span<uint8_t> payload;
+};
+
+} // namespace srsran
---
+++ tests/unittests/adt/byte_buffer_segment2_test.cpp
@@ -0,0 +1,140 @@
+/*
+ *
+ * Copyright 2021-2023 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "srsran/adt/detail/byte_buffer_segment2.h"
+#include "srsran/support/test_utils.h"
+#include <gtest/gtest.h>
+
+using namespace srsran;
+
+///////////////////////// byte_buffer_segment //////////////////////////////
+
+TEST(buffer_segment_test2, default_ctor)
+{
+  byte_buffer_segment2 segment;
+  ASSERT_TRUE(segment.empty());
+  ASSERT_EQ(segment.length(), 0);
+  ASSERT_EQ(segment.capacity(), 0);
+  ASSERT_EQ(segment.headroom(), 0);
+  ASSERT_EQ(segment.tailroom(), 0);
+  ASSERT_EQ(segment.begin(), segment.end());
+  ASSERT_EQ(segment, byte_buffer_segment2{});
+}
+
+TEST(buffer_segment_test2, segment_with_no_data)
+{
+  std::vector<uint8_t> buffer(1024);
+  byte_buffer_segment2 segment{buffer, 16}, segment2{buffer, 15};
+  ASSERT_TRUE(segment.empty());
+  ASSERT_EQ(0, segment.length());
+  ASSERT_EQ(segment.capacity(), buffer.size());
+  ASSERT_EQ(16, segment.headroom());
+  ASSERT_EQ(segment.capacity(), segment.tailroom() + segment.headroom());
+  ASSERT_EQ(segment.begin(), segment.end());
+  ASSERT_EQ(segment, segment2)
+      << "The fact that the segments have different headroom/tailroom space should not affect operator==";
+}
+
+TEST(buffer_segment_test2, append_bytes_on_empty_buffer)
+{
+  std::vector<uint8_t>       buffer(1024);
+  byte_buffer_segment2       segment{buffer, 16};
+  const std::vector<uint8_t> bytes = test_rgen::random_vector<uint8_t>(6);
+
+  segment.append(bytes);
+  ASSERT_EQ(bytes.size(), segment.length());
+  ASSERT_EQ(segment, bytes);
+  ASSERT_TRUE(std::equal(segment.begin(), segment.end(), bytes.begin(), bytes.end()));
+  ASSERT_EQ(segment[5], bytes[5]);
+}
+
+TEST(buffer_segment_test2, prepend_bytes_on_empty_buffer)
+{
+  std::vector<uint8_t> buffer(1024);
+  byte_buffer_segment2 segment{buffer, 16};
+
+  const std::vector<uint8_t> bytes = test_rgen::random_vector<uint8_t>(6);
+  segment.prepend(bytes);
+  ASSERT_EQ(bytes.size(), segment.length());
+  ASSERT_EQ(segment, bytes);
+  ASSERT_TRUE(std::equal(segment.begin(), segment.end(), bytes.begin(), bytes.end()));
+}
+
+TEST(buffer_segment_test2, multiple_appends)
+{
+  std::vector<uint8_t> buffer(1024);
+  byte_buffer_segment2 segment{buffer, 16};
+  std::vector<uint8_t> tot_bytes;
+
+  for (unsigned i = 0; i != 5; ++i) {
+    const std::vector<uint8_t> bytes = test_rgen::random_vector<uint8_t>(6);
+    tot_bytes.insert(tot_bytes.end(), bytes.begin(), bytes.end());
+    segment.append(bytes);
+  }
+  ASSERT_EQ(segment, tot_bytes);
+}
+
+TEST(buffer_segment_test2, trim_head)
+{
+  std::vector<uint8_t>       buffer(1024);
+  byte_buffer_segment2       segment{buffer, 16};
+  const std::vector<uint8_t> bytes = test_rgen::random_vector<uint8_t>(6);
+  segment.append(bytes);
+
+  const unsigned n = test_rgen::uniform_int<unsigned>(1, bytes.size());
+  segment.trim_head(n);
+  ASSERT_EQ(segment, span<const uint8_t>{bytes}.subspan(n, bytes.size() - n));
+  ASSERT_TRUE(std::equal(segment.begin(), segment.end(), bytes.begin() + n, bytes.end()));
+}
+
+TEST(buffer_segment_test2, trim_tail)
+{
+  std::vector<uint8_t>       buffer(1024);
+  byte_buffer_segment2       segment{buffer, 16};
+  const std::vector<uint8_t> bytes = test_rgen::random_vector<uint8_t>(6);
+  segment.append(bytes);
+
+  const unsigned n = test_rgen::uniform_int<unsigned>(1, bytes.size());
+  segment.trim_tail(n);
+  ASSERT_EQ(segment, span<const uint8_t>{bytes}.subspan(0, bytes.size() - n));
+  ASSERT_TRUE(std::equal(segment.begin(), segment.end(), bytes.begin(), bytes.end() - n));
+}
+
+TEST(buffer_segment_test2, copy_ctor)
+{
+  std::vector<uint8_t>       buffer(1024);
+  byte_buffer_segment2       segment{buffer, 16};
+  const std::vector<uint8_t> bytes = test_rgen::random_vector<uint8_t>(6);
+  segment.append(bytes);
+
+  byte_buffer_segment2 segment2 = segment;
+  ASSERT_EQ(segment2, segment);
+  ASSERT_TRUE(std::equal(segment2.begin(), segment2.end(), bytes.begin(), bytes.end()));
+
+  segment.trim_tail(1);
+  ASSERT_NE(segment.length(), segment2.length());
+  ASSERT_NE(segment, segment2);
+  segment.append(10);
+  ASSERT_EQ(segment, segment2);
+  segment[3] = 240;
+  ASSERT_EQ(segment, segment2);
+}
+
+TEST(buffer_segment_test2, move_ctor)
+{
+  std::vector<uint8_t>       buffer(1024);
+  byte_buffer_segment2       segment{buffer, 32};
+  const std::vector<uint8_t> bytes = test_rgen::random_vector<uint8_t>(6);
+  segment.append(bytes);
+
+  byte_buffer_segment2 segment2 = std::move(segment);
+  ASSERT_EQ(segment, segment2);
+  ASSERT_TRUE(std::equal(segment2.begin(), segment2.end(), bytes.begin(), bytes.end()));
+}
