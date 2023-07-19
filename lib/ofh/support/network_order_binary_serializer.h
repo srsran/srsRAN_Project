e573f9a6a2c11b45e486b0d40ab4fc0f64fab39e
sauka <pavel.harbanau@softwareradiosystems.com>
2023-07-19 16:45:10 +0300

    ofh: optimize serialization of byte buffers

--- lib/ofh/support/network_order_binary_serializer.h
+++ lib/ofh/support/network_order_binary_serializer.h
@@ -62,6 +62,16 @@ public:
     }
   }
 
+  /// Serializes the given span of bytes and advances the position by the span size.
+  void write(span<const uint8_t> s)
+  {
+    std::memcpy(ptr, s.data(), s.size());
+    advance(s.size());
+  }
+
+  /// Serializes the given span of bytes and advances the position by the span size.
+  void write(span<uint8_t> s) { write(span<const uint8_t>(s)); }
+
   /// Advances the offset by the given amount.
   void advance(unsigned x)
   {
