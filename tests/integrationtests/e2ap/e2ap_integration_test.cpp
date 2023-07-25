00997b48be3b2cfbe1237a546cb8b69fa629b447
Piotr Gawlowicz <piotr@srs.io>
2023-07-25 18:07:57 +0200

    e2ap: add integration test with e2_gateway_remote_connector

--- tests/integrationtests/e2ap/e2ap_integration_test.cpp
+++ tests/integrationtests/e2ap/e2ap_integration_test.cpp
@@ -8,7 +8,7 @@
  *
  */
 
-#include "lib/cu_cp/ue_manager_impl.h"
+#include "apps/gnb/adapters/e2_gateway_remote_connector.h"
 #include "lib/e2/common/e2ap_asn1_packer.h"
 #include "tests/unittests/e2/common/e2_test_helpers.h"
 #include "srsran/e2/e2_factory.h"
@@ -22,19 +22,18 @@
 #include <gtest/gtest.h>
 
 using namespace srsran;
-using namespace srs_cu_cp;
 
 /// This test is an integration test between:
 /// * E2AP (including ASN1 packer and E2 setup procedure)
 /// * SCTP network gateway
 /// * IO broker
-class e2ap_network_adapter : public e2_message_notifier,
-                             public e2_message_handler,
-                             public sctp_network_gateway_control_notifier,
-                             public network_gateway_data_notifier
+class dummy_e2ap_network_adapter : public e2_message_notifier,
+                                   public e2_message_handler,
+                                   public sctp_network_gateway_control_notifier,
+                                   public network_gateway_data_notifier
 {
 public:
-  e2ap_network_adapter(const sctp_network_gateway_config& nw_config_) :
+  dummy_e2ap_network_adapter(const sctp_network_gateway_config& nw_config_) :
     nw_config(nw_config_),
     epoll_broker(create_io_broker(io_broker_type::epoll)),
     gw(create_sctp_network_gateway({nw_config, *this, *this})),
@@ -44,19 +43,29 @@ public:
     epoll_broker->register_fd(gw->get_socket_fd(), [this](int fd) { gw->receive(); });
   }
 
-  ~e2ap_network_adapter() {}
+  ~dummy_e2ap_network_adapter() {}
 
   void connect_e2ap(e2_interface* e2ap_) { e2ap = e2ap_; }
 
+  e2_message last_e2_msg;
+
 private:
   // E2AP calls interface to send (unpacked) E2AP PDUs
-  void on_new_message(const e2_message& msg) override { packer.handle_message(msg); }
+  void on_new_message(const e2_message& msg) override
+  {
+    last_e2_msg = msg;
+    packer.handle_message(msg);
+  }
 
   // SCTP network gateway calls interface to inject received PDUs (ASN1 packed)
   void on_new_pdu(byte_buffer pdu) override { packer.handle_packed_pdu(pdu); }
 
   // The packer calls this interface to inject unpacked E2AP PDUs
-  void handle_message(const e2_message& msg) override { e2ap->handle_message(msg); }
+  void handle_message(const e2_message& msg) override
+  {
+    last_e2_msg = msg;
+    e2ap->handle_message(msg);
+  }
 
   /// \brief Simply log those events for now
   void on_connection_loss() override { test_logger.info("on_connection_loss"); }
@@ -91,7 +100,7 @@ protected:
     nw_config.bind_port         = 0;
     nw_config.non_blocking_mode = true;
 
-    adapter              = std::make_unique<e2ap_network_adapter>(nw_config);
+    adapter              = std::make_unique<dummy_e2ap_network_adapter>(nw_config);
     du_metrics           = std::make_unique<dummy_e2_du_metrics>();
     e2sm_packer          = std::make_unique<e2sm_kpm_asn1_packer>();
     e2sm_iface           = std::make_unique<e2sm_kpm_impl>(test_logger, *e2sm_packer, *du_metrics);
@@ -103,18 +112,18 @@ protected:
     adapter->connect_e2ap(e2ap.get());
   }
 
-  e2ap_configuration                       cfg;
-  timer_factory                            factory;
-  timer_manager                            timers;
-  std::unique_ptr<e2ap_network_adapter>    adapter;
-  manual_task_worker                       ctrl_worker{128};
-  std::unique_ptr<dummy_e2ap_pcap>         pcap;
-  std::unique_ptr<e2_subscription_manager> e2_subscription_mngr;
-  std::unique_ptr<e2sm_handler>            e2sm_packer;
-  std::unique_ptr<e2_du_metrics_interface> du_metrics;
-  std::unique_ptr<e2sm_interface>          e2sm_iface;
-  std::unique_ptr<e2_interface>            e2ap;
-  srslog::basic_logger&                    test_logger = srslog::fetch_basic_logger("TEST");
+  e2ap_configuration                          cfg;
+  timer_factory                               factory;
+  timer_manager                               timers;
+  std::unique_ptr<dummy_e2ap_network_adapter> adapter;
+  manual_task_worker                          ctrl_worker{128};
+  std::unique_ptr<dummy_e2ap_pcap>            pcap;
+  std::unique_ptr<e2_subscription_manager>    e2_subscription_mngr;
+  std::unique_ptr<e2sm_handler>               e2sm_packer;
+  std::unique_ptr<e2_du_metrics_interface>    du_metrics;
+  std::unique_ptr<e2sm_interface>             e2sm_iface;
+  std::unique_ptr<e2_interface>               e2ap;
+  srslog::basic_logger&                       test_logger = srslog::fetch_basic_logger("TEST");
 };
 
 /// Test successful e2 setup procedure
@@ -127,6 +136,64 @@ TEST_F(e2ap_integration_test, when_e2_setup_response_received_then_ric_connected
 
   // Status: Procedure not yet ready.
   ASSERT_FALSE(t.ready());
+
+  // Status: received E2 Setup Request.
+  ASSERT_EQ(adapter->last_e2_msg.pdu.type().value, asn1::e2ap::e2_ap_pdu_c::types_opts::init_msg);
+  ASSERT_EQ(adapter->last_e2_msg.pdu.init_msg().value.type().value,
+            asn1::e2ap::e2_ap_elem_procs_o::init_msg_c::types_opts::e2setup_request);
+
+  std::this_thread::sleep_for(std::chrono::seconds(3));
+
+  ASSERT_EQ(adapter->last_e2_msg.pdu.type().value, asn1::e2ap::e2_ap_pdu_c::types_opts::successful_outcome);
+  ASSERT_EQ(adapter->last_e2_msg.pdu.init_msg().value.type().value,
+            asn1::e2ap::e2_ap_elem_procs_o::successful_outcome_c::types_opts::e2setup_resp);
+}
+
+class e2ap_gw_connector_integration_test : public ::testing::Test
+{
+protected:
+  void SetUp() override
+  {
+    srslog::fetch_basic_logger("TEST").set_level(srslog::basic_levels::debug);
+    srslog::init();
+
+    cfg                  = srsran::config_helpers::make_default_e2ap_config();
+    cfg.e2sm_kpm_enabled = true;
+    cfg.gnb_id           = 123;
+
+    sctp_network_gateway_config nw_config;
+    nw_config.connect_address   = "127.0.0.1";
+    nw_config.connect_port      = 36421;
+    nw_config.bind_address      = "127.0.0.101";
+    nw_config.bind_port         = 0;
+    nw_config.non_blocking_mode = true;
+
+    epoll_broker = create_io_broker(io_broker_type::epoll);
+    factory      = timer_factory{timers, ctrl_worker};
+    pcap         = std::make_unique<dummy_e2ap_pcap>();
+    du_metrics   = std::make_unique<dummy_e2_du_metrics>();
+    e2_client    = std::make_unique<e2_gateway_remote_connector>(*epoll_broker, nw_config, *pcap);
+    e2ap         = create_e2_entity(cfg, e2_client.get(), *du_metrics, factory, ctrl_worker);
+  }
+
+  e2ap_configuration                           cfg;
+  std::unique_ptr<io_broker>                   epoll_broker;
+  timer_manager                                timers;
+  manual_task_worker                           ctrl_worker{128};
+  timer_factory                                factory;
+  std::unique_ptr<dummy_e2ap_pcap>             pcap;
+  std::unique_ptr<e2_du_metrics_interface>     du_metrics;
+  std::unique_ptr<e2_gateway_remote_connector> e2_client;
+  std::unique_ptr<e2_interface>                e2ap;
+  srslog::basic_logger&                        test_logger = srslog::fetch_basic_logger("TEST");
+};
+
+/// Test successful e2 setup procedure
+TEST_F(e2ap_gw_connector_integration_test, when_e2_setup_response_received_then_ric_connected)
+{
+  // Action 1: Launch E2 setup procedure
+  test_logger.info("Launching E2 setup procedure...");
+  e2ap->start();
 
   std::this_thread::sleep_for(std::chrono::seconds(3));
 }
