f67d356498a2cba675c36bf80f88adfcf58fda0f
Fabian Eckermann <fabian@srs.io>
2023-07-31 14:56:00 +0200

    cu_cp,rrc: Remove hardcoded values

--- lib/rrc/ue/rrc_ue_impl.cpp
+++ lib/rrc/ue/rrc_ue_impl.cpp
@@ -94,9 +94,7 @@ void rrc_ue_impl::on_new_as_security_context()
 
 async_task<bool> rrc_ue_impl::handle_init_security_context(const security::security_context& sec_ctx)
 {
-  context.sec_context.k                   = sec_ctx.k;
-  context.sec_context.supported_int_algos = {true, true, true};
-  context.sec_context.supported_enc_algos = {true, true, true};
+  context.sec_context = sec_ctx;
   //  Launch RRC security mode procedure
   return launch_async<rrc_security_mode_command_procedure>(context, sec_ctx, *this, *event_mng, logger);
 }
