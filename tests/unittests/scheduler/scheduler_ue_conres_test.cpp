1cdd68ec9c15cae0a9c0a1bc538872f096fbf060
Francisco Paisana <francisco.paisana@srs.io>
2023-08-14 17:33:51 +0200

    sched: reuse same test for msg4 with srb0 and srb1

--- tests/unittests/scheduler/scheduler_ue_conres_test.cpp
+++ tests/unittests/scheduler/scheduler_ue_conres_test.cpp
@@ -19,10 +19,10 @@
 
 using namespace srsran;
 
-class scheduler_ue_conres_test : public scheduler_test_bench, public ::testing::Test
+class base_scheduler_conres_test : public scheduler_test_bench
 {
-protected:
-  scheduler_ue_conres_test()
+public:
+  base_scheduler_conres_test()
   {
     add_cell(test_helpers::make_default_sched_cell_configuration_request());
 
@@ -37,7 +37,11 @@ protected:
   rnti_t        rnti     = to_rnti(0x4601);
 };
 
-TEST_F(scheduler_ue_conres_test, when_conres_ce_is_enqueued_and_no_msg4_is_enqueued_then_pdsch_is_not_scheduled)
+class scheduler_conres_without_pdu_test : public base_scheduler_conres_test, public ::testing::Test
+{};
+
+TEST_F(scheduler_conres_without_pdu_test,
+       when_conres_ce_is_enqueued_and_no_msg4_is_enqueued_then_pdsch_is_not_scheduled)
 {
   // Enqueue ConRes CE.
   this->sched->handle_dl_mac_ce_indication(dl_mac_ce_indication{ue_index, lcid_dl_sch_t::UE_CON_RES_ID});
@@ -46,15 +50,29 @@ TEST_F(scheduler_ue_conres_test, when_conres_ce_is_enqueued_and_no_msg4_is_enque
   ASSERT_FALSE(this->run_slot_until([this]() { return find_ue_pdsch(rnti, *this->last_sched_res) != nullptr; }));
 }
 
-TEST_F(scheduler_ue_conres_test, when_conres_ce_and_srb0_pdu_are_enqueued_then_tcrnti_is_used)
+// ------------------------------------------------------------------------------------------------------------------ //
+
+struct test_params {
+  lcid_t msg4_lcid;
+};
+
+class scheduler_con_res_msg4_test : public base_scheduler_conres_test, public ::testing::TestWithParam<test_params>
+{
+public:
+  scheduler_con_res_msg4_test() : params(GetParam()) {}
+
+  test_params params;
+};
+
+TEST_F(scheduler_con_res_msg4_test, when_conres_ce_and_srb_pdu_are_enqueued_then_tc_rnti_is_used)
 {
   const static unsigned msg4_size = 128;
 
   // Enqueue ConRes CE.
   this->sched->handle_dl_mac_ce_indication(dl_mac_ce_indication{ue_index, lcid_dl_sch_t::UE_CON_RES_ID});
 
-  // Enqueue Msg4 in SRB0.
-  this->push_dl_buffer_state(dl_buffer_state_indication_message{this->ue_index, LCID_SRB0, msg4_size});
+  // Enqueue Msg4 in SRB0/SRB1.
+  this->push_dl_buffer_state(dl_buffer_state_indication_message{this->ue_index, params.msg4_lcid, msg4_size});
 
   // Ensure the Msg4 is scheduled together with the ConRes CE.
   ASSERT_TRUE(this->run_slot_until([this]() { return find_ue_pdsch(rnti, *this->last_sched_res) != nullptr; }));
@@ -62,10 +80,13 @@ TEST_F(scheduler_ue_conres_test, when_conres_ce_and_srb0_pdu_are_enqueued_then_t
   ASSERT_EQ(msg4_alloc->tb_list.size(), 1);
   ASSERT_EQ(msg4_alloc->tb_list[0].lc_chs_to_sched.size(), 2);
   ASSERT_EQ(msg4_alloc->tb_list[0].lc_chs_to_sched[0].lcid, lcid_dl_sch_t::UE_CON_RES_ID);
-  ASSERT_EQ(msg4_alloc->tb_list[0].lc_chs_to_sched[1].lcid, LCID_SRB0);
+  ASSERT_EQ(msg4_alloc->tb_list[0].lc_chs_to_sched[1].lcid, params.msg4_lcid);
 
-  // Ensure enough bytes are scheduled to fit the full Msg4, as RLC-TM doesn't support segmentation.
-  ASSERT_GE(msg4_alloc->tb_list[0].lc_chs_to_sched[1].sched_bytes, msg4_size);
+  // In case of Msg4 with SRB0, ensure that enough bytes are scheduled to fit the full Msg4, as RLC-TM doesn't support
+  // segmentation.
+  if (params.msg4_lcid == LCID_SRB0) {
+    ASSERT_GE(msg4_alloc->tb_list[0].lc_chs_to_sched[1].sched_bytes, msg4_size);
+  }
 
   // Ensure TC-RNTI is used for Msg4.
   ASSERT_EQ(find_ue_dl_pdcch(rnti)->dci.type, dci_dl_rnti_config_type::tc_rnti_f1_0);
@@ -74,22 +95,6 @@ TEST_F(scheduler_ue_conres_test, when_conres_ce_and_srb0_pdu_are_enqueued_then_t
   ASSERT_TRUE(this->last_sched_res->dl.csi_rs.empty());
 }
 
-TEST_F(scheduler_ue_conres_test, when_conres_ce_and_srb1_pdu_are_enqueued_then_tcrnti_is_used)
-{
-  // Enqueue ConRes CE.
-  this->sched->handle_dl_mac_ce_indication(dl_mac_ce_indication{ue_index, lcid_dl_sch_t::UE_CON_RES_ID});
-
-  // Enqueue Msg4 in SRB1.
-  this->push_dl_buffer_state(dl_buffer_state_indication_message{this->ue_index, LCID_SRB1, 128});
-
-  // Ensure the Msg4 is scheduled with a ConRes CE.
-  ASSERT_TRUE(this->run_slot_until([this]() { return find_ue_pdsch(rnti, *this->last_sched_res) != nullptr; }));
-  const dl_msg_alloc* msg4_alloc = find_ue_pdsch(rnti, *this->last_sched_res);
-  ASSERT_EQ(msg4_alloc->tb_list.size(), 1);
-  ASSERT_EQ(msg4_alloc->tb_list[0].lc_chs_to_sched.size(), 2);
-  ASSERT_EQ(msg4_alloc->tb_list[0].lc_chs_to_sched[0].lcid, lcid_dl_sch_t::UE_CON_RES_ID);
-  ASSERT_EQ(msg4_alloc->tb_list[0].lc_chs_to_sched[1].lcid, LCID_SRB1);
-
-  // Ensure TC-RNTI is used.
-  ASSERT_EQ(find_ue_dl_pdcch(rnti)->dci.type, dci_dl_rnti_config_type::tc_rnti_f1_0);
-}
+INSTANTIATE_TEST_SUITE_P(scheduler_con_res_msg4_test,
+                         scheduler_con_res_msg4_test,
+                         ::testing::Values(test_params{LCID_SRB0}, test_params{LCID_SRB1}));
