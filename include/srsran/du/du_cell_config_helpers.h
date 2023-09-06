82bf6380af9fd474a4d7efa0af8acdc09912b35b
Supreeth Herle <supreeth.herle@srs.io>
2023-09-06 15:53:43 +0200

    du: increase max MCS for msg4 to handle cases where CORESET#0 occupies 3 OFDM symbols

--- include/srsran/du/du_cell_config_helpers.h
+++ include/srsran/du/du_cell_config_helpers.h
@@ -50,8 +50,11 @@ inline scheduler_expert_config make_default_scheduler_expert_config()
   cfg.ue.ul_mcs             = {0, 28};
   cfg.ue.pusch_rv_sequence  = {0};
   cfg.ue.max_nof_harq_retxs = 4;
-  // Note: A MCS index of 7 can handle Msg4 of size 458 bytes.
-  cfg.ue.max_msg4_mcs                            = 7;
+  // The UE is not expected to decode a PDSCH scheduled with P-RNTI, RA-RNTI, SI-RNTI and Qm > 2 i.e. MCS index of 9 in
+  // Table 5.1.3.1-1 of TS 38.214.
+  // Note: A MCS index of 7 can handle Msg4 of size 458 bytes for PDSCH occupying symbols (2,..,14].
+  // Note: A MCS index of 8 can handle Msg4 of size 325 bytes for PDSCH occupying symbols (3,..,14].
+  cfg.ue.max_msg4_mcs                            = 9;
   cfg.ue.initial_ul_sinr                         = 5;
   cfg.ue.enable_csi_rs_pdsch_multiplexing        = true;
   cfg.ue.ta_measurement_slot_period              = 20;
