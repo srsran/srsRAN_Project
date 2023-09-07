0cd8a848ebae4b7c4feb119a1caf9196a6f89a9f
Supreeth Herle <supreeth.herle@srs.io>
2023-09-07 19:15:20 +0200

    sched: remove outliers in TA offset measurements while computing average

--- lib/scheduler/ue_scheduling/ta_manager.cpp
+++ lib/scheduler/ue_scheduling/ta_manager.cpp
@@ -9,6 +9,7 @@
  */
 
 #include "ta_manager.h"
+#include <numeric>
 
 using namespace srsran;
 
@@ -70,11 +71,31 @@ void ta_manager::slot_indication(slot_point current_sl)
 
 int64_t ta_manager::compute_avg_n_ta_difference(uint8_t tag_id)
 {
+  // Adjust this threshold as needed.
+  static const double num_std_deviations = 2.0;
+
+  const auto n_ta_diff_meas = tag_n_ta_diff_measurements[tag_id];
+
+  // Compute mean.
+  const double sum  = std::accumulate(n_ta_diff_meas.begin(), n_ta_diff_meas.end(), 0.0);
+  const double mean = sum / static_cast<double>(n_ta_diff_meas.size());
+
+  // Compute standard deviation.
+  std::vector<double> diff(n_ta_diff_meas.size());
+  std::transform(
+      n_ta_diff_meas.begin(), n_ta_diff_meas.end(), diff.begin(), [mean](double value) { return value - mean; });
+  const double sq_sum  = std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
+  const double std_dev = std::sqrt(sq_sum / static_cast<double>(n_ta_diff_meas.size()));
+
   int64_t avg_n_ta_difference = 0;
-  for (const auto& meas : tag_n_ta_diff_measurements[tag_id]) {
-    avg_n_ta_difference += meas;
+  for (const auto& meas : n_ta_diff_meas) {
+    // Filter out outliers.
+    if (std::abs((double)meas - mean) <= num_std_deviations * std_dev) {
+      avg_n_ta_difference += meas;
+    }
   }
-  return avg_n_ta_difference / static_cast<int64_t>(tag_n_ta_diff_measurements[tag_id].size());
+
+  return avg_n_ta_difference / static_cast<int64_t>(n_ta_diff_meas.size());
 }
 
 unsigned ta_manager::compute_new_t_a(int64_t n_ta_diff)
