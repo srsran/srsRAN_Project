41af1ad41123f5a85d3b625e0a184681de32d121
Fabian Eckermann <fabian@srs.io>
2023-09-11 10:14:21 +0200

    e1ap: Move e1ap message to dedicated file

---
+++ include/srsran/e1ap/common/e1ap_message.h
@@ -0,0 +1,22 @@
+/*
+ *
+ * Copyright 2021-2023 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#pragma once
+
+#include "srsran/asn1/e1ap/e1ap.h"
+
+namespace srsran {
+
+/// \brief E1AP message transferred between a CU-CP and a CU-UP.
+struct e1ap_message {
+  asn1::e1ap::e1ap_pdu_c pdu;
+};
+
+} // namespace srsran
