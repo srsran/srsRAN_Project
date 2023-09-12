6ff89feb2e8a066e0cfba898a0424a6ecc9407b8
faluco <borja@srs.io>
2023-09-12 13:31:24 +0200

    srslog: Print once an error message to all file sinks when the backend queue is almost full, signaling that from that point onwards log lines may have been dropped

---
+++ include/srsran/srslog/sink_repository.h
@@ -0,0 +1,29 @@
+/*
+ *
+ * Copyright 2021-2023 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#pragma once
+
+#include <vector>
+
+namespace srslog {
+
+class sink;
+
+/// Contains the registered sinks used by the loggers.
+class sink_repository
+{
+public:
+  virtual ~sink_repository() = default;
+
+  /// Returns the contents of the sink repository.
+  virtual std::vector<sink*> contents() = 0;
+};
+
+} // namespace srslog
