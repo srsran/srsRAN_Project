ac630e49b864dc9d3e40744d1db1b630f093a9d3
Joaquim Broquetas <joaquim.broquetas@srs.io>
2023-10-05 16:43:10 +0200

    phy: PDSCH DM-RS generator benchmark

---
+++ tests/benchmarks/phy/upper/signal_processors/dmrs_pdsch_processor_benchmark.cpp
@@ -0,0 +1,143 @@
+/*
+ *
+ * Copyright 2021-2023 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "../../../tests/unittests/phy/support/resource_grid_mapper_test_doubles.h"
+#include "srsran/phy/upper/signal_processors/dmrs_pdsch_processor.h"
+#include "srsran/phy/upper/signal_processors/signal_processor_factories.h"
+#include "srsran/support/benchmark_utils.h"
+#include "srsran/support/srsran_test.h"
+#include <getopt.h>
+#include <random>
+
+using namespace srsran;
+
+// Random generator.
+static std::mt19937 rgen(0);
+
+static unsigned nof_repetitions = 1000;
+static unsigned nof_rb          = 106;
+static bool     silent          = false;
+
+struct channel_topology {
+  unsigned nof_ports;
+  unsigned nof_layers;
+};
+
+static std::vector<channel_topology> channel_topology_list{{1, 1}, {2, 2}, {3, 3}, {4, 4}};
+
+static void usage(const char* prog)
+{
+  fmt::print("Usage: {} [-R repetitions] [-s silent] [-n nof_rb]\n", prog);
+  fmt::print("\t-R Repetitions [Default {}]\n", nof_repetitions);
+  fmt::print("\t-s Toggle silent operation [Default {}]\n", silent);
+  fmt::print("\t-n Number of resource blocks [Default {}]\n", nof_rb);
+  fmt::print("\t-h Show this message\n");
+}
+
+static void parse_args(int argc, char** argv)
+{
+  int opt = 0;
+  while ((opt = getopt(argc, argv, "R:n:sh")) != -1) {
+    switch (opt) {
+      case 'R':
+        nof_repetitions = std::strtol(optarg, nullptr, 10);
+        break;
+      case 's':
+        silent = (!silent);
+        break;
+      case 'n':
+        nof_rb = std::strtol(optarg, nullptr, 10);
+        break;
+      case 'h':
+      default:
+        usage(argv[0]);
+        exit(0);
+    }
+  }
+}
+
+int main(int argc, char** argv)
+{
+  parse_args(argc, argv);
+
+  // Create pseudo-random sequence generator.
+  std::shared_ptr<pseudo_random_generator_factory> prg_factory = create_pseudo_random_generator_sw_factory();
+  TESTASSERT(prg_factory);
+
+  std::shared_ptr<dmrs_pdsch_processor_factory> dmrs_pdsch_proc_factory =
+      create_dmrs_pdsch_processor_factory_sw(prg_factory);
+  TESTASSERT(dmrs_pdsch_proc_factory);
+
+  std::unique_ptr<dmrs_pdsch_processor> dmrs_proc = dmrs_pdsch_proc_factory->create();
+  TESTASSERT(dmrs_proc);
+
+  resource_grid_mapper_dummy mapper;
+
+  // Precoding weight distribution.
+  std::uniform_real_distribution<float> weight_dist(-1.0F, 1.0F);
+
+  benchmarker perf_meas("DM-RS PDSCH processor", nof_repetitions);
+
+  for (auto topology : channel_topology_list) {
+    precoding_weight_matrix weights(topology.nof_layers, topology.nof_ports);
+
+    // Generate precoding weights.
+    for (unsigned i_port = 0; i_port != topology.nof_ports; ++i_port) {
+      span<cf_t> port_weights = weights.get_port_coefficients(i_port);
+      std::generate(port_weights.begin(), port_weights.end(), [&weight_dist]() {
+        return cf_t(weight_dist(rgen), weight_dist(rgen));
+      });
+    }
+
+    // Generate DM-RS PDSCH configuration.
+    dmrs_pdsch_processor::config_t dmrs_config;
+    dmrs_config.slot                 = slot_point(subcarrier_spacing::kHz15, 0);
+    dmrs_config.reference_point_k_rb = 0;
+    dmrs_config.scrambling_id        = 0;
+    dmrs_config.n_scid               = false;
+    dmrs_config.amplitude            = 1.0f;
+    dmrs_config.symbols_mask         = symbol_slot_mask(
+        {false, false, true, false, false, false, false, false, false, false, false, true, false, false});
+    dmrs_config.rb_mask   = ~bounded_bitset<MAX_RB>(nof_rb);
+    dmrs_config.precoding = precoding_configuration::make_wideband(weights);
+
+    // Test DM-RS Type 1 generation.
+    {
+      dmrs_config.type          = dmrs_type::options::TYPE1;
+      unsigned nof_dmrs_symbols = dmrs_config.rb_mask.count() * dmrs_config.symbols_mask.count() *
+                                  dmrs_config.type.nof_dmrs_per_rb() * topology.nof_layers;
+
+      std::string meas_descr = fmt::to_string(topology.nof_ports) + " ports x " + fmt::to_string(topology.nof_layers) +
+                               " layers, DM-RS Type 1 ";
+
+      perf_meas.new_measure(
+          meas_descr, nof_dmrs_symbols, [&dmrs_proc, &mapper, &dmrs_config]() { dmrs_proc->map(mapper, dmrs_config); });
+    }
+
+    // Test DM-RS Type 2 generation.
+    {
+      dmrs_config.type          = dmrs_type::options::TYPE2;
+      unsigned nof_dmrs_symbols = dmrs_config.rb_mask.count() * dmrs_config.symbols_mask.count() *
+                                  dmrs_config.type.nof_dmrs_per_rb() * topology.nof_layers;
+
+      std::string meas_descr = fmt::to_string(topology.nof_ports) + " ports x " + fmt::to_string(topology.nof_layers) +
+                               " layers, DM-RS Type 2 ";
+
+      perf_meas.new_measure(
+          meas_descr, nof_dmrs_symbols, [&dmrs_proc, &mapper, &dmrs_config]() { dmrs_proc->map(mapper, dmrs_config); });
+    }
+  }
+
+  if (!silent) {
+    perf_meas.print_percentiles_throughput("symbols");
+  }
+
+  return 0;
+}
