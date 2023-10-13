823dacdb6b91884409fb22a0a295f96441383376
Fabian Eckermann <fabian@srs.io>
2023-10-13 13:27:02 +0200

    cu_cp,du: Add amf connection to cu du test

--- tests/integrationtests/du_high_cu/du_high_cu_test_simulator.cpp
+++ tests/integrationtests/du_high_cu/du_high_cu_test_simulator.cpp
@@ -12,10 +12,12 @@
 #include "lib/du_high/du_high_executor_strategies.h"
 #include "tests/test_doubles/f1ap/f1ap_test_message_validators.h"
 #include "tests/test_doubles/mac/mac_test_messages.h"
+#include "tests/unittests/ngap/ngap_test_messages.h"
 #include "srsran/cu_cp/cu_cp_factory.h"
 #include "srsran/du/du_cell_config_helpers.h"
 #include "srsran/du_high/du_high_factory.h"
 #include "srsran/support/test_utils.h"
+#include <gtest/gtest.h>
 
 using namespace srsran;
 
@@ -102,9 +104,13 @@ du_high_cu_test_simulator::du_high_cu_test_simulator(const du_high_cu_cp_test_si
 
   // Instatiate CU-CP.
   cu_cp_inst = create_cu_cp(cu_cfg);
+  cu_cp_inst->handle_amf_connection();
 
   // Start CU-CP.
   cu_cp_inst->start();
+
+  // Connect AMF by injecting a ng_setup_response
+  cu_cp_inst->get_ngap_message_handler().handle_message(srs_cu_cp::generate_ng_setup_response());
 
   // Connect F1-C to CU-CP.
   f1c_gw.attach_cu_cp_du_repo(cu_cp_inst->get_connected_dus());
