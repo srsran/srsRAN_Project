06114809e9803c9be4b459695d9b1f032326d9f4
Fabian Eckermann <fabian@srs.io>
2023-10-19 11:59:57 +0200

    cu_cp,ue_mng: Fix logging

--- lib/cu_cp/ue_manager_impl.cpp
+++ lib/cu_cp/ue_manager_impl.cpp
@@ -48,16 +48,16 @@ void ue_manager::remove_ue(ue_index_t ue_index)
     if (c_rnti != rnti_t::INVALID_RNTI) {
       pci_rnti_to_ue_index.erase(std::make_tuple(pci, c_rnti));
     } else {
-      logger.warning("ue={} RNTI not found", ue_index);
+      logger.warning("ue={}: RNTI not found", ue_index);
     }
   } else {
-    logger.debug("ue={} PCI not found", ue_index);
+    logger.debug("ue={}: PCI not found", ue_index);
   }
 
   // Remove CU-CP UE from database
   ues.erase(ue_index);
 
-  logger.info("ue={} removed", ue_index);
+  logger.info("ue={}: Removed", ue_index);
   return;
 }
 
@@ -75,7 +75,7 @@ ue_index_t ue_manager::allocate_new_ue_index(du_index_t du_index)
   ues.emplace(
       std::piecewise_construct, std::forward_as_tuple(new_ue_index), std::forward_as_tuple(new_ue_index, up_config));
 
-  logger.debug("ue={} Allocated new UE index", new_ue_index);
+  logger.debug("ue={}: Allocated new UE index", new_ue_index);
 
   return new_ue_index;
 }
@@ -96,7 +96,7 @@ du_ue* ue_manager::add_ue(ue_index_t ue_index, pci_t pci, rnti_t rnti)
 
   // check if ue_index is in db
   if (ues.find(ue_index) == ues.end()) {
-    logger.warning("UE with ue_index={} not found", ue_index);
+    logger.warning("ue={}: UE not found", ue_index);
     return nullptr;
   }
 
@@ -112,7 +112,7 @@ du_ue* ue_manager::add_ue(ue_index_t ue_index, pci_t pci, rnti_t rnti)
   // Add PCI and RNTI to lookup.
   pci_rnti_to_ue_index.emplace(std::make_tuple(pci, rnti), ue_index);
 
-  logger.debug("ue={} updated UE with pci={} and rnti={}", ue_index, pci, rnti);
+  logger.debug("ue={}: Updated UE with pci={} and rnti={}", ue_index, pci, rnti);
 
   return &ue;
 }
@@ -136,7 +136,7 @@ ngap_ue* ue_manager::add_ue(ue_index_t                          ue_index,
 
   // check if the UE is already present
   if (ues.find(ue_index) != ues.end() && ues.at(ue_index).ngap_ue_created()) {
-    logger.warning("ue={} already exists", ue_index);
+    logger.warning("ue={}: UE already exists", ue_index);
     return nullptr;
   }
 
