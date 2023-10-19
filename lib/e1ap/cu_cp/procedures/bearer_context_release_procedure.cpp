8921c5422ad67167e2485b6425dce9439d3b073b
Fabian Eckermann <fabian@srs.io>
2023-10-19 14:03:05 +0200

    cu_cp,e1ap: Add prefixed logger

--- lib/e1ap/cu_cp/procedures/bearer_context_release_procedure.cpp
+++ lib/e1ap/cu_cp/procedures/bearer_context_release_procedure.cpp
@@ -9,17 +9,18 @@
  */
 
 #include "bearer_context_release_procedure.h"
+#include "cu_cp/e1ap_cu_cp_impl.h"
+#include "cu_cp/ue_context/e1ap_bearer_transaction_manager.h"
 
 using namespace srsran;
 using namespace srsran::srs_cu_cp;
 using namespace asn1::e1ap;
 
-bearer_context_release_procedure::bearer_context_release_procedure(const ue_index_t       ue_index_,
-                                                                   const e1ap_message&    command_,
-                                                                   e1ap_ue_context_list&  ue_ctxt_list_,
-                                                                   e1ap_message_notifier& e1ap_notif_,
-                                                                   srslog::basic_logger&  logger_) :
-  ue_index(ue_index_), command(command_), ue_ctxt_list(ue_ctxt_list_), e1ap_notifier(e1ap_notif_), logger(logger_)
+bearer_context_release_procedure::bearer_context_release_procedure(const e1ap_message&              command_,
+                                                                   e1ap_bearer_transaction_manager& ev_mng_,
+                                                                   e1ap_message_notifier&           e1ap_notif_,
+                                                                   e1ap_ue_logger&                  logger_) :
+  command(command_), ev_mng(ev_mng_), e1ap_notifier(e1ap_notif_), logger(logger_)
 {
 }
 
@@ -27,10 +28,10 @@ void bearer_context_release_procedure::operator()(coro_context<async_task<void>>
 {
   CORO_BEGIN(ctx);
 
-  logger.debug("ue={}: \"{}\" initialized.", ue_index, name());
+  logger.log_debug("\"{}\" initialized", name());
 
   // Subscribe to respective publisher to receive BEARER CONTEXT RELEASE COMPLETE message.
-  transaction_sink.subscribe_to(ue_ctxt_list[ue_index].bearer_ev_mng.context_release_complete);
+  transaction_sink.subscribe_to(ev_mng.context_release_complete);
 
   // Send command to CU-UP.
   send_bearer_context_release_command();
@@ -46,10 +47,10 @@ void bearer_context_release_procedure::operator()(coro_context<async_task<void>>
 
 void bearer_context_release_procedure::send_bearer_context_release_command()
 {
-  if (logger.debug.enabled()) {
+  if (logger.get_basic_logger().debug.enabled()) {
     asn1::json_writer js;
     command.pdu.to_json(js);
-    logger.debug("Containerized BearerContextReleaseCommand: {}", js.to_string());
+    logger.log_debug("Containerized BearerContextReleaseCommand: {}", js.to_string());
   }
 
   // send DL RRC message
@@ -60,17 +61,17 @@ void bearer_context_release_procedure::handle_bearer_context_release_complete()
 {
   if (transaction_sink.successful()) {
     const asn1::e1ap::bearer_context_release_complete_s& resp = transaction_sink.response();
-    logger.debug("Received BearerContextReleaseComplete");
-    if (logger.debug.enabled()) {
+    logger.log_debug("Received BearerContextReleaseComplete");
+    if (logger.get_basic_logger().debug.enabled()) {
       asn1::json_writer js;
       resp.to_json(js);
-      logger.debug("Containerized BearerContextReleaseComplete: {}", js.to_string());
+      logger.log_debug("Containerized BearerContextReleaseComplete: {}", js.to_string());
     }
 
-    logger.debug("ue={}: \"{}\" finalized.", ue_index, name());
+    logger.log_debug("\"{}\" finalized", name());
 
   } else {
-    logger.warning("BearerContextReleaseComplete timeout");
-    logger.error("ue={}: \"{}\" failed.", ue_index, name());
+    logger.log_warning("BearerContextReleaseComplete timeout");
+    logger.log_error("\"{}\" failed", name());
   }
 }
