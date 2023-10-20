674b7c2fc2935f56a60818158ebf41259ef6819c
Francisco Paisana <francisco.paisana@srs.io>
2023-10-20 17:29:27 +0200

    support: fix task_executor meta-functions

--- include/srsran/support/executors/task_executor_utils.h
+++ include/srsran/support/executors/task_executor_utils.h
@@ -21,7 +21,10 @@ template <typename T>
 class is_task_executor_ptr
 {
   template <typename U>
-  static auto test(U& ptr) -> decltype(ptr->execute(std::declval<unique_task>()), std::true_type{});
+  static auto test(U&& ptr) -> decltype(ptr->execute(std::declval<unique_task>()), std::true_type{});
+  template <typename U>
+  static auto test(const U* ptr)
+      -> decltype(std::declval<U*>()->execute(std::declval<unique_task>()), std::true_type{});
 
   static std::false_type test(...);
 
@@ -31,14 +34,14 @@ public:
 
 // Helper function to convert a task executor object, that can be a raw pointer or unique_ptr to a reference.
 template <typename T, std::enable_if_t<is_task_executor_ptr<T>::value, int> = 0>
-auto get_task_executor_ref(T& exec)
+std::decay_t<T>& get_task_executor_ref(T&& exec)
 {
   return *exec;
 }
 template <typename T, std::enable_if_t<not is_task_executor_ptr<T>::value, int> = 0>
-auto get_task_executor_ref(T& exec)
+auto get_task_executor_ref(T&& exec)
 {
-  return exec;
+  return std::forward<T>(exec);
 }
 
 template <typename T>
@@ -55,12 +58,12 @@ public:
 
 // Helper function to determine whether an executor allows a task to be run inline, given the caller thread.
 template <typename Executor, std::enable_if_t<task_executor_has_can_run_task_inline<Executor>::value, int> = 0>
-bool executor_lets_run_task_inline(Executor& exec)
+bool executor_lets_run_task_inline(const Executor& exec)
 {
   return get_task_executor_ref(exec).can_run_task_inline();
 }
 template <typename Executor, std::enable_if_t<not task_executor_has_can_run_task_inline<Executor>::value, int> = 0>
-bool executor_lets_run_task_inline(Executor& exec)
+bool executor_lets_run_task_inline(const Executor& exec)
 {
   return false;
 }
