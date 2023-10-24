4357e925e317cda0cca2a59f2217385e5d73a3f9
Francisco Paisana <francisco.paisana@srs.io>
2023-10-24 12:32:23 +0200

    support: fix race condition in the thread pool initialization due to the move of unique_thread

--- lib/support/executors/task_worker_pool.cpp
+++ lib/support/executors/task_worker_pool.cpp
@@ -14,14 +14,18 @@
 using namespace srsran;
 
 template <bool UseLockfreeMPMC>
-void task_worker_pool<UseLockfreeMPMC>::start_impl(os_thread_realtime_priority           prio_,
+void task_worker_pool<UseLockfreeMPMC>::start_impl(unsigned                              nof_workers,
+                                                   os_thread_realtime_priority           prio_,
                                                    span<const os_sched_affinity_bitmask> cpu_masks)
 {
+  workers.clear();
+  workers.reserve(nof_workers);
+
   if (cpu_masks.size() > 1) {
     // An array with a single mask is allowed, otherwise the number of masks must be equal to the number of workers.
-    srsran_assert(cpu_masks.size() == workers.size(), "Wrong array of CPU masks provided");
+    srsran_assert(cpu_masks.size() == nof_workers, "Wrong array of CPU masks provided");
   }
-  for (unsigned i = 0; i != workers.size(); ++i) {
+  for (unsigned i = 0; i != nof_workers; ++i) {
     auto task_func = [this, i]() {
       while (true) {
         optional<unique_task> task = pending_tasks.pop_blocking();
@@ -30,14 +34,14 @@ void task_worker_pool<UseLockfreeMPMC>::start_impl(os_thread_realtime_priority  
         }
         (*task)();
       }
-      logger.info("Task worker \"{}\" finished.", workers[i].t_handle.get_name());
+      logger.info("Task worker \"{}\" finished.", workers[i].get_name());
     };
     if (cpu_masks.empty()) {
-      workers[i].t_handle = unique_thread{fmt::format("{}#{}", pool_name, i), prio_, task_func};
+      workers.emplace_back(fmt::format("{}#{}", pool_name, i), prio_, task_func);
     } else {
       // Check whether a single mask for all workers should be used.
       os_sched_affinity_bitmask cpu_mask = (cpu_masks.size() == 1) ? cpu_masks[0] : cpu_masks[i];
-      workers[i].t_handle = unique_thread{fmt::format("{}#{}", pool_name, i), prio_, cpu_mask, task_func};
+      workers.emplace_back(fmt::format("{}#{}", pool_name, i), prio_, cpu_mask, task_func);
     }
   }
 }
@@ -51,10 +55,10 @@ task_worker_pool<UseLockfreeMPMC>::~task_worker_pool()
 template <bool UseLockfreeMPMC>
 void task_worker_pool<UseLockfreeMPMC>::stop()
 {
-  for (worker& w : workers) {
-    if (w.t_handle.running()) {
+  for (unique_thread& w : workers) {
+    if (w.running()) {
       pending_tasks.request_stop();
-      w.t_handle.join();
+      w.join();
     }
   }
 }
@@ -66,7 +70,7 @@ void task_worker_pool<UseLockfreeMPMC>::wait_pending_tasks()
   std::condition_variable cvar;
   unsigned                workers_running = 0;
   for (unsigned i = 0; i != workers.size(); ++i) {
-    if (workers[i].t_handle.running()) {
+    if (workers[i].running()) {
       workers_running++;
     }
   }
@@ -87,6 +91,15 @@ void task_worker_pool<UseLockfreeMPMC>::wait_pending_tasks()
 
   std::unique_lock<std::mutex> lock(mutex);
   cvar.wait(lock, [&count]() { return count == 0; });
+}
+
+template <bool UseLockfreeMPMC>
+bool srsran::task_worker_pool<UseLockfreeMPMC>::is_in_thread_pool() const
+{
+  std::any_of(workers.begin(), workers.end(), [id = std::this_thread::get_id()](const unique_thread& t) {
+    return t.get_id() == id;
+  });
+  return false;
 }
 
 template class srsran::task_worker_pool<false>;
