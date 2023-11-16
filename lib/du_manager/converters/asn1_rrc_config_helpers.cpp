5719be6ce9919abe58baf3ed20e5d2ee8c34feb9
yagoda <justin.tallon@softwareradiosystems.com>
2023-11-16 11:23:41 +0100

    rrc: setting other search spaces active for si messages

--- lib/du_manager/converters/asn1_rrc_config_helpers.cpp
+++ lib/du_manager/converters/asn1_rrc_config_helpers.cpp
@@ -360,8 +360,6 @@ asn1::rrc_nr::bwp_dl_common_s srsran::srs_du::make_asn1_init_dl_bwp(const dl_con
   }
   pdcch.search_space_sib1_present           = true;
   pdcch.search_space_sib1                   = cfg.init_dl_bwp.pdcch_common.sib1_search_space_id;
-  pdcch.search_space_other_sys_info_present = true;
-  pdcch.search_space_other_sys_info         = 1;
   pdcch.paging_search_space_present         = cfg.init_dl_bwp.pdcch_common.paging_search_space_id.has_value();
   if (pdcch.paging_search_space_present) {
     pdcch.paging_search_space = cfg.init_dl_bwp.pdcch_common.paging_search_space_id.value();
