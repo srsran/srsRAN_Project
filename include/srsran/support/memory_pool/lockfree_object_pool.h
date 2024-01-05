ae2ab8b26aa1f8eb0f0ad291bc5e001d47b49341
Francisco Paisana <francisco.paisana@srs.io>
2024-01-05 17:49:47 +0100

    support: add tsan label to several unit tests that require threads

--- include/srsran/support/memory_pool/lockfree_object_pool.h
+++ include/srsran/support/memory_pool/lockfree_object_pool.h
@@ -81,8 +81,8 @@ private:
     index_type index;
     index_type epoch;
   };
-  static_assert(sizeof(node) == sizeof(uint64_t),
-                "node sizeof should no go beyond 64-bits, otherwise we lose is_lock_free guarantees in most platforms");
+  static_assert(sizeof(node) <= sizeof(uint64_t),
+                "node sizeof should not go above 64-bits, otherwise we lose is_lock_free guarantees in most platforms");
 
   std::atomic<node>       top;
   std::vector<index_type> next_idx;
