fb224b88c9890387e6db613f5134ad2773338ec9
Francisco Paisana <francisco.paisana@srs.io>
2024-01-23 09:35:50 +0100

    cu-cp: remove unnecessary calls in gnb main

--- apps/gnb/gnb.cpp
+++ apps/gnb/gnb.cpp
@@ -456,11 +456,6 @@ int main(int argc, char** argv)
   // Connect F1-C to CU-CP.
   f1c_gw.attach_cu_cp(cu_cp_obj->get_connected_dus());
 
-  //  // Signal AMF connection for instant CU-CP start and to make sure test UEs do not get rejected
-  //  if (gnb_cfg.amf_cfg.no_core) {
-  //    cu_cp_obj->get_cu_cp_ngap_handler().handle_amf_connection_establishment();
-  //  }
-
   // start CU-CP
   gnb_logger.info("Starting CU-CP...");
   cu_cp_obj->start();
@@ -587,11 +582,9 @@ int main(int argc, char** argv)
   // Stop CU-CP activity.
   cu_cp_obj->stop();
 
-  if (not gnb_cfg.amf_cfg.no_core) {
-    gnb_logger.info("Closing network connections...");
-    ngap_adapter->disconnect();
-    gnb_logger.info("Network connections closed successfully");
-  }
+  gnb_logger.info("Closing network connections...");
+  ngap_adapter->disconnect();
+  gnb_logger.info("Network connections closed successfully");
 
   if (gnb_cfg.e2_cfg.enable_du_e2) {
     gnb_logger.info("Closing E2 network connections...");
