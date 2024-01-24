229823a2e1e973fdd3862ca88af9e71d3960c3ce
Xavier Arteaga <xavier@srs.io>
2024-01-24 01:24:52 +0100

    phy: initial NEON precoder
    phy: fix NEON precoder
    
    phy: more neon optimization

---
+++ lib/phy/generic_functions/precoding/channel_precoder_neon.h
@@ -0,0 +1,35 @@
+/*
+ *
+ * Copyright 2021-2024 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+/// \file
+/// \brief Channel precoder NEON implementation.
+
+#pragma once
+
+#include "channel_precoder_impl.h"
+
+namespace srsran {
+
+/// Channel precoder implementation.
+class channel_precoder_neon : public channel_precoder_impl
+{
+public:
+  // See interface for documentation.
+  void apply_precoding_port(span<cf_t>              port_re,
+                            const re_buffer_reader& input_re,
+                            span<const cf_t>        port_weights) const override;
+
+  // See interface for documentation.
+  void apply_layer_map_and_precoding(re_buffer_writer&              output,
+                                     span<const ci8_t>              input,
+                                     const precoding_weight_matrix& precoding) const override;
+};
+
+} // namespace srsran
--- tests/unittests/phy/generic_functions/precoding/channel_precoder_test.cpp
+++ tests/unittests/phy/generic_functions/precoding/channel_precoder_test.cpp
@@ -293,6 +293,11 @@ INSTANTIATE_TEST_SUITE_P(RandomWeights,
                                                "avx2",
                                                "avx512"
 #endif // __x86_64__
+#ifdef __aarch64__
+                                               // Precoder types for ARM.
+                                               ,
+                                               "neon"
+#endif // __aarch64__
                                                ),
                              // Number of RB.
                              ::testing::Values(13, 51, MAX_RB),
