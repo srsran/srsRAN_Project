3c0fc31a338a825121624c2667cded117d969c4b
Francisco Paisana <francisco.paisana@srs.io>
2024-01-29 11:26:42 +0100

    asn1: allow forward declaration of e1ap asn1 types

--- lib/e1ap/cu_cp/e1ap_cu_cp_asn1_helpers.h
+++ lib/e1ap/cu_cp/e1ap_cu_cp_asn1_helpers.h
@@ -12,6 +12,7 @@
 
 #include "../common/e1ap_asn1_converters.h"
 #include "srsran/asn1/asn1_utils.h"
+#include "srsran/asn1/e1ap/e1ap_pdu_contents.h"
 #include "srsran/ran/bcd_helpers.h"
 #include "srsran/ran/qos_prio_level.h"
 
