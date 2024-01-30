ae697dd151c0bfe54ba41205a9dc05a30e0bc877
Francisco Paisana <francisco.paisana@srs.io>
2024-01-30 16:22:09 +0100

    code format

--- tests/unittests/f1ap/cu_cp/f1ap_cu_test.cpp
+++ tests/unittests/f1ap/cu_cp/f1ap_cu_test.cpp
@@ -9,9 +9,9 @@
  */
 
 #include "f1ap_cu_test_helpers.h"
+#include "lib/f1ap/common/asn1_helpers.h"
 #include "srsran/asn1/f1ap/common.h"
 #include "srsran/asn1/f1ap/f1ap_pdu_contents.h"
-#include "lib/f1ap/common/asn1_helpers.h"
 #include "srsran/f1ap/cu_cp/f1ap_cu.h"
 #include <gtest/gtest.h>
 
