a8e224e14acd83e13702878aeff4640e4e79f650
Robert Falkenberg <robert.falkenberg@srs.io>
2024-02-13 06:58:35 +0100

    io_broker: refine logging

--- lib/support/network/io_broker_epoll.cpp
+++ lib/support/network/io_broker_epoll.cpp
@@ -34,7 +34,7 @@ io_broker_epoll::io_broker_epoll(const io_broker_config& config) : logger(srslog
     report_fatal_error("IO broker: failed to register event file descriptor. event_fd={}", event_fd);
   }
 
-  logger.debug("IO broker: starting event loop");
+  logger.debug("Starting event loop");
   // start thread to handle epoll events
   thread = unique_thread(config.thread_name, config.thread_prio, config.cpu_mask, [this]() {
     while (running) {
@@ -53,17 +53,17 @@ io_broker_epoll::~io_broker_epoll()
     uint64_t tmp = 1;
     ssize_t  ret = ::write(event_fd, &tmp, sizeof(tmp));
     if (ret == -1) {
-      logger.error("IO broker: failed to write stop event. event_fd={} error={}", event_fd, strerror(errno));
+      logger.error("Failed to write stop event. event_fd={} error={}", event_fd, strerror(errno));
     }
   }
 
-  logger.debug("IO broker: joining event loop");
+  logger.debug("Joining event loop");
   // wait for completion
   if (thread.running()) {
     thread.join();
   }
 
-  logger.debug("IO broker: closing epoll file descriptor. epoll_fd={}", epoll_fd);
+  logger.debug("Closing epoll file descriptor. epoll_fd={}", epoll_fd);
   std::lock_guard<std::mutex> lock(event_handler_mutex);
   event_handler.clear();
   close(epoll_fd);
@@ -80,16 +80,16 @@ void io_broker_epoll::thread_loop()
 
   // handle event
   if (nof_events == -1) {
-    logger.error("IO broker: epoll_wait failed. error={}", strerror(errno));
+    logger.error("epoll_wait failed. error={}", strerror(errno));
     /// TODO: shall we raise a fatal error here?
     return;
   }
   if (nof_events == 0) {
-    logger.error("IO broker: epoll_wait time out. timeout_ms={}", epoll_timeout_ms);
+    logger.error("epoll_wait time out. timeout_ms={}", epoll_timeout_ms);
     return;
   }
 
-  logger.debug("IO broker: handling events. nof_events={}", nof_events);
+  logger.debug("Handling events. nof_events={}", nof_events);
   for (int i = 0; i < nof_events; ++i) {
     int fd = events[i].data.fd;
     if ((events[i].events & EPOLLERR) || (events[i].events & EPOLLHUP) || (!(events[i].events & EPOLLIN))) {
@@ -97,16 +97,16 @@ void io_broker_epoll::thread_loop()
       // TODO: add notifier for these events and let the subscriber decide on further actions (e.g. unregister fd)
       if (events[i].events & EPOLLHUP) {
         // Note: some container environments hang up stdin (fd=0) in case of non-interactive sessions
-        logger.warning("IO broker: hang up on file descriptor. fd={} events={:#x}", fd, uint32_t(events[i].events));
+        logger.warning("Hang up on file descriptor. fd={} events={:#x}", fd, uint32_t(events[i].events));
       } else if (events[i].events & EPOLLERR) {
-        logger.error("IO broker: error on file descriptor. fd={} events={:#x}", fd, uint32_t(events[i].events));
+        logger.error("Error on file descriptor. fd={} events={:#x}", fd, uint32_t(events[i].events));
       } else {
-        logger.error("IO broker: unhandled epoll event. fd={} events={:#x}", fd, uint32_t(events[i].events));
+        logger.error("Unhandled epoll event. fd={} events={:#x}", fd, uint32_t(events[i].events));
       }
       // Unregister the faulty file descriptor from epoll
       bool success = unregister_fd(fd);
       if (!success) {
-        logger.error("IO broker: failed to unregister file descriptor. fd={}", fd);
+        logger.error("Failed to unregister file descriptor. fd={}", fd);
       }
       return;
     }
@@ -114,10 +114,10 @@ void io_broker_epoll::thread_loop()
     std::lock_guard<std::mutex> lock(event_handler_mutex);
     const auto&                 it = event_handler.find(fd);
     if (it != event_handler.end()) {
-      logger.debug("IO broker: handling event. fd={}", fd);
+      logger.debug("Handling event. fd={}", fd);
       it->second->handle_event(fd, events[i]);
     } else {
-      logger.error("IO broker: could not find event handler. fd={}", fd);
+      logger.error("Could not find event handler. fd={}", fd);
     }
   }
 }
@@ -130,14 +130,14 @@ bool io_broker_epoll::register_fd(int fd, recv_callback_t handler)
   ev.data.fd            = fd;
   ev.events             = EPOLLIN;
   if (::epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &ev) == -1) {
-    logger.error("IO broker: failed to register file descriptor. fd={} error={}", fd, strerror(errno));
+    logger.error("Failed to register file descriptor. fd={} error={}", fd, strerror(errno));
     return false;
   }
 
   std::lock_guard<std::mutex> lock(event_handler_mutex);
   event_handler.insert({fd, std::make_unique<epoll_receive_callback>(handler)});
 
-  logger.debug("IO broker: registered file descriptor. fd={}", fd);
+  logger.debug("Registered file descriptor. fd={}", fd);
   return true;
 }
 
@@ -148,13 +148,13 @@ bool io_broker_epoll::unregister_fd(int fd)
   ev.data.fd            = fd;
   ev.events             = EPOLLIN;
   if (::epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, &ev) == -1) {
-    logger.error("IO broker: failed to unregister file descriptor. fd={} error={}", fd, strerror(errno));
+    logger.error("Failed to unregister file descriptor. fd={} error={}", fd, strerror(errno));
     return false;
   }
 
   std::lock_guard<std::mutex> lock(event_handler_mutex);
   event_handler.erase(fd);
 
-  logger.debug("IO broker: unregistered file descriptor. fd={}", fd);
+  logger.debug("Unregistered file descriptor. fd={}", fd);
   return true;
 }
