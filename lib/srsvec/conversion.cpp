eca8223802671b34de40314686fe567b22c3baa8
Xavier Arteaga <xavier@srs.io>
2024-02-15 12:26:33 +0100

    srsvec: review conversion
    srsvec: fix int16 to float for small vector
    
    srsvec: review conversion
    
    srsvec: apply clang-format

--- lib/srsvec/conversion.cpp
+++ lib/srsvec/conversion.cpp
@@ -15,44 +15,11 @@
 using namespace srsran;
 using namespace srsvec;
 
-namespace details {
-
-#if SRSRAN_SIMD_F_SIZE && SRSRAN_SIMD_S_SIZE
-using simd_conv_func_type = simd_s_t (*)(simd_f_t a, simd_f_t b);
-
-template <bool R = false>
-struct simd_conversion_helper {
-  simd_conv_func_type convert = srsran_simd_convert_2f_s;
-};
-
-template <>
-struct simd_conversion_helper<true> {
-  simd_conv_func_type convert = srsran_simd_convert_2f_s_round;
-};
-#endif
-
-using gen_conv_func_type = int16_t (*)(float);
-
-template <bool R = false>
-struct gen_conversion_helper {
-  gen_conv_func_type convert = [](float a) { return (int16_t)a; };
-};
-
-template <>
-struct gen_conversion_helper<true> {
-  gen_conv_func_type convert = [](float a) { return static_cast<int16_t>(std::round(a)); };
-};
-
-} // namespace details
-
-template <bool ROUND = false>
 static inline void convert_fi_simd(const float* x, int16_t* z, float scale, unsigned len)
 {
   unsigned i = 0;
 
 #if SRSRAN_SIMD_F_SIZE && SRSRAN_SIMD_S_SIZE
-  details::simd_conversion_helper<ROUND> conversion_helper;
-
   simd_f_t s = srsran_simd_f_set1(scale);
   if (SIMD_IS_ALIGNED(x) && SIMD_IS_ALIGNED(z)) {
     for (; i + SRSRAN_SIMD_S_SIZE < len + 1; i += SRSRAN_SIMD_S_SIZE) {
@@ -62,7 +29,7 @@ static inline void convert_fi_simd(const float* x, int16_t* z, float scale, unsi
       simd_f_t sa = srsran_simd_f_mul(a, s);
       simd_f_t sb = srsran_simd_f_mul(b, s);
 
-      simd_s_t i16 = conversion_helper.convert(sa, sb);
+      simd_s_t i16 = srsran_simd_convert_2f_s(sa, sb);
 
       srsran_simd_s_store(&z[i], i16);
     }
@@ -74,46 +41,80 @@ static inline void convert_fi_simd(const float* x, int16_t* z, float scale, unsi
       simd_f_t sa = srsran_simd_f_mul(a, s);
       simd_f_t sb = srsran_simd_f_mul(b, s);
 
-      simd_s_t i16 = conversion_helper.convert(sa, sb);
+      simd_s_t i16 = srsran_simd_convert_2f_s(sa, sb);
 
       srsran_simd_s_storeu(&z[i], i16);
     }
   }
 #endif /* SRSRAN_SIMD_F_SIZE && SRSRAN_SIMD_S_SIZE */
-  details::gen_conversion_helper<ROUND> gen_conversion_helper;
-  for (; i < len; i++) {
-    z[i] = gen_conversion_helper.convert(x[i] * scale);
+
+  for (; i != len; i++) {
+    z[i] = static_cast<int16_t>(std::round(x[i] * scale));
   }
 }
 
-static inline void convert_if_simd(const int16_t* x, float* z, float scale, unsigned len)
+static inline void convert_if_simd(float* z, const int16_t* x, float scale, unsigned len)
 {
   unsigned    i    = 0;
   const float gain = 1.0f / scale;
 
-#ifdef mHAVE_SSE
-  __m128 s = _mm_set1_ps(gain);
-  if (SIMD_IS_ALIGNED(z)) {
-    for (; i < len - 3; i += 4) {
-      __m64* ptr = (__m64*)&x[i];
-      __m128 fl  = _mm_cvtpi16_ps(*ptr);
-      __m128 v   = _mm_mul_ps(fl, s);
+#if defined(__AVX__) && defined(__AVX512F__)
+  // Load the scale factor into a vector register.
+  __m512 scale512 = _mm512_set1_ps(gain);
 
-      _mm_store_ps(&z[i], v);
-    }
-  } else {
-    for (; i < len - 3; i += 4) {
-      __m64* ptr = (__m64*)&x[i];
-      __m128 fl  = _mm_cvtpi16_ps(*ptr);
-      __m128 v   = _mm_mul_ps(fl, s);
+  // Process 16 elements at a time (512 bits / 32 bits per float = 16 floats).
+  for (unsigned i_end = (len / 16) * 16; i != i_end; i += 16) {
+    // Load 16 int16_t elements into a 256-bit vector register.
+    __m256i input_vec = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(x + i));
 
-      _mm_storeu_ps(&z[i], v);
-    }
+    // Convert the int16_t elements to float and scale them.
+    __m512 float_vec = _mm512_cvtepi32_ps(_mm512_cvtepi16_epi32(input_vec));
+    float_vec        = _mm512_mul_ps(float_vec, scale512);
+
+    // Store the result back to memory.
+    _mm512_storeu_ps(z + i, float_vec);
   }
-#endif /* HAVE_SSE */
+#if defined(__AVX512VL__)
+  {
+    unsigned remainder = len % 16;
 
-  for (; i < len; i++) {
-    z[i] = ((float)x[i]) * gain;
+    // Select the LSB values.
+    __mmask16 mask = (1 << remainder) - 1;
+
+    // Load 16 int16_t elements into a 256-bit vector register.
+    __m256i input_vec = _mm256_maskz_loadu_epi16(mask, reinterpret_cast<const __m256i*>(x + i));
+
+    // Convert the int16_t elements to float and scale them.
+    __m512 float_vec = _mm512_cvtepi32_ps(_mm512_cvtepi16_epi32(input_vec));
+    float_vec        = _mm512_mul_ps(float_vec, scale512);
+
+    // Store the result back to memory.
+    _mm512_mask_storeu_ps(z + i, mask, float_vec);
+    return;
+  }
+#endif // defined(__AVX512VL__)
+#endif // defined(__AVX__) && defined(__AVX512F__)
+
+#if defined(__AVX__) && defined(__AVX2__)
+  // Load the scale factor into a vector register.
+  __m256 scale256 = _mm256_set1_ps(gain);
+
+  // Process 8 elements at a time (256 bits / 32 bits per float = 8 floats).
+  for (unsigned i_end = (len / 8) * 8; i != i_end; i += 8) {
+    // Load 8 int16_t elements into a 128-bit vector register.
+    __m128i input_vec = _mm_loadu_si128(reinterpret_cast<const __m128i*>(x + i));
+
+    // Convert the int16_t elements to float and scale them
+    __m256 float_vec = _mm256_cvtepi32_ps(_mm256_cvtepi16_epi32(input_vec));
+    float_vec        = _mm256_mul_ps(float_vec, scale256);
+
+    // Store the result back to memory
+    _mm256_storeu_ps(z + i, float_vec);
+  }
+#endif // defined(__AVX__) && defined(__AVX2__)
+
+  for (; i != len; i++) {
+    z[i] = static_cast<float>(x[i]) * gain;
   }
 }
 
@@ -121,48 +122,21 @@ void srsran::srsvec::convert(span<const cf_t> x, float scale, span<int16_t> z)
 {
   srsran_assert(2 * x.size() == z.size(), "Invalid input or output span sizes");
 
-  convert_fi_simd((const float*)x.data(), z.data(), scale, z.size());
-}
-
-void srsran::srsvec::convert_round(span<const cf_t> x, float scale, span<int16_t> z)
-{
-  srsran_assert(2 * x.size() == z.size(), "Invalid input or output span sizes");
-
-  convert_fi_simd</*ROUND =*/true>((const float*)x.data(), z.data(), scale, z.size());
-}
-
-void srsran::srsvec::convert_swap(span<const cf_t> x, float scale, span<int16_t> z)
-{
-  srsran_assert(2 * x.size() == z.size(), "Invalid input or output span sizes");
-
-  convert_fi_simd((const float*)x.data(), z.data(), scale, z.size());
-
-  // Perform I/Q swap
-  for (unsigned i = 0; i != z.size(); i += 2) {
-    int16_t temp = z[i];
-    z[i]         = z[i + 1];
-    z[i + 1]     = temp;
-  }
+  convert_fi_simd(reinterpret_cast<const float*>(x.data()), z.data(), scale, z.size());
 }
 
 void srsran::srsvec::convert(span<const int16_t> x, float scale, span<cf_t> z)
 {
   srsran_assert(x.size() == 2 * z.size(), "Invalid input or output span sizes");
 
-  convert_if_simd(x.data(), (float*)z.data(), scale, x.size());
+  convert_if_simd(reinterpret_cast<float*>(z.data()), x.data(), scale, x.size());
 }
 
-void srsran::srsvec::convert_swap(span<const int16_t> x, float scale, span<cf_t> z)
+void srsran::srsvec::convert(span<const int16_t> x, float scale, span<float> z)
 {
-  srsran_assert(x.size() == 2 * z.size(), "Invalid input or output span sizes");
+  srsran_assert(x.size() == z.size(), "Invalid input or output span sizes");
 
-  convert_if_simd(x.data(), (float*)z.data(), scale, x.size());
-
-  // Perform I/Q swap
-  for (unsigned i = 0; i != z.size(); i++) {
-    cf_t temp = z[i];
-    z[i]      = {temp.imag(), temp.real()};
-  }
+  convert_if_simd(z.data(), x.data(), scale, x.size());
 }
 
 void srsran::srsvec::convert(span<const float> x, float scale, span<int16_t> z)
@@ -170,18 +144,4 @@ void srsran::srsvec::convert(span<const float> x, float scale, span<int16_t> z)
   srsran_assert(x.size() == z.size(), "Invalid input or output span sizes");
 
   convert_fi_simd(x.data(), z.data(), scale, z.size());
-}
-
-void srsran::srsvec::convert_round(span<const float> x, float scale, span<int16_t> z)
-{
-  srsran_assert(x.size() == z.size(), "Invalid input or output span sizes");
-
-  convert_fi_simd</*ROUND =*/true>(x.data(), z.data(), scale, z.size());
-}
-
-void srsran::srsvec::convert(span<const int16_t> x, float scale, span<float> z)
-{
-  srsran_assert(x.size() == z.size(), "Invalid input or output span sizes");
-
-  convert_if_simd(x.data(), z.data(), scale, z.size());
 }
