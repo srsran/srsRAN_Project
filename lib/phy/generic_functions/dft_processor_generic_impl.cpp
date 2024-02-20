9edc0b8c8b37f095e8667fe739f8ba2396fb91d5
dvdgrgrtt <davidg@srs.io>
2024-02-20 15:25:06 +0100

    add missing copyright header

--- lib/phy/generic_functions/dft_processor_generic_impl.cpp
+++ lib/phy/generic_functions/dft_processor_generic_impl.cpp
@@ -1,3 +1,12 @@
+/*
+ *
+ * Copyright 2021-2024 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
 
 #include "dft_processor_generic_impl.h"
 #include "../../srsvec/simd.h"
