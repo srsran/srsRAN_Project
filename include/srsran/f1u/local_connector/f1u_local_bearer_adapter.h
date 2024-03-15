f066a33f434e90316bed1d7c8f3574e20dde6416
Robert Falkenberg <robert.falkenberg@srs.io>
2024-03-15 10:28:16 +0100

    f1u: sync concurrent access to handlers in local bearer adapters
    This synchronizes access to `handler` pointer, used by
    DU UL executors to forward UL NRUP PDUs and reset by
    CU-UP executors upon removal of bearers.

--- include/srsran/f1u/local_connector/f1u_local_bearer_adapter.h
+++ include/srsran/f1u/local_connector/f1u_local_bearer_adapter.h
@@ -19,6 +19,7 @@
 #include "srsran/f1u/du/f1u_rx_pdu_handler.h"
 #include "srsran/f1u/du/f1u_tx_pdu_notifier.h"
 #include "srsran/ran/up_transport_layer_info.h"
+#include <mutex>
 
 namespace srsran {
 class f1u_dl_local_adapter : public srs_cu_up::f1u_tx_pdu_notifier
@@ -31,12 +32,14 @@ public:
 
   void attach_du_handler(srs_du::f1u_rx_pdu_handler& handler_, const up_transport_layer_info& dl_tnl_info_)
   {
+    std::unique_lock<std::mutex> lock(handler_mutex);
     handler = &handler_;
     dl_tnl_info.emplace(dl_tnl_info_);
   }
 
   void detach_du_handler(const up_transport_layer_info& dl_tnl_info_)
   {
+    std::unique_lock<std::mutex> lock(handler_mutex);
     if (dl_tnl_info == dl_tnl_info_) {
       handler = nullptr;
       dl_tnl_info.reset();
@@ -49,6 +52,7 @@ public:
 
   void on_new_pdu(nru_dl_message msg) override
   {
+    std::unique_lock<std::mutex> lock(handler_mutex);
     if (handler == nullptr) {
       logger.log_info("Cannot handle NR-U DL message. DU bearer does not exist.");
       return;
@@ -58,8 +62,11 @@ public:
   };
 
 private:
-  srs_cu_up::f1u_bearer_logger      logger;
-  srs_du::f1u_rx_pdu_handler*       handler = nullptr;
+  srs_cu_up::f1u_bearer_logger logger;
+
+  srs_du::f1u_rx_pdu_handler* handler = nullptr;
+  std::mutex                  handler_mutex;
+
   optional<up_transport_layer_info> dl_tnl_info;
 };
 
@@ -81,11 +88,22 @@ public:
     logger("DU-F1-U", {ue_index, drb_id, dl_tnl_info})
   {
   }
-  void attach_cu_handler(srs_cu_up::f1u_rx_pdu_handler& handler_) { handler = &handler_; }
-  void detach_cu_handler() { handler = nullptr; }
+
+  void attach_cu_handler(srs_cu_up::f1u_rx_pdu_handler& handler_)
+  {
+    std::unique_lock<std::mutex> lock(handler_mutex);
+    handler = &handler_;
+  }
+
+  void detach_cu_handler()
+  {
+    std::unique_lock<std::mutex> lock(handler_mutex);
+    handler = nullptr;
+  }
 
   void on_new_pdu(nru_ul_message msg) override
   {
+    std::unique_lock<std::mutex> lock(handler_mutex);
     if (handler == nullptr) {
       logger.log_info("Cannot handle NR-U UL message. CU-UP bearer does not exist.");
       return;
@@ -94,8 +112,10 @@ public:
   };
 
 private:
+  srs_du::f1u_bearer_logger logger;
+
   srs_cu_up::f1u_rx_pdu_handler* handler = nullptr;
-  srs_du::f1u_bearer_logger      logger;
+  std::mutex                     handler_mutex;
 };
 
 } // namespace srsran
