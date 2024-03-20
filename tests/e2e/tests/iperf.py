58a7f1d431bed58d680d7be8e62bc6c2abab848f
asaezper <alfredo.saez@srs.io>
2024-03-20 12:00:23 +0100

    ci: android retries

--- tests/e2e/tests/iperf.py
+++ tests/e2e/tests/iperf.py
@@ -290,6 +290,7 @@ def test_android(
     ),
 )
 @mark.android_hp
+@mark.flaky(reruns=2, only_rerun=["failed to start", "Exception calling application"])
 # pylint: disable=too-many-arguments
 def test_android_hp(
     retina_manager: RetinaTestManager,
