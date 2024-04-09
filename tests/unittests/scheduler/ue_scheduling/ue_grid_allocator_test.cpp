f431c10e913b24a27f1faa5f7540ab5f984f51a9
Supreeth Herle <supreeth.herle@srs.io>
2024-04-09 14:42:30 +0200

    unittest: add test cases to verify allocation of remaining RBs when max PUCCH or UL grant per slot is reached

--- tests/unittests/scheduler/ue_scheduling/ue_grid_allocator_test.cpp
+++ tests/unittests/scheduler/ue_scheduling/ue_grid_allocator_test.cpp
@@ -11,6 +11,9 @@
 #include "../test_utils/config_generators.h"
 #include "../test_utils/dummy_test_components.h"
 #include "lib/scheduler/config/sched_config_manager.h"
+#include "lib/scheduler/pdcch_scheduling/pdcch_resource_allocator_impl.h"
+#include "lib/scheduler/pucch_scheduling/pucch_allocator_impl.h"
+#include "lib/scheduler/uci_scheduling/uci_allocator_impl.h"
 #include "lib/scheduler/ue_scheduling/ue.h"
 #include "lib/scheduler/ue_scheduling/ue_cell_grid_allocator.h"
 #include "srsran/du/du_cell_config_helpers.h"
@@ -24,6 +27,12 @@ class ue_grid_allocator_tester : public ::testing::Test
 {
 protected:
   ue_grid_allocator_tester() :
+    expert_cfg([this]() {
+      auto ue_expert_cfg                   = sched_cfg.ue;
+      ue_expert_cfg.max_ul_grants_per_slot = 2;
+      ue_expert_cfg.max_pucchs_per_slot    = 2;
+      return ue_expert_cfg;
+    }()),
     cell_cfg(*[this]() {
       cfg_builder_params.dl_arfcn       = 536020;
       cfg_builder_params.channel_bw_mhz = bs_channel_bandwidth_fr1::MHz20;
@@ -32,9 +41,13 @@ protected:
       return cfg;
     }())
   {
-    alloc.add_cell(to_du_cell_index(0), dummy_pdcch_alloc, dummy_uci_alloc, res_grid);
     // Initialize resource grid.
     res_grid.slot_indication({cfg_builder_params.scs_common, 0});
+    pdcch_alloc.slot_indication({cfg_builder_params.scs_common, 0});
+    pucch_alloc.slot_indication({cfg_builder_params.scs_common, 0});
+    uci_alloc.slot_indication({cfg_builder_params.scs_common, 0});
+
+    alloc.add_cell(to_du_cell_index(0), pdcch_alloc, uci_alloc, res_grid);
   }
 
   ue& add_ue(du_ue_index_t ue_index, const std::initializer_list<lcid_t>& lcids_to_activate)
@@ -58,24 +71,8 @@ protected:
     return ues[ue_creation_req.ue_index];
   }
 
-  void set_allocator_responses(const ue_pdsch_grant& grant)
-  {
-    const ue_cell& ue_cc = *grant.user->find_cell(grant.cell_index);
-
-    dummy_uci_alloc.next_uci_allocation.emplace();
-    // TODO: Fix k1 assigned to UCI allocation results when there is more than one entry in k1 list.
-    dummy_uci_alloc.next_uci_allocation.value().k1 =
-        ue_cc.cfg().find_search_space(grant.ss_id)->get_k1_candidates().back();
-
-    dummy_pdcch_alloc.next_ue_pdcch_alloc.ctx.rnti = grant.user->crnti;
-    dummy_pdcch_alloc.next_ue_pdcch_alloc.ctx.bwp_cfg =
-        &ue_cc.cfg().bwp(ue_cc.active_bwp_id()).dl_common->generic_params;
-    dummy_pdcch_alloc.next_ue_pdcch_alloc.ctx.coreset_cfg =
-        &*ue_cc.cfg().bwp(ue_cc.active_bwp_id()).dl_common->pdcch_common.coreset0;
-  }
-
-  const scheduler_expert_config           sched_cfg = config_helpers::make_default_scheduler_expert_config();
-  const scheduler_ue_expert_config&       expert_cfg{sched_cfg.ue};
+  scheduler_expert_config                 sched_cfg = config_helpers::make_default_scheduler_expert_config();
+  scheduler_ue_expert_config              expert_cfg{sched_cfg.ue};
   sched_cfg_dummy_notifier                mac_notif;
   scheduler_ue_metrics_dummy_notifier     metrics_notif;
   scheduler_ue_metrics_dummy_configurator metrics_ue_handler;
@@ -85,9 +82,11 @@ protected:
   sched_config_manager       cfg_mng{scheduler_config{sched_cfg, mac_notif, metrics_notif}, metrics_ue_handler};
   const cell_configuration&  cell_cfg;
 
-  dummy_pdcch_resource_allocator dummy_pdcch_alloc;
-  dummy_uci_allocator            dummy_uci_alloc;
-  cell_resource_allocator        res_grid{cell_cfg};
+  cell_resource_allocator res_grid{cell_cfg};
+
+  pdcch_resource_allocator_impl pdcch_alloc{cell_cfg};
+  pucch_allocator_impl pucch_alloc{cell_cfg, expert_cfg.max_pucchs_per_slot, expert_cfg.max_ul_grants_per_slot};
+  uci_allocator_impl   uci_alloc{pucch_alloc};
 
   ue_repository          ues;
   ue_cell_grid_allocator alloc{expert_cfg, ues, srslog::fetch_basic_logger("SCHED")};
@@ -100,11 +99,10 @@ TEST_F(ue_grid_allocator_tester, when_coreset0_grant_inside_coreset0_rb_lims_the
   ue_pdsch_grant grant{.user           = &u,
                        .cell_index     = to_du_cell_index(0),
                        .h_id           = to_harq_id(0),
-                       .ss_id          = to_search_space_id(1),
+                       .ss_id          = to_search_space_id(0),
                        .time_res_index = 0,
                        .crbs           = cell_cfg.dl_cfg_common.init_dl_bwp.pdcch_common.coreset0->coreset0_crbs(),
                        .aggr_lvl       = aggregation_level::n4};
-  set_allocator_responses(grant);
 
   ASSERT_EQ(alloc.allocate_dl_grant(grant), alloc_outcome::success);
 }
@@ -135,7 +133,6 @@ TEST_F(ue_grid_allocator_tester,
       .crbs           = {crbs.start(),
                          crbs.start() + cell_cfg.dl_cfg_common.init_dl_bwp.pdcch_common.coreset0->coreset0_crbs().length()},
       .aggr_lvl       = aggregation_level::n4};
-  set_allocator_responses(grant);
 
   ASSERT_EQ(alloc.allocate_dl_grant(grant), alloc_outcome::success);
 }
@@ -155,11 +152,10 @@ TEST_F(ue_grid_allocator_tester, when_using_fallback_dci_format_only_64_qam_mcs_
   const ue_pdsch_grant grant{.user           = &u,
                              .cell_index     = to_du_cell_index(0),
                              .h_id           = to_harq_id(0),
-                             .ss_id          = to_search_space_id(1),
+                             .ss_id          = to_search_space_id(0),
                              .time_res_index = 0,
                              .crbs     = cell_cfg.dl_cfg_common.init_dl_bwp.pdcch_common.coreset0->coreset0_crbs(),
                              .aggr_lvl = aggregation_level::n4};
-  set_allocator_responses(grant);
 
   ASSERT_EQ(alloc.allocate_dl_grant(grant), alloc_outcome::success);
   ASSERT_EQ(res_grid[0].result.dl.ue_grants.back().pdsch_cfg.codewords.back().mcs_table,
@@ -185,9 +181,96 @@ TEST_F(ue_grid_allocator_tester, when_using_non_fallback_dci_format_use_mcs_tabl
                              .time_res_index = 0,
                              .crbs     = cell_cfg.dl_cfg_common.init_dl_bwp.pdcch_common.coreset0->coreset0_crbs(),
                              .aggr_lvl = aggregation_level::n4};
-  set_allocator_responses(grant);
 
   ASSERT_EQ(alloc.allocate_dl_grant(grant), alloc_outcome::success);
   ASSERT_EQ(res_grid[0].result.dl.ue_grants.back().pdsch_cfg.codewords.back().mcs_table,
             srsran::pdsch_mcs_table::qam256);
+}
+
+TEST_F(ue_grid_allocator_tester, remaining_dl_rbs_are_allocated_if_max_pucch_per_slot_is_reached)
+{
+  sched_ue_creation_request_message ue_creation_req =
+      test_helpers::create_default_sched_ue_creation_request(this->cfg_builder_params);
+  ue_creation_req.ue_index = to_du_ue_index(0);
+  ue_creation_req.crnti    = to_rnti(0x4601);
+  const ue& u1             = add_ue(ue_creation_req);
+  ue_creation_req.ue_index = to_du_ue_index(1);
+  ue_creation_req.crnti    = to_rnti(0x4602);
+  const ue& u2             = add_ue(ue_creation_req);
+
+  const crb_interval   grant1_crbs = {cell_cfg.dl_cfg_common.init_dl_bwp.pdcch_common.coreset0->coreset0_crbs().start(),
+                                      cell_cfg.dl_cfg_common.init_dl_bwp.pdcch_common.coreset0->coreset0_crbs().start() +
+                                          2};
+  const ue_pdsch_grant grant1{.user           = &u1,
+                              .cell_index     = to_du_cell_index(0),
+                              .h_id           = to_harq_id(0),
+                              .ss_id          = to_search_space_id(2),
+                              .time_res_index = 0,
+                              .crbs           = grant1_crbs,
+                              .aggr_lvl       = aggregation_level::n4};
+
+  // Successfully allocates RBs corresponding to the grant.
+  ASSERT_EQ(alloc.allocate_dl_grant(grant1), alloc_outcome::success);
+  ASSERT_EQ(res_grid[0].result.dl.ue_grants.back().pdsch_cfg.rbs.type1().length(), grant1_crbs.length());
+
+  const crb_interval grant2_crbs = {
+      cell_cfg.dl_cfg_common.init_dl_bwp.pdcch_common.coreset0->coreset0_crbs().start() + 2,
+      cell_cfg.dl_cfg_common.init_dl_bwp.pdcch_common.coreset0->coreset0_crbs().start() + 4};
+  const ue_pdsch_grant grant2{.user           = &u2,
+                              .cell_index     = to_du_cell_index(0),
+                              .h_id           = to_harq_id(0),
+                              .ss_id          = to_search_space_id(2),
+                              .time_res_index = 0,
+                              .crbs           = grant2_crbs,
+                              .aggr_lvl       = aggregation_level::n4};
+
+  // Allocates all remaining RBs to UE2.
+  ASSERT_EQ(alloc.allocate_dl_grant(grant2), alloc_outcome::success);
+  ASSERT_GT(res_grid[0].result.dl.ue_grants.back().pdsch_cfg.rbs.type1().length(), grant2_crbs.length());
+}
+
+TEST_F(ue_grid_allocator_tester, remaining_ul_rbs_are_allocated_if_max_ul_grant_per_slot_is_reached)
+{
+  sched_ue_creation_request_message ue_creation_req =
+      test_helpers::create_default_sched_ue_creation_request(this->cfg_builder_params);
+  ue_creation_req.ue_index = to_du_ue_index(0);
+  ue_creation_req.crnti    = to_rnti(0x4601);
+  const ue& u1             = add_ue(ue_creation_req);
+  ue_creation_req.ue_index = to_du_ue_index(1);
+  ue_creation_req.crnti    = to_rnti(0x4602);
+  const ue& u2             = add_ue(ue_creation_req);
+
+  const crb_interval   grant1_crbs = {cell_cfg.ul_cfg_common.init_ul_bwp.generic_params.crbs.start(),
+                                      cell_cfg.ul_cfg_common.init_ul_bwp.generic_params.crbs.start() + 2};
+  const ue_pusch_grant grant1{.user       = &u1,
+                              .cell_index = to_du_cell_index(0),
+                              .h_id       = to_harq_id(0),
+                              .crbs       = grant1_crbs,
+                              .ss_id      = to_search_space_id(2),
+                              .aggr_lvl   = aggregation_level::n4,
+                              .mcs        = sch_mcs_index{24}};
+
+  // Successfully allocates RBs corresponding to the grant.
+  ASSERT_EQ(alloc.allocate_ul_grant(grant1), alloc_outcome::success);
+  unsigned k2 = cell_cfg.ul_cfg_common.init_ul_bwp.pusch_cfg_common
+                    ->pusch_td_alloc_list[res_grid[0].result.dl.ul_pdcchs.back().dci.c_rnti_f0_1.time_resource]
+                    .k2;
+  ASSERT_EQ(res_grid[k2].result.ul.puschs.back().pusch_cfg.rbs.type1().length(), grant1_crbs.length());
+
+  const crb_interval   grant2_crbs = {cell_cfg.ul_cfg_common.init_ul_bwp.generic_params.crbs.start() + 2,
+                                      cell_cfg.ul_cfg_common.init_ul_bwp.generic_params.crbs.start() + 4};
+  const ue_pusch_grant grant2{.user       = &u2,
+                              .cell_index = to_du_cell_index(0),
+                              .h_id       = to_harq_id(0),
+                              .crbs       = grant1_crbs,
+                              .ss_id      = to_search_space_id(2),
+                              .aggr_lvl   = aggregation_level::n4,
+                              .mcs        = sch_mcs_index{24}};
+
+  // Allocates all remaining RBs to UE2.
+  ASSERT_EQ(alloc.allocate_ul_grant(grant2), alloc_outcome::success);
+  k2 = cell_cfg.ul_cfg_common.init_ul_bwp.pusch_cfg_common
+           ->pusch_td_alloc_list[res_grid[0].result.dl.ul_pdcchs.back().dci.c_rnti_f0_1.time_resource]
+           .k2;
+  ASSERT_GT(res_grid[k2].result.ul.puschs.back().pusch_cfg.rbs.type1().length(), grant2_crbs.length());
 }
