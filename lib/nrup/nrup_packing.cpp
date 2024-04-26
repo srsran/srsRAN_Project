ed4ee05c146c891cdc8120a8688e6adabe0aea21
Robert Falkenberg <robert.falkenberg@srs.io>
2024-04-26 11:40:35 +0200

    nrup: verify read when unpacking  DL user data

--- lib/nrup/nrup_packing.cpp
+++ lib/nrup/nrup_packing.cpp
@@ -13,6 +13,13 @@
 
 namespace srsran {
 
+/// Macro used to check a read/unpack operation and log an error message if the validation fails.
+#define VERIFY_READ(cond)                                                                                              \
+  if (not(cond)) {                                                                                                     \
+    logger.error("Read failed in {}", __FUNCTION__);                                                                   \
+    return false;                                                                                                      \
+  }
+
 /// Macro used to check a write/append/pack operation and log an error message if the validation fails.
 #define VERIFY_WRITE(cond)                                                                                             \
   if (not(cond)) {                                                                                                     \
@@ -43,7 +50,7 @@ bool nrup_packing::unpack(nru_dl_user_data& dl_user_data, byte_buffer_view conta
 
   // PDU Type
   uint8_t pdu_type = {};
-  decoder.unpack(pdu_type, 4);
+  VERIFY_READ(decoder.unpack(pdu_type, 4));
   if (uint_to_nrup_pdu_type(pdu_type) != nrup_pdu_type::dl_user_data) {
     logger.error("Failed to unpack DL user data: Invalid pdu_type={}", uint_to_nrup_pdu_type(pdu_type));
     return false;
@@ -51,7 +58,7 @@ bool nrup_packing::unpack(nru_dl_user_data& dl_user_data, byte_buffer_view conta
 
   // Spare (v15.2.0)
   uint8_t spare = {};
-  decoder.unpack(spare, 1);
+  VERIFY_READ(decoder.unpack(spare, 1));
   if (spare != 0) {
     logger.error("Failed to unpack DL user data: Spare bit set in first octet. value={:#x}", spare);
     return false;
@@ -59,18 +66,18 @@ bool nrup_packing::unpack(nru_dl_user_data& dl_user_data, byte_buffer_view conta
 
   // DL discard blocks
   bool dl_discard_blocks = {};
-  decoder.unpack(dl_discard_blocks, 1);
+  VERIFY_READ(decoder.unpack(dl_discard_blocks, 1));
 
   // DL flush
   bool dl_flush = {};
-  decoder.unpack(dl_flush, 1);
+  VERIFY_READ(decoder.unpack(dl_flush, 1));
 
   // Report polling
-  decoder.unpack(dl_user_data.report_polling, 1);
+  VERIFY_READ(decoder.unpack(dl_user_data.report_polling, 1));
 
   // Spare (v15.2.0)
   spare = {};
-  decoder.unpack(spare, 6);
+  VERIFY_READ(decoder.unpack(spare, 6));
   if (spare != 0) {
     logger.error("Failed to unpack DL user data: Spare bits set in first octet. value={:#x}", spare);
     return false;
@@ -80,22 +87,23 @@ bool nrup_packing::unpack(nru_dl_user_data& dl_user_data, byte_buffer_view conta
   decoder.unpack(dl_user_data.assist_info_report_polling_flag, 1);
 
   // Retransmission flag
-  decoder.unpack(dl_user_data.retransmission_flag, 1);
+  VERIFY_READ(decoder.unpack(dl_user_data.retransmission_flag, 1));
 
   // NR-U Sequence Number
-  decoder.unpack(dl_user_data.nru_sn, 24);
+  VERIFY_READ(decoder.unpack(dl_user_data.nru_sn, 24));
 
   // DL discard NR PDCP PDU SN (if present)
   if (dl_flush) {
     uint32_t dl_discard_pdcp_sn = {};
-    decoder.unpack(dl_discard_pdcp_sn, 24);
+    VERIFY_READ(decoder.unpack(dl_discard_pdcp_sn, 24));
     dl_user_data.dl_discard_pdcp_sn = dl_discard_pdcp_sn;
   }
 
-  // DL discard number of blocks
+  // Discard blocks (if present)
   if (dl_discard_blocks) {
+    // DL discard number of blocks
     uint8_t nof_discard_blocks = {};
-    decoder.unpack(nof_discard_blocks, 8);
+    VERIFY_READ(decoder.unpack(nof_discard_blocks, 8));
 
     if (nof_discard_blocks > nru_max_nof_pdcp_sn_discard_blocks) {
       logger.error("Failed to unpack DL user data: nof_discard_blocks={} exceeds max_nof_pdcp_sn_discard_blocks={}",
@@ -104,11 +112,12 @@ bool nrup_packing::unpack(nru_dl_user_data& dl_user_data, byte_buffer_view conta
       return false;
     }
 
+    // Read all discard blocks
     dl_user_data.discard_blocks = nru_pdcp_sn_discard_blocks{};
     for (uint8_t i = 0; i < nof_discard_blocks; i++) {
       dl_user_data.discard_blocks.value().push_back(nru_pdcp_sn_discard_block{});
-      decoder.unpack(dl_user_data.discard_blocks.value().back().pdcp_sn_start, 24);
-      decoder.unpack(dl_user_data.discard_blocks.value().back().block_size, 8);
+      VERIFY_READ(decoder.unpack(dl_user_data.discard_blocks.value().back().pdcp_sn_start, 24));
+      VERIFY_READ(decoder.unpack(dl_user_data.discard_blocks.value().back().block_size, 8));
     }
   }
 
