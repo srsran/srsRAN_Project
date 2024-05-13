37574011958bb12f7b2ad1eab5e0e1050642af9f
Xavier Arteaga <xavier@srs.io>
2024-05-13 17:38:57 +0200

    phy: create channel state information test and fix

---
+++ include/srsran/phy/upper/channel_state_information_formatters.h
@@ -0,0 +1,110 @@
+/*
+ *
+ * Copyright 2021-2024 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#pragma once
+
+#include "srsran/phy/upper/channel_state_information.h"
+#include "srsran/support/format_utils.h"
+#include <fmt/core.h>
+
+namespace fmt {
+
+/// \brief Custom formatter for \c channel_state_information.
+template <>
+struct formatter<srsran::channel_state_information> {
+  /// Helper used to parse formatting options and format fields.
+  srsran::delimited_formatter helper;
+
+  /// Default constructor.
+  formatter() = default;
+
+  template <typename ParseContext>
+  auto parse(ParseContext& ctx) -> decltype(ctx.begin())
+  {
+    return helper.parse(ctx);
+  }
+
+  template <typename FormatContext>
+  auto format(const srsran::channel_state_information& csi, FormatContext& ctx)
+      -> decltype(std::declval<FormatContext>().out())
+  {
+    // Verbose representation prints all available SINR parameters. It appends [sel] to the label of the selected
+    // measurement point.
+    if (helper.is_verbose()) {
+      if (csi.sinr_ch_estimator_dB.has_value()) {
+        const char* selected =
+            (csi.sinr_report_type == srsran::channel_state_information::sinr_type::channel_estimator) ? "[sel]" : "";
+        helper.format_if_verbose(ctx, "sinr_ch_est{}={:.1f}dB", selected, csi.sinr_ch_estimator_dB.value());
+      }
+
+      if (csi.sinr_post_eq_dB.has_value()) {
+        const char* selected =
+            (csi.sinr_report_type == srsran::channel_state_information::sinr_type::post_equalization) ? "[sel]" : "";
+        helper.format_if_verbose(ctx, "sinr_eq{}={:.1f}dB", selected, csi.sinr_post_eq_dB.value());
+      }
+
+      if (csi.sinr_evm_dB.has_value()) {
+        const char* selected =
+            (csi.sinr_report_type == srsran::channel_state_information::sinr_type::evm) ? "[sel]" : "";
+        helper.format_if_verbose(ctx, "sinr_evm{}={:.1f}dB", selected, csi.sinr_evm_dB.value());
+      }
+
+      if (csi.evm.has_value()) {
+        helper.format_if_verbose(ctx, "evm={:.2f}", csi.evm.value());
+      }
+
+      // Extract CSI measurements. It could be some of them are not available.
+      srsran::optional<float>                 epre_dB       = csi.get_epre_dB();
+      srsran::optional<float>                 rsrp_dB       = csi.get_rsrp_dB();
+      srsran::optional<float>                 sinr_dB       = csi.get_sinr_dB();
+      srsran::optional<srsran::phy_time_unit> time_aligment = csi.get_time_alignment();
+      srsran::optional<float>                 cfo_Hz        = csi.get_cfo_Hz();
+
+      // Print the measurements that are present.
+      if (epre_dB.has_value()) {
+        helper.format_if_verbose(ctx, "epre={:+.1f}dB", epre_dB.value());
+      } else {
+        helper.format_if_verbose(ctx, "epre=na");
+      }
+      if (rsrp_dB.has_value()) {
+        helper.format_if_verbose(ctx, "rsrp={:+.1f}dB", rsrp_dB.value());
+      } else {
+        helper.format_if_verbose(ctx, "rsrp=na");
+      }
+      if (sinr_dB.has_value()) {
+        helper.format_if_verbose(ctx, "sinr={:+.1f}dB", sinr_dB.value());
+      } else {
+        helper.format_if_verbose(ctx, "sinr=na");
+      }
+      if (time_aligment.has_value()) {
+        helper.format_if_verbose(ctx, "t_align={:.2f}us", time_aligment.value().to_seconds() * 1e6);
+      } else {
+        helper.format_if_verbose(ctx, "t_align=na");
+      }
+      if (cfo_Hz.has_value()) {
+        helper.format_if_verbose(ctx, "cfo={:+.1f}Hz", cfo_Hz.value());
+      } else {
+        helper.format_if_verbose(ctx, "cfo=na");
+      }
+    } else {
+      // Short representation only prints the SINR selected for CSI reporting to higher layers.
+      srsran::optional<float> sinr_dB = csi.get_sinr_dB();
+      if (sinr_dB.has_value()) {
+        helper.format_always(ctx, "sinr={:.1f}dB", sinr_dB.value());
+      } else {
+        // SINR is not available.
+        helper.format_always(ctx, "sinr=na");
+      }
+    }
+    return ctx.out();
+  }
+};
+
+} // namespace fmt
---
+++ tests/unittests/phy/upper/channel_state_information_test.cpp
@@ -0,0 +1,107 @@
+/*
+ *
+ * Copyright 2021-2024 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "srsran/phy/upper/channel_state_information_formatters.h"
+#include <gtest/gtest.h>
+#include <random>
+
+using namespace srsran;
+
+namespace {
+
+using test_params = std::tuple<channel_state_information::sinr_type, bool, bool, bool, bool, bool, bool, bool>;
+
+class ChannelStateInformationFixture : public ::testing::TestWithParam<test_params>
+{
+protected:
+  void SetUp() override
+  {
+    Test::SetUp();
+    const test_params& params = GetParam();
+    sinr_type                 = std::get<0>(params);
+    enable_ce_sinr            = std::get<1>(params);
+    enable_eq_sinr            = std::get<2>(params);
+    enable_ev_sinr            = std::get<3>(params);
+    enable_rsrp               = std::get<4>(params);
+    enable_epre               = std::get<5>(params);
+    enable_cfo                = std::get<6>(params);
+    enable_ta                 = std::get<7>(params);
+  }
+
+  channel_state_information generate_random_csi()
+  {
+    channel_state_information csi(sinr_type);
+    if (enable_ce_sinr) {
+      csi.set_sinr_dB(channel_state_information::sinr_type::channel_estimator, real_dist(rgen));
+    }
+    if (enable_eq_sinr) {
+      csi.set_sinr_dB(channel_state_information::sinr_type::post_equalization, real_dist(rgen));
+    }
+    if (enable_ev_sinr) {
+      csi.set_sinr_dB(channel_state_information::sinr_type::evm, real_dist(rgen));
+      csi.set_evm(real_dist(rgen));
+    }
+    if (enable_rsrp) {
+      csi.set_rsrp(real_dist(rgen));
+    }
+    if (enable_epre) {
+      csi.set_epre(real_dist(rgen));
+    }
+    if (enable_cfo) {
+      csi.set_cfo(real_dist(rgen));
+    }
+    if (enable_ta) {
+      csi.set_time_alignment(phy_time_unit::from_seconds(real_dist(rgen)));
+    }
+    return csi;
+  }
+
+private:
+  static std::mt19937                          rgen;
+  static std::uniform_real_distribution<float> real_dist;
+  channel_state_information::sinr_type         sinr_type;
+  bool                                         enable_ce_sinr;
+  bool                                         enable_eq_sinr;
+  bool                                         enable_ev_sinr;
+  bool                                         enable_rsrp;
+  bool                                         enable_epre;
+  bool                                         enable_cfo;
+  bool                                         enable_ta;
+};
+
+std::mt19937                          ChannelStateInformationFixture::rgen;
+std::uniform_real_distribution<float> ChannelStateInformationFixture::real_dist(-100, +100);
+
+TEST_P(ChannelStateInformationFixture, FormatterDefault)
+{
+  fmt::memory_buffer buffer;
+  fmt::format_to(buffer, "{}", generate_random_csi());
+}
+
+TEST_P(ChannelStateInformationFixture, FormatterShort)
+{
+  fmt::memory_buffer buffer;
+  fmt::format_to(buffer, "{:s}", generate_random_csi());
+}
+
+INSTANTIATE_TEST_SUITE_P(PucchProcessorFormat1,
+                         ChannelStateInformationFixture,
+                         ::testing::Combine(::testing::Values(channel_state_information::sinr_type::channel_estimator,
+                                                              channel_state_information::sinr_type::post_equalization,
+                                                              channel_state_information::sinr_type::evm),
+                                            ::testing::Bool(),
+                                            ::testing::Bool(),
+                                            ::testing::Bool(),
+                                            ::testing::Bool(),
+                                            ::testing::Bool(),
+                                            ::testing::Bool(),
+                                            ::testing::Bool()));
+
+} // namespace
