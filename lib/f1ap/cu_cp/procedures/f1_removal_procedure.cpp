2ad6494766d95d2a597ada9f36305084c081cad4
Francisco Paisana <francisco.paisana@srs.io>
2024-05-20 13:38:50 +0200

    f1ap-cu: fix F1AP removal procedure, when transaction cancel causes the existing UEs to be removed

--- lib/f1ap/cu_cp/procedures/f1_removal_procedure.cpp
+++ lib/f1ap/cu_cp/procedures/f1_removal_procedure.cpp
@@ -45,14 +45,26 @@ void f1_removal_procedure::operator()(coro_context<async_task<void>>& ctx)
 async_task<void> f1_removal_procedure::handle_ue_transaction_info_loss()
 {
   f1_ue_transaction_info_loss_event ev;
+
+  // Add DU UEs to the list of UEs with transaction information lost.
   ev.ues_lost.reserve(ue_list.size());
   for (auto& ue : ue_list) {
-    // After receiving an F1 Removal Request, no more F1AP Rx PDUs are expected. Cancel running UE F1AP transactions.
-    ue.second.ev_mng.cancel_all();
-
-    // Add UE to the list of UEs with transaction information lost.
     ev.ues_lost.push_back(ue.second.ue_ids.ue_index);
   }
+
+  // After receiving an F1 Removal Request, no more F1AP Rx PDUs are expected. Cancel running UE F1AP transactions.
+  // Note: size of ue_list may change during this operation (e.g. if a concurrent UE context release was being
+  // processed and got cancelled). Therefore, we leverage the list ev.ues_lost for the iteration.
+  for (ue_index_t ue_idx : ev.ues_lost) {
+    auto* u = ue_list.find(ue_idx);
+    if (u != nullptr) {
+      u->ev_mng.cancel_all();
+    }
+  }
+  ev.ues_lost.erase(std::remove_if(ev.ues_lost.begin(),
+                                   ev.ues_lost.end(),
+                                   [this](ue_index_t ue_idx) { return ue_list.find(ue_idx) == nullptr; }),
+                    ev.ues_lost.end());
 
   logger.info("{}: F1 Removal Request received, but there are still UEs connected to the DU. Resetting {} UEs.",
               name(),
