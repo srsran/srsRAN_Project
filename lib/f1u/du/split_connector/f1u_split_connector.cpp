b364072fad23f5b09af194e7c53a7204fbcda068
Pedro Alvarez <pedro@srs.io>
2024-05-29 21:23:45 +0100

    du,f1u: shorten locking in DU GW

--- lib/f1u/du/split_connector/f1u_split_connector.cpp
+++ lib/f1u/du/split_connector/f1u_split_connector.cpp
@@ -28,16 +28,17 @@ f1u_split_connector::create_du_bearer(uint32_t                                  
 {
   logger_du.info(
       "Creating DU gateway local bearer with UL GTP Tunnel={} DL GTP Tunnel={}", ul_up_tnl_info, dl_up_tnl_info);
-  std::unique_lock<std::mutex> lock(map_mutex);
-  srsran_assert(du_map.find(ul_up_tnl_info) == du_map.end(),
-                "Cannot create DU gateway local bearer with already existing UL GTP Tunnel={}",
-                ul_up_tnl_info);
-
   std::unique_ptr<f1u_split_gateway_du_bearer> du_bearer = std::make_unique<f1u_split_gateway_du_bearer>(
       ue_index, drb_id, dl_up_tnl_info, du_rx, ul_up_tnl_info, *this, gtpu_pcap, peer_port);
+  {
+    std::unique_lock<std::mutex> lock(map_mutex);
+    srsran_assert(du_map.find(ul_up_tnl_info) == du_map.end(),
+                  "Cannot create DU gateway local bearer with already existing UL GTP Tunnel={}",
+                  ul_up_tnl_info);
 
-  du_bearer->gtpu_to_network_adapter.connect(*udp_session);
-  du_map.insert({dl_up_tnl_info, du_bearer.get()});
+    du_bearer->gtpu_to_network_adapter.connect(*udp_session);
+    du_map.insert({dl_up_tnl_info, du_bearer.get()});
+  }
 
   // attach RX to DEMUX
   if (!demux->add_tunnel(dl_up_tnl_info.gtp_teid, ue_executor, du_bearer->get_tunnel_rx_interface())) {
@@ -50,14 +51,15 @@ f1u_split_connector::create_du_bearer(uint32_t                                  
 
 void f1u_split_connector::remove_du_bearer(const up_transport_layer_info& dl_up_tnl_info)
 {
-  std::unique_lock<std::mutex> lock(map_mutex);
-
-  if (du_map.find(dl_up_tnl_info) == du_map.end()) {
-    logger_du.warning("Could not find UL GTP Tunnel at CU-CP to disconnect", dl_up_tnl_info);
-    return;
+  f1u_split_gateway_du_bearer* du_bearer = nullptr;
+  {
+    std::unique_lock<std::mutex> lock(map_mutex);
+    if (du_map.find(dl_up_tnl_info) == du_map.end()) {
+      logger_du.warning("Could not find UL GTP Tunnel at CU-CP to disconnect", dl_up_tnl_info);
+      return;
+    }
+    du_bearer = du_map.at(dl_up_tnl_info);
   }
-  f1u_split_gateway_du_bearer* du_bearer = du_map.at(dl_up_tnl_info);
-
   // disconnect adapters
   du_bearer->gtpu_to_network_adapter.disconnect();
   du_bearer->gtpu_to_f1u_adapter.disconnect();
@@ -66,6 +68,9 @@ void f1u_split_connector::remove_du_bearer(const up_transport_layer_info& dl_up_
   demux->remove_tunnel(dl_up_tnl_info.gtp_teid);
 
   // Remove DL path
-  du_map.erase(dl_up_tnl_info);
+  {
+    std::unique_lock<std::mutex> lock(map_mutex);
+    du_map.erase(dl_up_tnl_info);
+  }
   logger_du.debug("Removed CU F1-U bearer with UL GTP Tunnel={}.", dl_up_tnl_info);
 }
