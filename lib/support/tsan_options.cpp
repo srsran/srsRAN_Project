9046ff9a076933fc2d0dcdf3c55500556f38bcbd
Xavier Arteaga <xavier@srs.io>
2024-06-13 09:39:05 +0200

    support: make TSAN options always included
    Compile TSAN options only if TSAN is enabled
    
    apply clang-format

--- lib/support/tsan_options.cpp
+++ lib/support/tsan_options.cpp
@@ -10,7 +10,7 @@
  *
  */
 
-#pragma once
+#ifdef __SANITIZE_THREAD__
 
 // Options taken from Mozilla project
 //   abort_on_error=1 - Causes TSan to abort instead of using exit().
@@ -24,9 +24,7 @@
 //   allocations the same way we would handle them with a regular allocator and
 //   also uncovers potential bugs that might occur in these situations.
 
-#ifdef __cplusplus
 extern "C" {
-#endif
 
 const char* __tsan_default_options()
 {
@@ -47,7 +45,6 @@ const char* __tsan_default_suppressions()
       "race:libusb*\n"
       "race:libuhd*\n";
 }
-
-#ifdef __cplusplus
 }
-#endif
+
+#endif // __SANITIZE_THREAD__
