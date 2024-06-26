dedfbd80694becb6f88d2f1dfa9a368edf697a43
Pedro Alvarez <pedro@srs.io>
2024-06-26 17:34:57 +0100

    cu_up: move bearer setup/modification from cu to cu manager

--- lib/cu_up/cu_up_manager_impl.cpp
+++ lib/cu_up/cu_up_manager_impl.cpp
@@ -9,12 +9,6 @@
  */
 
 #include "cu_up_manager_impl.h"
-#include "routines/initial_cu_up_setup_routine.h"
-#include "srsran/e1ap/cu_up/e1ap_cu_up_factory.h"
-#include "srsran/gtpu/gtpu_demux_factory.h"
-#include "srsran/gtpu/gtpu_echo_factory.h"
-#include "srsran/gtpu/gtpu_teid_pool_factory.h"
-#include <future>
 
 using namespace srsran;
 using namespace srs_cu_up;
@@ -29,59 +23,22 @@ void assert_cu_up_configuration_valid(const cu_up_configuration& cfg)
   srsran_assert(cfg.gtpu_pcap != nullptr, "Invalid GTP-U pcap");
 }
 
+/// Helper functions
 void fill_sec_as_config(security::sec_as_config& sec_as_config, const e1ap_security_info& sec_info);
+void process_successful_pdu_resource_modification_outcome(
+    slotted_id_vector<pdu_session_id_t, e1ap_pdu_session_resource_modified_item>& pdu_session_resource_modified_list,
+    slotted_id_vector<pdu_session_id_t, e1ap_pdu_session_resource_failed_item>&
+                                           pdu_session_resource_failed_to_modify_list,
+    const pdu_session_modification_result& result,
+    const srslog::basic_logger&            logger);
+void process_successful_pdu_resource_setup_mod_outcome(
+    slotted_id_vector<pdu_session_id_t, e1ap_pdu_session_resource_setup_modification_item>&
+                                    pdu_session_resource_setup_list,
+    const pdu_session_setup_result& result);
 
 cu_up_manager_impl::cu_up_manager_impl(const cu_up_configuration& config_) : cfg(config_), main_ctrl_loop(128)
 {
   assert_cu_up_configuration_valid(cfg);
-
-  /// > Create and connect upper layers
-
-  // Create GTP-U demux
-  gtpu_demux_creation_request demux_msg = {};
-  demux_msg.cfg.warn_on_drop            = cfg.n3_cfg.warn_on_drop;
-  demux_msg.gtpu_pcap                   = cfg.gtpu_pcap;
-  ngu_demux                             = create_gtpu_demux(demux_msg);
-
-  ctrl_exec_mapper = cfg.ue_exec_pool->create_ue_executor_mapper();
-  report_error_if_not(ctrl_exec_mapper != nullptr, "Could not create CU-UP executor for control TEID");
-
-  // Create GTP-U echo and register it at demux
-  gtpu_echo_creation_message ngu_echo_msg = {};
-  ngu_echo_msg.gtpu_pcap                  = cfg.gtpu_pcap;
-  ngu_echo_msg.tx_upper                   = &gtpu_gw_adapter;
-  ngu_echo                                = create_gtpu_echo(ngu_echo_msg);
-  ngu_demux->add_tunnel(
-      GTPU_PATH_MANAGEMENT_TEID, ctrl_exec_mapper->dl_pdu_executor(), ngu_echo->get_rx_upper_layer_interface());
-
-  // Connect GTP-U DEMUX to adapter.
-  gw_data_gtpu_demux_adapter.connect_gtpu_demux(*ngu_demux);
-
-  // Establish new NG-U session and connect the instantiated session to the GTP-U DEMUX adapter, so that the latter
-  // is called when new NG-U DL PDUs are received.
-  ngu_session = cfg.ngu_gw->create(gw_data_gtpu_demux_adapter);
-  if (ngu_session == nullptr) {
-    report_error("Unable to allocate the required NG-U network resources");
-  }
-
-  // Connect GTPU GW adapter to NG-U session in order to send UL PDUs.
-  gtpu_gw_adapter.connect_network_gateway(*ngu_session);
-
-  // Create N3 TEID allocator
-  gtpu_allocator_creation_request n3_alloc_msg = {};
-  n3_alloc_msg.max_nof_teids                   = MAX_NOF_UES * MAX_NOF_PDU_SESSIONS;
-  n3_teid_allocator                            = create_gtpu_allocator(n3_alloc_msg);
-
-  // Create F1-U TEID allocator
-  gtpu_allocator_creation_request f1u_alloc_msg = {};
-  f1u_alloc_msg.max_nof_teids                   = MAX_NOF_UES * MAX_NOF_PDU_SESSIONS;
-  f1u_teid_allocator                            = create_gtpu_allocator(f1u_alloc_msg);
-
-  /// > Create e1ap
-  // e1ap = create_e1ap(*cfg.e1ap.e1_conn_client, e1ap_cu_up_ev_notifier, *cfg.timers, *cfg.ctrl_executor);
-  //  e1ap_cu_up_ev_notifier.connect_cu_up(*this);
-
-  cfg.e1ap.e1ap_conn_mng = e1ap.get();
 
   /// > Create UE manager
   ue_mng = std::make_unique<ue_manager>(cfg.net_cfg,
@@ -96,14 +53,6 @@ cu_up_manager_impl::cu_up_manager_impl(const cu_up_configuration& config_) : cfg
                                         *cfg.ue_exec_pool,
                                         *cfg.gtpu_pcap,
                                         logger);
-
-  // Start statistics report timer
-  if (cfg.statistics_report_period.count() > 0) {
-    statistics_report_timer = cfg.timers->create_unique_timer(*cfg.ctrl_executor);
-    statistics_report_timer.set(cfg.statistics_report_period,
-                                [this](timer_id_t /*tid*/) { on_statistics_report_timer_expired(); });
-    statistics_report_timer.run();
-  }
 }
 
 void cu_up_manager_impl::disconnect()
@@ -111,4 +60,187 @@ void cu_up_manager_impl::disconnect()
   gw_data_gtpu_demux_adapter.disconnect();
   gtpu_gw_adapter.disconnect();
   // e1ap_cu_up_ev_notifier.disconnect();
+}
+
+e1ap_bearer_context_modification_response
+cu_up_manager_impl::handle_bearer_context_modification_request(const e1ap_bearer_context_modification_request& msg)
+{
+  ue_context* ue_ctxt = ue_mng->find_ue(msg.ue_index);
+  if (ue_ctxt == nullptr) {
+    logger.error("Could not find UE context");
+    return {};
+  }
+
+  ue_ctxt->get_logger().log_debug("Handling BearerContextModificationRequest");
+
+  e1ap_bearer_context_modification_response response = {};
+  response.ue_index                                  = ue_ctxt->get_index();
+  response.success                                   = true;
+
+  bool new_ul_tnl_info_required = msg.new_ul_tnl_info_required == std::string("required");
+
+  if (msg.security_info.has_value()) {
+    security::sec_as_config security_info;
+    fill_sec_as_config(security_info, msg.security_info.value());
+    ue_ctxt->set_security_config(security_info);
+  }
+
+  if (msg.ng_ran_bearer_context_mod_request.has_value()) {
+    // Traverse list of PDU sessions to be setup/modified
+    for (const auto& pdu_session_item :
+         msg.ng_ran_bearer_context_mod_request.value().pdu_session_res_to_setup_mod_list) {
+      ue_ctxt->get_logger().log_debug("Setup/Modification of {}", pdu_session_item.pdu_session_id);
+      pdu_session_setup_result session_result = ue_ctxt->setup_pdu_session(pdu_session_item);
+      process_successful_pdu_resource_setup_mod_outcome(response.pdu_session_resource_setup_list, session_result);
+      response.success &= session_result.success; // Update final result.
+    }
+
+    // Traverse list of PDU sessions to be modified.
+    for (const auto& pdu_session_item : msg.ng_ran_bearer_context_mod_request.value().pdu_session_res_to_modify_list) {
+      ue_ctxt->get_logger().log_debug("Modifying {}", pdu_session_item.pdu_session_id);
+      pdu_session_modification_result session_result =
+          ue_ctxt->modify_pdu_session(pdu_session_item, new_ul_tnl_info_required);
+      process_successful_pdu_resource_modification_outcome(response.pdu_session_resource_modified_list,
+                                                           response.pdu_session_resource_failed_to_modify_list,
+                                                           session_result,
+                                                           logger);
+      ue_ctxt->get_logger().log_debug("Modification {}", session_result.success ? "successful" : "failed");
+
+      response.success &= session_result.success; // Update final result.
+    }
+
+    // Traverse list of PDU sessions to be removed.
+    for (const auto& pdu_session_item : msg.ng_ran_bearer_context_mod_request.value().pdu_session_res_to_rem_list) {
+      ue_ctxt->get_logger().log_info("Removing {}", pdu_session_item);
+      ue_ctxt->remove_pdu_session(pdu_session_item);
+      // There is no IE to confirm successful removal.
+    }
+  } else {
+    ue_ctxt->get_logger().log_warning("Ignoring empty Bearer Context Modification Request");
+  }
+
+  // 3. Create response
+  response.success = true;
+  return response;
+}
+
+void cu_up_manager_impl::handle_bearer_context_release_command(const e1ap_bearer_context_release_command& msg)
+{
+  ue_context* ue_ctxt = ue_mng->find_ue(msg.ue_index);
+  if (ue_ctxt == nullptr) {
+    logger.error("ue={}: Discarding E1 Bearer Context Release Command. UE context not found", msg.ue_index);
+    return;
+  }
+
+  ue_ctxt->get_logger().log_debug("Received E1 Bearer Context Release Command");
+
+  ue_mng->remove_ue(msg.ue_index);
+}
+
+/// Helper functions
+void process_successful_pdu_resource_modification_outcome(
+    slotted_id_vector<pdu_session_id_t, e1ap_pdu_session_resource_modified_item>& pdu_session_resource_modified_list,
+    slotted_id_vector<pdu_session_id_t, e1ap_pdu_session_resource_failed_item>&
+                                           pdu_session_resource_failed_to_modify_list,
+    const pdu_session_modification_result& result,
+    const srslog::basic_logger&            logger)
+{
+  if (result.success) {
+    e1ap_pdu_session_resource_modified_item modified_item;
+    modified_item.pdu_session_id = result.pdu_session_id;
+
+    for (const auto& drb_setup_item : result.drb_setup_results) {
+      logger.debug("Adding DRB setup result item. {}, success={}", drb_setup_item.drb_id, drb_setup_item.success);
+      if (drb_setup_item.success) {
+        e1ap_drb_setup_item_ng_ran res_drb_setup_item;
+        res_drb_setup_item.drb_id = drb_setup_item.drb_id;
+
+        e1ap_up_params_item up_param_item;
+        up_param_item.up_tnl_info = drb_setup_item.gtp_tunnel;
+        res_drb_setup_item.ul_up_transport_params.push_back(up_param_item);
+
+        for (const auto& flow_item : drb_setup_item.qos_flow_results) {
+          if (flow_item.success) {
+            e1ap_qos_flow_item res_flow_setup_item;
+            res_flow_setup_item.qos_flow_id = flow_item.qos_flow_id;
+            res_drb_setup_item.flow_setup_list.emplace(flow_item.qos_flow_id, res_flow_setup_item);
+          } else {
+            e1ap_qos_flow_failed_item res_flow_failed_item;
+            res_flow_failed_item.qos_flow_id = flow_item.qos_flow_id;
+            res_flow_failed_item.cause       = flow_item.cause;
+            res_drb_setup_item.flow_failed_list.emplace(flow_item.qos_flow_id, res_flow_failed_item);
+          }
+        }
+        modified_item.drb_setup_list_ng_ran.emplace(drb_setup_item.drb_id, res_drb_setup_item);
+      } else {
+        e1ap_drb_failed_item_ng_ran asn1_drb_failed_item;
+        asn1_drb_failed_item.drb_id = drb_setup_item.drb_id;
+        asn1_drb_failed_item.cause  = drb_setup_item.cause;
+
+        modified_item.drb_failed_list_ng_ran.emplace(drb_setup_item.drb_id, asn1_drb_failed_item);
+      }
+    }
+    for (const auto& drb_modified_item : result.drb_modification_results) {
+      logger.debug(
+          "Adding DRB modified result item. {}, success={}", drb_modified_item.drb_id, drb_modified_item.success);
+      e1ap_drb_modified_item_ng_ran e1ap_mod_item;
+      e1ap_mod_item.drb_id = drb_modified_item.drb_id;
+
+      e1ap_up_params_item up_param_item;
+      up_param_item.up_tnl_info = drb_modified_item.gtp_tunnel;
+      e1ap_mod_item.ul_up_transport_params.push_back(up_param_item);
+      modified_item.drb_modified_list_ng_ran.emplace(e1ap_mod_item.drb_id, e1ap_mod_item);
+    }
+
+    pdu_session_resource_modified_list.emplace(modified_item.pdu_session_id, modified_item);
+  } else {
+    e1ap_pdu_session_resource_failed_item failed_item;
+    failed_item.pdu_session_id = result.pdu_session_id;
+    failed_item.cause          = e1ap_cause_radio_network_t::unspecified;
+    pdu_session_resource_failed_to_modify_list.emplace(failed_item.pdu_session_id, failed_item);
+  }
+}
+
+void process_successful_pdu_resource_setup_mod_outcome(
+    slotted_id_vector<pdu_session_id_t, e1ap_pdu_session_resource_setup_modification_item>&
+                                    pdu_session_resource_setup_list,
+    const pdu_session_setup_result& result)
+{
+  if (result.success) {
+    e1ap_pdu_session_resource_setup_modification_item res_setup_item;
+    res_setup_item.pdu_session_id    = result.pdu_session_id;
+    res_setup_item.ng_dl_up_tnl_info = result.gtp_tunnel;
+    res_setup_item.security_result   = result.security_result;
+    for (const auto& drb_setup_item : result.drb_setup_results) {
+      if (drb_setup_item.success) {
+        e1ap_drb_setup_item_ng_ran res_drb_setup_item;
+        res_drb_setup_item.drb_id = drb_setup_item.drb_id;
+
+        e1ap_up_params_item up_param_item;
+        up_param_item.up_tnl_info = drb_setup_item.gtp_tunnel;
+        res_drb_setup_item.ul_up_transport_params.push_back(up_param_item);
+
+        for (const auto& flow_item : drb_setup_item.qos_flow_results) {
+          if (flow_item.success) {
+            e1ap_qos_flow_item res_flow_setup_item;
+            res_flow_setup_item.qos_flow_id = flow_item.qos_flow_id;
+            res_drb_setup_item.flow_setup_list.emplace(flow_item.qos_flow_id, res_flow_setup_item);
+          } else {
+            e1ap_qos_flow_failed_item res_flow_failed_item;
+            res_flow_failed_item.qos_flow_id = flow_item.qos_flow_id;
+            res_flow_failed_item.cause       = flow_item.cause;
+            res_drb_setup_item.flow_failed_list.emplace(flow_item.qos_flow_id, res_flow_failed_item);
+          }
+        }
+        res_setup_item.drb_setup_list_ng_ran.emplace(drb_setup_item.drb_id, res_drb_setup_item);
+      } else {
+        e1ap_drb_failed_item_ng_ran asn1_drb_failed_item;
+        asn1_drb_failed_item.drb_id = drb_setup_item.drb_id;
+        asn1_drb_failed_item.cause  = drb_setup_item.cause;
+
+        res_setup_item.drb_failed_list_ng_ran.emplace(drb_setup_item.drb_id, asn1_drb_failed_item);
+      }
+    }
+    pdu_session_resource_setup_list.emplace(result.pdu_session_id, res_setup_item);
+  }
 }
