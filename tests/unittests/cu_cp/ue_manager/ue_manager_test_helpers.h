62ab05a0958fb0031434c73b1ae47de900c781f7
Fabian Eckermann <fabian@srs.io>
2024-06-27 10:42:31 +0200

    cu_cp,ue_mng: remove unused test helper

--- tests/unittests/cu_cp/ue_manager/ue_manager_test_helpers.h
+++ tests/unittests/cu_cp/ue_manager/ue_manager_test_helpers.h
@@ -29,8 +29,6 @@ protected:
   ue_manager_test();
   ~ue_manager_test() override;
 
-  ue_index_t create_ue(du_index_t du_index, gnb_du_id_t du_id, pci_t pci, rnti_t rnti, du_cell_index_t pcell_index);
-
   srslog::basic_logger& test_logger   = srslog::fetch_basic_logger("TEST");
   srslog::basic_logger& ue_mng_logger = srslog::fetch_basic_logger("CU-UEMNG");
 
