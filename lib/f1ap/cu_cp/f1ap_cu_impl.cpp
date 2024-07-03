e5eb379649b6d8a4bfd04ba597e0fb263c952044
Fabian Eckermann <fabian@srs.io>
2024-07-03 12:33:13 +0200

    cu_cp,f1ap: add return to prevent segfault

--- lib/f1ap/cu_cp/f1ap_cu_impl.cpp
+++ lib/f1ap/cu_cp/f1ap_cu_impl.cpp
@@ -347,6 +347,7 @@ void f1ap_cu_impl::handle_successful_outcome(const asn1::f1ap::successful_outcom
       logger.warning("cu_ue={}: Discarding received \"{}\". Cause: UE was not found.",
                      *cu_ue_id,
                      outcome.value.type().to_string());
+      return;
     }
   }
 
@@ -373,6 +374,7 @@ void f1ap_cu_impl::handle_unsuccessful_outcome(const asn1::f1ap::unsuccessful_ou
       logger.warning("cu_ue={}: Discarding received \"{}\". Cause: UE was not found.",
                      *cu_ue_id,
                      outcome.value.type().to_string());
+      return;
     }
   }
 
