8e8fadb484e1b95455226e689a8ae5ed410896eb
Robert Falkenberg <robert.falkenberg@srs.io>
2024-07-10 06:47:05 +0200

    sec: extend and add NIA test vectors and integrity engine test
    This commit adds modified custom versions of
    the test set(s) expanded to next full byte.

---
+++ tests/unittests/security/nia_test_set.h
@@ -0,0 +1,24 @@
+/*
+ *
+ * Copyright 2021-2024 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#pragma once
+
+#include <cstdint>
+
+struct nia_test_set {
+  const char* name;
+  uint32_t    count_i;
+  uint8_t     bearer;
+  uint8_t     direction;
+  const char* ik_cstr;
+  uint32_t    length;
+  const char* message_cstr;
+  const char* mact_cstr;
+};
