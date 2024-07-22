5159bf51e216dfbfbab18e2b5b659526fd960479
Fabian Eckermann <fabian@srs.io>
2024-07-22 14:08:34 +0200

    cu_cp,e1ap: add e1ap test doubles

---
+++ tests/test_doubles/e1ap/e1ap_test_message_validators.cpp
@@ -0,0 +1,53 @@
+/*
+ *
+ * Copyright 2021-2024 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "e1ap_test_message_validators.h"
+#include "srsran/asn1/e1ap/e1ap.h"
+#include "srsran/e1ap/common/e1ap_message.h"
+
+using namespace srsran;
+
+#define TRUE_OR_RETURN(cond)                                                                                           \
+  if (not(cond))                                                                                                       \
+    return false;
+
+static bool is_packable(const e1ap_message& msg)
+{
+  byte_buffer   temp_pdu;
+  asn1::bit_ref bref{temp_pdu};
+  return msg.pdu.pack(bref) == asn1::SRSASN_SUCCESS;
+}
+
+bool srsran::test_helpers::is_valid_bearer_context_setup_request(const e1ap_message& msg)
+{
+  TRUE_OR_RETURN(msg.pdu.type() == asn1::e1ap::e1ap_pdu_c::types_opts::init_msg);
+  TRUE_OR_RETURN(msg.pdu.init_msg().proc_code == ASN1_E1AP_ID_BEARER_CONTEXT_SETUP);
+  TRUE_OR_RETURN(is_packable(msg));
+
+  return true;
+}
+
+bool srsran::test_helpers::is_valid_bearer_context_modification_request(const e1ap_message& msg)
+{
+  TRUE_OR_RETURN(msg.pdu.type() == asn1::e1ap::e1ap_pdu_c::types_opts::init_msg);
+  TRUE_OR_RETURN(msg.pdu.init_msg().proc_code == ASN1_E1AP_ID_BEARER_CONTEXT_MOD);
+  TRUE_OR_RETURN(is_packable(msg));
+
+  return true;
+}
+
+bool srsran::test_helpers::is_valid_bearer_context_release_command(const e1ap_message& msg)
+{
+  TRUE_OR_RETURN(msg.pdu.type() == asn1::e1ap::e1ap_pdu_c::types_opts::init_msg);
+  TRUE_OR_RETURN(msg.pdu.init_msg().proc_code == ASN1_E1AP_ID_BEARER_CONTEXT_RELEASE);
+  TRUE_OR_RETURN(is_packable(msg));
+
+  return true;
+}
---
+++ tests/test_doubles/e1ap/e1ap_test_message_validators.h
@@ -0,0 +1,30 @@
+/*
+ *
+ * Copyright 2021-2024 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#pragma once
+
+namespace srsran {
+
+struct e1ap_message;
+class byte_buffer;
+
+namespace test_helpers {
+
+/// \brief Check if an E1AP message is a valid Bearer Context Setup Request message.
+bool is_valid_bearer_context_setup_request(const e1ap_message& msg);
+
+/// \brief Check if an E1AP message is a valid Bearer Context Modification Request message.
+bool is_valid_bearer_context_modification_request(const e1ap_message& msg);
+
+/// \brief Check if an E1AP message is a valid Bearer Context Release Command message.
+bool is_valid_bearer_context_release_command(const e1ap_message& msg);
+
+} // namespace test_helpers
+} // namespace srsran
