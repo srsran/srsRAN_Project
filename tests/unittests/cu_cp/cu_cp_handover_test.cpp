8776c0a8dca077ba77b5bf451700155e4d292219
Fabian Eckermann <fabian@srs.io>
2024-08-01 13:05:54 +0200

    cu_cp: refactor cu-cp handover test

--- tests/unittests/cu_cp/cu_cp_handover_test.cpp
+++ tests/unittests/cu_cp/cu_cp_handover_test.cpp
@@ -12,6 +12,7 @@
 #include "tests/test_doubles/e1ap/e1ap_test_message_validators.h"
 #include "tests/test_doubles/f1ap/f1ap_test_message_validators.h"
 #include "tests/test_doubles/ngap/ngap_test_message_validators.h"
+#include "tests/unittests/cu_cp/test_helpers.h"
 #include "tests/unittests/e1ap/common/e1ap_cu_cp_test_messages.h"
 #include "tests/unittests/f1ap/common/f1ap_cu_test_messages.h"
 #include "tests/unittests/ngap/ngap_test_messages.h"
@@ -47,63 +48,68 @@ public:
     EXPECT_TRUE(this->run_e1_setup(cu_up_idx));
   }
 
-  void send_handover_request_and_await_bearer_context_setup_request()
+  [[nodiscard]] bool send_handover_request_and_await_bearer_context_setup_request()
   {
-    srsran_assert(not this->get_amf().try_pop_rx_pdu(ngap_pdu), "there are still NGAP messages to pop from AMF");
-    srsran_assert(not this->get_du(du_idx).try_pop_dl_pdu(f1ap_pdu), "there are still F1AP DL messages to pop from DU");
-    srsran_assert(not this->get_cu_up(cu_up_idx).try_pop_rx_pdu(e1ap_pdu),
-                  "there are still E1AP messages to pop from CU-UP");
+    report_fatal_error_if_not(not this->get_amf().try_pop_rx_pdu(ngap_pdu),
+                              "there are still NGAP messages to pop from AMF");
+    report_fatal_error_if_not(not this->get_du(du_idx).try_pop_dl_pdu(f1ap_pdu),
+                              "there are still F1AP DL messages to pop from DU");
+    report_fatal_error_if_not(not this->get_cu_up(cu_up_idx).try_pop_rx_pdu(e1ap_pdu),
+                              "there are still E1AP messages to pop from CU-UP");
 
     // Inject Handover Request and wait for Bearer Context Setup Request
     get_amf().push_tx_pdu(generate_valid_handover_request(amf_ue_id));
-    bool result = this->wait_for_e1ap_tx_pdu(cu_up_idx, e1ap_pdu);
-    report_fatal_error_if_not(result, "Failed to receive Bearer Context Setup Request");
+    report_fatal_error_if_not(this->wait_for_e1ap_tx_pdu(cu_up_idx, e1ap_pdu),
+                              "Failed to receive Bearer Context Setup Request");
     report_fatal_error_if_not(test_helpers::is_valid_bearer_context_setup_request(e1ap_pdu),
                               "Invalid Bearer Context Setup Request");
 
     cu_cp_e1ap_id =
         int_to_gnb_cu_cp_ue_e1ap_id(e1ap_pdu.pdu.init_msg().value.bearer_context_setup_request()->gnb_cu_cp_ue_e1ap_id);
+    return true;
   }
 
-  void send_bearer_context_setup_response_and_await_ue_context_setup_request()
+  [[nodiscard]] bool send_bearer_context_setup_response_and_await_ue_context_setup_request()
   {
     // Inject Bearer Context Setup Response and wait for UE Context Setup Request
     get_cu_up(cu_up_idx).push_tx_pdu(generate_bearer_context_setup_response(cu_cp_e1ap_id, cu_up_e1ap_id));
-    bool result = this->wait_for_f1ap_tx_pdu(du_idx, f1ap_pdu);
-    report_fatal_error_if_not(result, "Failed to receive UE Context Setup Request");
+    report_fatal_error_if_not(this->wait_for_f1ap_tx_pdu(du_idx, f1ap_pdu),
+                              "Failed to receive UE Context Setup Request");
     report_fatal_error_if_not(test_helpers::is_valid_ue_context_setup_request(f1ap_pdu),
                               "Invalid UE Context Setup Request");
 
     cu_ue_id = int_to_gnb_cu_ue_f1ap_id(f1ap_pdu.pdu.init_msg().value.ue_context_setup_request()->gnb_cu_ue_f1ap_id);
+    return true;
   }
 
-  void send_ue_context_setup_response_and_await_bearer_context_modification_request()
+  [[nodiscard]] bool send_ue_context_setup_response_and_await_bearer_context_modification_request()
   {
     // Inject UE Context Setup Response and wait for Bearer Context Modification Request
     get_du(du_idx).push_ul_pdu(generate_ue_context_setup_response(cu_ue_id, du_ue_id, crnti));
-    bool result = this->wait_for_e1ap_tx_pdu(cu_up_idx, e1ap_pdu);
-    report_fatal_error_if_not(result, "Failed to receive Bearer Context Modification Request");
+    report_fatal_error_if_not(this->wait_for_e1ap_tx_pdu(cu_up_idx, e1ap_pdu),
+                              "Failed to receive Bearer Context Modification Request");
     report_fatal_error_if_not(test_helpers::is_valid_bearer_context_modification_request(e1ap_pdu),
                               "Invalid Bearer Context Modification Request");
+    return true;
   }
 
-  void send_bearer_context_modification_response_and_await_handover_request_ack()
+  [[nodiscard]] bool send_bearer_context_modification_response_and_await_handover_request_ack()
   {
     // Inject Bearer Context Modification Response and wait for Handover Request Ack
     get_cu_up(cu_up_idx).push_tx_pdu(generate_bearer_context_modification_response(cu_cp_e1ap_id, cu_up_e1ap_id));
-    bool result = this->wait_for_ngap_tx_pdu(ngap_pdu);
-    report_fatal_error_if_not(result, "Failed to receive Handover Request Ack");
+    report_fatal_error_if_not(this->wait_for_ngap_tx_pdu(ngap_pdu), "Failed to receive Handover Request Ack");
     report_fatal_error_if_not(test_helpers::is_valid_handover_request_ack(ngap_pdu), "Invalid Handover Request Ack");
+    return true;
   }
 
-  void send_rrc_reconfiguration_complete_and_await_handover_notify()
+  [[nodiscard]] bool send_rrc_reconfiguration_complete_and_await_handover_notify()
   {
     // Inject UL RRC Message (containing RRC Reconfiguration Complete) and wait for Handover Notify
     get_du(du_idx).push_ul_pdu(generate_ul_rrc_message_transfer(
         cu_ue_id, du_ue_id, srb_id_t::srb1, make_byte_buffer("800008004e17dae3").value()));
-    bool result = this->wait_for_ngap_tx_pdu(ngap_pdu);
-    report_fatal_error_if_not(result, "Failed to receive Handover Notify");
+    report_fatal_error_if_not(this->wait_for_ngap_tx_pdu(ngap_pdu), "Failed to receive Handover Notify");
     report_fatal_error_if_not(test_helpers::is_valid_handover_notify(ngap_pdu), "Invalid Handover Notify");
+    return true;
   }
 
   unsigned du_idx    = 0;
@@ -125,17 +131,17 @@ public:
 TEST_F(cu_cp_handover_test, when_handover_request_received_then_handover_notify_is_sent)
 {
   // Inject Handover Request and await Bearer Context Setup Request
-  send_handover_request_and_await_bearer_context_setup_request();
+  ASSERT_TRUE(send_handover_request_and_await_bearer_context_setup_request());
 
   // Inject Bearer Context Setup Response and await UE Context Setup Request
-  send_bearer_context_setup_response_and_await_ue_context_setup_request();
+  ASSERT_TRUE(send_bearer_context_setup_response_and_await_ue_context_setup_request());
 
   // Inject UE Context Setup Response and await Bearer Context Modification Request
-  send_ue_context_setup_response_and_await_bearer_context_modification_request();
+  ASSERT_TRUE(send_ue_context_setup_response_and_await_bearer_context_modification_request());
 
   // Inject Bearer Context Modification Response and await Handover Request Ack
-  send_bearer_context_modification_response_and_await_handover_request_ack();
+  ASSERT_TRUE(send_bearer_context_modification_response_and_await_handover_request_ack());
 
   // Inject RRC Reconfiguration Complete and await Handover Notify
-  send_rrc_reconfiguration_complete_and_await_handover_notify();
+  ASSERT_TRUE(send_rrc_reconfiguration_complete_and_await_handover_notify());
 }
