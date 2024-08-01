75697c6db1d945fdffb21c8d6330534123d3e004
Fabian Eckermann <fabian@srs.io>
2024-08-01 13:35:18 +0200

    cu_cp: refactor paging test

--- tests/unittests/cu_cp/cu_cp_paging_test.cpp
+++ tests/unittests/cu_cp/cu_cp_paging_test.cpp
@@ -11,6 +11,7 @@
 #include "cu_cp_test_environment.h"
 #include "tests/test_doubles/f1ap/f1ap_test_message_validators.h"
 #include "tests/test_doubles/f1ap/f1ap_test_messages.h"
+#include "tests/unittests/cu_cp/test_helpers.h"
 #include "tests/unittests/ngap/ngap_test_messages.h"
 #include "srsran/asn1/f1ap/f1ap_pdu_contents.h"
 #include "srsran/asn1/ngap/ngap_pdu_contents.h"
@@ -30,7 +31,7 @@ public:
     run_ng_setup();
   }
 
-  unsigned connect_du()
+  [[nodiscard]] unsigned connect_du()
   {
     // Connect DU (note that this creates a DU processor, but the DU is only connected after the F1Setup procedure)
     std::optional<unsigned> ret = connect_new_du();
@@ -38,7 +39,7 @@ public:
     return ret.value();
   }
 
-  unsigned setup_du(const f1ap_message& f1_setup_request)
+  [[nodiscard]] unsigned setup_du(const f1ap_message& f1_setup_request)
   {
     // Setup DU
     unsigned tmp_du_idx = connect_du();
@@ -48,36 +49,42 @@ public:
     return tmp_du_idx;
   }
 
-  void send_ngap_paging(unsigned du_idx_, const ngap_message& paging_msg)
+  [[nodiscard]] bool send_ngap_paging(unsigned du_idx_, const ngap_message& paging_msg)
   {
-    srsran_assert(not this->get_amf().try_pop_rx_pdu(ngap_pdu), "there are still NGAP messages to pop from AMF");
-    srsran_assert(not this->get_du(du_idx_).try_pop_dl_pdu(f1ap_pdu),
-                  "there are still F1AP DL messages to pop from DU");
+    report_fatal_error_if_not(not this->get_amf().try_pop_rx_pdu(ngap_pdu),
+                              "there are still NGAP messages to pop from AMF");
+    report_fatal_error_if_not(not this->get_du(du_idx_).try_pop_dl_pdu(f1ap_pdu),
+                              "there are still F1AP DL messages to pop from DU");
 
     get_amf().push_tx_pdu(paging_msg);
+    return true;
   }
 
-  void send_minimal_ngap_paging_and_await_f1ap_paging(unsigned du_idx_)
+  [[nodiscard]] bool send_minimal_ngap_paging_and_await_f1ap_paging(unsigned du_idx_)
   {
     // Inject NGAP Paging and wait for F1AP Paging
-    send_ngap_paging(du_idx_, generate_valid_minimal_paging_message());
-    bool result = this->wait_for_f1ap_tx_pdu(du_idx_, f1ap_pdu);
-    report_fatal_error_if_not(result, "Failed to receive F1AP Paging");
+    if (!send_ngap_paging(du_idx_, generate_valid_minimal_paging_message())) {
+      return false;
+    }
+    report_fatal_error_if_not(this->wait_for_f1ap_tx_pdu(du_idx_, f1ap_pdu), "Failed to receive F1AP Paging");
     report_fatal_error_if_not(test_helpers::is_valid_paging(f1ap_pdu), "Invalid F1AP Paging");
     report_fatal_error_if_not(is_valid_minimal_paging_result(f1ap_pdu), "Invalid minimal F1AP Paging");
+    return true;
   }
 
-  void send_ngap_paging_and_await_f1ap_paging(unsigned du_idx_)
+  [[nodiscard]] bool send_ngap_paging_and_await_f1ap_paging(unsigned du_idx_)
   {
     // Inject NGAP Paging and wait for F1AP Paging
-    send_ngap_paging(du_idx_, generate_valid_paging_message());
-    bool result = this->wait_for_f1ap_tx_pdu(du_idx_, f1ap_pdu);
-    report_fatal_error_if_not(result, "Failed to receive F1AP Paging");
+    if (!send_ngap_paging(du_idx_, generate_valid_paging_message())) {
+      return false;
+    }
+    report_fatal_error_if_not(this->wait_for_f1ap_tx_pdu(du_idx_, f1ap_pdu), "Failed to receive F1AP Paging");
     report_fatal_error_if_not(test_helpers::is_valid_paging(f1ap_pdu), "Invalid F1AP Paging");
     report_fatal_error_if_not(is_valid_paging_result(f1ap_pdu), "Invalid F1AP Paging");
+    return true;
   }
 
-  bool is_valid_minimal_paging_result(const f1ap_message& msg)
+  [[nodiscard]] bool is_valid_minimal_paging_result(const f1ap_message& msg)
   {
     const auto& paging_msg = msg.pdu.init_msg().value.paging();
 
@@ -117,7 +124,7 @@ public:
     return true;
   }
 
-  bool is_valid_paging_result(const f1ap_message& msg)
+  [[nodiscard]] bool is_valid_paging_result(const f1ap_message& msg)
   {
     if (!is_valid_minimal_paging_result(msg)) {
       return false;
@@ -165,10 +172,10 @@ TEST_F(cu_cp_paging_test, when_du_connection_not_finished_then_paging_is_not_sen
   unsigned du_idx = connect_du();
 
   // Inject NGAP Paging with only mandatory values
-  send_ngap_paging(du_idx, generate_valid_minimal_paging_message());
+  ASSERT_TRUE(send_ngap_paging(du_idx, generate_valid_minimal_paging_message()));
 
   // Make sure that no paging was sent to the DU
-  srsran_assert(not this->get_du(du_idx).try_pop_dl_pdu(f1ap_pdu), "Paging was sent to DU before F1Setup");
+  ASSERT_FALSE(this->get_du(du_idx).try_pop_dl_pdu(f1ap_pdu));
 }
 
 TEST_F(cu_cp_paging_test, when_no_du_for_tac_exists_then_paging_is_not_sent_to_du)
@@ -179,10 +186,10 @@ TEST_F(cu_cp_paging_test, when_no_du_for_tac_exists_then_paging_is_not_sent_to_d
   // Inject NGAP Paging with unknown TAC
   ngap_message paging_msg = generate_valid_minimal_paging_message();
   paging_msg.pdu.init_msg().value.paging()->tai_list_for_paging[0].tai.tac.from_number(8);
-  send_ngap_paging(du_idx, paging_msg);
+  ASSERT_TRUE(send_ngap_paging(du_idx, paging_msg));
 
   // Make sure that no paging was sent to the DU
-  srsran_assert(not this->get_du(du_idx).try_pop_dl_pdu(f1ap_pdu), "Paging with unknown TAC was sent to DU");
+  ASSERT_FALSE(this->get_du(du_idx).try_pop_dl_pdu(f1ap_pdu));
 }
 
 TEST_F(cu_cp_paging_test, when_assist_data_for_paging_for_unknown_tac_is_included_then_paging_is_not_sent_to_du)
@@ -193,11 +200,10 @@ TEST_F(cu_cp_paging_test, when_assist_data_for_paging_for_unknown_tac_is_include
   // Inject NGAP Paging with unknown TAC but assist data for paging
   ngap_message paging_msg = generate_valid_paging_message();
   paging_msg.pdu.init_msg().value.paging()->tai_list_for_paging[0].tai.tac.from_number(8);
-  send_ngap_paging(du_idx, paging_msg);
+  ASSERT_TRUE(send_ngap_paging(du_idx, paging_msg));
 
   // Make sure that no paging was sent to the DU
-  srsran_assert(not this->get_du(du_idx).try_pop_dl_pdu(f1ap_pdu),
-                "Paging with unknown TAC but assist data was sent to DU");
+  ASSERT_FALSE(this->get_du(du_idx).try_pop_dl_pdu(f1ap_pdu));
 }
 
 TEST_F(cu_cp_paging_test, when_invalid_paging_message_received_then_paging_is_not_sent_to_du)
@@ -206,10 +212,10 @@ TEST_F(cu_cp_paging_test, when_invalid_paging_message_received_then_paging_is_no
   unsigned du_idx = setup_du(test_helpers::generate_f1_setup_request());
 
   // Inject invalid NGAP Paging
-  send_ngap_paging(du_idx, generate_invalid_paging_message());
+  ASSERT_TRUE(send_ngap_paging(du_idx, generate_invalid_paging_message()));
 
   // Make sure that no paging was sent to the DU
-  srsran_assert(not this->get_du(du_idx).try_pop_dl_pdu(f1ap_pdu), "Invalid Paging was sent to DU");
+  ASSERT_FALSE(this->get_du(du_idx).try_pop_dl_pdu(f1ap_pdu));
 }
 
 TEST_F(cu_cp_paging_test, when_valid_paging_message_received_then_paging_is_sent_to_du)
@@ -218,7 +224,7 @@ TEST_F(cu_cp_paging_test, when_valid_paging_message_received_then_paging_is_sent
   unsigned du_idx = setup_du(test_helpers::generate_f1_setup_request());
 
   // Inject NGAP Paging with only mandatory values and await F1AP Paging
-  send_minimal_ngap_paging_and_await_f1ap_paging(du_idx);
+  ASSERT_TRUE(send_minimal_ngap_paging_and_await_f1ap_paging(du_idx));
 }
 
 TEST_F(cu_cp_paging_test, when_valid_paging_message_received_then_paging_is_only_sent_to_du_with_matching_tac)
@@ -231,10 +237,10 @@ TEST_F(cu_cp_paging_test, when_valid_paging_message_received_then_paging_is_only
       test_helpers::generate_f1_setup_request(int_to_gnb_du_id(0x12), nr_cell_identity::create(6577).value(), 1, 8));
 
   // Inject NGAP Paging with only mandatory values and await F1AP Paging
-  send_minimal_ngap_paging_and_await_f1ap_paging(du_idx);
+  ASSERT_TRUE(send_minimal_ngap_paging_and_await_f1ap_paging(du_idx));
 
   // Make sure that no paging was sent to the second DU
-  srsran_assert(not this->get_du(du_idx2).try_pop_dl_pdu(f1ap_pdu), "Paging was sent to DU with different TAC");
+  ASSERT_FALSE(this->get_du(du_idx2).try_pop_dl_pdu(f1ap_pdu));
 }
 
 TEST_F(cu_cp_paging_test, when_valid_paging_message_received_then_paging_is_only_sent_to_du_with_matching_nci)
@@ -247,10 +253,10 @@ TEST_F(cu_cp_paging_test, when_valid_paging_message_received_then_paging_is_only
       test_helpers::generate_f1_setup_request(int_to_gnb_du_id(0x12), nr_cell_identity::create(6577).value(), 1, 7));
 
   // Inject NGAP Paging with only mandatory values and await F1AP Paging
-  send_minimal_ngap_paging_and_await_f1ap_paging(du_idx);
+  ASSERT_TRUE(send_minimal_ngap_paging_and_await_f1ap_paging(du_idx));
 
   // Make sure that no paging was sent to the second DU
-  srsran_assert(not this->get_du(du_idx2).try_pop_dl_pdu(f1ap_pdu), "Paging was sent to DU with different TAC");
+  ASSERT_FALSE(this->get_du(du_idx2).try_pop_dl_pdu(f1ap_pdu));
 }
 
 TEST_F(cu_cp_paging_test, when_valid_paging_message_with_optional_values_received_then_paging_is_sent_to_du)
@@ -259,5 +265,5 @@ TEST_F(cu_cp_paging_test, when_valid_paging_message_with_optional_values_receive
   unsigned du_idx = setup_du(test_helpers::generate_f1_setup_request());
 
   // Inject NGAP Paging with optional values and await F1AP Paging
-  send_ngap_paging_and_await_f1ap_paging(du_idx);
+  ASSERT_TRUE(send_ngap_paging_and_await_f1ap_paging(du_idx));
 }
