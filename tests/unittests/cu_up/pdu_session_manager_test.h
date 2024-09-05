7024860d320f67abd89ebc4e000d33d3200b39d8
Fabian Eckermann <fabian@srs.io>
2024-09-05 10:34:13 +0200

    cu_up: add unit test for f1u_ext_addr

--- tests/unittests/cu_up/pdu_session_manager_test.h
+++ tests/unittests/cu_up/pdu_session_manager_test.h
@@ -120,6 +120,21 @@ protected:
   void TearDown() override { finish(); }
 };
 
+/// Fixture class for PDU session manager tests with configurable F1-U ext addr
+class pdu_session_manager_test_set_f1u_ext_addr : public pdu_session_manager_test_base,
+                                                  public ::testing::TestWithParam<const char*>
+{
+protected:
+  network_interface_config get_net_config() override
+  {
+    network_interface_config cfg = net_config_default;
+    cfg.f1u_ext_addr             = GetParam();
+    return cfg;
+  }
+  void SetUp() override { init(); }
+  void TearDown() override { finish(); }
+};
+
 inline e1ap_pdu_session_res_to_setup_item
 generate_pdu_session_res_to_setup_item(pdu_session_id_t psi, drb_id_t drb_id, qos_flow_id_t qfi, five_qi_t five_qi)
 {
