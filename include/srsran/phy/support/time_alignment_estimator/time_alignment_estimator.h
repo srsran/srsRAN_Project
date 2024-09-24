1a1269c9f396b0ff2e365d0784f81f1be30c192f
dvdgrgrtt <davidg@srs.io>
2024-09-24 17:08:13 +0200

    ta estimator: allow multiple sources
    The time alignment estimator now can make a decision based on the
    comparison of multiple signals (e.g., multiple Rx antenna ports).

--- include/srsran/phy/support/time_alignment_estimator/time_alignment_estimator.h
+++ include/srsran/phy/support/time_alignment_estimator/time_alignment_estimator.h
@@ -12,7 +12,7 @@
 
 #include "srsran/adt/bounded_bitset.h"
 #include "srsran/adt/complex.h"
-#include "srsran/adt/span.h"
+#include "srsran/phy/support/re_buffer.h"
 #include "srsran/phy/support/time_alignment_estimator/time_alignment_measurement.h"
 #include "srsran/ran/resource_block.h"
 #include "srsran/ran/subcarrier_spacing.h"
@@ -31,20 +31,33 @@ public:
   /// Default destructor.
   virtual ~time_alignment_estimator() = default;
 
-  /// \brief Estimates the time alignment from frequency domain symbols.
+  /// \brief Estimates the time alignment from frequency domain symbols from a single antenna port.
   /// \param[in] symbols Complex frequency domain symbols.
   /// \param[in] mask    Distribution of the complex symbols within an OFDM symbol.
   /// \param[in] scs     Subcarrier spacing.
   /// \param[in] max_ta  Maximum absolute time alignment measurement if it is not zero.
   /// \return The measured time alignment.
   /// \remark An assertion is triggered if the number of symbols is not equal to the number of active elements in the
-  /// mask.
+  /// mask, or if the mask size is larger than the maximum supported number of subcarriers.
   virtual time_alignment_measurement estimate(span<const cf_t>                symbols,
                                               bounded_bitset<max_nof_symbols> mask,
                                               subcarrier_spacing              scs,
                                               double                          max_ta = 0.0) = 0;
 
-  /// \brief Estimates the time alignment from frequency domain symbols.
+  /// \brief Estimates the time alignment from frequency domain symbols from one or more antenna ports.
+  /// \param[in] symbols Complex frequency domain symbols (one slice for each antenna port).
+  /// \param[in] mask    Distribution of the complex symbols within an OFDM symbol.
+  /// \param[in] scs     Subcarrier spacing.
+  /// \param[in] max_ta  Maximum absolute time alignment measurement if it is not zero.
+  /// \return The measured time alignment.
+  /// \remark An assertion is triggered if the number of symbols is not equal to the number of active elements in the
+  /// mask, or if the mask size is larger than the maximum supported number of subcarriers.
+  virtual time_alignment_measurement estimate(const re_buffer_reader<cf_t>&   symbols,
+                                              bounded_bitset<max_nof_symbols> mask,
+                                              subcarrier_spacing              scs,
+                                              double                          max_ta = 0.0) = 0;
+
+  /// \brief Estimates the time alignment from frequency domain symbols from a single antenna port.
   /// \param[in] symbols Complex frequency domain symbols.
   /// \param[in] stride  Distance between the complex symbols within an OFDM symbol.
   /// \param[in] scs     Subcarrier spacing.
@@ -53,6 +66,16 @@ public:
   /// \remark An assertion is triggered if the number of symbols times the stride exceed the frequency domain buffer.
   virtual time_alignment_measurement
   estimate(span<const cf_t> symbols, unsigned stride, subcarrier_spacing scs, double max_ta = 0.0) = 0;
+
+  /// \brief Estimates the time alignment from frequency domain symbols from one or more antenna ports.
+  /// \param[in] symbols Complex frequency domain symbols (one slice for each antenna port).
+  /// \param[in] stride  Distance between the complex symbols within an OFDM symbol.
+  /// \param[in] scs     Subcarrier spacing.
+  /// \param[in] max_ta  Maximum absolute time alignment measurement if it is not zero.
+  /// \return The measured time alignment.
+  /// \remark An assertion is triggered if the number of symbols times the stride exceed the frequency domain buffer.
+  virtual time_alignment_measurement
+  estimate(const re_buffer_reader<cf_t>& symbols, unsigned stride, subcarrier_spacing scs, double max_ta = 0.0) = 0;
 };
 
 } // namespace srsran
