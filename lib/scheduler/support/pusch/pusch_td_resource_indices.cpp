707e81d53f4471271611f5f8352cf3c4e2a904d9
Supreeth Herle <supreeth.herle@srs.io>
2024-09-29 15:09:17 +0200

    sched: add helper functions to generate PUSCH time domain resouces per PDCCH slot ensuring fairness

--- lib/scheduler/support/pusch/pusch_td_resource_indices.cpp
+++ lib/scheduler/support/pusch/pusch_td_resource_indices.cpp
@@ -9,8 +9,6 @@
  */
 
 #include "pusch_td_resource_indices.h"
-#include "../../cell_scheduler.h"
-#include "../../policy/scheduler_policy.h"
 
 using namespace srsran;
 
@@ -56,17 +54,148 @@ compute_pusch_td_resource_indices(span<const pusch_time_domain_resource_allocati
   return pusch_td_res_index_list;
 }
 
+span<const pusch_time_domain_resource_allocation>
+srsran::get_pusch_time_domain_resource_table(const cell_configuration& cell_cfg, const search_space_info* ss_info)
+{
+  span<const pusch_time_domain_resource_allocation> pusch_time_domain_list =
+      cell_cfg.ul_cfg_common.init_ul_bwp.pusch_cfg_common.value().pusch_td_alloc_list;
+  if (ss_info != nullptr) {
+    pusch_time_domain_list = ss_info->pusch_time_domain_list;
+  }
+  return pusch_time_domain_list;
+}
+
 static_vector<unsigned, pusch_constants::MAX_NOF_PUSCH_TD_RES_ALLOCS>
 srsran::get_pusch_td_resource_indices(const cell_configuration& cell_cfg,
                                       slot_point                pdcch_slot,
                                       const search_space_info*  ss_info)
 {
   unsigned min_k1 = *std::min(cell_cfg.dl_data_to_ul_ack.begin(), cell_cfg.dl_data_to_ul_ack.end());
-  span<const pusch_time_domain_resource_allocation> pusch_time_domain_list =
-      cell_cfg.ul_cfg_common.init_ul_bwp.pusch_cfg_common.value().pusch_td_alloc_list;
   if (ss_info != nullptr) {
-    min_k1                 = *std::min(ss_info->get_k1_candidates().begin(), ss_info->get_k1_candidates().end());
-    pusch_time_domain_list = ss_info->pusch_time_domain_list;
+    min_k1 = *std::min(ss_info->get_k1_candidates().begin(), ss_info->get_k1_candidates().end());
   }
-  return compute_pusch_td_resource_indices(pusch_time_domain_list, cell_cfg, pdcch_slot, min_k1);
+  return compute_pusch_td_resource_indices(
+      get_pusch_time_domain_resource_table(cell_cfg, ss_info), cell_cfg, pdcch_slot, min_k1);
+}
+
+std::vector<static_vector<unsigned, pusch_constants::MAX_NOF_PUSCH_TD_RES_ALLOCS>>
+srsran::get_pusch_td_resource_indices_per_slot(const cell_configuration& cell_cfg, const search_space_info* ss_info)
+{
+  // NOTE: [Implementation-defined] In case of FDD, we consider only one slot as all slots are similar unlike in TDD
+  // where there can be DL/UL full or partial slots.
+  const unsigned nof_slots = cell_cfg.is_tdd() ? nof_slots_per_tdd_period(*cell_cfg.tdd_cfg_common) : 1;
+
+  // List circularly indexed by slot with the list of applicable PUSCH Time Domain resource indexes per slot.
+  // NOTE: The list would be empty for UL slots.
+  std::vector<static_vector<unsigned, pusch_constants::MAX_NOF_PUSCH_TD_RES_ALLOCS>> pusch_td_list_per_slot;
+  pusch_td_list_per_slot.resize(nof_slots);
+  // Populate the initial list of applicable PUSCH time domain resources per slot.
+  for (unsigned slot_idx = 0, e = nof_slots; slot_idx != e; ++slot_idx) {
+    slot_point pdcch_slot{to_numerology_value(cell_cfg.dl_cfg_common.init_dl_bwp.generic_params.scs), slot_idx};
+    if (cell_cfg.is_dl_enabled(pdcch_slot)) {
+      pusch_td_list_per_slot[slot_idx] = get_pusch_td_resource_indices(cell_cfg, pdcch_slot);
+    }
+  }
+  return pusch_td_list_per_slot;
+}
+
+std::vector<static_vector<unsigned, pusch_constants::MAX_NOF_PUSCH_TD_RES_ALLOCS>>
+srsran::get_fairly_distributed_pusch_td_resource_indices(const cell_configuration& cell_cfg,
+                                                         const search_space_info*  ss_info)
+{
+  // NOTE: [Implementation-defined] In case of FDD, we consider only one slot as all slots are similar unlike in TDD
+  // where there can be DL/UL full or partial slots.
+  const unsigned nof_slots = cell_cfg.is_tdd() ? nof_slots_per_tdd_period(*cell_cfg.tdd_cfg_common) : 1;
+
+  // List circularly indexed by slot with the list of applicable PUSCH Time Domain resource indexes per slot.
+  // NOTE: The list would be empty for UL slots.
+  std::vector<static_vector<unsigned, pusch_constants::MAX_NOF_PUSCH_TD_RES_ALLOCS>> initial_pusch_td_list_per_slot =
+      get_pusch_td_resource_indices_per_slot(cell_cfg, ss_info);
+
+  // Fetch the relevant PUSCH time domain resource list.
+  span<const pusch_time_domain_resource_allocation> pusch_time_domain_list =
+      get_pusch_time_domain_resource_table(cell_cfg, ss_info);
+
+  unsigned nof_dl_slots = 0;
+  unsigned nof_ul_slots = 0;
+  if (cell_cfg.is_tdd()) {
+    nof_dl_slots = nof_dl_slots_per_tdd_period(cell_cfg.tdd_cfg_common.value());
+    nof_ul_slots = nof_full_ul_slots_per_tdd_period(cell_cfg.tdd_cfg_common.value());
+  }
+
+  // Case of FDD or DL heavy TDD pattern.
+  if (not cell_cfg.is_tdd() or (nof_dl_slots >= nof_ul_slots)) {
+    return initial_pusch_td_list_per_slot;
+  }
+
+  // Estimate the nof. UL PDCCHs that can be scheduled in each PDCCH slot.
+  const auto nof_ul_pdcchs_per_dl_slot =
+      static_cast<unsigned>(std::round(static_cast<double>(nof_ul_slots) / static_cast<double>(nof_dl_slots)));
+
+  // List circularly indexed by slot with the list of applicable PUSCH Time Domain resource indexes per slot fairly
+  // distributed among all the PDCCH slots.
+  // NOTE: The list would be empty for UL slots.
+  std::vector<static_vector<unsigned, pusch_constants::MAX_NOF_PUSCH_TD_RES_ALLOCS>> final_pusch_td_list_per_slot;
+  final_pusch_td_list_per_slot.resize(nof_slots);
+
+  unsigned last_pdcch_slot_index_for_ul_slot = nof_slots;
+  // Iterate from latest UL slot to earliest and find the closest PDCCH slot.
+  // NOTE: There can be scenarios where the closest PDCCH slot may not be able to schedule PUSCH in the chosen UL slot.
+  // In this case we move on next closest PDCCH slot, so on and so forth.
+  for (int slot_idx1 = nof_slots; slot_idx1 >= 0; --slot_idx1) {
+    // Skip if it's not a UL slot.
+    // TODO: Revisit when scheduling of PUSCH over partial UL slots is supported.
+    if (not is_tdd_full_ul_slot(cell_cfg.tdd_cfg_common.value(), slot_idx1)) {
+      continue;
+    }
+    // Flag indicating whether a valid PDCCH slot for a given UL slot found or not.
+    bool no_pdcch_slot_found = true;
+    for (int slot_idx2 = slot_idx1; slot_idx2 >= 0; --slot_idx2) {
+      // Skip if it's not a DL slot.
+      if (not has_active_tdd_dl_symbols(cell_cfg.tdd_cfg_common.value(), slot_idx2)) {
+        continue;
+      }
+      // Check whether there is a PUSCH time domain resource with required k2 value for the PDCCH slot.
+      unsigned required_k2 = slot_idx1 - slot_idx2;
+      auto*    it          = std::find_if(initial_pusch_td_list_per_slot[slot_idx2].begin(),
+                              initial_pusch_td_list_per_slot[slot_idx2].end(),
+                              [&pusch_time_domain_list, required_k2](unsigned pusch_td_res_idx) {
+                                return pusch_time_domain_list[pusch_td_res_idx].k2 == required_k2;
+                              });
+      if (it == initial_pusch_td_list_per_slot[slot_idx2].end()) {
+        continue;
+      }
+      // Store PDCCH slot index at which a valid PUSCH time domain resource was found to schedule PUSCH at given UL
+      // slot.
+      last_pdcch_slot_index_for_ul_slot = slot_idx2;
+      // Skip if nof. PUSCH time domain resource indexes for this PDCCH slot exceed nof. UL PDCCHs that can be scheduled
+      // in each PDCCH slot.
+      if (final_pusch_td_list_per_slot[slot_idx2].size() >= nof_ul_pdcchs_per_dl_slot) {
+        continue;
+      }
+      // Store the nof. PUSCH time domain resource index for this PDCCH slot.
+      final_pusch_td_list_per_slot[slot_idx2].push_back(*it);
+      no_pdcch_slot_found = false;
+      break;
+    }
+    if (no_pdcch_slot_found) {
+      unsigned required_k2 = slot_idx1 - last_pdcch_slot_index_for_ul_slot;
+      auto*    it          = std::find_if(initial_pusch_td_list_per_slot[last_pdcch_slot_index_for_ul_slot].begin(),
+                              initial_pusch_td_list_per_slot[last_pdcch_slot_index_for_ul_slot].end(),
+                              [&pusch_time_domain_list, required_k2](unsigned pusch_td_res_idx) {
+                                return pusch_time_domain_list[pusch_td_res_idx].k2 == required_k2;
+                              });
+      final_pusch_td_list_per_slot[last_pdcch_slot_index_for_ul_slot].push_back(*it);
+    }
+  }
+
+  // Sort PUSCH time domain resource indexes (ascending order) in the final list of PUSCH time domain resources
+  // maintained per slot.
+  for (unsigned slot_idx = 0, e = nof_slots; slot_idx != e; ++slot_idx) {
+    if (has_active_tdd_dl_symbols(cell_cfg.tdd_cfg_common.value(), slot_idx)) {
+      std::sort(final_pusch_td_list_per_slot[slot_idx].begin(), final_pusch_td_list_per_slot[slot_idx].end());
+    }
+  }
+
+  return final_pusch_td_list_per_slot;
 }
--- lib/scheduler/support/pusch/pusch_td_resource_indices.h
+++ lib/scheduler/support/pusch/pusch_td_resource_indices.h
@@ -21,6 +21,11 @@ class ue_repository;
 class cell_configuration;
 class ue_cell_configuration;
 
+/// Retrieves the time resource allocation table for PUSCH.
+/// \remark See TS 38.214, Section 6.1.2.1.1 - Determination of the resource allocation table to be used for PUSCH.
+span<const pusch_time_domain_resource_allocation>
+get_pusch_time_domain_resource_table(const cell_configuration& cell_cfg, const search_space_info* ss_info);
+
 /// \brief Fetches a list of PUSCH Time Domain resource indexes based on cell, UE configuration and nof. symbols in
 /// PUSCH slot.
 /// \param[in] cell_cfg Cell configuration.
@@ -32,5 +37,37 @@ static_vector<unsigned, pusch_constants::MAX_NOF_PUSCH_TD_RES_ALLOCS>
 get_pusch_td_resource_indices(const cell_configuration& cell_cfg,
                               slot_point                pdcch_slot,
                               const search_space_info*  ss_info = nullptr);
+
+/// \brief Fetches a list circularly indexed by slot containing the list of applicable PUSCH Time Domain resource
+/// indexes per slot.
+/// \param[in] cell_cfg Cell configuration.
+/// \param[in] ss_info SearchSpace information.
+/// \return A list circularly indexed by slot containing the list of applicable PUSCH Time Domain resource indexes per
+/// slot.
+/// \remark If \c ss_info is nullptr, then minimum k1 is taken from \c cell_cfg.
+/// \remark The list of applicable PUSCH Time Domain resources would be empty for UL slots.
+/// \remark In case of FDD, the list returned would contain applicable PUSCH Time Domain resource indexes for only one
+/// slot.
+std::vector<static_vector<unsigned, pusch_constants::MAX_NOF_PUSCH_TD_RES_ALLOCS>>
+get_pusch_td_resource_indices_per_slot(const cell_configuration& cell_cfg, const search_space_info* ss_info = nullptr);
+
+/// Fetches a list circularly indexed by slot containing the list of applicable PUSCH Time Domain resource indexes per
+/// slot fairly distributed among all the PDCCH slots.
+///
+/// In case of UL heavy TDD pattern, this function ensures that UL PDCCH scheduling the PUSCH are fairly
+/// distributed across DL slots in a TDD configuration. In all other scenarios (FDD or DL heavy TDD pattern), the
+/// function behaves similar to \c get_pusch_td_resource_indices_per_slot().
+///
+/// \param[in] cell_cfg Cell configuration.
+/// \param[in] ss_info SearchSpace information.
+/// \return A list circularly indexed by slot containing the list of applicable PUSCH Time Domain resource indexes per
+/// slot.
+/// \remark If \c ss_info is nullptr, then minimum k1 is taken from \c cell_cfg.
+/// \remark The list of applicable PUSCH Time Domain resources would be empty for UL slots.
+/// \remark In case of FDD, the list returned would contain applicable PUSCH Time Domain resource indexes for only one
+/// slot.
+std::vector<static_vector<unsigned, pusch_constants::MAX_NOF_PUSCH_TD_RES_ALLOCS>>
+get_fairly_distributed_pusch_td_resource_indices(const cell_configuration& cell_cfg,
+                                                 const search_space_info*  ss_info = nullptr);
 
 } // namespace srsran
