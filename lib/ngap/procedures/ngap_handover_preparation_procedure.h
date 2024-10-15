bce214ebe2bb4607d264a8447a3cb2d99f3b19b0
Pedro Alvarez <pedro@srs.io>
2024-10-15 14:25:55 +0100

    ngap: mv ng ho weak symbol to cpp

--- lib/ngap/procedures/ngap_handover_preparation_procedure.h
+++ lib/ngap/procedures/ngap_handover_preparation_procedure.h
@@ -18,15 +18,15 @@
 
 namespace srsran::srs_cu_cp {
 
-SRSRAN_WEAK_SYMB async_task<ngap_handover_preparation_response>
-                 start_ngap_handover_preparation(const ngap_handover_preparation_request& req,
-                                                 const plmn_identity&                     serving_plmn,
-                                                 const ngap_ue_ids&                       ue_ids,
-                                                 ngap_message_notifier&                   amf_notifier,
-                                                 ngap_rrc_ue_notifier&                    rrc_ue_notifier,
-                                                 ngap_cu_cp_notifier&                     cu_cp_notifier,
-                                                 ngap_transaction_manager&                ev_mng,
-                                                 timer_factory                            timers,
-                                                 ngap_ue_logger&                          logger);
+async_task<ngap_handover_preparation_response>
+start_ngap_handover_preparation(const ngap_handover_preparation_request& req,
+                                const plmn_identity&                     serving_plmn,
+                                const ngap_ue_ids&                       ue_ids,
+                                ngap_message_notifier&                   amf_notifier,
+                                ngap_rrc_ue_notifier&                    rrc_ue_notifier,
+                                ngap_cu_cp_notifier&                     cu_cp_notifier,
+                                ngap_transaction_manager&                ev_mng,
+                                timer_factory                            timers,
+                                ngap_ue_logger&                          logger);
 
 } // namespace srsran::srs_cu_cp
