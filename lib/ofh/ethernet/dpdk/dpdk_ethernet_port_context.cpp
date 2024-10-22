ad9032e82ec5ed54aa19852a0e9d9f6bd70439b6
sauka <pavel.harbanau@softwareradiosystems.com>
2024-10-22 17:20:24 +0300

    ofh: increase shared memory pool size for DPDK Ethernet transmitters/receivers

--- lib/ofh/ethernet/dpdk/dpdk_ethernet_port_context.cpp
+++ lib/ofh/ethernet/dpdk/dpdk_ethernet_port_context.cpp
@@ -20,7 +20,7 @@ using namespace ether;
 static constexpr unsigned MBUF_CACHE_SIZE = 256;
 static constexpr unsigned RX_RING_SIZE    = 1024;
 static constexpr unsigned TX_RING_SIZE    = 1024;
-static constexpr unsigned NUM_MBUFS       = 8191;
+static constexpr unsigned NUM_MBUFS       = 13824;
 
 /// DPDK port initialization routine.
 static bool port_init(const dpdk_port_config& config, ::rte_mempool* mem_pool, unsigned port_id)
