506090c961af6b34191e5bc492041dd117dd2bae
Francisco Paisana <francisco.paisana@srs.io>
2024-10-28 10:31:06 +0100

    sched: add failint test to scheduler_tdd_test

--- tests/unittests/scheduler/scheduler_tdd_test.cpp
+++ tests/unittests/scheduler/scheduler_tdd_test.cpp
@@ -229,6 +229,7 @@ INSTANTIATE_TEST_SUITE_P(
   tdd_test_params{true, {subcarrier_spacing::kHz30, {5, 1, 10, 3, 0}, tdd_ul_dl_pattern{5, 1, 10, 3, 0}}, 2},
   tdd_test_params{true, {subcarrier_spacing::kHz30, {6, 2, 10, 3, 0}, tdd_ul_dl_pattern{4, 1, 0, 3, 0}}, 2},
   tdd_test_params{true, {subcarrier_spacing::kHz30, {4, 1, 10, 2, 0}, tdd_ul_dl_pattern{6, 1, 10, 4, 0}}, 2},
-  tdd_test_params{true, {subcarrier_spacing::kHz30, {10, 2, 10, 7, 0}}, 2}
+  tdd_test_params{true, {subcarrier_spacing::kHz30, {10, 2, 10, 7, 0}}, 2},
+  tdd_test_params{true, {subcarrier_spacing::kHz30, {10, 2, 6, 7, 0}}, 2}
 )); // DDDSUUDDDD
 // clang-format on
