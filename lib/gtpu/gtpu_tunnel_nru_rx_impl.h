7aa9fc5e23f6c0a75d143d59f5d4949144fd0f0c
Pedro Alvarez <pedro@srs.io>
2024-10-29 17:29:50 +0000

    nru: use node information to delay unpacking NR RAN container

--- lib/gtpu/gtpu_tunnel_nru_rx_impl.h
+++ lib/gtpu/gtpu_tunnel_nru_rx_impl.h
@@ -39,33 +39,16 @@ protected:
   // domain-specific PDU handler
   void handle_pdu(gtpu_dissected_pdu&& pdu, const sockaddr_storage& src_addr) final
   {
-    size_t                                                      pdu_len = pdu.buf.length();
-    gtpu_teid_t                                                 teid    = pdu.hdr.teid;
-    std::variant<nru_dl_user_data, nru_dl_data_delivery_status> nru_msg;
-    bool                                                        have_nr_ran_container = false;
+    size_t pdu_len = pdu.buf.length();
+    // gtpu_teid_t      teid                  = pdu.hdr.teid;
+    bool             have_nr_ran_container = false;
+    byte_buffer_view nr_ran_container;
     for (auto ext_hdr : pdu.hdr.ext_list) {
       switch (ext_hdr.extension_header_type) {
         case gtpu_extension_header_type::nr_ran_container:
           if (!have_nr_ran_container) {
-            nru_pdu_type pdu_type = packer.get_pdu_type(ext_hdr.container);
-            switch (pdu_type) {
-              case nru_pdu_type::dl_user_data:
-                nru_msg               = {nru_dl_user_data{}};
-                have_nr_ran_container = packer.unpack(std::get<nru_dl_user_data>(nru_msg), ext_hdr.container);
-                break;
-              case nru_pdu_type::dl_data_delivery_status:
-                nru_msg = {nru_dl_data_delivery_status{}};
-                have_nr_ran_container =
-                    packer.unpack(std::get<nru_dl_data_delivery_status>(nru_msg), ext_hdr.container);
-                break;
-              default:
-                logger.log_warning(
-                    "Unsupported PDU type in NR RAN container. pdu_type={} pdu_len={}", pdu_type, pdu_len);
-                return;
-            }
-            if (!have_nr_ran_container) {
-              logger.log_error("Failed to unpack NR RAN container. pdu_len={}", pdu_len);
-            }
+            have_nr_ran_container = true;
+            nr_ran_container      = ext_hdr.container;
           } else {
             logger.log_warning("Ignoring multiple NR RAN container. pdu_len={}", pdu_len);
           }
@@ -76,19 +59,23 @@ protected:
                              pdu_len);
       }
     }
-    if (!have_nr_ran_container) {
-      // As per TS 29.281 Sec. 5.2.2.6 the (...) NR RAN Container (...) may be transmitted in a G-PDU over the
-      // X2-U, Xn-U and F1-U user plane interfaces (...).
-      logger.log_info("T-PDU without NR RAN container. Assuming UL. pdu_len={} teid={}", pdu_len, teid);
-      nru_msg = {nru_dl_data_delivery_status{}}; // set to UL
-    }
 
     logger.log_debug(pdu.buf.begin(), pdu.buf.end(), "RX PDU. pdu_len={}", pdu_len);
 
-    if (std::holds_alternative<nru_dl_user_data>(nru_msg)) {
+    // DL message
+    if (config.node == nru_node::du) {
+      if (not have_nr_ran_container) {
+        logger.log_warning("Dropping DL message without NRU DL user data. pdu_len={}", pdu_len);
+        return;
+      }
+      nru_dl_user_data dud;
+      if (not packer.unpack(dud, nr_ran_container)) {
+        logger.log_warning("Dropping DL message. Cause: could not unpack NR RAN container. pdu_len={}", pdu_len);
+        return;
+      }
       nru_dl_message dl_message = {};
       dl_message.t_pdu          = gtpu_extract_msg(std::move(pdu)); // header is invalidated after extraction;
-      dl_message.dl_user_data   = std::move(std::get<nru_dl_user_data>(nru_msg));
+      dl_message.dl_user_data   = std::move(dud);
 
       logger.log_info(
           dl_message.t_pdu.begin(), dl_message.t_pdu.end(), "RX DL user data. t_pdu_len={}", dl_message.t_pdu.length());
@@ -96,8 +83,23 @@ protected:
       return;
     }
 
-    if (std::holds_alternative<nru_dl_data_delivery_status>(nru_msg)) {
-      nru_ul_message              ul_message = {};
+    // UL message
+    if (config.node == nru_node::cu_up) {
+      nru_ul_message ul_message = {};
+
+      // Get DDDS, if exists.
+      std::optional<nru_dl_data_delivery_status> ddds_opt;
+      if (have_nr_ran_container) {
+        nru_dl_data_delivery_status ddds;
+        if (not packer.unpack(ddds, nr_ran_container)) {
+          logger.log_warning("Dropping UL message. Cause: could not unpack NR RAN container. pdu_len={}", pdu_len);
+          return;
+        }
+        ddds_opt = ddds;
+      }
+      ul_message.data_delivery_status = ddds_opt;
+
+      // Extract T-PDU
       expected<byte_buffer_chain> buf =
           byte_buffer_chain::create(gtpu_extract_msg(std::move(pdu))); // header is invalidated after extraction;
       if (not buf.has_value()) {
@@ -107,8 +109,6 @@ protected:
       if (!buf.value().empty()) {
         ul_message.t_pdu = std::move(buf.value());
       }
-      ul_message.data_delivery_status = std::move(std::get<nru_dl_data_delivery_status>(nru_msg));
-
       if (ul_message.t_pdu.has_value()) {
         logger.log_info(ul_message.t_pdu.value().begin(),
                         ul_message.t_pdu.value().end(),
