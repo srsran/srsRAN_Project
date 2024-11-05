153888b00c4621dd23a78d4728321e80afa33c0f
Robert Falkenberg <robert.falkenberg@srs.io>
2024-11-05 06:01:18 +0100

    rlc_rx_am: assert lock-free atomic pointer to cached status PDU in constructor

--- lib/rlc/rlc_rx_am_entity.cpp
+++ lib/rlc/rlc_rx_am_entity.cpp
@@ -69,6 +69,8 @@ rlc_rx_am_entity::rlc_rx_am_entity(gnb_du_id_t                       gnb_du_id,
   }
 
   // initialize cached status report
+  static_assert(std::atomic<rlc_am_status_pdu*>::is_always_lock_free,
+                "RLC RX AM: atomic<rlc_am_status_pdu*> must always be lock free");
   rlc_am_status_pdu& init_cached_status = *status_cached.load(std::memory_order_relaxed);
   init_cached_status.ack_sn             = st.rx_next_highest;
   status_report_size.store(init_cached_status.get_packed_size(), std::memory_order_relaxed);
