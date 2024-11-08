399e5cab4ff6d6d4411c56d66b1b2b5a46a448d2
Pedro Alvarez <pedro@srs.io>
2024-11-08 16:33:01 +0000

    rlc: make pdu recycler lockfree

--- lib/rlc/rlc_pdu_recycler.h
+++ lib/rlc/rlc_pdu_recycler.h
@@ -12,9 +12,9 @@
 
 #include "rlc_bearer_logger.h"
 #include "srsran/adt/byte_buffer.h"
+#include "srsran/adt/spsc_queue.h"
 #include "srsran/instrumentation/traces/up_traces.h"
 #include "srsran/support/executors/task_executor.h"
-#include <mutex>
 
 namespace srsran {
 
@@ -27,32 +27,19 @@ class rlc_pdu_recycler
 public:
   rlc_pdu_recycler(size_t rlc_window_size, rlc_bearer_logger& logger_) : logger(logger_)
   {
-    for (std::vector<byte_buffer>& recycle_bin : recycle_bins) {
-      recycle_bin.reserve(rlc_window_size);
-    }
+    recycle_bin = std::make_unique<concurrent_queue<byte_buffer,
+                                                    concurrent_queue_policy::lockfree_spsc,
+                                                    concurrent_queue_wait_policy::non_blocking>>(rlc_window_size);
   }
 
   /// \brief Adds a discarded PDU to the recycler which shall be deleted later by another executor
   /// \param pdu The PDU that shall be deleted later
   /// \return True if the PDU was queued for later deletion; False if the PDU was deleted on the spot due to full queue.
-  bool add_discarded_pdu(byte_buffer pdu)
-  {
-    if (recycle_bin_to_fill->size() >= recycle_bin_to_fill->capacity()) {
-      // recycle bin is full; delete the PDUs upon return
-      return false;
-    }
-    recycle_bin_to_fill->push_back(std::move(pdu));
-    return true;
-  }
+  bool add_discarded_pdu(byte_buffer pdu) { return recycle_bin->try_push(std::move(pdu)); }
 
   /// \brief Delegates the deletion of all stored PDUs to the given task_executor.
   void clear_by_executor(task_executor& executor)
   {
-    // Swap recycle bins under a lock
-    {
-      std::lock_guard<std::mutex> recycle_bin_swap_lock(recycle_bin_swap_mutex);
-      std::swap(recycle_bin_to_fill, recycle_bin_to_swap);
-    }
     // Redirect recycling of unused byte_buffers to ue_executor
     auto handle_func = [this]() mutable { clear(); };
     if (not executor.execute(std::move(handle_func))) {
@@ -60,38 +47,32 @@ public:
     }
   }
 
-  /// \brief For unit-testing only: Expose inner recycle bins
-  /// \return Reference to inner recycle bins for testing purposes
-  std::array<std::vector<byte_buffer>, 3>& get_recycle_bins() { return recycle_bins; }
+  /// \brief Get the pdu recycler queue.
+  /// Should only be used for testing
+  concurrent_queue<byte_buffer, concurrent_queue_policy::lockfree_spsc, concurrent_queue_wait_policy::non_blocking>&
+  get_recycle_queue()
+  {
+    return *recycle_bin;
+  }
 
 private:
   /// \brief Deletes the stored PDUs. This function shall be called from the executor in \c clear_by_executor.
   void clear()
-  { // swap recycle bins under a lock
-    trace_point clear_tp = up_tracer.now();
-    {
-      std::lock_guard<std::mutex> lock(recycle_bin_swap_mutex);
-      std::swap(recycle_bin_to_swap, recycle_bin_to_dump);
+  {
+    trace_point clear_tp   = up_tracer.now();
+    uint32_t    queue_size = recycle_bin->size();
+    for (uint32_t i = 0; i < queue_size; ++i) {
+      recycle_bin->try_pop();
     }
-    // delete all PDUs to return their memory segments to the pool
-    recycle_bin_to_dump->clear();
 
     up_tracer << trace_event{"rlc_clear_pdus", clear_tp};
   }
 
   rlc_bearer_logger& logger;
 
-  /// Recycle bins for discarded PDUs
-  std::array<std::vector<byte_buffer>, 3> recycle_bins;
-
-  /// Pointer to an empty recycle bin that shall be filled with discarded PDUs by \c add_discarded_pdu.
-  std::vector<byte_buffer>* recycle_bin_to_fill = &recycle_bins[0];
-  /// Pointer to a recycle bin that can be swapped with either the recycle_bin_to_fill or the recycle_bin_to_dump.
-  std::vector<byte_buffer>* recycle_bin_to_swap = &recycle_bins[1];
-  /// Pointer to a recycle bin filled with PDUs that can be deleted (to return their memory segments to the pool).
-  std::vector<byte_buffer>* recycle_bin_to_dump = &recycle_bins[2];
-  /// Mutex for swapping the recycle bins.
-  std::mutex recycle_bin_swap_mutex;
+  std::unique_ptr<
+      concurrent_queue<byte_buffer, concurrent_queue_policy::lockfree_spsc, concurrent_queue_wait_policy::non_blocking>>
+      recycle_bin;
 };
 
 } // namespace srsran
