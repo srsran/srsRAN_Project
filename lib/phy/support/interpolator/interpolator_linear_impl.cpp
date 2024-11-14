4e359641efdeb9217ff317f30029bd5af85e266a
Xavier Arteaga <xavier@srs.io>
2024-11-14 21:24:22 +0100

    phy: implement interleaver in SIMD

--- lib/phy/support/interpolator/interpolator_linear_impl.cpp
+++ lib/phy/support/interpolator/interpolator_linear_impl.cpp
@@ -13,9 +13,92 @@
 
 #include "interpolator_linear_impl.h"
 #include "srsran/srsvec/copy.h"
+#include "srsran/srsvec/simd.h"
 #include "srsran/support/srsran_assert.h"
 
 using namespace srsran;
+
+static void simd_interpolate(span<cf_t> output, span<const cf_t> input, unsigned offset, unsigned stride)
+{
+  unsigned i_output = offset;
+  unsigned i_input  = 0;
+
+  // First, fill the offset with the first known value.
+  std::fill_n(output.begin(), i_output + 1, input[i_input]);
+
+#if SRSRAN_SIMD_CF_SIZE
+  // Maximum interpolation factor.
+  static constexpr unsigned max_simd_stride = 3;
+  if (stride <= max_simd_stride) {
+    // Linear interpolation filter, it is constant across all the SIMD batches.
+    static_vector<float, max_simd_stride * SRSRAN_SIMD_CF_SIZE> interpolate_stairs(stride * SRSRAN_SIMD_CF_SIZE);
+    for (unsigned i = 0; i != stride * SRSRAN_SIMD_CF_SIZE; ++i) {
+      interpolate_stairs[i] = static_cast<float>(i % stride) / static_cast<float>(stride);
+    }
+
+    // Temporary up-sampled input. Separated in real and imaginary to avoid interleaving and deinterleaving. Also, It
+    // leaves extra space for one more SIMD register.
+    static constexpr unsigned max_temp_input_size =
+        max_simd_stride * (SRSRAN_SIMD_CF_SIZE + 1) + (SRSRAN_SIMD_CF_SIZE - max_simd_stride);
+    std::array<float, max_temp_input_size> interpolate_input_re;
+    std::array<float, max_temp_input_size> interpolate_input_im;
+
+    // Process SIMD batch in chunks of SIMD register size.
+    for (unsigned i_input_end = ((input.size() - 1) / SRSRAN_SIMD_CF_SIZE) * SRSRAN_SIMD_CF_SIZE;
+         i_input != i_input_end;
+         i_input += SRSRAN_SIMD_CF_SIZE) {
+      // Load input and repeat values to match the up sampling factor. It assumes the maximum stride is smaller than or
+      // equal to the register size.
+      static_assert(max_simd_stride <= SRSRAN_SIMD_CF_SIZE);
+      for (unsigned i = 0; i != SRSRAN_SIMD_CF_SIZE + 1; ++i) {
+        // Prepare real part.
+        simd_f_t simd_in_re = srsran_simd_f_set1(input[i_input + i].real());
+        srsran_simd_f_storeu(&interpolate_input_re[i * stride], simd_in_re);
+
+        // Prepare imaginary part.
+        simd_f_t simd_in_im = srsran_simd_f_set1(input[i_input + i].imag());
+        srsran_simd_f_storeu(&interpolate_input_im[i * stride], simd_in_im);
+      }
+
+      // Process each of the SIMD registers.
+      for (unsigned i_simd = 0; i_simd != stride; ++i_simd) {
+        // Load the temporary data into SIMD registers.
+        simd_f_t  simd_interpolate_stairs = srsran_simd_f_loadu(&interpolate_stairs[i_simd * SRSRAN_SIMD_CF_SIZE]);
+        simd_cf_t simd_interpolate_input0 = srsran_simd_cf_loadu(&interpolate_input_re[i_simd * SRSRAN_SIMD_CF_SIZE],
+                                                                 &interpolate_input_im[i_simd * SRSRAN_SIMD_CF_SIZE]);
+        simd_cf_t simd_interpolate_input1 =
+            srsran_simd_cf_loadu(&interpolate_input_re[i_simd * SRSRAN_SIMD_CF_SIZE + stride],
+                                 &interpolate_input_im[i_simd * SRSRAN_SIMD_CF_SIZE + stride]);
+
+        // Calculate results.
+        simd_cf_t simd_result =
+            (simd_interpolate_input1 - simd_interpolate_input0) * simd_interpolate_stairs + simd_interpolate_input0;
+
+        // Store and advance output index.
+        srsran_simd_cfi_storeu(&output[i_output], simd_result);
+        i_output += SRSRAN_SIMD_CF_SIZE;
+      }
+    }
+    output[i_output] = input[i_input];
+  }
+#endif // SRSRAN_SIMD_CF_SIZE
+
+  // Linear interpolation.
+  for (unsigned next = i_output + stride, out_length = output.size(); next < out_length; next += stride) {
+    cf_t       jump       = (input[i_input + 1] - input[i_input]) / static_cast<float>(stride);
+    span<cf_t> output_tmp = output.subspan(i_output + 1, stride);
+    std::generate(output_tmp.begin(), output_tmp.end(), [jump, val = output[i_output]]() mutable {
+      val += jump;
+      return val;
+    });
+    i_output = next;
+    ++i_input;
+  }
+
+  // At this point, "current" points at the last known observation. Repeat its value for all the remaining missing
+  // observations.
+  std::fill(output.begin() + i_output + 1, output.end(), input[i_input]);
+}
 
 void interpolator_linear_impl::interpolate(span<cf_t> output, span<const cf_t> input, const configuration& cfg)
 {
@@ -43,25 +126,5 @@ void interpolator_linear_impl::interpolate(span<cf_t> output, span<const cf_t> i
     return;
   }
 
-  unsigned i_output = cfg.offset;
-  unsigned i_input  = 0;
-
-  // First, fill the offset with the first known value.
-  std::fill_n(output.begin(), i_output + 1, input[i_input]);
-
-  // Linear interpolation.
-  for (unsigned next = i_output + cfg.stride, out_length = output.size(); next < out_length; next += cfg.stride) {
-    cf_t       jump       = (input[i_input + 1] - input[i_input]) / static_cast<float>(cfg.stride);
-    span<cf_t> output_tmp = output.subspan(i_output + 1, cfg.stride);
-    std::generate(output_tmp.begin(), output_tmp.end(), [jump, val = output[i_output]]() mutable {
-      val += jump;
-      return val;
-    });
-    i_output = next;
-    ++i_input;
-  }
-
-  // At this point, "current" points at the last known observation. Repeat its value for all the remaining missing
-  // observations.
-  std::fill(output.begin() + i_output + 1, output.end(), input[i_input]);
+  simd_interpolate(output, input, cfg.offset, cfg.stride);
 }
