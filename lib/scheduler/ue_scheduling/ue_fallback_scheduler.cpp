7c912b3f517d9a63cfff52c1803b968b29562155
Carlo Galiotto <carlo@srs.io>
2024-12-12 19:52:23 +0100

    sched: update pusch pw ctrl state in fallback sched
    Signed-off-by: Carlo Galiotto <carlo@srs.io>

--- lib/scheduler/ue_scheduling/ue_fallback_scheduler.cpp
+++ lib/scheduler/ue_scheduling/ue_fallback_scheduler.cpp
@@ -1096,6 +1096,9 @@ ue_fallback_scheduler::schedule_ul_srb(ue&                                      
   pusch_alloc.ul_res_grid.fill(
       grant_info{cell_cfg.ul_cfg_common.init_ul_bwp.generic_params.scs, pusch_td.symbols, ue_grant_crbs});
 
+  // Update the number of PRBs used in the PUSCH allocation.
+  u.get_pcell().get_ul_power_controller().update_pusch_pw_ctrl_state(pusch_alloc.slot, ue_grant_crbs.length());
+
   // Update DRX controller state.
   u.drx_controller().on_new_pdcch_alloc(pdcch_alloc.slot);
 
