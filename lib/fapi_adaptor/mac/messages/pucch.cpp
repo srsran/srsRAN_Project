11290bc7937714d5075cf7c82ac467154a151322
Jonathan Pichel Carrera <jonathan.pichel@srs.io>
2024-12-13 14:05:54 +0100

    fapi: document fapi parameters for pucch format 3 and 4

--- lib/fapi_adaptor/mac/messages/pucch.cpp
+++ lib/fapi_adaptor/mac/messages/pucch.cpp
@@ -119,13 +119,13 @@ static void fill_format3_parameters(fapi::ul_pucch_pdu_builder& builder, const p
   // Hopping parameters.
   const prb_interval& hop_prbs            = mac_pdu.resources.second_hop_prbs;
   const bool          intra_slot_freq_hop = hop_prbs.empty() ? false : true;
-
-  // Cyclic shift index (m0) is always 0 for PUCCH Format 3, as per TS 38.211 6.4.1.3.3.1.
-  constexpr unsigned m0 = 0;
+  // Both FAPI parameters initialCyclicShift (ref. TS 38.211 6.3.2.2.2) and m0PucchDmrsCyclicShift (ref.
+  // TS 38.211 6.4.1.3.3.1) map to the same value, which is always 0 for PUCCH Format 3.
+  constexpr unsigned m0_format3 = 0;
 
   // Parameter initial_cyclic_shift is not applicable to PUCCH Format 3.
   builder.set_hopping_information_parameters(
-      intra_slot_freq_hop, hop_prbs.start(), f3.group_hopping, f3.n_id_hopping, m0);
+      intra_slot_freq_hop, hop_prbs.start(), f3.group_hopping, f3.n_id_hopping, m0_format3);
 
   // Common parameters.
   builder.set_common_parameters(mac_pdu.format, f3.slot_repetition, f3.pi_2_bpsk);
@@ -134,7 +134,7 @@ static void fill_format3_parameters(fapi::ul_pucch_pdu_builder& builder, const p
   builder.set_scrambling_parameters(f3.n_id_scrambling);
 
   // DM-RS.
-  builder.set_dmrs_parameters(f3.additional_dmrs, f3.n_id_0_scrambling, m0);
+  builder.set_dmrs_parameters(f3.additional_dmrs, f3.n_id_0_scrambling, m0_format3);
 
   // Max coding rate.
   builder.set_maintenance_v3_basic_parameters({static_cast<unsigned>(f3.max_code_rate)}, {});
@@ -143,7 +143,7 @@ static void fill_format3_parameters(fapi::ul_pucch_pdu_builder& builder, const p
   builder.set_bit_length_parameters(convert_sr_bits_to_unsigned(f3.sr_bits), f3.harq_ack_nof_bits, f3.csi_part1_bits);
 }
 
-/// Gets the cyclic shift index (m0) for PUCCH Format 4, as per TS38.211 Table 6.4.1.3.3.1-1.
+/// Gets the cyclic shift index (m0) for PUCCH Format 4, as per TS 38.211 Table 6.4.1.3.3.1-1.
 static unsigned get_pucch_format4_m0(unsigned occ_index)
 {
   switch (occ_index) {
@@ -168,12 +168,12 @@ static void fill_format4_parameters(fapi::ul_pucch_pdu_builder& builder, const p
   // Hopping parameters.
   const prb_interval& hop_prbs            = mac_pdu.resources.second_hop_prbs;
   const bool          intra_slot_freq_hop = hop_prbs.empty() ? false : true;
-  // Parameter initial_cyclic_shift is not applicable to PUCCH Format 4.
-  builder.set_hopping_information_parameters(intra_slot_freq_hop,
-                                             hop_prbs.start(),
-                                             f4.group_hopping,
-                                             f4.n_id_hopping,
-                                             get_pucch_format4_m0(mac_pdu.format_4.orthog_seq_idx));
+  // Both FAPI parameters initialCyclicShift (ref. TS 38.211 6.3.2.2.2) and m0PucchDmrsCyclicShift
+  // (ref. TS 38.211 6.4.1.3.3.1) map to the same value.
+  const unsigned m0_format4 = get_pucch_format4_m0(mac_pdu.format_4.orthog_seq_idx);
+
+  builder.set_hopping_information_parameters(
+      intra_slot_freq_hop, hop_prbs.start(), f4.group_hopping, f4.n_id_hopping, m0_format4);
 
   // Common parameters.
   builder.set_common_parameters(mac_pdu.format, f4.slot_repetition, f4.pi_2_bpsk);
@@ -182,8 +182,7 @@ static void fill_format4_parameters(fapi::ul_pucch_pdu_builder& builder, const p
   builder.set_scrambling_parameters(f4.n_id_scrambling);
 
   // DM-RS.
-  builder.set_dmrs_parameters(
-      f4.additional_dmrs, f4.n_id_0_scrambling, get_pucch_format4_m0(mac_pdu.format_4.orthog_seq_idx));
+  builder.set_dmrs_parameters(f4.additional_dmrs, f4.n_id_0_scrambling, m0_format4);
 
   // Specific format 4 parameters.
   builder.set_format4_parameters(f4.orthog_seq_idx, static_cast<uint8_t>(f4.n_sf_pucch_f4));
