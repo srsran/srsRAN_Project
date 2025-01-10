87ba1f268d3492d5330004f8270e0eedc465cac4
frankist <francisco.paisana@srs.io>
2025-01-10 18:41:45 +0100

    Revert last UL grant extension feature

--- lib/scheduler/ue_scheduling/ue_cell_grid_allocator.h
+++ lib/scheduler/ue_scheduling/ue_cell_grid_allocator.h
@@ -66,8 +66,6 @@ private:
     return *cells[cell_index].cell_alloc;
   }
 
-  void post_process_ul_results(du_cell_index_t cell_idx, slot_point pusch_slot);
-
   const scheduler_ue_expert_config& expert_cfg;
 
   ue_repository&        ues;
