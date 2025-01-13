f04ad6337a01d8fddaed66309a1e360d61dc655b
frankist <francisco.paisana@srs.io>
2025-01-13 10:29:55 +0100

    sched: reduce the number of tables for pusch time domain resources

--- lib/scheduler/support/pusch/pusch_default_time_allocation.cpp
+++ lib/scheduler/support/pusch/pusch_default_time_allocation.cpp
@@ -24,126 +24,94 @@ constexpr ofdm_symbol_range s_and_len(unsigned s, unsigned l)
   return ofdm_symbol_range::start_and_len(s, l);
 }
 
-unsigned pusch_default_time_allocation_default_A_get_j(subcarrier_spacing scs)
+constexpr unsigned pusch_default_time_allocation_default_A_get_j(subcarrier_spacing scs)
 {
   // TS38.214 Table 6.1.2.1.1-4. Definition of value j.
-  static std::array<unsigned, 4> values = {1, 1, 2, 3};
+  constexpr std::array<unsigned, 4> values = {1, 1, 2, 3};
 
   unsigned numerology = to_numerology_value(scs);
   srsran_assert(numerology < values.size(), "Invalid numerology.");
   return values[numerology];
 }
 
-pusch_time_domain_resource_allocation pusch_default_time_allocation_default_A_get_normal(unsigned           row_index,
-                                                                                         subcarrier_spacing scs)
-{
-  // TS38.214 Table 6.1.2.1.1-2. Default PUSCH time domain resource allocation A for normal CP.
-  static const std::array<pusch_time_domain_resource_allocation, 16> TABLE = {
-      {{0, sch_mapping_type::typeA, s_and_len(0, 14)},
-       {0, sch_mapping_type::typeA, s_and_len(0, 12)},
-       {0, sch_mapping_type::typeA, s_and_len(0, 10)},
-       {0, sch_mapping_type::typeB, s_and_len(2, 10)},
-       {0, sch_mapping_type::typeB, s_and_len(4, 10)},
-       {0, sch_mapping_type::typeB, s_and_len(4, 8)},
-       {0, sch_mapping_type::typeB, s_and_len(4, 6)},
-       {1, sch_mapping_type::typeA, s_and_len(0, 14)},
-       {1, sch_mapping_type::typeA, s_and_len(0, 12)},
-       {1, sch_mapping_type::typeA, s_and_len(0, 10)},
-       {2, sch_mapping_type::typeA, s_and_len(0, 14)},
-       {2, sch_mapping_type::typeA, s_and_len(0, 12)},
-       {2, sch_mapping_type::typeA, s_and_len(0, 10)},
-       {0, sch_mapping_type::typeB, s_and_len(8, 6)},
-       {3, sch_mapping_type::typeA, s_and_len(0, 14)},
-       {3, sch_mapping_type::typeA, s_and_len(0, 10)}}};
-
-  if (row_index >= TABLE.size()) {
-    return PUSCH_DEFAULT_TIME_ALLOCATION_RESERVED;
-  }
-
-  // Select row.
-  pusch_time_domain_resource_allocation result = TABLE[row_index];
-
-  // Add parameter j.
-  result.k2 += pusch_default_time_allocation_default_A_get_j(scs);
-
-  return result;
-}
-
-pusch_time_domain_resource_allocation pusch_default_time_allocation_default_A_get_extended(unsigned           row_index,
-                                                                                           subcarrier_spacing scs)
-{
-  // TS38.214 Table 6.1.2.1.1-3. Default PUSCH time domain resource allocation A for extended CP.
-  static const std::array<pusch_time_domain_resource_allocation, 16> TABLE = {
-      {{0, sch_mapping_type::typeA, s_and_len(0, 8)},
-       {0, sch_mapping_type::typeA, s_and_len(0, 12)},
-       {0, sch_mapping_type::typeA, s_and_len(0, 10)},
-       {0, sch_mapping_type::typeB, s_and_len(2, 10)},
-       {0, sch_mapping_type::typeB, s_and_len(4, 4)},
-       {0, sch_mapping_type::typeB, s_and_len(4, 8)},
-       {0, sch_mapping_type::typeB, s_and_len(4, 6)},
-       {1, sch_mapping_type::typeA, s_and_len(0, 8)},
-       {1, sch_mapping_type::typeA, s_and_len(0, 12)},
-       {1, sch_mapping_type::typeA, s_and_len(0, 10)},
-       {2, sch_mapping_type::typeA, s_and_len(0, 6)},
-       {2, sch_mapping_type::typeA, s_and_len(0, 12)},
-       {2, sch_mapping_type::typeA, s_and_len(0, 10)},
-       {0, sch_mapping_type::typeB, s_and_len(8, 4)},
-       {3, sch_mapping_type::typeA, s_and_len(0, 8)},
-       {3, sch_mapping_type::typeA, s_and_len(0, 10)}}};
-
-  if (row_index >= TABLE.size()) {
-    return PUSCH_DEFAULT_TIME_ALLOCATION_RESERVED;
-  }
-
-  // Select row.
-  pusch_time_domain_resource_allocation result = TABLE[row_index];
-
-  // Add parameter j.
-  result.k2 += pusch_default_time_allocation_default_A_get_j(scs);
-
-  return result;
-}
-
 } // namespace
-
-pusch_time_domain_resource_allocation
-srsran::pusch_default_time_allocation_default_A_get(cyclic_prefix cp, unsigned row_index, subcarrier_spacing scs)
-{
-  switch (cp) {
-    case cyclic_prefix::NORMAL:
-      return pusch_default_time_allocation_default_A_get_normal(row_index, scs);
-    case cyclic_prefix::EXTENDED:
-    default:
-      return pusch_default_time_allocation_default_A_get_extended(row_index, scs);
-  }
-}
 
 span<const pusch_time_domain_resource_allocation>
 srsran::pusch_default_time_allocations_default_A_table(cyclic_prefix cp, subcarrier_spacing scs)
 {
-  // TS38.214 Table 5.1.2.1.1-2.
-  static constexpr size_t PUSCH_TD_RES_ALLOC_TABLE_SIZE = 16;
-
-  // Build PUSCH-TimeDomain tables statically.
-  auto table_builder = [](cyclic_prefix cp_, subcarrier_spacing scs_) {
-    std::array<pusch_time_domain_resource_allocation, PUSCH_TD_RES_ALLOC_TABLE_SIZE> table;
-    for (unsigned i = 0; i < PUSCH_TD_RES_ALLOC_TABLE_SIZE; ++i) {
-      table[i] = pusch_default_time_allocation_default_A_get(cp_, i, scs_);
+  // TS38.214 Table 6.1.2.1.1-2. Default PUSCH time domain resource allocation A for normal CP.
+  static constexpr std::array<std::array<pusch_time_domain_resource_allocation, 16>, 4> table_normal_cp = []() {
+    std::array<std::array<pusch_time_domain_resource_allocation, 16>, 4> table = {};
+    table[0] = {{{0, sch_mapping_type::typeA, s_and_len(0, 14)},
+                 {0, sch_mapping_type::typeA, s_and_len(0, 12)},
+                 {0, sch_mapping_type::typeA, s_and_len(0, 10)},
+                 {0, sch_mapping_type::typeB, s_and_len(2, 10)},
+                 {0, sch_mapping_type::typeB, s_and_len(4, 10)},
+                 {0, sch_mapping_type::typeB, s_and_len(4, 8)},
+                 {0, sch_mapping_type::typeB, s_and_len(4, 6)},
+                 {1, sch_mapping_type::typeA, s_and_len(0, 14)},
+                 {1, sch_mapping_type::typeA, s_and_len(0, 12)},
+                 {1, sch_mapping_type::typeA, s_and_len(0, 10)},
+                 {2, sch_mapping_type::typeA, s_and_len(0, 14)},
+                 {2, sch_mapping_type::typeA, s_and_len(0, 12)},
+                 {2, sch_mapping_type::typeA, s_and_len(0, 10)},
+                 {0, sch_mapping_type::typeB, s_and_len(8, 6)},
+                 {3, sch_mapping_type::typeA, s_and_len(0, 14)},
+                 {3, sch_mapping_type::typeA, s_and_len(0, 10)}}};
+    table[1] = table[0];
+    table[2] = table[0];
+    table[3] = table[0];
+    for (unsigned u = 0; u != 4; ++u) {
+      for (unsigned j = 0; j != 16; ++j) {
+        // Add parameter j.
+        table[u][j].k2 += pusch_default_time_allocation_default_A_get_j(to_subcarrier_spacing(u));
+      }
     }
     return table;
-  };
-  static const std::array<std::array<pusch_time_domain_resource_allocation, PUSCH_TD_RES_ALLOC_TABLE_SIZE>, 8> tables =
-      {table_builder(cyclic_prefix::NORMAL, to_subcarrier_spacing(0)),
-       table_builder(cyclic_prefix::NORMAL, to_subcarrier_spacing(1)),
-       table_builder(cyclic_prefix::NORMAL, to_subcarrier_spacing(2)),
-       table_builder(cyclic_prefix::NORMAL, to_subcarrier_spacing(3)),
-       table_builder(cyclic_prefix::EXTENDED, to_subcarrier_spacing(0)),
-       table_builder(cyclic_prefix::EXTENDED, to_subcarrier_spacing(1)),
-       table_builder(cyclic_prefix::EXTENDED, to_subcarrier_spacing(2)),
-       table_builder(cyclic_prefix::EXTENDED, to_subcarrier_spacing(3))};
+  }();
 
-  // Retrieve respective table.
-  return tables[static_cast<unsigned>(cp.value) * 4 + to_numerology_value(scs)];
+  // TS38.214 Table 6.1.2.1.1-3. Default PUSCH time domain resource allocation A for extended CP.
+  static constexpr std::array<std::array<pusch_time_domain_resource_allocation, 16>, 4> table_extended_cp = []() {
+    std::array<std::array<pusch_time_domain_resource_allocation, 16>, 4> table = {};
+    table[0] = {{{0, sch_mapping_type::typeA, s_and_len(0, 8)},
+                 {0, sch_mapping_type::typeA, s_and_len(0, 12)},
+                 {0, sch_mapping_type::typeA, s_and_len(0, 10)},
+                 {0, sch_mapping_type::typeB, s_and_len(2, 10)},
+                 {0, sch_mapping_type::typeB, s_and_len(4, 4)},
+                 {0, sch_mapping_type::typeB, s_and_len(4, 8)},
+                 {0, sch_mapping_type::typeB, s_and_len(4, 6)},
+                 {1, sch_mapping_type::typeA, s_and_len(0, 8)},
+                 {1, sch_mapping_type::typeA, s_and_len(0, 12)},
+                 {1, sch_mapping_type::typeA, s_and_len(0, 10)},
+                 {2, sch_mapping_type::typeA, s_and_len(0, 6)},
+                 {2, sch_mapping_type::typeA, s_and_len(0, 12)},
+                 {2, sch_mapping_type::typeA, s_and_len(0, 10)},
+                 {0, sch_mapping_type::typeB, s_and_len(8, 4)},
+                 {3, sch_mapping_type::typeA, s_and_len(0, 8)},
+                 {3, sch_mapping_type::typeA, s_and_len(0, 10)}}};
+    table[1] = table[0];
+    table[2] = table[0];
+    table[3] = table[0];
+    for (unsigned u = 0; u != 4; ++u) {
+      for (unsigned j = 0; j != 16; ++j) {
+        // Add parameter j.
+        table[u][j].k2 += pusch_default_time_allocation_default_A_get_j(to_subcarrier_spacing(u));
+      }
+    }
+    return table;
+  }();
+
+  return (cp == cyclic_prefix::NORMAL ? table_normal_cp : table_extended_cp)[to_numerology_value(scs)];
+}
+
+pusch_time_domain_resource_allocation
+srsran::pusch_default_time_allocation_default_A_get(cyclic_prefix cp, unsigned row_index, subcarrier_spacing scs)
+{
+  span<const pusch_time_domain_resource_allocation> table = pusch_default_time_allocations_default_A_table(cp, scs);
+  if (row_index >= table.size()) {
+    return PUSCH_DEFAULT_TIME_ALLOCATION_RESERVED;
+  }
+  return table[row_index];
 }
 
 span<const pusch_time_domain_resource_allocation>
