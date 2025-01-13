491fc69f5ff849478250f544e99c8c4ca32b7d05
frankist <francisco.paisana@srs.io>
2025-01-13 15:48:36 +0100

    sched: extend sched QoS unit test for saturation case

--- tests/unittests/scheduler/scheduler_qos_test.cpp
+++ tests/unittests/scheduler/scheduler_qos_test.cpp
@@ -26,11 +26,13 @@ class scheduler_qos_test : public scheduler_test_simulator
   };
 
 public:
-  scheduler_qos_test(unsigned nof_gbr_ues          = 1,
-                     unsigned nof_non_gbr_ues      = 7,
+  scheduler_qos_test(unsigned nof_gbr_ues_         = 1,
+                     unsigned nof_non_gbr_ues_     = 7,
                      unsigned gbr_dl_bitrate_mbps_ = 10e6,
                      unsigned gbr_ul_bitrate_mbps_ = 5e6) :
     scheduler_test_simulator(4, subcarrier_spacing::kHz30),
+    nof_gbr_ues(nof_gbr_ues_),
+    nof_non_gbr_ues(nof_non_gbr_ues_),
     gbr_dl_bitrate_mbps(gbr_dl_bitrate_mbps_),
     gbr_ul_bitrate_mbps(gbr_ul_bitrate_mbps_)
   {
@@ -102,32 +104,11 @@ public:
     }
   }
 
-  const unsigned gbr_dl_bitrate_mbps;
-  const unsigned gbr_ul_bitrate_mbps;
+  void run_slot()
+  {
+    scheduler_test_simulator::run_slot();
 
-  cell_config_builder_params params;
-
-  pucch_res_builder_test_helper pucch_cfg_builder;
-
-  std::vector<ue_stats> ue_stats_map;
-};
-
-/// Test case when 1 UE has a GBR bearer and the remaining UEs do not.
-class scheduler_1_gbr_ue_qos_test : public scheduler_qos_test, public ::testing::Test
-{
-public:
-  scheduler_1_gbr_ue_qos_test() : scheduler_qos_test(1) {}
-};
-
-TEST_F(scheduler_1_gbr_ue_qos_test, when_ue_has_gbr_drb_it_gets_higher_priority)
-{
-  // number of slots discarded that were used to fill average window.
-  const unsigned PREAMBLE_SLOT_RUNS = 200;
-  const unsigned MAX_NOF_SLOT_RUNS  = 1000;
-
-  for (unsigned r = 0; r != MAX_NOF_SLOT_RUNS + PREAMBLE_SLOT_RUNS; ++r) {
-    this->run_slot();
-
+    // Handle UCI and CRC indications.
     uci_indication uci_ind;
     uci_ind.cell_index = to_du_cell_index(0);
     uci_ind.slot_rx    = this->last_result_slot();
@@ -137,16 +118,6 @@ TEST_F(scheduler_1_gbr_ue_qos_test, when_ue_has_gbr_drb_it_gets_higher_priority)
 
     span<const dl_msg_alloc>  ue_grants = this->last_sched_res_list[0]->dl.ue_grants;
     span<const ul_sched_info> ul_grants = this->last_sched_res_list[0]->ul.puschs;
-    if (r >= PREAMBLE_SLOT_RUNS) {
-      // Register DL sched bytes.
-      for (const dl_msg_alloc& grant : ue_grants) {
-        ue_stats_map[grant.context.ue_index].dl_bytes_sum += grant.pdsch_cfg.codewords[0].tb_size_bytes;
-      }
-      // Register UL sched bytes.
-      for (const ul_sched_info& grant : ul_grants) {
-        ue_stats_map[grant.context.ue_index].ul_bytes_sum += grant.pusch_cfg.tb_size_bytes;
-      }
-    }
 
     // Handle PUCCHs.
     for (const pucch_info& pucch : this->last_sched_res_list[to_du_cell_index(0)]->ul.pucchs) {
@@ -173,15 +144,86 @@ TEST_F(scheduler_1_gbr_ue_qos_test, when_ue_has_gbr_drb_it_gets_higher_priority)
     if (not crc_ind.crcs.empty()) {
       this->sched->handle_crc_indication(crc_ind);
     }
+
+    // Register DL sched bytes.
+    for (const dl_msg_alloc& grant : ue_grants) {
+      ue_stats_map[grant.context.ue_index].dl_bytes_sum += grant.pdsch_cfg.codewords[0].tb_size_bytes;
+    }
+    // Register UL sched bytes.
+    for (const ul_sched_info& grant : ul_grants) {
+      ue_stats_map[grant.context.ue_index].ul_bytes_sum += grant.pusch_cfg.tb_size_bytes;
+    }
+
+    metric_slot_count++;
   }
 
-  double nof_sec_elapsed = (MAX_NOF_SLOT_RUNS / (double)get_nof_slots_per_subframe(params.scs_common)) / 1000;
-  std::vector<double> ue_dl_rate_mbps(ue_stats_map.size(), 0);
-  std::vector<double> ue_ul_rate_mbps(ue_stats_map.size(), 0);
-  for (unsigned i = 0; i != ue_stats_map.size(); ++i) {
-    ue_dl_rate_mbps[i] = ue_stats_map[i].dl_bytes_sum * 8U * 1e-6 / nof_sec_elapsed;
-    ue_ul_rate_mbps[i] = ue_stats_map[i].ul_bytes_sum * 8U * 1e-6 / nof_sec_elapsed;
+  void reset_stats()
+  {
+    metric_slot_count = 0;
+    for (auto& ue_st : ue_stats_map) {
+      ue_st.dl_bytes_sum = 0;
+      ue_st.ul_bytes_sum = 0;
+    }
   }
+
+  std::vector<double> get_ue_dl_bitrate_mbps() const
+  {
+    double nof_sec_elapsed = (metric_slot_count / (double)get_nof_slots_per_subframe(params.scs_common)) / 1000;
+    std::vector<double> ue_dl_rate_mbps(ue_stats_map.size(), 0);
+    for (unsigned i = 0; i != ue_stats_map.size(); ++i) {
+      ue_dl_rate_mbps[i] = ue_stats_map[i].dl_bytes_sum * 8U * 1e-6 / nof_sec_elapsed;
+    }
+    return ue_dl_rate_mbps;
+  }
+
+  std::vector<double> get_ue_ul_bitrate_mbps() const
+  {
+    double nof_sec_elapsed = (metric_slot_count / (double)get_nof_slots_per_subframe(params.scs_common)) / 1000;
+    std::vector<double> ue_ul_rate_mbps(ue_stats_map.size(), 0);
+    for (unsigned i = 0; i != ue_stats_map.size(); ++i) {
+      ue_ul_rate_mbps[i] = ue_stats_map[i].ul_bytes_sum * 8U * 1e-6 / nof_sec_elapsed;
+    }
+    return ue_ul_rate_mbps;
+  }
+
+  const unsigned nof_gbr_ues;
+  const unsigned nof_non_gbr_ues;
+  const unsigned gbr_dl_bitrate_mbps;
+  const unsigned gbr_ul_bitrate_mbps;
+
+  cell_config_builder_params params;
+
+  pucch_res_builder_test_helper pucch_cfg_builder;
+
+  std::vector<ue_stats> ue_stats_map;
+
+  unsigned metric_slot_count = 0;
+};
+
+/// Test case when 1 UE has a GBR bearer and the remaining UEs do not.
+class scheduler_1_gbr_ue_qos_test : public scheduler_qos_test, public ::testing::Test
+{
+public:
+  scheduler_1_gbr_ue_qos_test() : scheduler_qos_test(1) {}
+};
+
+TEST_F(scheduler_1_gbr_ue_qos_test, when_ue_has_gbr_drb_it_gets_higher_priority)
+{
+  // number of slots discarded that were used to fill average window.
+  const unsigned PREAMBLE_SLOT_RUNS = 200;
+  const unsigned MAX_NOF_SLOT_RUNS  = 1000;
+
+  for (unsigned r = 0; r != MAX_NOF_SLOT_RUNS + PREAMBLE_SLOT_RUNS; ++r) {
+    this->run_slot();
+
+    if (r == PREAMBLE_SLOT_RUNS) {
+      // Reset stats when we pass the preamble.
+      this->reset_stats();
+    }
+  }
+
+  std::vector<double> ue_dl_rate_mbps = this->get_ue_dl_bitrate_mbps();
+  std::vector<double> ue_ul_rate_mbps = this->get_ue_ul_bitrate_mbps();
 
   fmt::memory_buffer dl_fmtbuf;
   fmt::memory_buffer ul_fmtbuf;
@@ -203,7 +245,47 @@ TEST_F(scheduler_1_gbr_ue_qos_test, when_ue_has_gbr_drb_it_gets_higher_priority)
       << "UE UL GBR rate < expected UL GBR";
 }
 
-class scheduler_saturated_gbr_ue_qos_test : public scheduler_qos_test
+class scheduler_saturated_gbr_ue_qos_test : public scheduler_qos_test, public ::testing::Test
 {
 public:
+  scheduler_saturated_gbr_ue_qos_test() : scheduler_qos_test(2, 6, 25e6, 10e6) {}
 };
+
+TEST_F(scheduler_saturated_gbr_ue_qos_test, when_gbrs_saturate_channel_then_non_gbr_still_get_some_traffic)
+{
+  // number of slots discarded that were used to fill average window.
+  const unsigned PREAMBLE_SLOT_RUNS = 200;
+  const unsigned MAX_NOF_SLOT_RUNS  = 1000;
+
+  for (unsigned r = 0; r != MAX_NOF_SLOT_RUNS + PREAMBLE_SLOT_RUNS; ++r) {
+    this->run_slot();
+
+    if (r == PREAMBLE_SLOT_RUNS) {
+      // Reset stats when we pass the preamble.
+      this->reset_stats();
+    }
+  }
+
+  std::vector<double> ue_dl_rate_mbps = this->get_ue_dl_bitrate_mbps();
+  std::vector<double> ue_ul_rate_mbps = this->get_ue_ul_bitrate_mbps();
+
+  fmt::memory_buffer dl_fmtbuf;
+  fmt::memory_buffer ul_fmtbuf;
+  for (unsigned i = 0; i != ue_dl_rate_mbps.size(); ++i) {
+    fmt::format_to(std::back_inserter(dl_fmtbuf), " {:.3}", ue_dl_rate_mbps[i]);
+    fmt::format_to(std::back_inserter(ul_fmtbuf), " {:.3}", ue_ul_rate_mbps[i]);
+  }
+  test_logger.info("DL bit rates [Mbps]: [{}]", fmt::to_string(dl_fmtbuf));
+  test_logger.info("UL bit rates [Mbps]: [{}]", fmt::to_string(ul_fmtbuf));
+
+  double min_dl_gbr_rate = *std::min_element(ue_dl_rate_mbps.begin(), ue_dl_rate_mbps.begin() + nof_gbr_ues);
+  double min_ul_gbr_rate = *std::min_element(ue_ul_rate_mbps.begin(), ue_ul_rate_mbps.begin() + nof_gbr_ues);
+  for (unsigned i = nof_gbr_ues; i != ue_stats_map.size(); ++i) {
+    ASSERT_GT(min_dl_gbr_rate, ue_dl_rate_mbps[i]) << "UE DL GBR rate < UE DL non-GBR rate";
+    ASSERT_GT(min_ul_gbr_rate, ue_ul_rate_mbps[i]) << "UE UL GBR rate < UE UL non-GBR rate";
+  }
+  double min_dl_rate = *std::min_element(ue_dl_rate_mbps.begin(), ue_dl_rate_mbps.end());
+  double min_ul_rate = *std::min_element(ue_ul_rate_mbps.begin(), ue_ul_rate_mbps.end());
+  ASSERT_GT(min_dl_rate, 0.5);
+  ASSERT_GT(min_ul_rate, 0.5);
+}
