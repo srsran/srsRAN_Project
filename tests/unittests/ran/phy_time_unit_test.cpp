fb3caef87e22204e47e34059e19445acb7ed724e
Xavier Arteaga <xavier@srs.io>
2025-01-24 13:05:14 +0100

    ran: implement PHY time unit to UL-RTOA conversion

--- tests/unittests/ran/phy_time_unit_test.cpp
+++ tests/unittests/ran/phy_time_unit_test.cpp
@@ -65,3 +65,86 @@ TEST(phy_time_unit_test, store_positive_seconds)
   ASSERT_GT(phy_time.to_Tc(), 0);
   ASSERT_NEAR(time, phy_time.to_seconds<float>(), 1e-9);
 }
+
+TEST(phy_time_unit_test, ul_rtoa)
+{
+  // Use TS38.133 Table 13.1.1-1 entries as test points.
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985025).to_ul_rtoa(0), 0);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985024).to_ul_rtoa(0), 1);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985023).to_ul_rtoa(0), 2);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-2).to_ul_rtoa(0), 985023);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-1).to_ul_rtoa(0), 985024);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(0).to_ul_rtoa(0), 985024);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(1).to_ul_rtoa(0), 985025);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(2).to_ul_rtoa(0), 985026);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(3).to_ul_rtoa(0), 985027);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(985023).to_ul_rtoa(0), 1970047);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(985024).to_ul_rtoa(0), 1970048);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(985025).to_ul_rtoa(0), 1970049);
+
+  // Use TS38.133 Table 13.1.1-2 entries as test points.
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985025).to_ul_rtoa(1), 0);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985024).to_ul_rtoa(1), 1);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985023).to_ul_rtoa(1), 1);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985022).to_ul_rtoa(1), 2);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985021).to_ul_rtoa(1), 2);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-4).to_ul_rtoa(1), 492511);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-3).to_ul_rtoa(1), 492511);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-2).to_ul_rtoa(1), 492512);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-1).to_ul_rtoa(1), 492512);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(0).to_ul_rtoa(1), 492512);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(1).to_ul_rtoa(1), 492513);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(2).to_ul_rtoa(1), 492513);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(3).to_ul_rtoa(1), 492514);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(4).to_ul_rtoa(1), 492514);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(5).to_ul_rtoa(1), 492515);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(6).to_ul_rtoa(1), 492515);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(985023).to_ul_rtoa(1), 985024);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(985024).to_ul_rtoa(1), 985024);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(985025).to_ul_rtoa(1), 985025);
+
+  // Use TS38.133 Table 13.1.1-3 entries as test points.
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985025).to_ul_rtoa(2), 0);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985024).to_ul_rtoa(2), 1);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985021).to_ul_rtoa(2), 1);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985020).to_ul_rtoa(2), 2);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985017).to_ul_rtoa(2), 2);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-8).to_ul_rtoa(2), 246255);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-5).to_ul_rtoa(2), 246255);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-4).to_ul_rtoa(2), 246256);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-1).to_ul_rtoa(2), 246256);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(0).to_ul_rtoa(2), 246256);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(1).to_ul_rtoa(2), 246257);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(4).to_ul_rtoa(2), 246257);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(5).to_ul_rtoa(2), 246258);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(8).to_ul_rtoa(2), 246258);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(9).to_ul_rtoa(2), 246259);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(12).to_ul_rtoa(2), 246259);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(985021).to_ul_rtoa(2), 492512);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(985024).to_ul_rtoa(2), 492512);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(985025).to_ul_rtoa(2), 492513);
+
+  // Use TS38.133 Table 13.1.1-4 entries as test points.
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985025).to_ul_rtoa(3), 0);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985024).to_ul_rtoa(3), 1);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985017).to_ul_rtoa(3), 1);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985016).to_ul_rtoa(3), 2);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-985009).to_ul_rtoa(3), 2);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-16).to_ul_rtoa(3), 123127);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-9).to_ul_rtoa(3), 123127);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-8).to_ul_rtoa(3), 123128);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(-1).to_ul_rtoa(3), 123128);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(0).to_ul_rtoa(3), 123128);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(1).to_ul_rtoa(3), 123129);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(8).to_ul_rtoa(3), 123129);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(9).to_ul_rtoa(3), 123130);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(16).to_ul_rtoa(3), 123130);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(17).to_ul_rtoa(3), 123131);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(24).to_ul_rtoa(3), 123131);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(985017).to_ul_rtoa(3), 246256);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(985024).to_ul_rtoa(3), 246256);
+  ASSERT_EQ(phy_time_unit::from_units_of_Tc(985025).to_ul_rtoa(3), 246257);
+
+  // Assume TS38.133 Tables 13.1.1-5/6 are covered from the cases above.
+  // ...
+}
