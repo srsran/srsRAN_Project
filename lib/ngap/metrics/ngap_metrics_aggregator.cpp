40f1a2160265b471622059670056cb8674ef70bd
Fabian Eckermann <fabian@srs.io>
2025-03-31 13:23:52 +0200

    cu_cp,ngap: add ngap metrics aggregator

---
+++ lib/ngap/metrics/ngap_metrics_aggregator.cpp
@@ -0,0 +1,53 @@
+/*
+ *
+ * Copyright 2021-2025 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "ngap_metrics_aggregator.h"
+
+using namespace srsran;
+using namespace srs_cu_cp;
+
+void ngap_metrics_aggregator::handle_requested_pdu_session(s_nssai_t s_nssai)
+{
+  if (aggregated_ngap_metrics.pdu_session_metrics.find(s_nssai) == aggregated_ngap_metrics.pdu_session_metrics.end()) {
+    aggregated_ngap_metrics.pdu_session_metrics.emplace(s_nssai, pdu_session_metrics_t{1, 0, {}});
+  } else {
+    aggregated_ngap_metrics.pdu_session_metrics[s_nssai].nof_pdu_sessions_requested_to_setup++;
+  }
+}
+
+void ngap_metrics_aggregator::handle_successful_pdu_session_setup(s_nssai_t s_nssai)
+{
+  if (aggregated_ngap_metrics.pdu_session_metrics.find(s_nssai) == aggregated_ngap_metrics.pdu_session_metrics.end()) {
+    aggregated_ngap_metrics.pdu_session_metrics.emplace(s_nssai, pdu_session_metrics_t{0, 1, {}});
+  } else {
+    aggregated_ngap_metrics.pdu_session_metrics[s_nssai].nof_pdu_sessions_successfully_setup++;
+  }
+}
+
+void ngap_metrics_aggregator::handle_failed_pdu_session_setup(s_nssai_t s_nssai, ngap_cause_t cause)
+{
+  if (aggregated_ngap_metrics.pdu_session_metrics.find(s_nssai) == aggregated_ngap_metrics.pdu_session_metrics.end()) {
+    aggregated_ngap_metrics.pdu_session_metrics.emplace(s_nssai, pdu_session_metrics_t{0, 0, {{cause, 1}}});
+  } else {
+    std::map<ngap_cause_t, unsigned>& failed_sessions =
+        aggregated_ngap_metrics.pdu_session_metrics[s_nssai].nof_pdu_sessions_failed_to_setup;
+
+    if (failed_sessions.find(cause) == failed_sessions.end()) {
+      failed_sessions.emplace(cause, 1);
+    } else {
+      failed_sessions[cause]++;
+    }
+  }
+}
+
+ngap_metrics ngap_metrics_aggregator::request_metrics_report() const
+{
+  return aggregated_ngap_metrics;
+}
---
+++ lib/ngap/metrics/ngap_metrics_aggregator.h
@@ -0,0 +1,39 @@
+/*
+ *
+ * Copyright 2021-2025 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#pragma once
+
+#include "srsran/ngap/ngap_metrics.h"
+#include "srsran/ran/s_nssai.h"
+#include <cmath>
+
+namespace srsran {
+namespace srs_cu_cp {
+
+class ngap_metrics_aggregator final
+{
+public:
+  ngap_metrics_aggregator()  = default;
+  ~ngap_metrics_aggregator() = default;
+
+  void handle_requested_pdu_session(s_nssai_t s_nssai);
+
+  void handle_successful_pdu_session_setup(s_nssai_t s_nssai);
+
+  void handle_failed_pdu_session_setup(s_nssai_t s_nssai, ngap_cause_t cause);
+
+  ngap_metrics request_metrics_report() const;
+
+private:
+  ngap_metrics aggregated_ngap_metrics;
+};
+
+} // namespace srs_cu_cp
+} // namespace srsran
