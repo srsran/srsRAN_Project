9441dc870444cb2dffa2a960c43880501ea79115
frankist <francisco.paisana@srs.io>
2025-04-03 16:56:42 +0200

    f1ap-du: create cell in the F1AP DU context even if the cell is not activated

--- lib/f1ap/du/procedures/f1ap_du_setup_procedure.cpp
+++ lib/f1ap/du/procedures/f1ap_du_setup_procedure.cpp
@@ -212,16 +212,17 @@ f1_setup_result f1ap_du_setup_procedure::create_f1_setup_result()
   // Update F1 DU Context (taking values from request).
   du_ctxt.du_id       = request.gnb_du_id;
   du_ctxt.gnb_du_name = request.gnb_du_name;
+  du_ctxt.served_cells.clear();
+  for (unsigned i = 0, sz = request.served_cells.size(); i != sz; ++i) {
+    const du_cell_index_t cell_idx = request.served_cells[i].cell_index;
+    du_ctxt.served_cells.emplace(cell_idx, f1ap_du_cell_context{cell_idx, request.served_cells[i].cell_info.nr_cgi});
+  }
   if (asn1_success.cells_to_be_activ_list_present) {
     success.cells_to_activate.resize(asn1_success.cells_to_be_activ_list.size());
-    for (unsigned i = 0, e = asn1_success.cells_to_be_activ_list.size(); i != e; ++i) {
+    for (unsigned i = 0, sz = asn1_success.cells_to_be_activ_list.size(); i != sz; ++i) {
       auto&               asn1_cell    = asn1_success.cells_to_be_activ_list[i]->cells_to_be_activ_list_item();
       nr_cell_global_id_t cgi          = cgi_from_asn1(asn1_cell.nr_cgi).value();
       success.cells_to_activate[i].cgi = cgi;
-      auto it                          = std::find_if(request.served_cells.begin(),
-                             request.served_cells.end(),
-                             [&cgi](const f1_cell_setup_params& c) { return c.cell_info.nr_cgi == cgi; });
-      du_ctxt.served_cells.emplace(it->cell_index, f1ap_du_cell_context{it->cell_index, cgi});
     }
   }
 
