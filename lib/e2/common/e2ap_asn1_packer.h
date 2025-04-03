98c7a70270336a28fccb564054a61fb97c06f080
Piotr Gawlowicz <piotr.gawlowicz@srs.io>
2025-04-03 09:56:53 +0200

    e2: remove e2ap_packer interface as not used anmore

--- lib/e2/common/e2ap_asn1_packer.h
+++ lib/e2/common/e2ap_asn1_packer.h
@@ -19,16 +19,16 @@
 namespace srsran {
 
 /// This E2AP packer class is used to pack outgoing and unpack incoming E2 message in ASN1 format.
-class e2ap_asn1_packer : public e2ap_packer
+class e2ap_asn1_packer
 {
 public:
   explicit e2ap_asn1_packer(sctp_network_gateway_data_handler& gw, e2_message_handler& e2, dlt_pcap& pcap_);
 
   /// Received packed E2AP PDU that needs to be unpacked and forwarded.
-  void handle_packed_pdu(const byte_buffer& pdu) override;
+  void handle_packed_pdu(const byte_buffer& pdu);
 
   /// Receive populated ASN1 struct that needs to be packed and forwarded.
-  void handle_message(const e2_message& msg) override;
+  void handle_message(const e2_message& msg);
 
 private:
   srslog::basic_logger&              logger;
