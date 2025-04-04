d8a06ef41f52009517d2848aaafea8308a4807f8
frankist <francisco.paisana@srs.io>
2025-04-04 17:06:31 +0200

    move du_cell thread to phy thread pool

--- apps/services/worker_manager/worker_manager.h
+++ apps/services/worker_manager/worker_manager.h
@@ -80,6 +80,13 @@ struct worker_manager : public worker_manager_executor_getter {
   worker_manager_executor_getter* get_executor_getter() { return this; }
 
 private:
+  struct du_crit_path_executor_desc {
+    // Description of L2 executors for the DU-high.
+    srs_du::du_high_executor_config::cell_executor_config l2_execs;
+    // Description of L1 executors for DU-low.
+    // TODO
+  };
+
   std::unique_ptr<srs_du::du_high_executor_mapper> du_high_exec_mapper;
   std::vector<std::vector<task_executor*>>         du_low_dl_executors;
 
@@ -118,9 +125,6 @@ private:
 
   std::vector<execution_config_helper::single_worker> create_fapi_workers(unsigned nof_cells);
 
-  std::vector<execution_config_helper::priority_multiqueue_worker> create_du_hi_slot_workers(unsigned nof_cells,
-                                                                                             bool     rt_mode);
-
   /// Helper method that creates the CU-UP executors.
   void create_cu_up_executors(const worker_manager_config::cu_up_config& config, timer_manager& timers);
 
@@ -131,11 +135,10 @@ private:
                            timer_manager&                                      timers);
 
   /// Helper method that creates the low Distributed Unit executors.
-  void create_du_low_executors(bool     is_blocking_mode_active,
-                               unsigned nof_ul_workers,
-                               unsigned nof_dl_workers,
-                               unsigned nof_pusch_decoder_workers,
-                               unsigned nof_cells);
+  du_crit_path_executor_desc
+  create_du_crit_path_prio_executors(unsigned                                                   nof_cells,
+                                     bool                                                       rt_mode,
+                                     const std::optional<worker_manager_config::du_low_config>& du_low);
 
   /// Helper method that creates the Radio Unit dummy executors.
   void create_ru_dummy_executors();
