1d1e2deb5650e8f3475faad5542b8588c77a5da4
Jonathan Pichel Carrera <jonathan.pichel@srs.io>
2025-04-14 16:14:08 +0200

    sched: SI change notification short messages unit test

--- tests/unittests/scheduler/common_scheduling/si_scheduler_test.cpp
+++ tests/unittests/scheduler/common_scheduling/si_scheduler_test.cpp
@@ -11,10 +11,14 @@
 #include "../test_utils/scheduler_test_suite.h"
 #include "lib/scheduler/common_scheduling/si_scheduler.h"
 #include "lib/scheduler/pdcch_scheduling/pdcch_resource_allocator_impl.h"
+#include "lib/scheduler/support/paging_helpers.h"
 #include "tests/test_doubles/scheduler/cell_config_builder_profiles.h"
 #include "tests/test_doubles/scheduler/scheduler_config_helper.h"
+#include "srsran/adt/bounded_bitset.h"
 #include "srsran/asn1/rrc_nr/ue_cap.h"
+#include "srsran/ran/pdcch/dci_packing.h"
 #include "srsran/scheduler/config/scheduler_expert_config_factory.h"
+#include "srsran/scheduler/result/dci_info.h"
 #include "srsran/scheduler/scheduler_configurator.h"
 #include "srsran/support/test_utils.h"
 #include <gtest/gtest.h>
@@ -188,4 +192,76 @@ TEST_F(si_scheduler_test, when_si_is_updated_then_new_version_is_applied_at_si_c
   }
 
   ASSERT_EQ(last_version, 1);
+}
+
+TEST_F(si_scheduler_test, when_si_is_updated_all_ues_in_rrc_idle_get_notified_exactly_once)
+{
+  const paging_slot_helper slot_helper(
+      cell_cfg, make_sched_configuration_request(DEFAULT_SIB1_PAYLOAD_SIZE, DEFAULT_SI_SCHED_CFG));
+  const units::bytes   new_sib1_len     = DEFAULT_SIB1_PAYLOAD_SIZE + units::bytes{64U};
+  si_scheduling_config new_si_sched_cfg = DEFAULT_SI_SCHED_CFG;
+  new_si_sched_cfg.si_messages[0].msg_len += units::bytes{64U};
+
+  // Update SI scheduling.
+  si_sched.handle_si_update_request(
+      si_scheduling_update_request{to_du_cell_index(0), 1, new_sib1_len, new_si_sched_cfg});
+
+  const unsigned si_ch_wind_len_rfs = static_cast<unsigned>(cell_cfg.dl_cfg_common.bcch_cfg.mod_period_coeff) *
+                                      static_cast<unsigned>(cell_cfg.dl_cfg_common.pcch_cfg.default_paging_cycle);
+
+  const unsigned                   nof_test_slots      = 2 * si_ch_wind_len_rfs * next_slot.nof_slots_per_frame();
+  bool                             new_version_applied = false;
+  static constexpr unsigned        total_nof_ue_ids    = 1024;
+  bounded_bitset<total_nof_ue_ids> notified_ue_ids(total_nof_ue_ids);
+  for (unsigned i = 0; i != nof_test_slots; ++i) {
+    slot_point current_slot = next_slot;
+    run_slot();
+
+    for (const auto& sib : res_grid[0].result.dl.bc.sibs) {
+      if (sib.version == 1) {
+        new_version_applied = true;
+      }
+    }
+
+    for (const auto& pdcch : res_grid[0].result.dl.dl_pdcchs) {
+      if (pdcch.dci.type != srsran::dci_dl_rnti_config_type::p_rnti_f1_0) {
+        continue;
+      }
+
+      const auto& dci = pdcch.dci.p_rnti_f1_0;
+      if (dci.short_messages_indicator != srsran::dci_1_0_p_rnti_configuration::payload_info::short_messages) {
+        continue;
+      }
+      ASSERT_EQ(dci.short_messages, 0x80);
+
+      // Notifications shouldn't be sent after the new version is applied.
+      ASSERT_FALSE(new_version_applied);
+
+      const unsigned paging_frame_offset  = cell_cfg.dl_cfg_common.pcch_cfg.paging_frame_offset;
+      const auto     drx_cycle            = static_cast<unsigned>(cell_cfg.dl_cfg_common.pcch_cfg.default_paging_cycle);
+      const auto     nof_pf_per_drx_cycle = static_cast<unsigned>(cell_cfg.dl_cfg_common.pcch_cfg.nof_pf);
+      const auto     nof_po_per_pf        = static_cast<unsigned>(cell_cfg.dl_cfg_common.pcch_cfg.ns);
+      const unsigned N                    = drx_cycle / nof_pf_per_drx_cycle;
+      const unsigned t_div_n              = drx_cycle / N;
+      for (unsigned ue_id = 0; ue_id < total_nof_ue_ids; ++ue_id) {
+        // Check for paging frame.
+        // (SFN + PF_offset) mod T = (T div N)*(UE_ID mod N). See TS 38.304, clause 7.1.
+        const unsigned ue_id_mod_n = ue_id % N;
+        if (((current_slot.sfn() + paging_frame_offset) % drx_cycle) != (t_div_n * ue_id_mod_n)) {
+          continue;
+        }
+
+        // Index (i_s), indicating the index of the PO.
+        // i_s = floor (UE_ID/N) mod Ns.
+        const unsigned i_s = (ue_id / N) % nof_po_per_pf;
+        if (slot_helper.is_paging_slot(current_slot, i_s)) {
+          ASSERT_FALSE(notified_ue_ids.test(ue_id));
+          notified_ue_ids.set(ue_id);
+          logger.debug("TEST: UE ID {} notified at {}", ue_id, current_slot);
+        }
+      }
+    }
+  }
+
+  ASSERT_TRUE(notified_ue_ids.all());
 }
