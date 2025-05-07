a75df68230ee0631a2e52dba3e1bdef76c753bfa
Pedro Alvarez <pedro@srs.io>
2025-05-07 14:43:57 +0100

    pdcp: fix incorrect check for full TX window

--- lib/pdcp/pdcp_entity_tx.cpp
+++ lib/pdcp/pdcp_entity_tx.cpp
@@ -960,7 +960,7 @@ pdcp_entity_tx::early_drop_reason pdcp_entity_tx::check_early_drop(const byte_bu
   if (updated_buffer_size > desired_buffer_size) {
     return early_drop_reason::full_rlc_queue;
   }
-  if ((st.tx_next - st.tx_trans) >= (window_size - 1)) {
+  if ((st.tx_next - st.tx_next_ack) >= (window_size - 1)) {
     return early_drop_reason::full_window;
   }
   return early_drop_reason::no_drop;
