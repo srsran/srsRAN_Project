621eeb21c46c4866babc69a4556d2f7101e5ee25
frankist <francisco.paisana@srs.io>
2025-05-08 14:44:09 +0200

    adt: implementation of flat_map

---
+++ tests/unittests/adt/flat_map_test.cpp
@@ -0,0 +1,141 @@
+/*
+ *
+ * Copyright 2021-2025 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#include "srsran/adt/flat_map.h"
+#include "srsran/support/test_utils.h"
+#include <gtest/gtest.h>
+
+using namespace srsran;
+
+static_assert(std::is_same_v<flat_map<int, char>::key_type, int>, "Invalid key type");
+static_assert(std::is_same_v<flat_map<int, char>::mapped_type, char>, "Invalid value type");
+
+TEST(flat_map_test, empty_flat_map)
+{
+  flat_map<int, int> m;
+  ASSERT_EQ(m.size(), 0);
+  ASSERT_TRUE(m.empty());
+  ASSERT_EQ(m.capacity(), 0);
+  ASSERT_EQ(m.begin(), m.end());
+  ASSERT_EQ(m.cbegin(), m.cend());
+  ASSERT_EQ(m.find(0), m.end());
+  ASSERT_EQ(m.lower_bound(0), m.end());
+  m.clear();
+  ASSERT_EQ(m.size(), 0);
+}
+
+TEST(flat_map_test, flat_map_creation_from_containers)
+{
+  std::vector<int>   keys   = {1, 2, 1, 1, 4};
+  std::vector<int>   values = {1, 2, 1, 1, 4};
+  flat_map<int, int> m{keys, values};
+
+  ASSERT_FALSE(m.empty());
+  ASSERT_EQ(m.size(), 3);
+  ASSERT_EQ(m.begin()->first, 1);
+  ASSERT_EQ((m.begin() + 1)->first, 2);
+  ASSERT_EQ((m.begin() + 2)->first, 4);
+  ASSERT_EQ(m.begin() + 3, m.end());
+  for (const auto& p : m) {
+    ASSERT_EQ(p.first, p.second);
+  }
+}
+
+TEST(flat_map_test, flat_map_creation_from_containers_with_reverse_order)
+{
+  std::vector<int>                      keys   = {1, 2, 1, 1, 4};
+  std::vector<int>                      values = {1, 2, 1, 1, 4};
+  flat_map<int, int, std::greater<int>> m{keys, values};
+
+  ASSERT_FALSE(m.empty());
+  ASSERT_EQ(m.size(), 3);
+  ASSERT_EQ(m.begin()->first, 4);
+  ASSERT_EQ((m.begin() + 1)->first, 2);
+  ASSERT_EQ((m.begin() + 3)->first, 1);
+  ASSERT_EQ(m.begin() + 3, m.end());
+  for (const auto& p : m) {
+    ASSERT_EQ(p.first, p.second);
+  }
+}
+
+TEST(flat_map_test, flat_map_creation_from_containers_of_move_only_types)
+{
+  std::vector<int>                  keys = {1, 2, 1, 1, 4};
+  std::vector<moveonly_test_object> values;
+  values.reserve(keys.size());
+  for (unsigned i = 0; i != keys.size(); ++i) {
+    values.emplace_back(keys[i]);
+  }
+  flat_map<int, moveonly_test_object> m{keys, std::move(values)};
+
+  ASSERT_FALSE(m.empty());
+  ASSERT_EQ(m.size(), 3);
+  ASSERT_EQ(m.begin()->first, 1);
+  ASSERT_EQ((m.begin() + 1)->first, 2);
+  ASSERT_EQ((m.begin() + 2)->first, 4);
+  ASSERT_EQ(m.begin() + 3, m.end());
+  for (const auto& p : m) {
+    ASSERT_EQ(p.first, p.second.value());
+  }
+}
+
+TEST(flat_map_test, flat_map_creation_from_containers_of_move_only_types_in_reverse_order)
+{
+  std::vector<int>                  keys = {1, 2, 1, 1, 4};
+  std::vector<moveonly_test_object> values;
+  values.reserve(keys.size());
+  for (unsigned i = 0; i != keys.size(); ++i) {
+    values.emplace_back(keys[i]);
+  }
+  flat_map<int, moveonly_test_object, std::greater<int>> m{keys, std::move(values)};
+
+  ASSERT_FALSE(m.empty());
+  ASSERT_EQ(m.size(), 3);
+  ASSERT_EQ(m.begin()->first, 4);
+  ASSERT_EQ((m.begin() + 1)->first, 2);
+  ASSERT_EQ((m.begin() + 2)->first, 1);
+  ASSERT_EQ(m.begin() + 3, m.end());
+  for (const auto& p : m) {
+    ASSERT_EQ(p.first, p.second.value());
+  }
+}
+
+TEST(flat_map_test, flat_map_copy)
+{
+  std::vector<int>   keys   = {1, 2, 1, 1, 4};
+  std::vector<int>   values = {1, 2, 1, 1, 4};
+  flat_map<int, int> m{keys, values};
+  flat_map<int, int> copy = m;
+
+  ASSERT_EQ(copy, m);
+  ASSERT_EQ(copy.size(), 3);
+}
+
+TEST(flat_map_test, flat_map_move)
+{
+  std::vector<int>   keys   = {1, 2, 1, 1, 4};
+  std::vector<int>   values = {1, 2, 1, 1, 4};
+  flat_map<int, int> m{keys, values};
+  flat_map<int, int> moved = std::move(m);
+
+  ASSERT_EQ(moved.size(), 3);
+  ASSERT_EQ(m.size(), 0);
+}
+
+TEST(flat_map_test, flat_map_clear)
+{
+  std::vector<int>   keys   = {1, 2, 1, 1, 4};
+  std::vector<int>   values = {1, 2, 1, 1, 4};
+  flat_map<int, int> m{keys, values};
+
+  ASSERT_EQ(m.size(), 3);
+  m.clear();
+  ASSERT_EQ(m.size(), 0);
+}
