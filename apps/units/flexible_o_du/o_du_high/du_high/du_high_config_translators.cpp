90f40e3681c1b7f0563ca305536c2ddf4b21f4ab
Jonathan Pichel Carrera <jonathan.pichel@srs.io>
2025-06-23 10:13:28 +0200

    sched: fix other issues

--- apps/units/flexible_o_du/o_du_high/du_high/du_high_config_translators.cpp
+++ apps/units/flexible_o_du/o_du_high/du_high/du_high_config_translators.cpp
@@ -777,7 +777,11 @@ std::vector<srs_du::du_cell_config> srsran::generate_du_cell_config(const du_hig
           static_cast<float>(get_nof_slots_per_subframe(base_cell.common_scs)) * user_srs_cfg.srs_period_ms.value());
       du_srs_cfg.srs_period.emplace(static_cast<srs_periodicity>(srs_period_slots));
     }
-    du_srs_cfg.max_nof_symbols = user_srs_cfg.max_nof_symbols_per_slot;
+    du_srs_cfg.tx_comb                   = user_srs_cfg.tx_comb == 2 ? tx_comb_size::n2 : tx_comb_size::n4;
+    du_srs_cfg.max_nof_symbols           = user_srs_cfg.max_nof_symbols_per_slot;
+    du_srs_cfg.nof_symbols               = static_cast<srs_nof_symbols>(user_srs_cfg.nof_symbols);
+    du_srs_cfg.cyclic_shift_reuse_factor = static_cast<nof_cyclic_shifts>(user_srs_cfg.cyclic_shift_reuse_factor);
+    du_srs_cfg.sequence_id_reuse_factor  = user_srs_cfg.sequence_id_reuse_factor;
 
     // Parameters for PUSCH-Config.
     if (not out_cell.ue_ded_serv_cell_cfg.ul_config.has_value()) {
--- lib/scheduler/ue_scheduling/intra_slice_scheduler.cpp
+++ lib/scheduler/ue_scheduling/intra_slice_scheduler.cpp
@@ -741,10 +741,10 @@ unsigned intra_slice_scheduler::max_pdschs_to_alloc(const dl_ran_slice_candidate
   }
 
   // Determine how many PDCCHs can be allocated in this slot.
-  const auto& pdcch_res = cell_alloc[pdcch_slot].result;
-  pdschs_to_alloc       = std::min({pdschs_to_alloc,
-                                    static_cast<int>(MAX_DL_PDCCH_PDUS_PER_SLOT - pdcch_res.dl.dl_pdcchs.size()),
-                                    static_cast<int>(expert_cfg.max_pdcch_alloc_attempts_per_slot - dl_attempts_count)});
+  const auto& pdcch_res  = cell_alloc[pdcch_slot].result;
+  const int   max_pdcchs = std::min(static_cast<int>(MAX_DL_PDCCH_PDUS_PER_SLOT - pdcch_res.dl.dl_pdcchs.size()),
+                                  static_cast<int>(expert_cfg.max_pdcch_alloc_attempts_per_slot - dl_attempts_count));
+  pdschs_to_alloc        = std::min(pdschs_to_alloc, max_pdcchs);
   if (pdschs_to_alloc <= 0) {
     return 0;
   }
@@ -793,10 +793,10 @@ unsigned intra_slice_scheduler::max_puschs_to_alloc(const ul_ran_slice_candidate
   }
 
   // Determine how many PDCCHs can be allocated in this slot.
-  const auto& pdcch_res = cell_alloc[pdcch_slot].result;
-  puschs_to_alloc       = std::min({puschs_to_alloc,
-                                    static_cast<int>(MAX_UL_PDCCH_PDUS_PER_SLOT - pdcch_res.dl.ul_pdcchs.size()),
-                                    static_cast<int>(expert_cfg.max_pdcch_alloc_attempts_per_slot - ul_attempts_count)});
+  const auto& pdcch_res  = cell_alloc[pdcch_slot].result;
+  const auto  max_pdcchs = std::min(static_cast<int>(MAX_UL_PDCCH_PDUS_PER_SLOT - pdcch_res.ul.puschs.size()),
+                                   static_cast<int>(expert_cfg.max_pdcch_alloc_attempts_per_slot - ul_attempts_count));
+  puschs_to_alloc        = std::min(puschs_to_alloc, max_pdcchs);
   if (puschs_to_alloc <= 0) {
     return 0;
   }
