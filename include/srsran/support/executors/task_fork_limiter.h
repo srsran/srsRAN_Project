f54def9cb10d70a608e501dec92c25c4b2ec379a
frankist <francisco.paisana@srs.io>
2025-07-10 17:29:33 +0200

    support: fix task fork limiter

--- include/srsran/support/executors/task_fork_limiter.h
+++ include/srsran/support/executors/task_fork_limiter.h
@@ -77,11 +77,12 @@ private:
       if (not pop_task(task)) {
         break;
       }
-      // Decrement the job count after successful pop.
-      jobs_left = job_count.fetch_sub(1, std::memory_order_acq_rel) - 1;
 
       // Execute the task.
       task();
+
+      // Decrement the job count after successful pop and task execution.
+      jobs_left = job_count.fetch_sub(1, std::memory_order_acq_rel) - 1;
 
       // Check if we should yield back control to the worker.
       if (--max_pops == 0 and jobs_left > 0) {
@@ -126,27 +127,32 @@ private:
   {
     if (not queue.try_pop(task)) {
       // Failed to pop a task from the queue.
+
+      // Check if this is the last fork alive.
+      unsigned active_forks_count = active_forks.load(std::memory_order_acquire);
+      if (active_forks_count > 1) {
+        // There are other forks alive.
+        return false;
+      }
+
       // Re-read pending job count.
-      unsigned jobs_left = job_count.load(std::memory_order_acquire);
+      unsigned jobs_left = job_count.load(std::memory_order_relaxed);
       if (jobs_left == 0) {
-        // No more jobs left to process. Probably, another fork did the pop operation concurrently.
+        // No more jobs left to process. Probably, another fork did the pop operation concurrently and depleted the
+        // queue.
         count_pop_fails.store(0, std::memory_order_relaxed);
         return false;
       }
 
-      // There are still jobs left to process.
-      unsigned active_forks_count = active_forks.load(std::memory_order_acquire);
-      if (active_forks_count <= 1) {
-        // Sometimes, the pop operation may fail spuriously, even when there are tasks in the queue.
-        // Last fork alive. Do not give up yet and defer the popping.
-        auto count_copy = count_pop_fails.fetch_add(1, std::memory_order_relaxed) + 1;
-        if (count_copy >= 1000) {
-          count_pop_fails.store(0, std::memory_order_relaxed);
-          srslog::fetch_basic_logger("ALL").warning(
-              "task_fork_limiter: Too many pop failures detected. There was likely a lost job in the queue");
-        }
-        defer_remaining_tasks();
+      // There are still jobs left in the queue and this is the last fork alive. This can happen because the pop
+      // operation fails spuriously for some queue implementations.
+      auto count_copy = count_pop_fails.fetch_add(1, std::memory_order_relaxed) + 1;
+      if (count_copy >= 1000) {
+        count_pop_fails.store(0, std::memory_order_relaxed);
+        srslog::fetch_basic_logger("ALL").warning(
+            "task_fork_limiter: Too many pop failures detected. There was likely a lost job in the queue");
       }
+      defer_remaining_tasks();
       return false;
     }
     return true;
