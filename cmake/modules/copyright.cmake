d680f723abbe13adc76a468ad4ef6a102de91558
Alfredo Sáez <alfredo.saez@srs.io>
2025-07-11 12:21:33 +0200

    cmake: add external into sbom

---
+++ cmake/modules/copyright.cmake
@@ -0,0 +1,83 @@
+#
+# Copyright 2021-2025 Software Radio Systems Limited
+#
+# By using this file, you agree to the terms and conditions set
+# forth in the LICENSE file which can be found at the top level of
+# the distribution.
+#
+
+# This function reads the COPYRIGHT file, parse the external info and call sbom
+function(parse_copyright_file FILE)
+  if(NOT EXISTS "${FILE}")
+    message(FATAL_ERROR "COPYRIGHT file not found: ${FILE}")
+  endif()
+
+  file(READ "${FILE}" _raw)
+  string(REPLACE "\r\n" "\n" _lines "${_raw}") # Normalize line endings
+  string(REPLACE "\n\n\n" ";" _lines "${_lines}") # Split by sections when multiple new lines are present
+  string(REPLACE "\n" " " _lines "${_lines}") # Remove single new lines inside sections
+
+  foreach(_line IN LISTS _lines)
+    if(_line MATCHES "^Files:")
+      string(REGEX MATCH "Files: +(.*) Copyright: +(.*) License: +(.*)" _ ${_line})
+      set(_files_str "${CMAKE_MATCH_1}")
+      set(_copy_str "${CMAKE_MATCH_2}")
+      set(_license_str "${CMAKE_MATCH_3}")
+
+      # Module name
+      string(STRIP "${_files_str}" _files_str)
+      string(REPLACE " " ";" _file_list "${_files_str}")
+      get_module_name_from_files("${_file_list}" _module_name)
+
+      if("${_module_name}" STREQUAL "*" OR ${_module_name} STREQUAL "docs")
+        continue()
+      endif()
+
+      # Supplier
+      string(REGEX REPLACE
+        "^[0-9][0-9][0-9][0-9](-present|-[0-9][0-9][0-9][0-9])?[, ]*"
+        ""
+        _copy_clean
+        "${_copy_str}"
+      )
+
+      # Add to SBOM
+      message(STATUS "Adding external module: ${_module_name} with license: ${_license_str} supplier: ${_copy_clean} and version: ${_version}")
+      sbom_add(
+        PACKAGE "${_module_name}"
+        SUPPLIER "${_copy_clean}"
+        LICENSE "${_license_str}"
+      )
+    endif()
+  endforeach()
+endfunction(parse_copyright_file)
+
+function(get_module_name_from_files FILE_LIST MODULE)
+  list(LENGTH FILE_LIST _count)
+
+  if(_count EQUAL 1)
+    list(GET FILE_LIST 0 _f)
+    get_filename_component(_name "${_f}" NAME_WE)
+    set(${MODULE} "${_name}" PARENT_SCOPE)
+    return()
+  endif()
+
+  list(GET FILE_LIST 0 _first)
+  get_filename_component(_common "${_first}" DIRECTORY)
+
+  foreach(_f IN LISTS FILE_LIST)
+    # if the file is empty, skip it
+    if("${_f}" STREQUAL "")
+      continue()
+    endif()
+
+    get_filename_component(_dir "${_f}" DIRECTORY)
+
+    while(NOT _dir MATCHES "^${_common}")
+      get_filename_component(_common "${_common}" DIRECTORY)
+    endwhile()
+  endforeach()
+
+  get_filename_component(_mod "${_common}" NAME)
+  set(${MODULE} "${_mod}" PARENT_SCOPE)
+endfunction()
