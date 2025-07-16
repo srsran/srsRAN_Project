ae9336e46b3206a60cfb1dbd5c58af6bb1e83ad0
Fabian Eckermann <fabian@srs.io>
2025-07-16 16:36:50 +0200

    ci: add inter du handover test

---
+++ tests/e2e/tests/inter_du_handover.py
@@ -0,0 +1,67 @@
+#
+# Copyright 2021-2025 Software Radio Systems Limited
+#
+# By using this file, you agree to the terms and conditions set
+# forth in the LICENSE file which can be found at the top level of
+# the distribution.
+#
+
+"""
+Handover Tests
+"""
+from pytest import mark
+from retina.client.manager import RetinaTestManager
+from retina.launcher.artifacts import RetinaTestData
+from retina.launcher.public import MetricsSummary
+from retina.launcher.utils import param
+from retina.protocol.fivegc_pb2_grpc import FiveGCStub
+from retina.protocol.gnb_pb2_grpc import CUStub, DUStub
+from retina.protocol.ue_pb2_grpc import UEStub
+
+from .handover import _handover_sequentially
+
+
+@mark.parametrize(
+    "band, common_scs, bandwidth, noise_spd",
+    (
+        param(3, 15, 50, 0, id="band:%s-scs:%s-bandwidth:%s-noise:%s"),
+        param(41, 30, 50, 0, id="band:%s-scs:%s-bandwidth:%s-noise:%s"),
+        # param(3, 15, 50, -74, id="band:%s-scs:%s-bandwidth:%s-noise:%s"),
+        # param(41, 30, 50, -74, id="band:%s-scs:%s-bandwidth:%s-noise:%s"),
+    ),
+)
+@mark.zmq
+@mark.flaky(reruns=1, only_rerun=["failed to start", "Attach timeout reached", "StatusCode.ABORTED"])
+# pylint: disable=too-many-arguments,too-many-positional-arguments
+def test_zmq_inter_du_handover_sequentially(
+    retina_manager: RetinaTestManager,
+    retina_data: RetinaTestData,
+    ue_8: UEStub,
+    fivegc: FiveGCStub,
+    cu: CUStub,
+    du_2: DUStub,
+    metrics_summary: MetricsSummary,
+    band: int,
+    common_scs: int,
+    bandwidth: int,
+    noise_spd: int,
+):
+    """
+    ZMQ Inter-DU Handover tests
+    """
+
+    # _inter_du_handover_sequentially(
+    _handover_sequentially(
+        retina_manager=retina_manager,
+        retina_data=retina_data,
+        ue_array=ue_8,
+        fivegc=fivegc,
+        cu=cu,
+        du_array=du_2,
+        metrics_summary=metrics_summary,
+        band=band,
+        common_scs=common_scs,
+        bandwidth=bandwidth,
+        noise_spd=noise_spd,
+        sleep_between_movement_steps=10,
+    )
