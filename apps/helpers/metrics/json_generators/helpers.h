337e0f4ca1c2e05bc4be459a31f98b1adb85d18d
faluco <borja@srs.io>
2025-07-30 16:31:26 +0200

    Change metrics timestamp format

--- apps/helpers/metrics/json_generators/helpers.h
+++ apps/helpers/metrics/json_generators/helpers.h
@@ -11,6 +11,7 @@
 #pragma once
 
 #include "srsran/ran/slot_point.h"
+#include "fmt/chrono.h"
 #include <chrono>
 #include <cmath>
 
@@ -24,11 +25,13 @@ inline void to_json(nlohmann::json& json, const slot_point& slot)
 namespace app_helpers {
 namespace json_generators {
 
-/// Returns the current time in seconds with ms precision since UNIX epoch.
-inline double get_time_stamp()
+/// Returns the current UTC time and date with millisecond precision.
+inline std::string get_time_stamp()
 {
-  auto tp = std::chrono::system_clock::now().time_since_epoch();
-  return std::chrono::duration_cast<std::chrono::milliseconds>(tp).count() * 1e-3;
+  auto    tp           = std::chrono::high_resolution_clock::now();
+  std::tm current_time = fmt::gmtime(tp);
+  auto    ms_fraction  = std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch()).count() % 1000u;
+  return fmt::format("{:%F}T{:%H:%M:%S}.{:03} ", current_time, current_time, ms_fraction);
 }
 
 /// Return the given value if it is not a Nan or Inf, otherwise returns 0.
