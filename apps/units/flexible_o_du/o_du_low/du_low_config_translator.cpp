92a5b59fab5aa421a5b2b08b2f4b0bf42e48ef78
frankist <francisco.paisana@srs.io>
2025-07-30 11:00:29 +0200

    du_low: fix affinity list derivation

--- apps/units/flexible_o_du/o_du_low/du_low_config_translator.cpp
+++ apps/units/flexible_o_du/o_du_low/du_low_config_translator.cpp
@@ -169,9 +169,7 @@ void srsran::fill_du_low_worker_manager_config(worker_manager_config&    config,
     du_low_cfg.metrics_period.emplace(unit_cfg.metrics_cfg.du_report_period);
   }
 
-  srsran_assert(config.config_affinities.size() == unit_cfg.expert_execution_cfg.cell_affinities.size(),
-                "Invalid number of cell affinities");
-
+  config.config_affinities.resize(nof_cells);
   for (unsigned i = 0, e = nof_cells; i != e; ++i) {
     config.config_affinities[i].push_back(unit_cfg.expert_execution_cfg.cell_affinities[i].l1_dl_cpu_cfg);
   }
