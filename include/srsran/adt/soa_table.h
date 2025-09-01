6560cbee4d9ba2895eea3638188fecce5ec8d19a
frankist <francisco.paisana@srs.io>
2025-09-01 10:46:59 +0200

    adt: implementation of soa table iterator and more tests

--- include/srsran/adt/soa_table.h
+++ include/srsran/adt/soa_table.h
@@ -18,27 +18,72 @@
 namespace srsran {
 namespace soa {
 
-template <typename T>
-class cell_view
+/// Type representing a row ID.
+class row_id
 {
-  using VectorType = std::conditional_t<std::is_const_v<T>, const std::vector<std::remove_const_t<T>>, std::vector<T>>;
-
 public:
-  explicit cell_view(VectorType& data_, unsigned offset_) : col(&data_), offset(offset_) {}
+  explicit row_id(uint32_t row_id_val) : val(row_id_val) {}
 
-  T&       value() { return (*col)[offset]; }
-  const T& value() const { return (*col)[offset]; }
-  T&       operator*() { return (*col)[offset]; }
-  const T& operator*() const { return (*col)[offset]; }
-  T*       operator->() { return &(*col)[offset]; }
-  const T* operator->() const { return &(*col)[offset]; }
+  bool operator==(const row_id& other) const { return val == other.val; }
+  bool operator!=(const row_id& other) const { return val != other.val; }
+  bool operator<(const row_id& other) const { return val < other.val; }
+  bool operator>(const row_id& other) const { return val > other.val; }
+  bool operator<=(const row_id& other) const { return val <= other.val; }
+  bool operator>=(const row_id& other) const { return val >= other.val; }
 
-  bool operator==(const cell_view& other) const { return value() == other.value(); }
-  bool operator!=(const cell_view& other) const { return not(*this == other); }
+  uint32_t value() const { return val; }
 
 private:
-  VectorType* col    = nullptr;
-  unsigned    offset = 0;
+  uint32_t val;
+};
+
+/// View of a row in a table.
+template <typename TableType>
+class row_view
+{
+public:
+  row_view(TableType& table_, unsigned offset_) : table(&table_), offset(offset_) {}
+
+  /// Number of columns in the table.
+  static constexpr size_t nof_columns() { return TableType::nof_columns(); }
+
+  /// Row ID of the row.
+  row_id id() const { return row_id{table->index_reverse_map[offset]}; }
+
+  /// Retrieves a cell from the table for column I.
+  template <std::size_t I>
+  auto& at()
+  {
+    static_assert(I < nof_columns(), "Column index out of range");
+    auto& cols = std::get<I>(table->columns);
+    srsran_assert(offset < cols.size(), "Row offset out of range");
+    return cols[offset];
+  }
+  template <std::size_t I>
+  const auto& at() const
+  {
+    static_assert(I < nof_columns(), "Column index out of range");
+    auto& cols = std::get<I>(table->columns);
+    srsran_assert(offset < cols.size(), "Row offset out of range");
+    return cols[offset];
+  }
+
+  bool operator==(const row_view& other) const
+  {
+    bool result = true;
+    std::apply(
+        [&](const auto&... lhs) mutable {
+          std::apply([&](const auto&... rhs) mutable { result = ((lhs[offset] == rhs[other.offset]) && ...); },
+                     other.table->columns);
+        },
+        table->columns);
+    return result;
+  }
+  bool operator!=(const row_view& other) const { return not(*this == other); }
+
+private:
+  TableType* table;
+  unsigned   offset;
 };
 
 /// View of a column in a table.
@@ -48,97 +93,132 @@ class column_view
   static constexpr unsigned INVALID_INDEX = std::numeric_limits<unsigned>::max();
   using VectorType = std::conditional_t<std::is_const_v<T>, const std::vector<std::remove_const_t<T>>, std::vector<T>>;
 
-  template <typename Data>
-  class unsorted_iter_impl
-  {
-    using parent_type = std::conditional_t<std::is_const_v<Data>, column_view<T>*, const column_view<T>*>;
-
-  public:
-    using value_type      = T;
-    using difference_type = std::ptrdiff_t;
-
-    unsorted_iter_impl(parent_type* parent_, unsigned index_) : parent(parent_), index(index_) {}
-
-    unsorted_iter_impl& operator++()
-    {
-      ++index;
-      return *this;
-    }
-
-    unsorted_iter_impl operator++(int)
-    {
-      unsorted_iter_impl tmp = *this;
-      ++index;
-      return tmp;
-    }
-
-    T&       operator*() { return (*parent->data)[index]; }
-    const T& operator*() const { return (*parent->data)[index]; }
-
-    T* operator->() { return &(*parent->data)[index]; }
-
-    bool operator==(const unsorted_iter_impl& other) const { return index == other.index and parent == other.parent; }
-
-    bool operator!=(const unsorted_iter_impl& other) const { return not(*this == other); }
-
-    parent_type* parent;
-    unsigned     index;
-  };
-
 public:
   explicit column_view(VectorType&                  data_,
                        const std::vector<unsigned>& index_map_,
-                       const std::vector<unsigned>& index_reverse_map_) :
+                       const std::vector<row_id>&   index_reverse_map_) :
     col_elems(&data_), index_map(&index_map_), index_reverse_map(&index_reverse_map_)
   {
   }
 
-  bool has_row_id(unsigned row_id) const
+  [[nodiscard]] bool has_row_id(row_id rid) const
   {
-    if (row_id >= index_map->size()) {
+    if (rid.value() >= index_map->size()) {
       return false;
     }
-    unsigned offset = (*index_map)[row_id];
-    return offset < index_reverse_map->size();
+    unsigned offset = (*index_map)[rid.value()];
+    if (offset >= index_reverse_map->size()) {
+      return false;
+    }
+    return (*index_reverse_map)[offset] == rid;
   }
 
-  cell_view<T> operator[](unsigned row_id)
+  T& operator[](row_id rid)
   {
-    srsran_assert(has_row_id(row_id), "Invalid row_id");
-    return cell_view<T>{*col_elems, (*index_map)[row_id]};
+    srsran_assert(has_row_id(rid), "Invalid row_id");
+    return (*col_elems)[(*index_map)[rid.value()]];
   }
 
-  cell_view<const T> operator[](unsigned row_id) const
+  const T& operator[](row_id rid) const
   {
-    srsran_assert(has_row_id(row_id), "Invalid row_id");
-    return cell_view<const T>{*col_elems, (*index_map)[row_id]};
+    srsran_assert(has_row_id(rid), "Invalid row_id");
+    return (*col_elems)[(*index_map)[rid.value()]];
   }
 
+  /// Retrieves the number of rows of the table.
   std::size_t size() const { return index_reverse_map->size(); }
-  bool        empty() const { return index_reverse_map->empty(); }
-  T*          data() { return col_elems->data(); }
-  const T*    data() const { return col_elems->data(); }
 
-  /// Unsorted view of the column (i.e. the index does not correspond to the row id).
-  span<const T> unsorted() const { return span<const T>(col_elems->data(), col_elems->size()); }
-  span<T>       unsorted() { return span<T>(col_elems->data(), col_elems->size()); }
+  /// Checks if the table contains any rows.
+  bool     empty() const { return index_reverse_map->empty(); }
+  T*       data() { return col_elems->data(); }
+  const T* data() const { return col_elems->data(); }
+
+  /// Unsorted iterator of the column.
+  auto begin() { return col_elems->begin(); }
+  auto begin() const { return col_elems->begin(); }
+  auto cbegin() const { return col_elems->begin(); }
+  auto end() { return col_elems->end(); }
+  auto end() const { return col_elems->end(); }
+  auto cend() const { return col_elems->end(); }
+
+  span<const T> to_span() const { return span<const T>(col_elems->data(), col_elems->size()); }
+  span<T>       to_span() { return span<T>(col_elems->data(), col_elems->size()); }
 
 private:
   VectorType*                  col_elems;
   const std::vector<unsigned>* index_map;
-  const std::vector<unsigned>* index_reverse_map;
+  const std::vector<row_id>*   index_reverse_map;
 };
 
 /// \brief Struct-of-arrays representation of a list of objects.
 /// Each row corresponds to an object, and each column to a field of the object.
 /// Objects have a stable row ID, but their offset or address in the internal storage may change.
-/// Each column of the table is represented as a vector. All elements are stored contiguously in memory, with no holes.
+/// Each column of the table is represented as a vector. All elements are stored contiguously in memory, with no
+/// holes.
 template <typename... ColumnTypes>
 class table
 {
+  struct sentinel {};
+
+  /// Iterator of rows in the table. The iterator is not sorted by row ID.
+  template <typename TableType>
+  class iter_impl
+  {
+    struct arrow_proxy {
+      row_view<const TableType>        r;
+      const row_view<const TableType>* operator->() const { return &r; }
+    };
+
+  public:
+    using value_type      = row_view<table>;
+    using difference_type = std::ptrdiff_t;
+    using reference       = row_view<TableType>;
+    // No meaningful raw pointer available.
+    using pointer = void;
+    // forward iterator requires *it to be an actual reference to a stable object.
+    using iterator_category = std::input_iterator_tag;
+
+    iter_impl() = default;
+    iter_impl(TableType& table_, size_t offset_) : parent(&table_), offset(offset_) {}
+
+    operator iter_impl<const table>() const noexcept { return iter_impl<const table>{*parent, offset}; }
+
+    row_view<TableType>       operator*() { return row_view<TableType>{*parent, offset}; }
+    row_view<const TableType> operator*() const { return row_view<const TableType>{*parent, offset}; }
+    arrow_proxy               operator->() const { return arrow_proxy{**this}; }
+
+    iter_impl& operator++()
+    {
+      ++offset;
+      return *this;
+    }
+    iter_impl operator++(int)
+    {
+      iter_impl tmp = *this;
+      ++offset;
+      return tmp;
+    }
+
+    bool        operator==(const iter_impl& other) const { return offset == other.offset and parent == other.parent; }
+    bool        operator!=(const iter_impl& other) const { return not(*this == other); }
+    bool        operator==(sentinel /* unused */) const { return offset == parent->size(); }
+    bool        operator!=(sentinel /* unused */) const { return offset != parent->size(); }
+    friend bool operator==(sentinel /* unused */, const iter_impl& rhs) { return rhs.offset == rhs.parent->size(); }
+    friend bool operator!=(sentinel /* unused */, const iter_impl& rhs) { return rhs.offset != rhs.parent->size(); }
+
+  private:
+    friend class table<ColumnTypes...>;
+
+    TableType* parent = nullptr;
+    unsigned   offset = 0;
+  };
+
 public:
   template <std::size_t ColIndex>
   using column_type = std::tuple_element_t<ColIndex, std::tuple<ColumnTypes...>>;
+
+  using iterator       = iter_impl<table>;
+  using const_iterator = iter_impl<const table>;
 
   template <std::size_t I>
   auto column()
@@ -151,75 +231,107 @@ public:
     return make_column_view(std::get<I>(columns));
   }
 
-  bool     empty() const { return index_reverse_map.empty(); }
-  unsigned size() const { return index_reverse_map.size(); }
-  unsigned capacity() const { return index_reverse_map.capacity(); }
-
-  bool has_row_id(unsigned row_id) const
+  row_view<table> row(row_id rid)
   {
-    if (row_id >= index_map.size()) {
+    srsran_assert(has_row_id(rid), "Invalid row_id");
+    return row_view<table>{*this, index_map[rid.value()]};
+  }
+  row_view<const table> row(row_id rid) const
+  {
+    srsran_assert(has_row_id(rid), "Invalid row_id");
+    return row_view<const table>{*this, index_map[rid.value()]};
+  }
+
+  /// Gets number of columns of the table.
+  static constexpr size_t nof_columns() { return sizeof...(ColumnTypes); }
+
+  /// Retrieves the number of rows of the table.
+  [[nodiscard]] size_t size() const { return index_reverse_map.size(); }
+
+  /// Checks if the table contains any rows.
+  [[nodiscard]] bool empty() const { return index_reverse_map.empty(); }
+
+  /// Pre-reserved space for the table.
+  [[nodiscard]] size_t capacity() const { return index_reverse_map.capacity(); }
+
+  /// Checks whether the table contains row with provided ID.
+  [[nodiscard]] bool has_row_id(row_id rid) const
+  {
+    if (rid.value() >= index_map.size()) {
       return false;
     }
-    unsigned offset = index_map[row_id];
-    return offset < index_reverse_map.size();
+    const unsigned offset = index_map[rid.value()];
+    if (offset >= index_reverse_map.size()) {
+      return false;
+    }
+    return index_reverse_map[offset] == rid;
   }
 
   template <std::size_t I>
-  auto cell(unsigned row_id)
+  auto& at(row_id rid)
   {
-    srsran_assert(has_row_id(row_id), "Invalid row_id");
-    return make_cell_view(std::get<I>(columns), index_map[row_id]);
+    static_assert(I < sizeof...(ColumnTypes), "Column index out of bounds");
+    srsran_assert(has_row_id(rid), "Invalid row_id");
+    return std::get<I>(columns)[index_map[rid.value()]];
   }
   template <std::size_t I>
-  auto cell(unsigned row_id) const
+  const auto& at(row_id rid) const
   {
-    srsran_assert(has_row_id(row_id), "Invalid row_id");
-    return make_cell_view(std::get<I>(columns), index_map[row_id]);
+    static_assert(I < sizeof...(ColumnTypes), "Column index out of bounds");
+    srsran_assert(has_row_id(rid), "Invalid row_id");
+    return std::get<I>(columns)[index_map[rid.value()]];
   }
 
   template <typename... Vals>
-  unsigned insert(Vals&&... vals)
+  row_id insert(Vals&&... vals)
   {
     static_assert(sizeof...(Vals) == sizeof...(ColumnTypes), "Invalid number of values to insert");
     static_assert((std::is_constructible_v<ColumnTypes, Vals&&> && ...),
                   "Argument types must be constructible into corresponding vector::value_type.");
     unsigned offset = index_reverse_map.size();
-    unsigned row_id = free_list_head;
-    if (row_id < index_map.size()) {
-      free_list_head    = index_map[row_id];
-      index_map[row_id] = offset;
+    unsigned rid    = free_list_head;
+    if (rid < index_map.size()) {
+      free_list_head = index_map[rid];
+      index_map[rid] = offset;
     } else {
-      srsran_sanity_check(row_id == index_map.size(), "Invalid free_list_head");
+      srsran_sanity_check(rid == index_map.size(), "Invalid free_list_head");
       index_map.push_back(offset);
-      free_list_head = row_id + 1;
+      free_list_head = rid + 1;
     }
-    index_reverse_map.push_back(row_id);
+    index_reverse_map.push_back(row_id{rid});
     push_back_impl(std::index_sequence_for<ColumnTypes...>{}, std::forward<Vals>(vals)...);
 
-    return row_id;
+    return row_id{rid};
   }
 
-  bool erase(unsigned row_id)
+  bool erase(row_id rid)
   {
-    if (row_id >= index_map.size()) {
+    if (rid.value() >= index_map.size()) {
       return false;
     }
-    unsigned offset = index_map[row_id];
+    unsigned offset = index_map[rid.value()];
     if (offset >= index_reverse_map.size()) {
       return false;
     }
-    srsran_sanity_check(index_reverse_map[offset] == row_id, "Invalid row_id");
+    srsran_sanity_check(index_reverse_map[offset] == rid, "Invalid row_id");
 
     if (offset != index_reverse_map.size() - 1) {
       std::apply([offset](auto&... cols) { (std::swap(cols[offset], cols.back()), ...); }, columns);
-      index_reverse_map[offset]            = index_reverse_map.back();
-      index_map[index_reverse_map[offset]] = offset;
+      index_reverse_map[offset]                    = index_reverse_map.back();
+      index_map[index_reverse_map[offset].value()] = offset;
     }
     std::apply([](auto&... cols) { (cols.pop_back(), ...); }, columns);
     index_reverse_map.pop_back();
-    index_map[row_id] = free_list_head;
-    free_list_head    = row_id;
+    index_map[rid.value()] = free_list_head;
+    free_list_head         = rid.value();
     return true;
+  }
+
+  void erase(const_iterator it)
+  {
+    srsran_assert(it.parent == this, "Iterator does not belong to this table");
+    srsran_assert(it.offset < size(), "Iterator out of range");
+    erase(index_reverse_map[it.offset]);
   }
 
   void reserve(unsigned size)
@@ -229,17 +341,17 @@ public:
     index_reverse_map.reserve(size);
   }
 
+  /// Get iterator of rows pointing to the beginning. Note: this iterator is not sorted by row_id.
+  iterator       begin() { return iterator{*this, 0}; }
+  const_iterator begin() const { return const_iterator{*this, 0}; }
+  const_iterator cbegin() const { return const_iterator{*this, 0}; }
+  sentinel       end() const { return {}; }
+  sentinel       cend() const { return {}; }
+
 private:
-  template <typename T>
-  cell_view<T> make_cell_view(std::vector<T>& data, unsigned offset)
-  {
-    return cell_view<T>{data, offset};
-  }
-  template <typename T>
-  cell_view<const T> make_cell_view(const std::vector<T>& data, unsigned offset) const
-  {
-    return cell_view<const T>{data, offset};
-  }
+  friend class row_view<table>;
+  friend class row_view<const table>;
+
   template <typename T>
   column_view<T> make_column_view(std::vector<T>& data)
   {
@@ -252,7 +364,7 @@ private:
   }
 
   template <typename... Vals, std::size_t... Is>
-  void push_back_impl(std::index_sequence<Is...>, Vals&&... vals)
+  void push_back_impl(std::index_sequence<Is...> /* unused */, Vals&&... vals)
   {
     ((std::get<Is>(columns).emplace_back(std::forward<Vals>(vals))), ...);
   }
@@ -262,7 +374,7 @@ private:
   /// Map from row_id to offset in the columns vectors.
   std::vector<unsigned> index_map;
   /// Map from offset in the columns vectors to row_id.
-  std::vector<unsigned> index_reverse_map;
+  std::vector<row_id> index_reverse_map;
   /// Linked List of free row_ids (using holes of index_map as next pointer).
   unsigned free_list_head{0};
 };
--- tests/unittests/adt/soa_table_test.cpp
+++ tests/unittests/adt/soa_table_test.cpp
@@ -20,39 +20,47 @@ TEST(soa_table_test, empty_table)
   ASSERT_TRUE(table.empty());
   ASSERT_EQ(table.size(), 0);
   ASSERT_EQ(table.capacity(), 0);
-  ASSERT_FALSE(table.has_row_id(0));
+  ASSERT_FALSE(table.has_row_id(soa::row_id{0}));
 
   auto col0 = table.column<0>();
   ASSERT_TRUE(col0.empty());
   ASSERT_EQ(col0.size(), 0);
-  ASSERT_FALSE(col0.has_row_id(0));
+  ASSERT_FALSE(col0.has_row_id(soa::row_id{0}));
 }
 
 TEST(soa_table_test, insert_one_row)
 {
   soa::table<int, float> table;
 
-  unsigned row_id = table.insert(42, 3.14f);
-  ASSERT_EQ(row_id, 0);
+  soa::row_id row_id = table.insert(42, 3.14f);
+  ASSERT_EQ(row_id.value(), 0);
   ASSERT_FALSE(table.empty());
   ASSERT_EQ(table.size(), 1);
   ASSERT_GE(table.capacity(), 1);
-  ASSERT_TRUE(table.has_row_id(0));
-  ASSERT_EQ(table.cell<0>(0).value(), 42);
-  ASSERT_EQ(table.cell<1>(0).value(), 3.14f);
+  ASSERT_TRUE(table.has_row_id(soa::row_id{0}));
+  ASSERT_EQ(table.at<0>(row_id), 42);
+  ASSERT_EQ(table.at<1>(row_id), 3.14f);
 
+  // Column test
   auto col0 = table.column<0>();
   auto col1 = table.column<1>();
   ASSERT_FALSE(col0.empty());
   ASSERT_FALSE(col1.empty());
   ASSERT_EQ(col0.size(), 1);
   ASSERT_EQ(col1.size(), 1);
-  ASSERT_TRUE(col0.has_row_id(0));
-  ASSERT_TRUE(col1.has_row_id(0));
-  ASSERT_EQ(*col0[0], 42);
-  ASSERT_EQ(*col1[0], 3.14f);
+  ASSERT_TRUE(col0.has_row_id(row_id));
+  ASSERT_TRUE(col1.has_row_id(row_id));
+  ASSERT_EQ(col0[row_id], 42);
+  ASSERT_EQ(col1[row_id], 3.14f);
 
-  auto col0_span = col0.unsorted();
+  // Row test
+  auto row0 = table.row(row_id);
+  ASSERT_EQ(row0.nof_columns(), 2);
+  ASSERT_EQ(row0.id(), row_id);
+  ASSERT_EQ(row0.at<0>(), 42);
+  ASSERT_EQ(row0.at<1>(), 3.14f);
+
+  auto col0_span = col0.to_span();
   ASSERT_EQ(col0_span.size(), 1);
   std::array<int, 1> expected_col0{42};
   ASSERT_TRUE(std::equal(col0_span.begin(), col0_span.end(), expected_col0.begin(), expected_col0.end()));
@@ -67,6 +75,7 @@ TEST(soa_table_test, insert_and_rem_one_row)
   ASSERT_EQ(table.size(), 0);
   ASSERT_FALSE(table.has_row_id(row_id));
 
+  // Column view test.
   auto col0 = table.column<0>();
   ASSERT_TRUE(col0.empty());
   auto col1 = table.column<1>();
@@ -84,12 +93,23 @@ TEST(soa_table_test, insert_two)
   ASSERT_EQ(table.size(), 2);
   ASSERT_TRUE(table.has_row_id(row_id0));
   ASSERT_TRUE(table.has_row_id(row_id1));
+
+  // Column view test.
   auto col0 = table.column<0>();
-  ASSERT_EQ(*col0[row_id0], 42);
-  ASSERT_EQ(*col0[row_id1], 43);
+  ASSERT_EQ(col0[row_id0], 42);
+  ASSERT_EQ(col0[row_id1], 43);
   auto col1 = table.column<1>();
-  ASSERT_EQ(*col1[row_id0], 3.14f);
-  ASSERT_EQ(*col1[row_id1], 4.14f);
+  ASSERT_EQ(col1[row_id0], 3.14f);
+  ASSERT_EQ(col1[row_id1], 4.14f);
+
+  // Row view test.
+  auto row0 = table.row(row_id0);
+  ASSERT_EQ(row0.at<0>(), 42);
+  ASSERT_EQ(row0.at<1>(), 3.14f);
+  auto row1 = table.row(row_id1);
+  ASSERT_EQ(row1.at<0>(), 43);
+  ASSERT_EQ(row1.at<1>(), 4.14f);
+  ASSERT_NE(row0, row1);
 }
 
 TEST(soa_table_test, insert_and_rem_two)
@@ -105,10 +125,10 @@ TEST(soa_table_test, insert_and_rem_two)
   ASSERT_TRUE(table.has_row_id(row_id1));
   auto col0 = table.column<0>();
   ASSERT_EQ(col0.size(), 1);
-  ASSERT_EQ(*col0[row_id1], 43);
+  ASSERT_EQ(col0[row_id1], 43);
   auto col1 = table.column<1>();
   ASSERT_EQ(col1.size(), 1);
-  ASSERT_EQ(*col1[row_id1], 4.14f);
+  ASSERT_EQ(col1[row_id1], 4.14f);
 
   ASSERT_TRUE(table.erase(row_id1));
   ASSERT_TRUE(table.empty());
@@ -117,20 +137,70 @@ TEST(soa_table_test, insert_and_rem_two)
   ASSERT_FALSE(table.has_row_id(row_id1));
 }
 
-TEST(soa_table_test, insert_rem_and_insert)
+TEST(soa_table_test, insert_two_rem_and_insert)
 {
   soa::table<int, float> table;
   auto                   row_id0 = table.insert(42, 3.14f);
   auto                   row_id1 = table.insert(43, 4.14f);
   ASSERT_TRUE(table.erase(row_id0));
+  ASSERT_FALSE(table.has_row_id(row_id0));
   auto row_id2 = table.insert(44, 5.14f);
 
+  ASSERT_TRUE(table.has_row_id(row_id2));
   ASSERT_EQ(row_id2, row_id0) << "row_id should be reused";
   ASSERT_NE(row_id1, row_id2);
   ASSERT_EQ(table.size(), 2);
   ASSERT_EQ(table.column<0>().size(), 2);
-  ASSERT_EQ(table.cell<0>(row_id1).value(), 43) << "row_id must stay stable";
-  ASSERT_EQ(table.cell<1>(row_id1).value(), 4.14f);
-  ASSERT_EQ(table.cell<0>(row_id2).value(), 44);
-  ASSERT_EQ(table.cell<1>(row_id2).value(), 5.14f);
+  ASSERT_EQ(table.at<0>(row_id1), 43) << "row_id must stay stable";
+  ASSERT_EQ(table.at<1>(row_id1), 4.14f);
+  ASSERT_EQ(table.at<0>(row_id2), 44);
+  ASSERT_EQ(table.at<1>(row_id2), 5.14f);
+  ASSERT_NE(row_id1, row_id2);
+}
+
+TEST(soa_table_test, insert_three_rem_two_and_insert)
+{
+  soa::table<int, float> table;
+  auto                   row_id0 = table.insert(42, 3.14f);
+  auto                   row_id1 = table.insert(43, 4.14f);
+  auto                   row_id2 = table.insert(44, 5.14f);
+  ASSERT_TRUE(table.erase(row_id0));
+  ASSERT_TRUE(table.erase(row_id1));
+  ASSERT_FALSE(table.has_row_id(row_id0));
+  ASSERT_FALSE(table.column<0>().has_row_id(row_id0));
+  ASSERT_FALSE(table.has_row_id(row_id1));
+  ASSERT_FALSE(table.column<1>().has_row_id(row_id1));
+  auto row_id3 = table.insert(45, 6.14f);
+
+  ASSERT_TRUE(table.has_row_id(row_id2));
+  ASSERT_TRUE(table.column<0>().has_row_id(row_id2));
+  ASSERT_TRUE(table.has_row_id(row_id3));
+  ASSERT_TRUE(table.column<1>().has_row_id(row_id3));
+}
+
+TEST(soa_table_test, iterator)
+{
+  soa::table<int, float> table;
+  ASSERT_EQ(table.begin(), table.end());
+
+  auto row0 = table.insert(42, 3.14f);
+  auto row1 = table.insert(43, 4.14f);
+
+  auto it = table.begin();
+  ASSERT_NE(it, table.end());
+  ASSERT_EQ(*it, table.row(row0));
+  ASSERT_EQ(it->at<0>(), 42);
+  ++it;
+  ASSERT_NE(it, table.end());
+  ASSERT_EQ(*it, table.row(row1));
+  ASSERT_EQ(it->at<0>(), 43);
+  ++it;
+  ASSERT_EQ(it, table.end());
+
+  table.erase(table.begin());
+  ASSERT_EQ(table.size(), 1);
+  it = table.begin();
+  ASSERT_NE(it, table.end());
+  ASSERT_EQ(*it, table.row(row1));
+  ASSERT_EQ(it->at<0>(), 43);
 }
