58ec954a911c95f7403f101701d968000170a929
dvdgrgrtt <davidg@srs.io>
2025-10-16 16:28:52 +0200

    phy: refactor port_channel_estimator interface
    Before: the compute method took as input a reference to a
    channel_estimate object where the estimation results were written.
    
    Now: the compute method now has no channel_estimate object as input.
    Instead, it returns a port_channel_estimator_reader interface that
    allows retrieving the estimated quantities at a later time. Also, the
    estimator only stores the estimated channel coefficients corresponding
    to the OFDM symbols carrying DM-RS, while the others are computed when
    requested via the reader interface.

--- lib/phy/upper/signal_processors/channel_estimator/port_channel_estimator_pool.h
+++ lib/phy/upper/signal_processors/channel_estimator/port_channel_estimator_pool.h
@@ -12,9 +12,43 @@
 
 #include "srsran/phy/upper/signal_processors/channel_estimator/port_channel_estimator.h"
 #include "srsran/srslog/srslog.h"
+#include "srsran/srsvec/zero.h"
 #include "srsran/support/memory_pool/bounded_object_pool.h"
 
 namespace srsran {
+
+/// \brief Dummy port channel estimation reader.
+///
+/// Returned by the \c port_channel_estimator_pool when fetching a port channel estimator fails.
+class port_channel_estimator_results_dummy : public port_channel_estimator_results
+{
+  // See interface for documentation.
+  void get_symbol_ch_estimate(span<cbf16_t> symbol, unsigned i_symbol, unsigned tx_layer) const override
+  {
+    srsvec::zero(symbol);
+  }
+
+  // See interface for documentation.
+  float get_epre() const override { return std::numeric_limits<float>::quiet_NaN(); }
+
+  // See interface for documentation.
+  float get_noise_variance() const override { return std::numeric_limits<float>::quiet_NaN(); }
+
+  // See interface for documentation.
+  float get_snr() const override { return std::numeric_limits<float>::quiet_NaN(); }
+
+  // See interface for documentation.
+  float get_rsrp(unsigned tx_layer) const override { return std::numeric_limits<float>::quiet_NaN(); }
+
+  // See interface for documentation.
+  std::optional<float> get_cfo_Hz() const override { return std::nullopt; }
+
+  // See interface for documentation.
+  phy_time_unit get_time_alignment() const override
+  {
+    return phy_time_unit::from_seconds(std::numeric_limits<float>::quiet_NaN());
+  }
+};
 
 /// Concurrent port channel estimator.
 class port_channel_estimator_pool : public port_channel_estimator
@@ -30,25 +64,26 @@ public:
   }
 
   // See interface for documentation.
-  void compute(channel_estimate&           estimate,
-               const resource_grid_reader& grid,
-               unsigned                    port,
-               const dmrs_symbol_list&     pilots,
-               const configuration&        cfg) override
+  const port_channel_estimator_results& compute(const resource_grid_reader& grid,
+                                                unsigned                    port,
+                                                const dmrs_symbol_list&     pilots,
+                                                const configuration&        cfg) override
   {
     auto estimator = estimators->get();
     if (!estimator) {
       logger.error("Failed to retrieve a port channel estimator.");
-      return;
+      return dummy_results;
     }
 
-    estimator->compute(estimate, grid, port, pilots, cfg);
+    return estimator->compute(grid, port, pilots, cfg);
   }
 
 private:
   srslog::basic_logger& logger;
   // Pool of port channel estimators.
   std::shared_ptr<estimator_pool> estimators;
+  // Dummy reader for replacing missing estimators.
+  port_channel_estimator_results_dummy dummy_results;
 };
 
 } // namespace srsran
