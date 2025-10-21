4189a18133a18e933b9f5743fec29f8b0bca52a4
frankist <francisco.paisana@srs.io>
2025-10-21 18:10:04 +0200

    adt: move bitmask operations to separate header

--- include/srsran/adt/bounded_bitset.h
+++ include/srsran/adt/bounded_bitset.h
@@ -12,226 +12,14 @@
 
 #include "srsran/adt/span.h"
 #include "srsran/adt/static_vector.h"
+#include "srsran/support/math/bit_ops.h"
 #include "srsran/support/math/math_utils.h"
 #include "srsran/support/srsran_assert.h"
 #include "fmt/format.h"
-#include <cstdint>
 #include <inttypes.h>
 #include <string>
 
 namespace srsran {
-
-/// \brief Returns an unsigned integer with the N most significant bits (MSB) set to zero, and the remaining bits set
-/// to 1.
-/// \tparam Integer Type of unsigned integer returned by the function.
-/// \param[in] N Number of MSB bits set to zero.
-/// \return Resulting integer bitmap.
-template <typename Integer>
-constexpr Integer mask_msb_zeros(size_t N)
-{
-  static_assert(std::is_unsigned<Integer>::value, "T must be unsigned integer");
-  return (N == 0) ? static_cast<Integer>(-1) : (N == sizeof(Integer) * 8U) ? 0 : (static_cast<Integer>(-1) >> (N));
-}
-
-/// \brief Returns an unsigned integer with the N least significant bits (LSB) set to zero, and the remaining bits set
-/// to 1.
-/// \tparam Integer Type of unsigned integer returned by the function.
-/// \param[in] N Number of LSB bits set to zero.
-/// \return Resulting integer bitmap.
-template <typename Integer>
-constexpr Integer mask_lsb_ones(size_t N)
-{
-  return mask_msb_zeros<Integer>(sizeof(Integer) * 8U - N);
-}
-
-/// \brief Returns an unsigned integer with the N most significant bits (MSB) set to one, and the remaining bits set
-/// to zero.
-/// \tparam Integer Type of unsigned integer returned by the function.
-/// \param[in] N Number of MSB bits set to one.
-/// \return Resulting integer bitmap.
-template <typename Integer>
-constexpr Integer mask_msb_ones(size_t N)
-{
-  return ~mask_msb_zeros<Integer>(N);
-}
-
-/// \brief Returns an unsigned integer with the N least significant bits (LSB) set to one, and the remaining bits set
-/// to zero.
-/// \tparam Integer Type of unsigned integer returned by the function.
-/// \param[in] N Number of LSB bits set to one.
-/// \return Resulting integer bitmap.
-template <typename Integer>
-Integer mask_lsb_zeros(size_t N)
-{
-  return ~mask_lsb_ones<Integer>(N);
-}
-
-namespace detail {
-
-template <typename Integer>
-struct bitset_builtin_helper {
-  /// \brief Returns the number of leading 0-bits in value, starting at the most significant bit position.
-  /// If value is 0, the result is number of bits of Integer.
-  static Integer zero_msb_count(Integer value)
-  {
-    if (value == 0) {
-      return std::numeric_limits<Integer>::digits;
-    }
-    Integer ret = 0;
-    for (Integer shift = std::numeric_limits<Integer>::digits >> 1; shift != 0; shift >>= 1) {
-      Integer tmp = value >> shift;
-      if (tmp != 0) {
-        value = tmp;
-      } else {
-        ret |= shift;
-      }
-    }
-    return ret;
-  }
-  /// \brief Returns the number of trailing 0-bits in value, starting at the least significant bit position.
-  /// If value is 0, the result is number of bits of Integer.
-  static Integer zero_lsb_count(Integer value)
-  {
-    if (value == 0) {
-      return std::numeric_limits<Integer>::digits;
-    }
-    Integer ret = 0;
-    for (Integer shift = std::numeric_limits<Integer>::digits >> 1, mask = std::numeric_limits<Integer>::max() >> shift;
-         shift != 0;
-         shift >>= 1, mask >>= shift) {
-      if ((value & mask) == 0) {
-        value >>= shift;
-        ret |= shift;
-      }
-    }
-    return ret;
-  }
-
-  /// \brief Returns the number of 1-bits in value.
-  static int count_ones(Integer value)
-  {
-    // Note: use an "int" for count triggers popcount optimization if SSE instructions are enabled.
-    int c = 0;
-    for (Integer w = value; w > 0; c++) {
-      w &= w - 1;
-    }
-    return c;
-  }
-};
-
-#ifdef __GNUC__ // clang and gcc
-
-/// Specializations of bitset operations for unsigned.
-template <>
-struct bitset_builtin_helper<unsigned> {
-  static unsigned zero_msb_count(unsigned value)
-  {
-    return (value) ? __builtin_clz(value) : std::numeric_limits<unsigned>::digits;
-  }
-  static unsigned zero_lsb_count(unsigned value)
-  {
-    return (value) ? __builtin_ctz(value) : std::numeric_limits<unsigned>::digits;
-  }
-  static int count_ones(unsigned value) { return __builtin_popcount(value); }
-};
-
-/// Specializations of bitset operations for unsigned long.
-template <>
-struct bitset_builtin_helper<unsigned long> {
-  static unsigned long zero_msb_count(unsigned long value)
-  {
-    return (value) ? __builtin_clzl(value) : std::numeric_limits<unsigned long>::digits;
-  }
-  static unsigned long zero_lsb_count(unsigned long value)
-  {
-    return (value) ? __builtin_ctzl(value) : std::numeric_limits<unsigned long>::digits;
-  }
-  static int count_ones(unsigned long value) { return __builtin_popcountl(value); }
-};
-
-/// Specializations of bitset operations for unsigned long long.
-template <>
-struct bitset_builtin_helper<unsigned long long> {
-  static unsigned long long zero_msb_count(unsigned long long value)
-  {
-    return (value) ? __builtin_clzll(value) : std::numeric_limits<unsigned long long>::digits;
-  }
-  static unsigned long long zero_lsb_count(unsigned long long value)
-  {
-    return (value) ? __builtin_ctzll(value) : std::numeric_limits<unsigned long long>::digits;
-  }
-  static int count_ones(unsigned long long value) { return __builtin_popcountll(value); }
-};
-
-#endif
-
-/// \brief Knuth's swap of upper and lower sections of a bitset.
-/// \param m Mask of bits to swap.
-/// \param k shift amount.
-template <typename T, T m, int k>
-T swapbits(T p)
-{
-  T q = ((p >> k) ^ p) & m;
-  return p ^ q ^ (q << k);
-}
-
-} // namespace detail
-
-/// \brief Knuth's 64-bit reverse. E.g. 0x0000000000000001 -> 0x8000000000000000.
-/// For more information see: https://matthewarcus.wordpress.com/2012/11/18/reversing-a-64-bit-word/
-/// \param n Number to reverse.
-/// \return Reversed number.
-inline uint64_t bit_reverse(uint64_t n)
-{
-  static const uint64_t m0 = 0x5555555555555555LLU;
-  static const uint64_t m1 = 0x0300c0303030c303LLU;
-  static const uint64_t m2 = 0x00c0300c03f0003fLLU;
-  static const uint64_t m3 = 0x00000ffc00003fffLLU;
-  n                        = ((n >> 1U) & m0) | (n & m0) << 1U;
-  n                        = detail::swapbits<uint64_t, m1, 4>(n);
-  n                        = detail::swapbits<uint64_t, m2, 8>(n);
-  n                        = detail::swapbits<uint64_t, m3, 20>(n);
-  n                        = (n >> 34U) | (n << 30U);
-  return n;
-}
-
-/// \brief Counts the number of contiguous bits set to zero, starting from the MSB.
-/// \tparam Integer Integer type of received bitmap.
-/// \param[in] value Integer bitmap
-/// \return count of bits set to zero.
-template <typename Integer>
-Integer zero_msb_count(Integer value)
-{
-  return (~value) ? detail::bitset_builtin_helper<Integer>::zero_msb_count(value) : 0;
-}
-
-/// \brief Finds the position of the first bit set to one, starting from the MSB.
-/// \tparam Integer Integer type of received bitmap.
-/// \param[in] value Integer bitmap
-/// \return MSB position with the bit set to one. The MSB has position sizeof(Integer) * 8 - 1.
-template <typename Integer>
-Integer find_first_msb_one(Integer value)
-{
-  return (value) ? (sizeof(Integer) * 8U - 1 - detail::bitset_builtin_helper<Integer>::zero_msb_count(value))
-                 : std::numeric_limits<Integer>::digits;
-}
-
-/// \brief Finds the position of the first bit set to one, starting from the LSB.
-/// \tparam Integer Integer type of received bitmap.
-/// \param[in] value Integer bitmap
-/// \return LSB position with the bit set to one. The LSB has position zero.
-template <typename Integer>
-Integer find_first_lsb_one(Integer value)
-{
-  return detail::bitset_builtin_helper<Integer>::zero_lsb_count(value);
-}
-
-/// \brief Counts the number of bits set to one in an integer.
-template <typename Integer>
-int count_ones(Integer value)
-{
-  return detail::bitset_builtin_helper<Integer>::count_ones(value);
-}
 
 namespace detail {
 
@@ -296,10 +84,8 @@ public:
   /// \tparam Iterator Boolean iterator type.
   /// \param[in] begin Begin iterator.
   /// \param[in] end End iterator.
-  template <
-      typename Iterator,
-      typename std::enable_if_t<std::is_convertible<typename std::iterator_traits<Iterator>::value_type, bool>::value,
-                                int> = 0>
+  template <typename Iterator,
+            std::enable_if_t<std::is_convertible_v<typename std::iterator_traits<Iterator>::value_type, bool>, int> = 0>
   constexpr bounded_bitset(Iterator begin, Iterator end)
   {
     resize(end - begin);
@@ -1125,15 +911,12 @@ private:
 
   SRSRAN_FORCE_INLINE size_t get_bitidx_(size_t bitpos) const noexcept
   {
-    return get_bitidx_(bitpos, std::integral_constant<bool, LowestInfoBitIsMSB>{});
+    if constexpr (LowestInfoBitIsMSB) {
+      return size() - 1 - bitpos;
+    } else {
+      return bitpos;
+    }
   }
-  // Tag dispatching for LowestInfoBitIsMSB==true.
-  SRSRAN_FORCE_INLINE size_t get_bitidx_(size_t bitpos, std::true_type /*unused*/) const noexcept
-  {
-    return size() - 1 - bitpos;
-  }
-  // Tag dispatching for LowestInfoBitIsMSB==false.
-  SRSRAN_FORCE_INLINE size_t get_bitidx_(size_t bitpos, std::false_type /*unused*/) const noexcept { return bitpos; }
 
   SRSRAN_FORCE_INLINE bool test_(size_t bitpos) const noexcept
   {
@@ -1616,4 +1399,5 @@ struct formatter<srsran::bounded_bitset<N, LowestInfoBitIsMSB, Tag>> {
     return s.template to_string_of_bits<decltype(std::declval<FormatContext>().out())>(ctx.out(), order == reverse);
   }
 };
+
 } // namespace fmt
