159b75001effdbfaa4634c250609f949b046d3da
Fabian Eckermann <fabian@srs.io>
2025-10-29 16:42:35 +0100

    cu_cp,rrc: improve logging

--- lib/rrc/ue/rrc_ue_impl.h
+++ lib/rrc/ue/rrc_ue_impl.h
@@ -108,7 +108,7 @@ private:
   void on_ue_release_required(const ngap_cause_t& cause) override;
 
   // rrc_ue_security_mode_command_proc_notifier
-  void on_new_dl_dcch(srb_id_t srb_id, const asn1::rrc_nr::dl_dcch_msg_s& dl_ccch_msg) override;
+  void on_new_dl_dcch(srb_id_t srb_id, const asn1::rrc_nr::dl_dcch_msg_s& dl_dcch_msg) override;
   void on_new_as_security_context() override;
 
   rrc_ue_context_t                context;
