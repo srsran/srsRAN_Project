3ec989c46c4fb0d604a6399cb878e020392a434b
Robert Falkenberg <robert.falkenberg@srs.io>
2025-11-19 15:48:46 +0100

    cmake: also search ROHC library in custom install path ROHC_DIR

--- cmake/modules/FindROHC.cmake
+++ cmake/modules/FindROHC.cmake
@@ -12,10 +12,37 @@
 #  ROHC_FOUND        - System has ROHC library installed
 #  ROHC_INCLUDE_DIRS - The ROHC include directories
 #  ROHC_LIBRARIES    - The ROHC library
-#  ROHC_VERSION      - The ROHC library version
 
 find_package(PkgConfig REQUIRED)
-pkg_check_modules(ROHC rohc>=2.3.1)
+pkg_check_modules(PC_ROHC rohc>=2.3.1)
+
+#find Mbedtls
+find_path(
+    ROHC_INCLUDE_DIRS
+    NAMES rohc/rohc.h
+    HINTS $ENV{ROHC_DIR}/include
+          ${PC_ROHC_INCLUDEDIR}
+          ${CMAKE_INSTALL_PREFIX}/include
+    PATHS /usr/local/include
+          /usr/include
+)
+
+find_library(
+    ROHC_LIBRARIES
+    NAMES rohc
+    HINTS $ENV{ROHC_DIR}/lib
+          ${PC_ROHC_LIBDIR}
+          ${CMAKE_INSTALL_PREFIX}/lib
+          ${CMAKE_INSTALL_PREFIX}/lib64
+    PATHS /usr/local/lib
+          /usr/local/lib64
+          /usr/lib
+          /usr/lib64
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ROHC DEFAULT_MSG ROHC_LIBRARIES ROHC_INCLUDE_DIRS)
+mark_as_advanced(ROHC_LIBRARIES ROHC_INCLUDE_DIRS)
 
 if(ROHC_FOUND)
   message(STATUS "ROHC LIBRARIES: " ${ROHC_LIBRARIES})
