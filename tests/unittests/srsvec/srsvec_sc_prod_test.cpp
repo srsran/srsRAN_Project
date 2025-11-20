8899eb5054c381c989644712bf6cee28a689334a
Xavier Arteaga <xavier@srs.io>
2025-11-20 11:05:51 +0100

    srsvec: add missing scalar product function and upgrade test

--- tests/unittests/srsvec/srsvec_sc_prod_test.cpp
+++ tests/unittests/srsvec/srsvec_sc_prod_test.cpp
@@ -10,6 +10,7 @@
 
 #include "srsran/srsvec/sc_prod.h"
 #include "srsran/support/srsran_test.h"
+#include <gtest/gtest.h>
 #include <random>
 
 static std::mt19937 rgen(0);
@@ -17,103 +18,136 @@ static const float  ASSERT_MAX_ERROR = 1e-6;
 
 using namespace srsran;
 
-void test_sc_prod_ccc(std::size_t N)
+namespace {
+using SrsvecScProdFixtureParams = unsigned;
+
+class SrsvecScProdFixture : public ::testing::TestWithParam<SrsvecScProdFixtureParams>
+{
+protected:
+  unsigned size;
+
+  void SetUp() override
+  {
+    // Get test parameters.
+    auto params = GetParam();
+    size        = params;
+  }
+};
+
+using namespace srsran;
+
+TEST_P(SrsvecScProdFixture, SrsvecScProdFloatComplex)
 {
   std::uniform_real_distribution<float> dist(-1.0, 1.0);
 
-  std::vector<cf_t> x(N);
+  std::vector<cf_t> x(size);
   for (cf_t& v : x) {
     v = {dist(rgen), dist(rgen)};
   }
 
   cf_t h = {dist(rgen), dist(rgen)};
 
-  std::vector<cf_t> z(N);
+  std::vector<cf_t> z(size);
 
   srsvec::sc_prod(z, x, h);
 
-  for (size_t i = 0; i != N; i++) {
+  for (size_t i = 0; i != size; i++) {
     cf_t  gold_z = x[i] * h;
     float err    = std::abs(gold_z - z[i]);
     TESTASSERT(err < ASSERT_MAX_ERROR);
   }
 }
 
-void test_sc_prod_ccc_bf16(std::size_t N)
+TEST_P(SrsvecScProdFixture, SrsvecScProdBrainFloatComplex)
 {
   std::uniform_real_distribution<float> dist(-1.0, 1.0);
 
-  std::vector<cbf16_t> x(N);
+  std::vector<cbf16_t> x(size);
   std::generate(x.begin(), x.end(), [&dist]() { return to_cbf16(cf_t{dist(rgen), dist(rgen)}); });
 
   cf_t h = {dist(rgen), dist(rgen)};
 
-  std::vector<cbf16_t> z(N);
+  std::vector<cbf16_t> z(size);
 
   srsvec::sc_prod(z, x, h);
 
-  std::vector<cf_t> expected(N);
+  std::vector<cf_t> expected(size);
   std::transform(x.begin(), x.end(), expected.begin(), [&h](cbf16_t value) { return h * to_cf(value); });
 
-  for (size_t i = 0; i != N; i++) {
+  for (size_t i = 0; i != size; i++) {
     cf_t  gold_z = to_cf(x[i]) * h;
     float err    = std::abs(gold_z - to_cf(z[i]));
     TESTASSERT(err < 5e-3, " err={}", err);
   }
 }
 
-void test_sc_prod_cfc(std::size_t N)
+TEST_P(SrsvecScProdFixture, SrsvecScProdBrainFloatComplexReal)
 {
   std::uniform_real_distribution<float> dist(-1.0, 1.0);
 
-  std::vector<cf_t> x(N);
+  std::vector<cbf16_t> x(size);
+  std::generate(x.begin(), x.end(), [&dist]() { return to_cbf16(cf_t{dist(rgen), dist(rgen)}); });
+
+  float h = dist(rgen);
+
+  std::vector<cbf16_t> z(size);
+
+  srsvec::sc_prod(z, x, h);
+
+  std::vector<cf_t> expected(size);
+  std::transform(x.begin(), x.end(), expected.begin(), [&h](cbf16_t value) { return h * to_cf(value); });
+
+  for (size_t i = 0; i != size; i++) {
+    cf_t  gold_z = to_cf(x[i]) * h;
+    float err    = std::abs(gold_z - to_cf(z[i]));
+    TESTASSERT(err < 5e-3, " err={}", err);
+  }
+}
+
+TEST_P(SrsvecScProdFixture, SrsvecScProdFloatComplexReal)
+{
+  std::uniform_real_distribution<float> dist(-1.0, 1.0);
+
+  std::vector<cf_t> x(size);
   for (cf_t& v : x) {
     v = {dist(rgen), dist(rgen)};
   }
 
   float h = dist(rgen);
 
-  std::vector<cf_t> z(N);
+  std::vector<cf_t> z(size);
 
   srsvec::sc_prod(z, x, h);
 
-  for (size_t i = 0; i != N; i++) {
+  for (size_t i = 0; i != size; i++) {
     cf_t  gold_z = x[i] * h;
     float err    = std::abs(gold_z - z[i]);
     TESTASSERT(err < ASSERT_MAX_ERROR);
   }
 }
 
-void test_sc_prod_fff(std::size_t N)
+TEST_P(SrsvecScProdFixture, SrsvecScProdFloat)
 {
   std::uniform_real_distribution<float> dist(-1.0, 1.0);
 
-  std::vector<float> x(N);
+  std::vector<float> x(size);
   for (float& v : x) {
     v = dist(rgen);
   }
 
   float h = dist(rgen);
 
-  std::vector<float> z(N);
+  std::vector<float> z(size);
 
   srsvec::sc_prod(z, x, h);
 
-  for (size_t i = 0; i != N; i++) {
-    cf_t  gold_z = x[i] * h;
+  for (size_t i = 0; i != size; i++) {
+    float gold_z = x[i] * h;
     float err    = std::abs(gold_z - z[i]);
     TESTASSERT(err < ASSERT_MAX_ERROR);
   }
 }
 
-int main()
-{
-  std::vector<std::size_t> sizes = {1, 5, 7, 19, 23, 257};
+INSTANTIATE_TEST_SUITE_P(SrsvecScProdTest, SrsvecScProdFixture, ::testing::Values(1, 5, 7, 19, 23, 257, 1234));
 
-  for (std::size_t N : sizes) {
-    test_sc_prod_ccc(N);
-    test_sc_prod_ccc_bf16(N);
-    test_sc_prod_cfc(N);
-    test_sc_prod_fff(N);
-  }
-}
+} // namespace
